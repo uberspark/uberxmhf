29a48a0ad99e4057e7e1e85e21f657c05df0c929
Amit Vasudevan <amitvasudevan@acm.org>
2010-05-03 16:43:36 +0000

    updated installation script on ADAMS
    git-svn-id: https://sparrow.ece.cmu.edu/svn/amit/projects/lockdown@941 f23f674a-de99-4fcc-816c-cbdf24235276

--- lockdown/build/install.pl
+++ lockdown/build/install.pl
@@ -3,16 +3,12 @@
 # copies loader and runtime to /mnt/mbrgrub/
 
 print "Copying loader.bin...";
-# system "rm -rf /mnt/mbrgrub/loader.bin";
-# system "cp loader.bin /mnt/mbrgrub/.";
-system "rm -rf /boot/loader.bin";
-system "cp loader.bin /boot/loader.bin";
+system "rm -rf /mnt/mbrgrub/ldn_loader.bin";
+system "cp loader.bin /mnt/mbrgrub/ldn_loader.bin";
 print "Done.\n";
 print "Copying runtime.bin.gz...";
-# system "rm -rf /mnt/mbrgrub/runtime.bin.gz";
-# system "cp runtime.bin.gz /mnt/mbrgrub/.";
-system "rm -rf /boot/runtime.bin.gz";
-system "cp runtime.bin.gz /boot/runtime.bin.gz";
+system "rm -rf /mnt/mbrgrub/ldn_runtime.bin.gz";
+system "cp runtime.bin.gz /mnt/mbrgrub/ldn_runtime.bin.gz";
 print "Done.\n";
 
 
