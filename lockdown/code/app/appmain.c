f71553baef8b8032715e898813e7cecfa180e29d
Amit Vasudevan <amitvasudevan@acm.org>
2010-07-30 17:12:44 +0000

    tested sechyp reboot and selective I/O interception with lockdown ACPI control port intercepting hibernation signal. [supermicro]
    git-svn-id: https://sparrow.ece.cmu.edu/svn/amit/projects/lockdown@1085 f23f674a-de99-4fcc-816c-cbdf24235276

--- lockdown/code/app/appmain.c
+++ lockdown/code/app/appmain.c
@@ -5,22 +5,19 @@
 #include <target.h>
 
 #include <acpi.h>
-#include <libsechyp.h>
 
-// a placeholder for now...
+u32 acpi_control_portnum=0;
+
 u32 sechyp_app_main(VCPU *vcpu){
-  u32 acpi_control_portnum, acpi_status_portnum;
-  
   printf("\nCPU(0x%02x): Lockdown initiaizing...", vcpu->id);
   
 	//ACPI initialization
 	ACPIInitializeRegisters();
 
   acpi_control_portnum = (u32)PM1_CNTa;
-  acpi_status_portnum = (u32)PM1a_STS;
   
-  printf("\nCPU(0x%02x): Lockdown; ACPI cp=0x%08x, sp=0x%08x",
-    vcpu->id, acpi_control_portnum, acpi_status_portnum);
+  printf("\nCPU(0x%02x): Lockdown; ACPI control port=0x%08x",
+    vcpu->id, acpi_control_portnum);
   
   //set I/O port intercept for ACPI control port
   sechyp_iopm_set_write(vcpu, acpi_control_portnum, 2); //16-bit port
@@ -31,11 +28,14 @@ u32 sechyp_app_main(VCPU *vcpu){
 u32 sechyp_app_handleintercept_portaccess(VCPU *vcpu, struct regs *r, 
   u32 portnum, u32 access_type, u32 access_size){
   
-  printf("\nCPU(0x%02x): App, IO intercept, p=0x%04x, at=0x%02x, size=0x%08x",
-      vcpu->id, portnum, access_type, access_size);
-  printf("\nCPU(0x%02x): HALT!", vcpu->id);
-  HALT();
-    
+  if(access_type == IO_TYPE_OUT && portnum == acpi_control_portnum && 
+      access_size == IO_SIZE_WORD && ((u16)r->eax & (u16)(1 << 13)) ){
+      printf("\nCPU(0x%02x): Lockdown; ACPI SLEEP_EN signal caught. resetting...",
+          vcpu->id);
+      sechyp_reboot();
+      //we should never get here
+      HALT();  
+  }
+  
   return APP_IOINTERCEPT_CHAIN; //chain and do the required I/O    
-    
 }
