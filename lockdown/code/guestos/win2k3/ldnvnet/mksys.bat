0dd4574fbd3907e258d5de7b32180762d6dafc5e
Amit Vasudevan <amitvasudevan@acm.org>
2010-08-11 17:00:48 +0000

    changed makefile/ddk batch file to switch between our VPCVM vs real environment.
    git-svn-id: https://sparrow.ece.cmu.edu/svn/amit/projects/lockdown@1138 f23f674a-de99-4fcc-816c-cbdf24235276

--- lockdown/code/guestos/win2k3/ldnvnet/mksys.bat
+++ lockdown/code/guestos/win2k3/ldnvnet/mksys.bat
@@ -1,6 +1,7 @@
-set TEMP=e:\temp
+set TEMP=c:\windows\temp
 call c:\WinDDK\7600.16385.1\bin\setenv.bat c:\WinDDK\7600.16385.1\ chk WNET
-e:
-cd e:\amit\projects\lockdown\code\guestos\win2k3\ldnvnet\sys
+rem e:
+rem cd e:\amit\projects\lockdown\code\guestos\win2k3\ldnvnet\sys
+cd c:\amit\projects\lockdown\code\guestos\win2k3\ldnvnet\sys
 build -ceZ
 				 
