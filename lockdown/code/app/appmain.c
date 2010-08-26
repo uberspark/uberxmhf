8b3675533455a29b39f09ea518874c7f33cd64c0
Amit Vasudevan <amitvasudevan@acm.org>
2010-08-26 19:36:08 +0000

    added makefile vars to split hyperswitch, hyperpartitioning and approved execution implementations. allows us to enable/disable each of them seperately for debugging and evaluation purposes. also added basic approved execution skeleton logic implemented using HW pagetable callback.
    git-svn-id: https://sparrow.ece.cmu.edu/svn/amit/projects/lockdown@1158 f23f674a-de99-4fcc-816c-cbdf24235276

--- lockdown/code/app/appmain.c
+++ lockdown/code/app/appmain.c
@@ -8,14 +8,33 @@
 #include "./include/acpi.h"
 #include "./include/ata-atapi.h"
 
+#if defined(__LDN_HYPERSWITCHING__)
 u32 acpi_control_portnum=0;
+#endif
+
+
 u32 currentenvironment = LDN_ENV_UNTRUSTED_SIGNATURE; //default to untrusted env.
 
                             
 u32 sechyp_app_main(VCPU *vcpu, APP_PARAM_BLOCK *apb){
   LDNPB *pldnPb;
   printf("\nCPU(0x%02x): Lockdown initiaizing...", vcpu->id);
-  
+
+#if defined(__LDN_APPROVEDEXEC__)
+  {
+    u32 endpfn = __runtimephysicalbase / PAGE_SIZE_4K;
+    u32 i;
+    //setup all guest physical memory region to non-executable to
+    //start with
+    for(i=0; i < endpfn; i++)
+      emhf_hwpgtbl_setprot(vcpu, (i*PAGE_SIZE_4K), (emhf_hwpgtbl_getprot(vcpu, (i*PAGE_SIZE_4K)) & ~HWPGTBL_FLAG_EXECUTE));     
+
+    emhf_hwpgtbl_flushall(vcpu);  //flush all NPT/EPT mappings
+    printf("\nCPU(0x%02x): Setup trusted execution and flushed all HW pagetable mappings...");
+  }
+#endif
+
+#if defined(__LDN_HYPERSWITCHING__)  
 	//ACPI initialization
 	ACPIInitializeRegisters();
 
@@ -26,15 +45,16 @@ u32 sechyp_app_main(VCPU *vcpu, APP_PARAM_BLOCK *apb){
   
   //set I/O port intercept for ACPI control port
   sechyp_iopm_set_write(vcpu, acpi_control_portnum, 2); //16-bit port
+#endif
 
-
+#if defined(__LDN_HYPERPARTITIONING__)
   //set IDE port intercepts for hyper-partitioning
   sechyp_iopm_set_write(vcpu, ATA_COMMAND(ATA_BUS_PRIMARY), 1); //8-bit port
   sechyp_iopm_set_write(vcpu, ATA_SECTOR_COUNT(ATA_BUS_PRIMARY), 1); //8-bit port
   sechyp_iopm_set_write(vcpu, ATA_LBALOW(ATA_BUS_PRIMARY), 1); //8-bit port
   sechyp_iopm_set_write(vcpu, ATA_LBAMID(ATA_BUS_PRIMARY), 1); //8-bit port
   sechyp_iopm_set_write(vcpu, ATA_LBAHIGH(ATA_BUS_PRIMARY), 1); //8-bit port
-
+#endif
 
   //grab the ldn parameter block from verifier, this tells us the
   //destination environment characteristics
@@ -57,7 +77,8 @@ extern u32 hp(VCPU *vcpu, struct regs *r, u32 portnum, u32 access_type, u32 acce
 
 u32 sechyp_app_handleintercept_portaccess(VCPU *vcpu, struct regs *r, 
   u32 portnum, u32 access_type, u32 access_size){
-  
+
+#if defined(__LDN_HYPERSWITCHING__)  
   if(access_type == IO_TYPE_OUT && portnum == acpi_control_portnum && 
       access_size == IO_SIZE_WORD && ((u16)r->eax & (u16)(1 << 13)) ){
       printf("\nCPU(0x%02x): Lockdown; ACPI SLEEP_EN signal caught. resetting...",
@@ -66,7 +87,9 @@ u32 sechyp_app_handleintercept_portaccess(VCPU *vcpu, struct regs *r,
       //we should never get here
       HALT();  
   }
-  
+#endif
+
+#if defined(__LDN_HYPERPARTITIONING__)  
   if( portnum == ATA_COMMAND(ATA_BUS_PRIMARY) ||
 		portnum == ATA_SECTOR_COUNT(ATA_BUS_PRIMARY) ||
 		portnum == ATA_LBALOW(ATA_BUS_PRIMARY) ||
@@ -78,6 +101,21 @@ u32 sechyp_app_handleintercept_portaccess(VCPU *vcpu, struct regs *r,
 			retval = hp(vcpu, r, portnum, access_type, access_size);
 			return retval;
 	}
+#endif
   
   return APP_IOINTERCEPT_CHAIN; //chain and do the required I/O    
 }
+
+                       
+#if defined(__LDN_APPROVEDEXEC__)
+extern u32 approvedexec_handleevent(VCPU *vcpu, struct regs *r, 
+  u64 gpa, u64 gva, u64 violationcode);
+
+//for now this always returns APP_SUCCESS
+u32 emhf_app_handleintercept_hwpgtblviolation(VCPU *vcpu,
+      struct regs *r,
+      u64 gpa, u64 gva, u64 violationcode){
+
+  return( approvedexec_handleevent(vcpu, r, gpa, gva, violationcode) );
+}
+#endif
