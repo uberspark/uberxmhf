ecb78850c27524d3f4bdba09c8cbcee09c3f2102
Jonathan McCune <jonmccune@cmu.edu>
2011-01-04 21:57:40 +0000

    code imported from tboot-20101005 largely unchanged except for minor changes in #includes, types, and overall structure to play nice in our build environment.  this code compiles in and seems to cause no damage.  the great majority of it remains untouched while executing so far.
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@428 1c601a1c-44f4-4d93-90ac-4b632a93592e

---
+++ emhf/trunk/code/x86/init/txt_heap.c
@@ -0,0 +1,418 @@
+/*
+ * @XMHF_LICENSE_HEADER_START@
+ *
+ * eXtensible, Modular Hypervisor Framework (XMHF)
+ * Copyright (c) 2009-2012 Carnegie Mellon University
+ * Copyright (c) 2010-2012 VDG Inc.
+ * All Rights Reserved.
+ *
+ * Developed by: XMHF Team
+ *               Carnegie Mellon University / CyLab
+ *               VDG Inc.
+ *               http://xmhf.org
+ *
+ * This file is part of the EMHF historical reference
+ * codebase, and is released under the terms of the
+ * GNU General Public License (GPL) version 2.
+ * Please see the LICENSE file for details.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * @XMHF_LICENSE_HEADER_END@
+ */
+
+/*
+ * heap.c: fns for verifying and printing the Intel(r) TXT heap data structs
+ *
+ * Copyright (c) 2003-2010, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/*
+ * Modified for EMHF by jonmccune@cmu.edu, 2011.01.04
+ */
+
+#include <target.h>
+#include "txt_config_regs.h"
+#include "txt_hash.h"
+
+/* XXX order is important for these; fix it */
+#include "txt_mle.h"
+#include "txt_acmod.h"
+#include "txt_mtrrs.h"
+#include "txt_heap.h"
+
+static inline void print_heap_hash(sha1_hash_t hash)
+{
+    print_hash((const tb_hash_t *)hash, TB_HALG_SHA1);
+}
+
+static void print_bios_data(bios_data_t *bios_data)
+{
+    printf("bios_data (@%p, %Lx):\n", bios_data,
+           *((uint64_t *)bios_data - 1));
+    printf("\t version: %u\n", bios_data->version);
+    printf("\t bios_sinit_size: 0x%x (%u)\n", bios_data->bios_sinit_size,
+           bios_data->bios_sinit_size);
+    printf("\t lcp_pd_base: 0x%Lx\n", bios_data->lcp_pd_base);
+    printf("\t lcp_pd_size: 0x%Lx (%Lu)\n", bios_data->lcp_pd_size,
+           bios_data->lcp_pd_size);
+    printf("\t num_logical_procs: %u\n", bios_data->num_logical_procs);
+    if ( bios_data->version >= 3 )
+        printf("\t flags: 0x%08Lx\n", bios_data->flags);
+}
+
+bool verify_bios_data(txt_heap_t *txt_heap)
+{
+    uint64_t size, heap_size;
+    bios_data_t *bios_data;
+
+    /* check size */
+    heap_size = read_priv_config_reg(TXTCR_HEAP_SIZE);
+    size = get_bios_data_size(txt_heap);
+    if ( size == 0 ) {
+        printf("BIOS data size is 0\n");
+        return false;
+    }
+    if ( size > heap_size ) {
+        printf("BIOS data size is larger than heap size "
+               "(%Lx, heap size=%Lx)\n", size, heap_size);
+        return false;
+    }
+
+    bios_data = get_bios_data_start(txt_heap);
+
+    /* check version */
+    if ( bios_data->version < 2 ) {
+        printf("unsupported BIOS data version (%u)\n", bios_data->version);
+        return false;
+    }
+    /* we assume backwards compatibility but print a warning */
+    if ( bios_data->version > 3 )
+        printf("unsupported BIOS data version (%u)\n", bios_data->version);
+
+    /* all TXT-capable CPUs support at least 2 cores */
+    if ( bios_data->num_logical_procs < 2 ) {
+        printf("BIOS data has incorrect num_logical_procs (%u)\n",
+               bios_data->num_logical_procs);
+        return false;
+    }
+#define NR_CPUS 8 // XXX arbitrary value; use something sane
+    else if ( bios_data->num_logical_procs > NR_CPUS ) {
+        printf("BIOS data specifies too many CPUs (%u)\n",
+               bios_data->num_logical_procs);
+        return false;
+    }
+
+    print_bios_data(bios_data);
+
+    return true;
+}
+
+static void print_os_mle_data(os_mle_data_t *os_mle_data)
+{
+    printf("os_mle_data (@%p, %Lx):\n", os_mle_data,
+           *((uint64_t *)os_mle_data - 1));
+    printf("\t version: %u\n", os_mle_data->version);
+    /* TBD: perhaps eventually print saved_mtrr_state field */
+    printf("\t mbi: %p\n", os_mle_data->mbi);
+}
+
+static bool verify_os_mle_data(txt_heap_t *txt_heap)
+{
+    uint64_t size, heap_size;
+    os_mle_data_t *os_mle_data;
+
+    /* check size */
+    heap_size = read_priv_config_reg(TXTCR_HEAP_SIZE);
+    size = get_os_mle_data_size(txt_heap);
+    if ( size == 0 ) {
+        printf("OS to MLE data size is 0\n");
+        return false;
+    }
+    if ( size > heap_size ) {
+        printf("OS to MLE data size is larger than heap size "
+               "(%Lx, heap size=%Lx)\n", size, heap_size);
+        return false;
+    }
+    if ( size != (sizeof(os_mle_data_t) + sizeof(size)) ) {
+        printf("OS to MLE data size (%Lx) is not equal to "
+               "os_mle_data_t size (%x)\n", size, sizeof(os_mle_data_t));
+        return false;
+    }
+
+    os_mle_data = get_os_mle_data_start(txt_heap);
+
+    /* check version */
+    /* since this data is from our pre-launch to post-launch code only, it */
+    /* should always be this */
+    if ( os_mle_data->version != 2 ) {
+        printf("unsupported OS to MLE data version (%u)\n",
+               os_mle_data->version);
+        return false;
+    }
+
+    /* field checks */
+    if ( os_mle_data->mbi == NULL ) {
+        printf("OS to MLE data mbi field is NULL\n");
+        return false;
+    }
+
+    print_os_mle_data(os_mle_data);
+
+    return true;
+}
+
+void print_os_sinit_data(os_sinit_data_t *os_sinit_data)
+{
+    printf("os_sinit_data (@%p, %Lx):\n", os_sinit_data,
+           *((uint64_t *)os_sinit_data - 1));
+    printf("\t version: %u\n", os_sinit_data->version);
+    printf("\t mle_ptab: 0x%Lx\n", os_sinit_data->mle_ptab);
+    printf("\t mle_size: 0x%Lx (%Lu)\n", os_sinit_data->mle_size,
+           os_sinit_data->mle_size);
+    printf("\t mle_hdr_base: 0x%Lx\n", os_sinit_data->mle_hdr_base);
+    printf("\t vtd_pmr_lo_base: 0x%Lx\n", os_sinit_data->vtd_pmr_lo_base);
+    printf("\t vtd_pmr_lo_size: 0x%Lx\n", os_sinit_data->vtd_pmr_lo_size);
+    printf("\t vtd_pmr_hi_base: 0x%Lx\n", os_sinit_data->vtd_pmr_hi_base);
+    printf("\t vtd_pmr_hi_size: 0x%Lx\n", os_sinit_data->vtd_pmr_hi_size);
+    printf("\t lcp_po_base: 0x%Lx\n", os_sinit_data->lcp_po_base);
+    printf("\t lcp_po_size: 0x%Lx (%Lu)\n", os_sinit_data->lcp_po_size,
+           os_sinit_data->lcp_po_size);
+    print_txt_caps("\t ", os_sinit_data->capabilities);
+    if ( os_sinit_data->version >= 5 )
+        printf("\t efi_rsdt_ptr: 0x%Lx\n", os_sinit_data->efi_rsdt_ptr);
+}
+
+static bool verify_os_sinit_data(txt_heap_t *txt_heap)
+{
+    uint64_t size, heap_size;
+    os_sinit_data_t *os_sinit_data;
+
+    /* check size */
+    heap_size = read_priv_config_reg(TXTCR_HEAP_SIZE);
+    size = get_os_sinit_data_size(txt_heap);
+    if ( size == 0 ) {
+        printf("OS to SINIT data size is 0\n");
+        return false;
+    }
+    if ( size > heap_size ) {
+        printf("OS to SINIT data size is larger than heap size "
+               "(%Lx, heap size=%Lx)\n", size, heap_size);
+        return false;
+    }
+
+    os_sinit_data = get_os_sinit_data_start(txt_heap);
+
+    /* check version (but since we create this, it should always be OK) */
+    if ( os_sinit_data->version < 4 || os_sinit_data->version > 5 ) {
+        printf("unsupported OS to SINIT data version (%u)\n",
+               os_sinit_data->version);
+        return false;
+    }
+
+    if ( (os_sinit_data->version == 4 &&
+          size != offsetof(os_sinit_data_t, efi_rsdt_ptr) + sizeof(uint64_t))
+         || (os_sinit_data->version == 5 &&
+             size != sizeof(os_sinit_data_t) + sizeof(uint64_t)) ) {
+        printf("OS to SINIT data size (%Lx) does not match for version (%x)\n",
+               size, sizeof(os_sinit_data_t));
+        return false;
+    }
+
+    print_os_sinit_data(os_sinit_data);
+
+    return true;
+}
+
+static void print_sinit_mdrs(sinit_mdr_t mdrs[], uint32_t num_mdrs)
+{
+    static const char *mem_types[] = {"GOOD", "SMRAM OVERLAY",
+                                      "SMRAM NON-OVERLAY",
+                                      "PCIE EXTENDED CONFIG", "PROTECTED"};
+    unsigned int i;
+
+    printf("\t sinit_mdrs:\n");
+    for ( i = 0; i < num_mdrs; i++ ) {
+        printf("\t\t %016Lx - %016Lx ", mdrs[i].base,
+               mdrs[i].base + mdrs[i].length);
+        if ( mdrs[i].mem_type < sizeof(mem_types)/sizeof(mem_types[0]) )
+            printf("(%s)\n", mem_types[mdrs[i].mem_type]);
+        else
+            printf("(%d)\n", (int)mdrs[i].mem_type);
+    }
+}
+
+static void print_sinit_mle_data(sinit_mle_data_t *sinit_mle_data)
+{
+    printf("sinit_mle_data (@%p, %Lx):\n", sinit_mle_data,
+           *((uint64_t *)sinit_mle_data - 1));
+    printf("\t version: %u\n", sinit_mle_data->version);
+    printf("\t bios_acm_id: \n\t");
+    print_heap_hash(sinit_mle_data->bios_acm_id);
+    printf("\t edx_senter_flags: 0x%08x\n",
+           sinit_mle_data->edx_senter_flags);
+    printf("\t mseg_valid: 0x%Lx\n", sinit_mle_data->mseg_valid);
+    printf("\t sinit_hash:\n\t"); print_heap_hash(sinit_mle_data->sinit_hash);
+    printf("\t mle_hash:\n\t"); print_heap_hash(sinit_mle_data->mle_hash);
+    printf("\t stm_hash:\n\t"); print_heap_hash(sinit_mle_data->stm_hash);
+    printf("\t lcp_policy_hash:\n\t");
+        print_heap_hash(sinit_mle_data->lcp_policy_hash);
+    printf("\t lcp_policy_control: 0x%08x\n",
+           sinit_mle_data->lcp_policy_control);
+    printf("\t rlp_wakeup_addr: 0x%x\n", sinit_mle_data->rlp_wakeup_addr);
+    printf("\t num_mdrs: %u\n", sinit_mle_data->num_mdrs);
+    printf("\t mdrs_off: 0x%x\n", sinit_mle_data->mdrs_off);
+    printf("\t num_vtd_dmars: %u\n", sinit_mle_data->num_vtd_dmars);
+    printf("\t vtd_dmars_off: 0x%x\n", sinit_mle_data->vtd_dmars_off);
+    print_sinit_mdrs((sinit_mdr_t *)
+                     (((void *)sinit_mle_data - sizeof(uint64_t)) +
+                      sinit_mle_data->mdrs_off), sinit_mle_data->num_mdrs);
+    if ( sinit_mle_data->version >= 8 )
+        printf("\t proc_scrtm_status: 0x%08x\n",
+               sinit_mle_data->proc_scrtm_status);
+}
+
+static bool verify_sinit_mle_data(txt_heap_t *txt_heap)
+{
+    uint64_t size, heap_size;
+    sinit_mle_data_t *sinit_mle_data;
+
+    /* check size */
+    heap_size = read_priv_config_reg(TXTCR_HEAP_SIZE);
+    size = get_sinit_mle_data_size(txt_heap);
+    if ( size == 0 ) {
+        printf("SINIT to MLE data size is 0\n");
+        return false;
+    }
+    if ( size > heap_size ) {
+        printf("SINIT to MLE data size is larger than heap size\n"
+               "(%Lx, heap size=%Lx)\n", size, heap_size);
+        return false;
+    }
+
+    sinit_mle_data = get_sinit_mle_data_start(txt_heap);
+
+    /* check version */
+    sinit_mle_data = get_sinit_mle_data_start(txt_heap);
+    if ( sinit_mle_data->version < 6 ) {
+        printf("unsupported SINIT to MLE data version (%u)\n",
+               sinit_mle_data->version);
+        return false;
+    }
+    else if ( sinit_mle_data->version > 8 ) {
+        printf("unsupported SINIT to MLE data version (%u)\n",
+               sinit_mle_data->version);
+    }
+
+    /* this data is generated by SINIT and so is implicitly trustworthy, */
+    /* so we don't need to validate it's fields */
+
+    print_sinit_mle_data(sinit_mle_data);
+
+    return true;
+}
+
+bool verify_txt_heap(txt_heap_t *txt_heap, bool bios_data_only)
+{
+    uint64_t size1, size2, size3, size4;
+
+    /* verify BIOS to OS data */
+    if ( !verify_bios_data(txt_heap) )
+        return false;
+
+    if ( bios_data_only )
+        return true;
+
+    /* check that total size is within the heap */
+    size1 = get_bios_data_size(txt_heap);
+    size2 = get_os_mle_data_size(txt_heap);
+    size3 = get_os_sinit_data_size(txt_heap);
+    size4 = get_sinit_mle_data_size(txt_heap);
+
+    /* overflow? */
+    if ( plus_overflow_u64(size1, size2) ) {
+        printf("TXT heap data size overflows\n");
+        return false;
+    }
+    if ( plus_overflow_u64(size3, size4) ) {
+        printf("TXT heap data size overflows\n");
+        return false;
+    }
+    if ( plus_overflow_u64(size1 + size2, size3 + size4) ) {
+        printf("TXT heap data size overflows\n");
+        return false;
+    }
+
+    if ( (size1 + size2 + size3 + size4) >
+         read_priv_config_reg(TXTCR_HEAP_SIZE) ) {
+        printf("TXT heap data sizes (%Lx, %Lx, %Lx, %Lx) are larger than\n"
+               "heap total size (%Lx)\n", size1, size2, size3, size4,
+               read_priv_config_reg(TXTCR_HEAP_SIZE));
+        return false;
+    }
+
+    /* verify OS to MLE data */
+    if ( !verify_os_mle_data(txt_heap) )
+        return false;
+
+    /* verify OS to SINIT data */
+    if ( !verify_os_sinit_data(txt_heap) )
+        return false;
+
+    /* verify SINIT to MLE data */
+    if ( !verify_sinit_mle_data(txt_heap) )
+        return false;
+
+    return true;
+}
+
+
+/*
+ * Local variables:
+ * mode: C
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
---
+++ emhf/trunk/code/x86/init/txt_heap.h
@@ -0,0 +1,274 @@
+/*
+ * @XMHF_LICENSE_HEADER_START@
+ *
+ * eXtensible, Modular Hypervisor Framework (XMHF)
+ * Copyright (c) 2009-2012 Carnegie Mellon University
+ * Copyright (c) 2010-2012 VDG Inc.
+ * All Rights Reserved.
+ *
+ * Developed by: XMHF Team
+ *               Carnegie Mellon University / CyLab
+ *               VDG Inc.
+ *               http://xmhf.org
+ *
+ * This file is part of the EMHF historical reference
+ * codebase, and is released under the terms of the
+ * GNU General Public License (GPL) version 2.
+ * Please see the LICENSE file for details.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * @XMHF_LICENSE_HEADER_END@
+ */
+
+/*
+ * heap.h: Intel(r) TXT heap definitions
+ *
+ * Copyright (c) 2003-2008, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/*
+ * Modified for EMHF by jonmccune@cmu.edu, 2011.01.04
+ */
+
+#ifndef __TXT_HEAP_H__
+#define __TXT_HEAP_H__
+
+/*
+ * data-passing structures contained in TXT heap:
+ *   - BIOS
+ *   - OS/loader to MLE
+ *   - OS/loader to SINIT
+ *   - SINIT to MLE
+ */
+
+/*
+ * BIOS structure
+ */
+typedef struct {
+    uint32_t  version;              /* WB = 2, current = 3 */
+    uint32_t  bios_sinit_size;
+    uint64_t  lcp_pd_base;
+    uint64_t  lcp_pd_size;
+    uint32_t  num_logical_procs;
+    /* versions >= 3 */
+    uint64_t  flags;
+} bios_data_t;
+
+/*
+ * OS/loader to MLE structure
+ *   - private to tboot (so can be any format we need)
+ */
+#define MAX_LCP_PO_DATA_SIZE     64*1024  /* 64k */
+
+typedef struct {
+    uint32_t          version;           /* currently 2 */
+    mtrr_state_t      saved_mtrr_state;  /* saved prior to changes for SINIT */
+    multiboot_info_t* mbi;               /* needs to be restored to ebx */
+    uint32_t          saved_misc_enable_msr;  /* saved prior to SENTER */
+                                         /* PO policy data */
+    uint8_t           lcp_po_data[MAX_LCP_PO_DATA_SIZE];
+} os_mle_data_t;
+
+/*
+ * OS/loader to SINIT structure
+ */
+typedef struct {
+    uint32_t    version;           /* currently 4, 5 */
+    uint32_t    reserved;
+    uint64_t    mle_ptab;
+    uint64_t    mle_size;
+    uint64_t    mle_hdr_base;
+    uint64_t    vtd_pmr_lo_base;
+    uint64_t    vtd_pmr_lo_size;
+    uint64_t    vtd_pmr_hi_base;
+    uint64_t    vtd_pmr_hi_size;
+    uint64_t    lcp_po_base;
+    uint64_t    lcp_po_size;
+    txt_caps_t  capabilities;
+    /* versions >= 5 */
+    uint64_t    efi_rsdt_ptr;
+} os_sinit_data_t;
+
+/*
+ * SINIT to MLE structure
+ */
+#define MDR_MEMTYPE_GOOD                0x00
+#define MDR_MEMTYPE_SMM_OVERLAY         0x01
+#define MDR_MEMTYPE_SMM_NONOVERLAY      0x02
+#define MDR_MEMTYPE_PCIE_CONFIG_SPACE   0x03
+#define MDR_MEMTYPE_PROTECTED           0x04
+
+typedef struct __attribute__ ((packed)) {
+    uint64_t  base;
+    uint64_t  length;
+    uint8_t   mem_type;
+    uint8_t   reserved[7];
+} sinit_mdr_t;
+
+#define SHA1_SIZE      20
+typedef uint8_t   sha1_hash_t[SHA1_SIZE];
+
+typedef struct {
+    uint32_t     version;             /* currently 6-8 */
+    sha1_hash_t  bios_acm_id;
+    uint32_t     edx_senter_flags;
+    uint64_t     mseg_valid;
+    sha1_hash_t  sinit_hash;
+    sha1_hash_t  mle_hash;
+    sha1_hash_t  stm_hash;
+    sha1_hash_t  lcp_policy_hash;
+    uint32_t     lcp_policy_control;
+    uint32_t     rlp_wakeup_addr;
+    uint32_t     reserved;
+    uint32_t     num_mdrs;
+    uint32_t     mdrs_off;
+    uint32_t     num_vtd_dmars;
+    uint32_t     vtd_dmars_off;
+    /* versions >= 8 */
+    uint32_t     proc_scrtm_status;
+} sinit_mle_data_t;
+
+
+/*
+ * TXT heap data format and field accessor fns
+ */
+
+/*
+ * offset                 length                      field
+ * ------                 ------                      -----
+ *  0                      8                          bios_data_size
+ *  8                      bios_data_size - 8      bios_data
+ *
+ *  bios_data_size      8                          os_mle_data_size
+ *  bios_data_size +    os_mle_data_size - 8       os_mle_data
+ *   8
+ *
+ *  bios_data_size +    8                          os_sinit_data_size
+ *   os_mle_data_size
+ *  bios_data_size +    os_sinit_data_size - 8     os_sinit_data
+ *   os_mle_data_size +
+ *   8
+ *
+ *  bios_data_size +    8                          sinit_mle_data_size
+ *   os_mle_data_size +
+ *   os_sinit_data_size
+ *  bios_data_size +    sinit_mle_data_size - 8    sinit_mle_data
+ *   os_mle_data_size +
+ *   os_sinit_data_size +
+ *   8
+ */
+
+typedef void   txt_heap_t;
+
+/* this is a common use with annoying casting, so make it an inline */
+static inline txt_heap_t *get_txt_heap(void)
+{
+    return (txt_heap_t *)(unsigned long)read_pub_config_reg(TXTCR_HEAP_BASE);
+}
+
+static inline uint64_t get_bios_data_size(txt_heap_t *heap)
+{
+    return *(uint64_t *)heap;
+}
+
+static inline bios_data_t *get_bios_data_start(txt_heap_t *heap)
+{
+    return (bios_data_t *)((char*)heap + sizeof(uint64_t));
+}
+
+static inline uint64_t get_os_mle_data_size(txt_heap_t *heap)
+{
+    return *(uint64_t *)(heap + get_bios_data_size(heap));
+}
+
+static inline os_mle_data_t *get_os_mle_data_start(txt_heap_t *heap)
+{
+    return (os_mle_data_t *)(heap + get_bios_data_size(heap) +
+                              sizeof(uint64_t));
+}
+
+static inline uint64_t get_os_sinit_data_size(txt_heap_t *heap)
+{
+    return *(uint64_t *)(heap + get_bios_data_size(heap) +
+                         get_os_mle_data_size(heap));
+}
+
+static inline os_sinit_data_t *get_os_sinit_data_start(txt_heap_t *heap)
+{
+    return (os_sinit_data_t *)(heap + get_bios_data_size(heap) +
+                               get_os_mle_data_size(heap) +
+                               sizeof(uint64_t));
+}
+
+static inline uint64_t get_sinit_mle_data_size(txt_heap_t *heap)
+{
+    return *(uint64_t *)(heap + get_bios_data_size(heap) +
+                         get_os_mle_data_size(heap) +
+                         get_os_sinit_data_size(heap));
+}
+
+static inline sinit_mle_data_t *get_sinit_mle_data_start(txt_heap_t *heap)
+{
+    return (sinit_mle_data_t *)(heap + get_bios_data_size(heap) +
+                                get_os_mle_data_size(heap) +
+                                get_os_sinit_data_size(heap) +
+                                sizeof(uint64_t));
+}
+
+extern bool verify_txt_heap(txt_heap_t *txt_heap, bool bios_data_only);
+extern bool verify_bios_data(txt_heap_t *txt_heap);
+extern void print_os_sinit_data(os_sinit_data_t *os_sinit_data);
+
+#endif      /* __TXT_HEAP_H__ */
+
+
+/*
+ * Local variables:
+ * mode: C
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
