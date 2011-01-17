1b62589e5295db6153d7f2da774eff84ec6b7cb8
Jonathan McCune <jonmccune@cmu.edu>
2011-01-17 16:54:45 +0000

    more newlines for failed assertions to make serial log line break correctly
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@503 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/x86/include/error.h
+++ emhf/trunk/code/x86/include/error.h
@@ -42,8 +42,8 @@
 #ifndef __ASSEMBLY__
 
 #define HALT()	__asm__ __volatile__ ("hlt\r\n");
-#define ASSERT(_p) { if ( !(_p) ) { printf("Assertion '%s' failed, line %d, file %s\n", #_p , __LINE__, __FILE__); HALT(); } }
-#define WARNING(_p) { if ( !(_p) ) { printf("Warning Assertion '%s' failed, line %d, file %s\n", #_p , __LINE__, __FILE__);} }
+#define ASSERT(_p) { if ( !(_p) ) { printf("\nAssertion '%s' failed, line %d, file %s\n", #_p , __LINE__, __FILE__); HALT(); } }
+#define WARNING(_p) { if ( !(_p) ) { printf("\nWarning Assertion '%s' failed, line %d, file %s\n", #_p , __LINE__, __FILE__);} }
 
 /* Overflow functions from tboot-20101005/tboot/include/misc.h */
 
