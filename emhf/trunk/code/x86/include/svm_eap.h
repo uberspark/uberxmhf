9ae82bd5bd6fed6aa38a858d9f8a9a3bcbe52097
Amit Vasudevan <amitvasudevan@acm.org>
2011-01-24 21:24:50 +0000

    added DEV function codes and corresponding structure and bit definitions.
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@563 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/x86/include/svm_eap.h
+++ emhf/trunk/code/x86/include/svm_eap.h
@@ -47,7 +47,129 @@
 #define DEV_PCI_DEVICE		0x18
 #define DEV_PCI_FUNCTION	0x3
 
+
+//DEV function codes (15.24.5, AMD Dev. vol-2)
+#define DEV_BASE_LO     	0
+#define DEV_BASE_HI     	1
+#define DEV_MAP         	2
+#define DEV_CAP         	3
+#define DEV_CR          	4
+#define DEV_ERR_STATUS  	5
+#define DEV_ERR_ADDR_LO 	6
+#define DEV_ERR_ADDR_HI 	7
+
+
 #ifndef __ASSEMBLY__
+
+//DEV DEV_BASE_LO structure (p. 329, AMD BKDG)
+typedef union dev_base_low {
+	u32 bytes;
+  struct{
+    u32 valid:1; 			//1= DEV enabled, 0= DEV disabled
+    u32 protect:1; 		//0= allow out-of-range addresses, 1= disallow out-of-range addresses
+    u32 size:5; 			//size of memory region that DEV covers (4GB*(2^size))
+    u32 resv:5; 			//reserved
+    u32 base_addr:20; //bits 12-31 of physical address of DEV table
+  }fields;
+} __attribute__ ((packed)) dev_base_low_t;
+
+//DEV DEV_BASE_HI structure (p. 330, AMD BKDG)
+typedef union dev_base_hi {
+  u32 bytes;
+  struct{
+    u32 base_addr:16; //bits 32-47 of physical address of DEV table
+    u32 resv:16; 			//reserved
+  }fields;
+} __attribute__ ((packed)) dev_base_hi_t;
+
+//DEV DEV_MAP structure (p. 330, AMD BKDG)
+typedef union dev_map{
+  u32 bytes;
+  struct{
+    u32 unit0:5; 	//I/O link unit 0 ID
+    u32 valid0:1; //1= enable DEV for unit 0 and dom 0
+    u32 unit1:5; 	//I/O link unit 1 ID
+    u32 valid1:1; //1= enable DEV for unit 1 and dom 1
+    u32 busno:8; 	//HT bus number
+    u32 dom0:6; 	//protection domain number assigned to unit 0
+    u32 dom1:6;		//protection domain number assigned to unit 1 
+  }fields;
+} __attribute__ ((packed)) dev_map_t;
+
+//DEV DEV_CAP structure (p. 330, AMD BKDG)
+typedef union dev_cap{
+  u32 bytes;
+  struct{
+    u32 rev:8; 		//DEV register set revision number (00h for current)
+    u32 n_dom:8; 	//number of protection domains implemented
+    u32 n_maps:8; //number of map registers implemented
+    u32 resv:8; 	//reserved
+  }fields;
+} __attribute__ ((packed)) dev_cap_t;
+
+//DEV DEV_CR structure (p. 331, AMD BKDG)
+typedef union dev_cr {
+  u32 bytes;
+  struct{
+    u32 deven:1; 		//1=enable DEV
+    u32 resv0:1;			//reserved
+    u32 iodis:1; 		//1=disable upstream i/o cycles, set to 1 after SKINIT
+    u32 mceen:1; 		//1=enable logging and reporting of DEV violations through MCE
+    u32 devinv:1; 	//1=invalidate DEV table walk cache. bit cleared by h/w when complete
+    u32 sldev:1; 		//1=memory region associated with SKINIT is internally protected, 0=use DEV table instead
+    u32 walkprobe:1;//1=disable probing of CPU caches during DEV table walks
+    u32 resv1:25; 		//bits 7-31 reserved 
+  }fields;
+} __attribute__ ((packed)) dev_cr_t;
+
+
+//DEV DEV_ERR_STATUS structure (p	331 & 332, AMD BKDG)
+typedef union dev_err_status {
+	u32 bytes;
+	struct{
+		u32 accesstype:2;	//error code access type
+#define	DEV_ERR_STATUS_ACCESSTYPE_GENERIC						0
+#define	DEV_ERR_STATUS_ACCESSTYPE_READ              1
+#define DEV_ERR_STATUS_ACCESSTYPE_WRITE             2
+#define DEV_ERR_STATUS_ACCESSTYPE_READMODIFYWRITE		3		
+		u32 source:3;		//error code source
+#define DEV_ERR_STATUS_SOURCE_GENERIC								0
+#define DEV_ERR_STATUS_SOURCE_CPU                   1
+#define DEV_ERR_STATUS_SOURCE_DEVICE                2
+		u32 dest:3;			//error code destination
+#define DEV_ERR_STATUS_DEST_GENERIC									0
+#define DEV_ERR_STATUS_DEST_DRAM                    1
+#define DEV_ERR_STATUS_DEST_MMIO                    2
+#define DEV_ERR_STATUS_DEST_IO                      4
+#define DEV_ERR_STATUS_DEST_CONFIGURATION           5
+		u32 resv0:8;			//reserved
+		u32 mserror:8;		//model-specific error
+		u32 resv1:5;			//reserved
+		u32 addrvalid:1;	//error address valid
+		u32 overflow:1;		//error overflow, 1=DEV violation was detected
+		u32 valid:1;			//error valid, 1=DEV violation has been logged
+	}fields;
+} __attribute__ ((packed)) dev_err_status_t;
+
+
+//DEV DEV_ERROR_ADDR_LO structure (p. 332, AMD BKDG)
+typedef union dev_err_addr_lo {
+	u32 bytes;
+	struct{
+		u32 resv0:2;	//reserved
+		u32 addr:30;	//bits 2:31 of error address	
+	}fields;
+} __attribute__((packed)) dev_err_addr_lo_t;
+
+//DEV DEV_ERROR_ADDR_HI structure (p. 332, AMD BKDG)
+typedef union dev_err_addr_hi {
+	u32 bytes;
+	struct{
+		u32 addr:16;	//bits 32-47 of error address
+		u32 resv:16;	//reserved	
+	}fields;
+} __attribute__((packed)) dev_err_addr_hi_t;
+
 
 //initialize SVM EAP a.k.a DEV
 //returns 1 if all went well, else 0
