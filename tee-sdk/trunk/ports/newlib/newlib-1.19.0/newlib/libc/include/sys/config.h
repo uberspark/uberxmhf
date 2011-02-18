74e3805de0883d004a84eac4a4922283103d138d
James Newsome <jnewsome@cmu.edu>
2011-02-18 23:33:49 +0000

    undefine __LARGE64_FILES, since libnosys doesn't implement the corresponding stubs. ugly hack- we ought to include 'trustvisor' flag in config and build system
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@716 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tee-sdk/trunk/ports/newlib/newlib-1.19.0/newlib/libc/include/sys/config.h
+++ tee-sdk/trunk/ports/newlib/newlib-1.19.0/newlib/libc/include/sys/config.h
@@ -86,9 +86,9 @@
 #define HAVE_GETDATE
 #define _HAVE_SYSTYPES
 #define _READ_WRITE_RETURN_TYPE _ssize_t
-#define __LARGE64_FILES 1
-/* we use some glibc header files so turn on glibc large file feature */
-#define _LARGEFILE64_SOURCE 1
+/* #define __LARGE64_FILES 1 */
+/* /\* we use some glibc header files so turn on glibc large file feature *\/ */
+/* #define _LARGEFILE64_SOURCE 1 */
 #endif
 #endif
 
