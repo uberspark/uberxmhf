d82ad512e50e1bc07867251a4895967fdbaca6d8
James Newsome <jnewsome@cmu.edu>
2011-02-18 23:33:48 +0000

    install to a local directory
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@715 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tee-sdk/trunk/ports/newlib/build.sh
+++ tee-sdk/trunk/ports/newlib/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 mkdir build
+mkdir install
+
 cd build
 
-STRIP_FOR_TARGET=strip RANLIB_FOR_TARGET=ranlib OBJDUMP_FOR_TARGET=objdump NM_FOR_TARGET=nm LD_FOR_TARGET=ld AS_FOR_TARGET=as AR_FOR_TARGET=ar CC_FOR_TARGET=cc GCC_FOR_TARGET=gcc ../newlib-1.19.0/configure --target=i586 --disable-multilib
+STRIP_FOR_TARGET=strip RANLIB_FOR_TARGET=ranlib OBJDUMP_FOR_TARGET=objdump NM_FOR_TARGET=nm LD_FOR_TARGET=ld AS_FOR_TARGET=as AR_FOR_TARGET=ar CC_FOR_TARGET=cc GCC_FOR_TARGET=gcc ../newlib-1.19.0/configure --target=i586 --disable-multilib --prefix=`pwd`/../install
 
 make
-
+make install
