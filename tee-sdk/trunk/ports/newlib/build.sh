77714095d0fbd1f3de218dd0cffcaec7061b9d41
James Newsome <jnewsome@cmu.edu>
2011-02-18 23:33:46 +0000

    --disable-multilib to prevent trying to build a 64 bit version
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@714 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tee-sdk/trunk/ports/newlib/build.sh
+++ tee-sdk/trunk/ports/newlib/build.sh
@@ -3,7 +3,7 @@
 mkdir build
 cd build
 
-STRIP_FOR_TARGET=strip RANLIB_FOR_TARGET=ranlib OBJDUMP_FOR_TARGET=objdump NM_FOR_TARGET=nm LD_FOR_TARGET=ld AS_FOR_TARGET=as AR_FOR_TARGET=ar CC_FOR_TARGET=cc GCC_FOR_TARGET=gcc ../newlib-1.19.0/configure --target=i586
+STRIP_FOR_TARGET=strip RANLIB_FOR_TARGET=ranlib OBJDUMP_FOR_TARGET=objdump NM_FOR_TARGET=nm LD_FOR_TARGET=ld AS_FOR_TARGET=as AR_FOR_TARGET=ar CC_FOR_TARGET=cc GCC_FOR_TARGET=gcc ../newlib-1.19.0/configure --target=i586 --disable-multilib
 
 make
 
