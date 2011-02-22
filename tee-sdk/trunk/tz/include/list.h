c6a8cf0c417a7692a3cb2833e70dd7a2e9e4b7ca
James Newsome <jnewsome@cmu.edu>
2011-02-22 23:42:06 +0000

    a simple linked list implementation
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@740 1c601a1c-44f4-4d93-90ac-4b632a93592e

---
+++ tee-sdk/trunk/tz/include/list.h
@@ -0,0 +1,101 @@
+/*
+ * @XMHF_LICENSE_HEADER_START@
+ *
+ * eXtensible, Modular Hypervisor Framework (XMHF)
+ * Copyright (c) 2009-2012 Carnegie Mellon University
+ * Copyright (c) 2010-2012 VDG Inc.
+ * All Rights Reserved.
+ *
+ * Developed by: XMHF Team
+ *               Carnegie Mellon University / CyLab
+ *               VDG Inc.
+ *               http://xmhf.org
+ *
+ * This file is part of the EMHF historical reference
+ * codebase, and is released under the terms of the
+ * GNU General Public License (GPL) version 2.
+ * Please see the LICENSE file for details.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * @XMHF_LICENSE_HEADER_END@
+ */
+
+#ifndef LIST_H
+#define LIST_H
+
+/* loosely based on ch 7 of "C Interfaces and Implementations" by
+   David Hanson */
+
+typedef struct ll_t {
+  void *first;
+  struct ll_t *rest;
+} ll_t;
+
+__attribute__((unused))
+static void* LL_first(ll_t *list)
+{
+  return list->first;
+}
+
+__attribute__((unused))
+static ll_t* LL_rest(ll_t *list)
+{
+  return list->rest;
+}
+
+__attribute__((unused))
+static ll_t* LL_push(void *first, ll_t *rest)
+{
+  ll_t *rv = malloc(sizeof(ll_t));
+  *rv = (ll_t) {
+    .first = first,
+    .rest = rest
+  };
+
+  return rv;
+}
+
+__attribute__((unused))
+static ll_t* LL_pop(ll_t *list, void **x)
+{
+  ll_t *rest = NULL;
+
+  if(list) {
+    rest = list->rest;
+    if (x) {
+      *x = list->first;
+    }
+    free(list);
+  }
+
+  return rest;
+}
+
+/* frees the list structure itself, not the items in it */
+__attribute__((unused))
+static void LL_free(ll_t *list)
+{
+  while (list != NULL) {
+    list = LL_pop(list, NULL);
+  }
+}
+
+/* for loop. on each iteration x is set to the current head item, l is set to the current tail */
+#define LL_FOR_EACH(l, x) for((x) = (l)->first ; (l) != NULL ; (x) = (l)->first, (l) = (l)->rest)
+
+
+
+#endif
