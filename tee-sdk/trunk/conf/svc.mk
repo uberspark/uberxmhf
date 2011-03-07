f04aec1faac4c7bb4c2403443584f00ef8065f8d
James Newsome <jnewsome@cmu.edu>
2011-03-07 22:31:25 +0000

    libtz now compiled with proper flags for services. also generate pkg-config manifests
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@801 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tee-sdk/trunk/conf/svc.mk
+++ tee-sdk/trunk/conf/svc.mk
@@ -1,7 +1,6 @@
-SVCAPIDIR := $(dir $(lastword $(MAKEFILE_LIST)))
-
 #linker script to ensure proper layout, padding, etc.
-LINK_SCRIPT = $(SVCAPIDIR)/svcapi/svc.ld 
+SVC_PREFIX ?= /usr/local
+SVC_LINK_SCRIPT ?= $(SVC_PREFIX)/conf/svc/svc.ld 
 
 # prevent gcc from compiling switch statements as jump tables.
 # can probably get rid of this in the future by somehow ensuring that
@@ -11,8 +10,5 @@ SVC_CFLAGS+=-fno-jump-tables
 # Not sure whether this is necessary. Carried over from an old makefile
 SVC_CFLAGS+=-fno-stack-protector
 
-# include this directory. layout.h needs this.
-SVC_CFLAGS+=-I $(SVCAPIDIR)/common
-
 # use the designated linker script
-SVC_LDFLAGS+= -T $(LINK_SCRIPT)
+SVC_LDFLAGS+= -T $(SVC_LINK_SCRIPT)
