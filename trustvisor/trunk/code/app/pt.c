a012d957b80a99730e7e478c53d3e780f28ebecc
James Newsome <jnewsome@cmu.edu>
2011-03-28 19:22:27 +0000

    refactor guest_walker_internal to use VCPU_gcr[34]
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@888 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- trustvisor/trunk/code/app/pt.c
+++ trustvisor/trunk/code/app/pt.c
@@ -630,23 +630,8 @@ u32 guest_pt_walker_internal(VCPU *vcpu, u32 vaddr, u64 *pdp, u64 *pd, u64 *pt, 
 	u32 pd_index, pt_index, offset;
 	u64 paddr;
 
-	void * linux_vmcb;
-	u32 is_pae;
-	u64 gcr3;
-
-	if (vcpu->cpu_vendor == CPU_VENDOR_INTEL) {
-		linux_vmcb = (struct _vmx_vmcsfields *)(&(vcpu->vmcs));
-		is_pae = ((struct _vmx_vmcsfields *)linux_vmcb)->guest_CR4 & CR4_PAE;
-		gcr3 = ((struct _vmx_vmcsfields *)linux_vmcb)->guest_CR3;
-	} else if (vcpu->cpu_vendor == CPU_VENDOR_AMD) {
-		linux_vmcb = (struct vmcb_struct *)(vcpu->vmcb_vaddr_ptr);
-		gcr3 = ((struct vmcb_struct *)linux_vmcb)->cr3;
-		is_pae = ((struct vmcb_struct *)linux_vmcb)->cr4 & CR4_PAE;
-	} else {
-		printf("unknown cpu vendor!\n");
-		HALT();
-	}
-
+	const u32 is_pae = VCPU_gcr4(vcpu) & CR4_PAE;
+	const u64 gcr3 = VCPU_gcr3(vcpu);
 
 	/* intialize PT page phys addr to -1 */
 	if (pdp) *pdp = 0xFFFFFFFF;
