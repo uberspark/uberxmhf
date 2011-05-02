8bd6fcd31bebedffd6c6ef3d87818a1605453728
James Newsome <jnewsome@cmu.edu>
2011-05-02 22:22:43 +0000

    grab more recent cmdline.c from tboot
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@1194 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/x86/init/cmdline.c
+++ emhf/trunk/code/x86/init/cmdline.c
@@ -36,7 +36,7 @@
 /*
  * cmdline.c: command line parsing fns
  *
- * Copyright (c) 2006-2008, Intel Corporation
+ * Copyright (c) 2006-2010, Intel Corporation
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -68,32 +68,29 @@
  *
  */
 
-/* #include <config.h> */
+#include <config.h>
 #include <types.h>
-#include <str.h>
+#include <stdbool.h>
 #include <ctype.h>
-#include <print.h>
-/* #include <compiler.h> */
-/* #include <string2.h> */
-/* #include <misc.h> */
-/* #include <stdbool.h> */
+#include <compiler.h>
+#include <string.h>
+#include <misc.h>
+#include <printk.h>
 #include <cmdline.h>
 
 /*
  * copy of original command line
  * part of tboot measurement (hence in .text section)
  */
-char g_cmdline[CMDLINE_SIZE] = { 0 };
+__text char g_cmdline[CMDLINE_SIZE] = { 0 };
 
-#define ARRAY_SIZE(a)     (sizeof(a) / sizeof((a)[0]))
-
-/*Used for kernel command line parameter setup */
+/* Used for kernel command line parameter setup */
 typedef struct {
     const char *name;          /* set to NULL for last item in list */
     const char *def_val;
 } cmdline_option_t;
 
-#define MAX_VALUE_LEN 32
+#define MAX_VALUE_LEN 64
 
 /*
  * the option names and default values must be separate from the actual
@@ -104,30 +101,32 @@ typedef struct {
 
 /* global option array for command line */
 static const cmdline_option_t g_tboot_cmdline_options[] = {
-    /* { "loglvl", "all" },     /\* all|none *\/ */
-    { "logging", "serial" }, /* vga,serial,memory|none */
-    { "serial", "" },        /* <baud>[/<clock_hz>][,<DPS>[,<io-base>]] or
-                                auto[/<clock_hz>][,<DPS>[,<io-base>]]*/
+    { "loglvl",     "all" },     /* all|none */
+    { "logging",    "serial,vga" },  /* vga,serial,memory|none */
+    { "serial",     "115200,8n1,0x3f8" },
+    /* serial=<baud>[/<clock_hz>][,<DPS>[,<io-base>[,<irq>[,<serial-bdf>[,<bridge-bdf>]]]]] */
+    { "vga_delay",  "0" },      /* # secs */
+    { "no_usb",     "true" },   /* true|false */
     { NULL, NULL }
 };
 static char g_tboot_param_values[ARRAY_SIZE(g_tboot_cmdline_options)][MAX_VALUE_LEN];
 
-void print_tboot_values(void)
-{
-    int i;
-    for ( i = 0; g_tboot_cmdline_options[i].name != NULL; i++ )
-        printf("val[%d]: %s\n", i, &(g_tboot_param_values[i][0]));
-}
+static const cmdline_option_t g_linux_cmdline_options[] = {
+    { "vga", "" },
+    { "mem", "" },
+    { NULL, NULL }
+};
+static char g_linux_param_values[ARRAY_SIZE(g_linux_cmdline_options)][MAX_VALUE_LEN];
 
 static const char* get_option_val(const cmdline_option_t *options,
                                   char vals[][MAX_VALUE_LEN],
                                   const char *opt_name)
 {
-    int i;
-    for ( i = 0; options[i].name != NULL; i++ ) {
+    for ( int i = 0; options[i].name != NULL; i++ ) {
         if ( strcmp(options[i].name, opt_name) == 0 )
             return vals[i];
     }
+    printk("requested unknown option: %s\n", opt_name);
     return NULL;
 }
 
@@ -156,38 +155,31 @@ static void cmdline_parse(char *cmdline, const cmdline_option_t *options,
             break;
 
         /* find end of current option */
-        {
-            const char *opt_start = p;
-            const char *opt_end = strchr(opt_start, ' ');
-            if ( opt_end == NULL )
-                opt_end = opt_start + strlen(opt_start);
-            p = opt_end;
+        const char *opt_start = p;
+        const char *opt_end = strchr(opt_start, ' ');
+        if ( opt_end == NULL )
+            opt_end = opt_start + strlen(opt_start);
+        p = opt_end;
 
-            /* find value part; if no value found, use default and continue */
-            {
-                const char *val_start = strchr(opt_start, '=');
-                unsigned int opt_name_size;
-                unsigned int copy_size;
-                if ( val_start == NULL || val_start > opt_end )
-                    continue;
-                val_start++;
+        /* find value part; if no value found, use default and continue */
+        const char *val_start = strchr(opt_start, '=');
+        if ( val_start == NULL || val_start > opt_end )
+            continue;
+        val_start++;
 
-                opt_name_size = val_start - opt_start - 1;
-                /* (+1 because we'll overwrite the last char with '\0' when we copy) */
-                copy_size = opt_end - val_start + 1;
-                if ( copy_size > MAX_VALUE_LEN - 1 )
-                    copy_size = MAX_VALUE_LEN - 1;
-                if ( opt_name_size == 0 || copy_size == 0 )
-                    continue;
+        unsigned int opt_name_size = val_start - opt_start - 1;
+        unsigned int copy_size = opt_end - val_start;
+        if ( copy_size > MAX_VALUE_LEN - 1 )
+            copy_size = MAX_VALUE_LEN - 1;
+        if ( opt_name_size == 0 || copy_size == 0 )
+            continue;
 
-                /* value found, so copy it */
-                for ( i = 0; options[i].name != NULL; i++ ) {
-                    if ( strncmp(options[i].name, opt_start, opt_name_size ) == 0 ) {
-                        strncpy(vals[i], val_start, copy_size);
-                        vals[i][copy_size] = '\0';
-                        break;
-                    }
-                }
+        /* value found, so copy it */
+        for ( i = 0; options[i].name != NULL; i++ ) {
+            if ( strncmp(options[i].name, opt_start, opt_name_size ) == 0 ) {
+                strncpy(vals[i], val_start, copy_size);
+                vals[i][copy_size] = '\0'; /* add '\0' to the end of string */
+                break;
             }
         }
     }
@@ -198,21 +190,21 @@ void tboot_parse_cmdline(void)
     cmdline_parse(g_cmdline, g_tboot_cmdline_options, g_tboot_param_values);
 }
 
-/* void linux_parse_cmdline(char *cmdline) */
-/* { */
-/*     cmdline_parse(cmdline, g_linux_cmdline_options, g_linux_param_values); */
-/* } */
+void linux_parse_cmdline(char *cmdline)
+{
+    cmdline_parse(cmdline, g_linux_cmdline_options, g_linux_param_values);
+}
 
-/* void get_tboot_loglvl(void) */
-/* { */
-/*     const char *loglvl = get_option_val(g_tboot_cmdline_options, */
-/*                                         g_tboot_param_values, "loglvl"); */
-/*     if ( loglvl == NULL ) */
-/*         return; */
+void get_tboot_loglvl(void)
+{
+    const char *loglvl = get_option_val(g_tboot_cmdline_options,
+                                        g_tboot_param_values, "loglvl");
+    if ( loglvl == NULL )
+        return;
 
-/*     if ( strcmp(loglvl, "none") == 0 ) */
-/*         g_log_level = TBOOT_LOG_LEVEL_NONE; /\* print nothing *\/ */
-/* } */
+    if ( strcmp(loglvl, "none") == 0 )
+        g_log_level = TBOOT_LOG_LEVEL_NONE; /* print nothing */
+}
 
 void get_tboot_log_targets(void)
 {
@@ -225,27 +217,27 @@ void get_tboot_log_targets(void)
 
     /* determine if no targets set explicitly */
     if ( strcmp(targets, "none") == 0 ) {
-        g_log_targets = LOG_TARGET_NONE; /* print nothing */
+        g_log_targets = TBOOT_LOG_TARGET_NONE; /* print nothing */
         return;
     }
 
     /* else init to nothing and parse the possible targets */
-    g_log_targets = LOG_TARGET_NONE;
+    g_log_targets = TBOOT_LOG_TARGET_NONE;
 
     while ( *targets != '\0' ) {
         if ( strncmp(targets, "memory", 6) == 0 ) {
-            g_log_targets |= LOG_TARGET_MEMORY;
+            g_log_targets |= TBOOT_LOG_TARGET_MEMORY;
             targets += 6;
         }
         else if ( strncmp(targets, "serial", 6) == 0 ) {
-            g_log_targets |= LOG_TARGET_SERIAL;
+            g_log_targets |= TBOOT_LOG_TARGET_SERIAL;
             targets += 6;
         }
         else if ( strncmp(targets, "vga", 3) == 0 ) {
-            g_log_targets |= LOG_TARGET_VGA;
+            g_log_targets |= TBOOT_LOG_TARGET_VGA;
             targets += 3;
         }
-        else
+        else 
             break; /* unrecognized, end loop */
 
         if ( *targets == ',' )
@@ -253,87 +245,249 @@ void get_tboot_log_targets(void)
         else
             break; /* unrecognized, end loop */
     }
-
-    if ( g_log_targets & LOG_TARGET_SERIAL ) {
-        const char *serial = get_option_val(g_tboot_cmdline_options,
-                                            g_tboot_param_values, "serial");
-
-        /* nothing set, leave defaults */
-        if ( ( serial == NULL ) || ( *serial == '\0' ) )
-            return;
-
-        /* call configuration parser in early serial code to do the rest */
-        early_serial_parse_port_config(serial);
-    }
 }
 
-/* bool get_linux_vga(int *vid_mode) */
-/* { */
-/*     const char *vga = get_option_val(g_linux_cmdline_options, */
-/*                                      g_linux_param_values, "vga"); */
-/*     if ( vga == NULL || vid_mode == NULL ) */
-/*         return false; */
+static bool parse_pci_bdf(const char **bdf, uint32_t *bus, uint32_t *slot,
+                          uint32_t *func)
+{
+    *bus = strtoul(*bdf, (char **)bdf, 16);
+    if ( **bdf != ':' )
+        return false;
+    (*bdf)++;
+    *slot = strtoul(*bdf, (char **)bdf, 16);
+    if ( **bdf != '.' )
+        return false;
+    (*bdf)++;
+    *func = strtoul(*bdf, (char **)bdf, 16);
 
-/*     if ( strcmp(vga, "normal") == 0 ) */
-/*         *vid_mode = 0xFFFF; */
-/*     else if ( strcmp(vga, "ext") == 0 ) */
-/*         *vid_mode = 0xFFFE; */
-/*     else if ( strcmp(vga, "ask") == 0 ) */
-/*         *vid_mode = 0xFFFD; */
-/*     else */
-/*         *vid_mode = simple_strtol(vga, NULL, 0); */
+    return true;
+}
 
-/*     return true; */
-/* } */
+bool g_psbdf_enabled = false;
+static bool parse_com_psbdf(const char **bdf)
+{
+    g_psbdf_enabled = parse_pci_bdf(bdf,
+                  &g_com_port.comc_psbdf.bus,
+                  &g_com_port.comc_psbdf.slot,
+                  &g_com_port.comc_psbdf.func);
 
-/* bool get_linux_mem(uint64_t *max_mem) */
-/* { */
-/*     char *last = NULL; */
-/*     const char *mem = get_option_val(g_linux_cmdline_options, */
-/*                                      g_linux_param_values, "mem"); */
-/*     if ( mem == NULL || max_mem == NULL ) */
-/*         return false; */
+    return g_psbdf_enabled;
+}
 
-/*     *max_mem = simple_strtoul(mem, &last, 0); */
-/*     if ( *max_mem == 0 ) */
-/*         return false; */
+bool g_pbbdf_enabled = false;
+static bool parse_com_pbbdf(const char **bdf)
+{
+    g_pbbdf_enabled = parse_pci_bdf(bdf,
+                  &g_com_port.comc_pbbdf.bus,
+                  &g_com_port.comc_pbbdf.slot,
+                  &g_com_port.comc_pbbdf.func);
 
-/*     if ( last == NULL ) */
-/*         return true; */
+    return g_pbbdf_enabled;
+}
 
-/*     switch (*last) { */
-/*         case 'G': */
-/*         case 'g': */
-/*             *max_mem = *max_mem << 30; */
-/*             return true; */
-/*         case 'M': */
-/*         case 'm': */
-/*             *max_mem = *max_mem << 20; */
-/*             return true; */
-/*         case 'K': */
-/*         case 'k': */
-/*             *max_mem = *max_mem << 10; */
-/*             return true; */
-/*     } */
+static bool parse_com_fmt(const char **fmt)
+{
+    /* fmt:  <5|6|7|8><n|o|e|m|s><0|1> */
+    /* default 8n1 */
+    uint8_t data_bits = 8;
+    uint8_t parity = 'n';
+    uint8_t stop_bits = 1;
 
-/*     return true; */
-/* } */
 
-/* const char *skip_filename(const char *cmdline) */
-/* { */
-/*     if ( cmdline == NULL || *cmdline == '\0' ) */
-/*         return cmdline; */
+    /* must specify all values */
+    if ( strlen(*fmt) < 3 )
+        return false;
 
-/*     /\* strip leading spaces, file name, then any spaces until the next  */
-/*      non-space char (e.g. "  /foo/bar   baz" -> "baz"; "/foo/bar" -> "")*\/  */
-/*     while ( *cmdline != '\0' && isspace(*cmdline) ) */
-/*         cmdline++; */
-/*     while ( *cmdline != '\0' && !isspace(*cmdline) ) */
-/*         cmdline++; */
-/*     while ( *cmdline != '\0' && isspace(*cmdline) ) */
-/*         cmdline++; */
-/*     return cmdline; */
-/* } */
+    /* data bits */
+    if ( **fmt >= '5' && **fmt <= '8' )
+        data_bits = **fmt - '0';
+    else
+        return false;
+    (*fmt)++;
+
+    /* parity */
+    if ( **fmt == 'n' || **fmt == 'o' || **fmt == 'e' || **fmt == 'm' ||
+         **fmt == 's' )
+        parity = **fmt;
+    else
+        return false;
+    (*fmt)++;
+
+    /* stop bits */
+    if ( **fmt == '0' || **fmt == '1' )
+        stop_bits = **fmt - '0';
+    else
+        return false;
+    (*fmt)++;
+
+    g_com_port.comc_fmt = GET_LCR_VALUE(data_bits, stop_bits, parity);
+
+    return true;
+}
+
+static bool parse_serial_param(const char *com)
+{
+    /* parse baud */
+    g_com_port.comc_curspeed = strtoul(com, (char **)&com, 10);
+    if ( (g_com_port.comc_curspeed < 1200) ||
+         (g_com_port.comc_curspeed > 115200) )
+        return false;
+
+    /* parse clock hz */
+    if ( *com == '/' ) {
+        ++com;
+        g_com_port.comc_clockhz = strtoul(com, (char **)&com, 0) << 4;
+        if ( g_com_port.comc_clockhz == 0 )
+            return false;
+    }
+
+    /* parse data_bits/parity/stop_bits */
+    if ( *com != ',' )
+        goto exit;
+    ++com;
+    while ( isspace(*com) )
+        com++;
+    if ( !parse_com_fmt(&com) )
+        return false;
+
+    /* parse IO base */
+    if ( *com != ',' )
+        goto exit;
+    ++com;
+    g_com_port.comc_port = strtoul(com, (char **)&com, 0);
+    if ( g_com_port.comc_port == 0 )
+        return false;
+
+    /* parse irq */
+    if ( *com != ',' )
+        goto exit;
+    ++com;
+    g_com_port.comc_irq = strtoul(com, (char **)&com, 10);
+    if ( g_com_port.comc_irq == 0 )
+        return false;
+
+    /* parse PCI serial controller bdf */
+    if ( *com != ',' )
+        goto exit;
+    ++com;
+    if ( !parse_com_psbdf(&com) )
+        return false;
+
+    /* parse PCI bridge bdf */
+    if ( *com != ',' )
+        goto exit;
+    ++com;
+    if ( !parse_com_pbbdf(&com) )
+        return false;
+
+ exit:
+    return true;
+}
+
+bool get_tboot_serial(void)
+{
+    const char *serial = get_option_val(g_tboot_cmdline_options,
+                                        g_tboot_param_values, "serial");
+    if ( serial == NULL || *serial == '\0' )
+        return false;
+
+    return parse_serial_param(serial);
+}
+
+void get_tboot_vga_delay(void)
+{
+    const char *vga_delay = get_option_val(g_tboot_cmdline_options,
+                                           g_tboot_param_values, "vga_delay");
+    if ( vga_delay == NULL )
+        return;
+
+    g_vga_delay = strtoul(vga_delay, NULL, 0);
+}
+
+void get_tboot_no_usb(void)
+{
+    const char *no_usb = get_option_val(g_tboot_cmdline_options,
+                                        g_tboot_param_values, "no_usb");
+    if ( no_usb == NULL )
+        return;
+
+    g_no_usb = ( strcmp(no_usb, "true") == 0 );
+}
+
+
+/*
+ * linux kernel command line parsing
+ */
+
+bool get_linux_vga(int *vid_mode)
+{
+    const char *vga = get_option_val(g_linux_cmdline_options,
+                                     g_linux_param_values, "vga");
+    if ( vga == NULL || vid_mode == NULL )
+        return false;
+
+    if ( strcmp(vga, "normal") == 0 )
+        *vid_mode = 0xFFFF;
+    else if ( strcmp(vga, "ext") == 0 )
+        *vid_mode = 0xFFFE;
+    else if ( strcmp(vga, "ask") == 0 )
+        *vid_mode = 0xFFFD;
+    else
+        *vid_mode = strtoul(vga, NULL, 0);
+
+    return true;
+}
+
+bool get_linux_mem(uint64_t *max_mem)
+{
+    char *last = NULL;
+    const char *mem = get_option_val(g_linux_cmdline_options,
+                                     g_linux_param_values, "mem");
+    if ( mem == NULL || max_mem == NULL )
+        return false;
+
+    *max_mem = strtoul(mem, &last, 0);
+    if ( *max_mem == 0 )
+        return false;
+
+    if ( last == NULL )
+        return true;
+
+    switch ( *last ) {
+        case 'G':
+        case 'g':
+            *max_mem = *max_mem << 30;
+            return true;
+        case 'M':
+        case 'm':
+            *max_mem = *max_mem << 20;
+            return true;
+        case 'K':
+        case 'k':
+            *max_mem = *max_mem << 10;
+            return true;
+        default:
+            return false;
+    }
+
+    return true;
+}
+
+const char *skip_filename(const char *cmdline)
+{
+    if ( cmdline == NULL || *cmdline == '\0' )
+        return cmdline;
+
+    /* strip leading spaces, file name, then any spaces until the next
+     non-space char (e.g. "  /foo/bar   baz" -> "baz"; "/foo/bar" -> "")*/
+    while ( *cmdline != '\0' && isspace(*cmdline) )
+        cmdline++;
+    while ( *cmdline != '\0' && !isspace(*cmdline) )
+        cmdline++;
+    while ( *cmdline != '\0' && isspace(*cmdline) )
+        cmdline++;
+    return cmdline;
+}
 
 
 /*
