e677ff02a69103217590fe7627f43c0afa44e315
James Newsome <jnewsome@cmu.edu>
2011-06-01 20:44:55 +0000

    compile with -fno-stack-protector
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@1298 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tee-sdk/trunk/ports/newlib/build.sh
+++ tee-sdk/trunk/ports/newlib/build.sh
@@ -5,7 +5,7 @@ mkdir install
 
 cd build
 
-STRIP_FOR_TARGET=strip RANLIB_FOR_TARGET=ranlib OBJDUMP_FOR_TARGET=objdump NM_FOR_TARGET=nm LD_FOR_TARGET=ld AS_FOR_TARGET=as AR_FOR_TARGET=ar CC_FOR_TARGET=cc GCC_FOR_TARGET=gcc ../newlib-1.19.0/configure --target=i586 --disable-multilib --prefix=`pwd`/../install
+CFLAGS="-fno-stack-protector" STRIP_FOR_TARGET=strip RANLIB_FOR_TARGET=ranlib OBJDUMP_FOR_TARGET=objdump NM_FOR_TARGET=nm LD_FOR_TARGET=ld AS_FOR_TARGET=as AR_FOR_TARGET=ar CC_FOR_TARGET=cc GCC_FOR_TARGET=gcc ../newlib-1.19.0/configure --target=i586 --disable-multilib --prefix=`pwd`/../install
 
 make
 make install
