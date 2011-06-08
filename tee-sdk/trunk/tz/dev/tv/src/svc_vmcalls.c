0144e83ad1091f128689c4593e8a071330a0fa37
Jonathan McCune <jonmccune@cmu.edu>
2011-06-08 13:44:55 +0000

    add service backend for TV_HC_UTPM_ID_GETPUB
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@1483 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tee-sdk/trunk/tz/dev/tv/src/svc_vmcalls.c
+++ tee-sdk/trunk/tz/dev/tv/src/svc_vmcalls.c
@@ -97,6 +97,15 @@ int svc_utpm_pcr_extend(uint32_t idx,   /* in */
                 0);
 }
 
+int svc_utpm_id_getpub(uint8_t *N) /* out */
+{
+    return vmcall(TV_HC_UTPM_ID_GETPUB,
+                  (uint32_t)N,
+                  0,
+                  0,
+                  0);
+}
+
 int svc_utpm_pcr_read(uint32_t idx, /* in */
                       uint8_t *val) /* out */
 {
