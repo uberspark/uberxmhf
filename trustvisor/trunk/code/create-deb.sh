8e5c156989c994aa46f0ae10748c20648702459f
Jonathan McCune <jonmccune@cmu.edu>
2011-07-06 22:15:30 +0000

    Use the new "proper" way to build a debian package
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@1726 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- trustvisor/trunk/code/create-deb.sh
+++ trustvisor/trunk/code/create-deb.sh
@@ -8,19 +8,7 @@ fi
 
 echo "Detected SVN Revision: $SVNREV"
 
-sudo checkinstall -y --pkgname=trustvisor --pkgversion=0.0.$SVNREV --pkgarch=i386 --pkglicense=Commercial --maintainer=jonmccune@cmu.edu 
+debuild -us -uc -b
 
-#0 -  Maintainer: [ root@mccune ]
-#1 -  Summary: [ TrustVisor hypervisor and critical system .h files. ]
-#2 -  Name:    [ code ]
-#3 -  Version: [  ]
-#4 -  Release: [ 1 ]
-#5 -  License: [ GPL ]
-#6 -  Group:   [ checkinstall ]
-#7 -  Architecture: [ i386 ]
-#8 -  Source location: [ code ]
-#9 -  Alternate source location: [  ]
-#10 - Requires: [  ]
-#11 - Provides: [ code ]
-
+#sudo checkinstall -y --pkgname=trustvisor --pkgversion=0.0.$SVNREV --pkgarch=i386 --pkglicense=Commercial --maintainer=jonmccune@cmu.edu 
 
