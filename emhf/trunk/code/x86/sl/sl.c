43a87a5f49f29dcfdebd3220a644e5c218f7fd84
Jonathan McCune <jonmccune@cmu.edu>
2011-07-06 18:35:25 +0000

    This will not boot successfully on Linux.  Suspect problem with 64-bit values.
    Last visible serial output:
    
    executing GETSEC[SENTER]...
    
    SL: at 0xd0000000, starting...
    SL: slpb at = 0x00010000
    	hashSL=0x00000000
    	errorHandler=0x00000000
    	isEarlyInit=0x00000001
    	numE820Entries=16
    	e820map at 0x00010014
    	numCPUEntries=4
    	pcpus at 0x00010518
    	runtime size= 81674240 bytes
    	OS bootmodule at 0x06e18000, size=512 bytes
    
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@1718 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/x86/sl/sl.c
+++ emhf/trunk/code/x86/sl/sl.c
@@ -265,7 +265,7 @@ void slmain(u32 baseaddr, u32 rdtsc_eax, u32 rdtsc_edx){
 	printf("\n	OS bootmodule at 0x%08x, size=%u bytes", 
 		slpb.runtime_osbootmodule_base, slpb.runtime_osbootmodule_size);
 
-    /* slpb.rdtsc_after_drtm = (u64)rdtsc_eax | ((u64)rdtsc_edx << 32); */
+    slpb.rdtsc_after_drtm = (u64)rdtsc_eax | ((u64)rdtsc_edx << 32);
     /* printf("\nSL: RDTSC before_drtm 0x%llx, after_drtm 0x%llx", */
     /*        slpb.rdtsc_before_drtm, slpb.rdtsc_after_drtm); */
     /* printf("\nSL: [PERF] RDTSC DRTM elapsed cycles: 0x%llx", */
