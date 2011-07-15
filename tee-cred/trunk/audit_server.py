faeb1ce2ec43f1fdd4409a1c21963f43a71a5b73
James Newsome <jnewsome@cmu.edu>
2011-07-15 20:58:55 +0000

    starting simple audit server in python
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@1816 1c601a1c-44f4-4d93-90ac-4b632a93592e

---
+++ tee-cred/trunk/audit_server.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import socket
+import struct
+
+PORT=9001
+HOST=""
+
+def recvall(conn, to_recv):
+    recvd = 0
+    chunks = []
+    while recvd < to_recv:
+        data = conn.recv(to_recv - recvd)
+        recvd += len(data)
+        chunks.append(data)
+    return ''.join(chunks)
+
+def read_audit_nonce(conn):
+    l = struct.unpack("!I", conn.recv(4))[0]
+    return recvall(conn, l)
+
+def read_audit_string(conn):
+    l = struct.unpack("!I", conn.recv(4))[0]
+    return recvall(conn, l)
+
+def gen_audit_token(audit_nonce, audit_string):
+    return "audit token!"
+
+def send_audit_token(audit_token):
+    l = struct.pack("!I", len(audit_token))
+    conn.send(l)
+    conn.send(audit_token)
+    return
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen(1)
+    while True:
+        conn, addr = s.accept()
+        print 'connected by', addr
+
+        audit_nonce = read_audit_nonce(conn)
+        audit_string = read_audit_string(conn)
+        audit_token = gen_audit_token(audit_nonce, audit_string)
+        send_audit_token(audit_token)
+
+if __name__ == '__main__':
+    main()
