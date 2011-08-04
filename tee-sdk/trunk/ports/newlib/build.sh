1b9fee748a5ba61e17d6dafcaada2a17500417d9
James Newsome <jnewsome@cmu.edu>
2011-08-04 18:42:55 +0000

    change target name to new target 'i586-tsvc'
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@2072 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tee-sdk/trunk/ports/newlib/build.sh
+++ tee-sdk/trunk/ports/newlib/build.sh
@@ -7,7 +7,7 @@ cd build
 
 CFLAGS=`pkg-config --cflags tee-sdk-svc tee-sdk-svc-tv`
 
-CFLAGS="$CFLAGS" STRIP_FOR_TARGET=strip RANLIB_FOR_TARGET=ranlib OBJDUMP_FOR_TARGET=objdump NM_FOR_TARGET=nm LD_FOR_TARGET=ld AS_FOR_TARGET=as AR_FOR_TARGET=ar CC_FOR_TARGET=cc GCC_FOR_TARGET=gcc ../newlib-1.19.0/configure --target=i586 --disable-multilib --prefix=`pwd`/../install
+CFLAGS="$CFLAGS" STRIP_FOR_TARGET=strip RANLIB_FOR_TARGET=ranlib OBJDUMP_FOR_TARGET=objdump NM_FOR_TARGET=nm LD_FOR_TARGET=ld AS_FOR_TARGET=as AR_FOR_TARGET=ar CC_FOR_TARGET=cc GCC_FOR_TARGET=gcc ../newlib-1.19.0/configure --target=i586-tsvc --enable-languages=c --with-newlib --disable-multilib --prefix=`pwd`/../install
 
 make
 make install
