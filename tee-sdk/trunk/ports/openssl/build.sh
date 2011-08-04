3f9afef13fc10e91b71ddba3b993f0639eb7b576
James Newsome <jnewsome@cmu.edu>
2011-08-04 18:45:40 +0000

    update openssl build script to use i586-tsvc-cc
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@2155 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tee-sdk/trunk/ports/openssl/build.sh
+++ tee-sdk/trunk/ports/openssl/build.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-NEWLIBINC=`pwd`/../newlib/install/i586-tsvc/include
-
-CCINC=`gcc --print-file-name=include`
-CFLAGS=`pkg-config --cflags tee-sdk-svc tee-sdk-svc-tv`"-nostdinc -fno-builtin -I$NEWLIBINC -I$CCINC -I$CCINC-fixed -DOPENSSL_NO_DGRAM -DOPENSSL_NO_SOCK"
-PREFIX=`pwd`/install
+export CC=i586-tsvc-cc
+export LD=i586-tsvc-ld
+PREFIX=/usr/local/i586-tsvc/usr
+CFLAGS="-DOPENSSL_NO_DGRAM -DOPENSSL_NO_SOCK"
 
 cd openssl-1.0.0d
 ./Configure cc --prefix="$PREFIX" no-threads no-zlib no-shared no-sse2 no-dso no-hw $CFLAGS
