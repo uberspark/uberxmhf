1df08b2b892f60b258dbcd50656b95e7f4cbd8c5
James Newsome <jnewsome@cmu.edu>
2011-08-04 18:45:49 +0000

    update port build scripts to accept HOST and PREFIX overrides from environment
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@2161 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tee-sdk/trunk/ports/newlib/build.sh
+++ tee-sdk/trunk/ports/newlib/build.sh
@@ -8,11 +8,18 @@ cd build
 #CFLAGS=`pkg-config --cflags tee-sdk-svc tee-sdk-svc-tv`
 
 #CFLAGS="$CFLAGS" STRIP_FOR_TARGET=strip RANLIB_FOR_TARGET=ranlib OBJDUMP_FOR_TARGET=objdump NM_FOR_TARGET=nm LD_FOR_TARGET=ld AS_FOR_TARGET=as AR_FOR_TARGET=ar CC_FOR_TARGET=cc GCC_FOR_TARGET=gcc ../newlib-1.19.0/configure --target=i586-tsvc --enable-languages=c --with-newlib --disable-multilib --prefix=`pwd`/../install
-ARCH=i586
-OS=tsvc
-PREFIX=/usr/local
+if [ -z $HOST ]
+then
+	HOST=i586-tsvc
+fi
 
-../newlib-1.19.0/configure --target=i586-tsvc --enable-languages=c --with-newlib --disable-multilib --prefix=$PREFIX
+if [ -z $PREFIX ]
+then
+	PREFIX=/usr/local
+fi
+
+../newlib-1.19.0/configure --target=${HOST} --enable-languages=c --with-newlib --disable-multilib --prefix=$PREFIX
 
 make
-make install
+sudo make install
+
--- tee-sdk/trunk/ports/openssl/build.sh
+++ tee-sdk/trunk/ports/openssl/build.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-export CC=i586-tsvc-cc
-export LD=i586-tsvc-ld
-PREFIX=/usr/local/i586-tsvc/usr
+if [ -z "$HOST" ]
+then
+	HOST=i586-tsvc
+fi
+
+if [ -z "$PREFIX" ]
+then
+	PREFIX=/usr/local/$HOST/usr
+fi
+
+if [ -z "$CC" ]
+then
+	CC=${HOST}-cc
+fi
+export CC
+
 CFLAGS="-DOPENSSL_NO_DGRAM -DOPENSSL_NO_SOCK"
 
 cd openssl-1.0.0d
 ./Configure cc --prefix="$PREFIX" no-threads no-zlib no-shared no-sse2 no-dso no-hw $CFLAGS
 make
 make install
+
