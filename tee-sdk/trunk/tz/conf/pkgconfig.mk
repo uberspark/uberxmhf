590db7539519a620058b62d774224900d9b5c6d1
James Newsome <jnewsome@cmu.edu>
2011-08-04 18:45:15 +0000

    when invoking pkg-config, set PKG_CONFIG_PATH in pkg-config's environment from the Makefile variable
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@2138 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tee-sdk/trunk/tz/conf/pkgconfig.mk
+++ tee-sdk/trunk/tz/conf/pkgconfig.mk
@@ -1,6 +1,6 @@
 #### generic pkgconfig helpers
 # to be used with $(call pkgconfig_*, $pkgconfig_dependencies)
-pkgconfig_cflags = $(shell pkg-config --cflags $(1))
-pkgconfig_ldlibs = $(shell pkg-config --libs-only-l --static $(1))
-pkgconfig_ldflags =$(filter-out -l%, $(shell pkg-config --libs --static $(1)))
+pkgconfig_cflags = $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --cflags $(1))
+pkgconfig_ldlibs = $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --libs-only-l --static $(1))
+pkgconfig_ldflags =$(filter-out -l%, $(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config --libs --static $(1)))
 ####
