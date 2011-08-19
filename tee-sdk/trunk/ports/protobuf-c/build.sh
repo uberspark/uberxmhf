8282ef55d5f5b7a3fb37eb6a3e5e558506e78e29
James Newsome <jnewsome@cmu.edu>
2011-08-19 17:56:32 +0000

    protobuf-c port build script
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@2293 1c601a1c-44f4-4d93-90ac-4b632a93592e

---
+++ tee-sdk/trunk/ports/protobuf-c/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ -z "$HOST" ]
+then
+	HOST=i586-tsvc
+fi
+
+if [ -z "$BUILD" ]
+then
+	HOST=i686-pc-linux-gnu
+fi
+
+if [ -z "$PREFIX" ]
+then
+	PREFIX=/usr/local/$HOST/usr
+fi
+
+cd protobuf-c-0.15
+./configure --prefix=${PREFIX} --host=${HOST} --build=${BUILD} --disable-protoc
+make
+make install
+
