5209c7b1bcac8ec0bb3916f40a1468fa0bab96f9
James Newsome <jnewsome@cmu.edu>
2011-10-06 21:33:07 +0000

    export sealed master secret
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@2635 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tee-cred/trunk/audited-kv-pal-storage.c
+++ tee-cred/trunk/audited-kv-pal-storage.c
@@ -34,7 +34,11 @@
  */
 
 #include <google/protobuf-c/protobuf-c.h>
+#include <openssl/sha.h>
 
+#include <string.h>
+
+#include "audited-kv-pal-int.h"
 #include "audited.h"
 #include "audited-kv-pal-storage.h"
 
@@ -59,10 +63,65 @@ static akv_err_t export_header(AkvpStorage__Header *h)
   return rv;
 }
 
+akv_err_t composite_hash_of_current_pcrs(TPM_DIGEST *composite_hash,
+                                         TPM_PCR_SELECTION *pcr_selection) /* FIXME: should be
+                                                                              const. need fo fix
+                                                                              utpm_pcr_is_selected
+                                                                              in tv_utpm.h */
+{
+  SHA_CTX sha_ctx;
+  int i;
+  uint8_t pcr[TPM_HASH_SIZE];
+  int svcrv;
+
+  SHA1_Init(&sha_ctx);
+  SHA1_Update(&sha_ctx, pcr_selection, sizeof(*pcr_selection));
+  for(i=0; i<TPM_PCR_NUM; i++) {
+    if(utpm_pcr_is_selected(pcr_selection, i)) {
+      svcrv = svc_utpm_pcr_read(i, &pcr[0]);
+      if(svcrv) {
+        return AKV_ESVC | (svcrv << 8);
+      }
+      SHA1_Update(&sha_ctx, &pcr[0], TPM_HASH_SIZE);
+    }
+  }
+  SHA1_Final(composite_hash->value, &sha_ctx);
+
+  return 0;
+}
+
 akv_err_t akvp_export(const void *req, AkvpStorage__Everything *res)
 {
   akv_err_t rv=0;
   AkvpStorage__Header *header;
+  size_t sealed_master_secret_len=akv_ctx.master_secret_len+(256/8+128/8);
+  void *sealed_master_secret;
+
+  {
+    TPM_PCR_INFO pcr_info;
+    int svcrv;
+
+    memset(&pcr_info, 0, sizeof(pcr_info));
+
+    /* select pcr0 */
+    utpm_pcr_select_i(&pcr_info.pcrSelection, 0); /* seal to code identity */
+    rv = composite_hash_of_current_pcrs(&pcr_info.digestAtRelease,
+                                        &pcr_info.pcrSelection);
+    if(rv) {
+      goto out;
+    }
+    
+    sealed_master_secret=malloc(sealed_master_secret_len);
+    if(!sealed_master_secret) {
+      rv = AKV_ENOMEM;
+      goto out;
+    }
+    svcrv = svc_utpm_seal(&pcr_info,
+                          akv_ctx.master_secret,
+                          akv_ctx.master_secret_len,
+                          sealed_master_secret,
+                          &sealed_master_secret_len);
+  }
 
   header=malloc(sizeof(*header));
   if(!header) {
@@ -73,6 +132,10 @@ akv_err_t akvp_export(const void *req, AkvpStorage__Everything *res)
   rv = export_header(header);
   *res = (AkvpStorage__Everything) {
     .base =  PROTOBUF_C_MESSAGE_INIT (&akvp_storage__everything__descriptor),
+    .sealed_master_secret = {
+      .data=sealed_master_secret,
+      .len=sealed_master_secret_len,
+    },
     .header = header,
   };
 
