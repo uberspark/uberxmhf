1b24a55988dc9297840e20cd29452b9d82df0e12
James Newsome <jnewsome@cmu.edu>
2011-10-07 22:33:33 +0000

    use debug macros
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@2679 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tee-cred/trunk/audited-kv-pal-storage.c
+++ tee-cred/trunk/audited-kv-pal-storage.c
@@ -39,6 +39,7 @@
 
 #include <string.h>
 
+#include "dbg.h"
 #include "audited-kv-pal-int.h"
 #include "audited.h"
 #include "audited-kv-pal-storage.h"
@@ -52,32 +53,22 @@ static akv_err_t compute_header_mac(const AkvpStorage__Header *h,
   akv_err_t rv=0;
 
   HMAC_CTX_init(&ctx);
+
   cryptorv = HMAC_Init_ex(&ctx,
                           akv_ctx.hmac_key, akv_ctx.hmac_key_len,
                           EVP_sha256(), NULL);
-  if(!cryptorv) {
-    rv = AKV_ECRYPTO;
-    goto out;
-  }
+  CHECK(cryptorv, AKV_ECRYPTO, "HMAC_Init_ex");
 
   cryptorv = HMAC_Update(&ctx, (uint8_t*)h->audit_pub_key_pem, strlen(h->audit_pub_key_pem));
-  if(!cryptorv) {
-    rv = AKV_ECRYPTO;
-    goto cleanup_ctx;
-  }
+  CHECK(cryptorv, AKV_ECRYPTO, "HMAC_Update");
 
   cryptorv = HMAC_Final(&ctx, md, md_len);
-  if(!cryptorv) {
-    rv = AKV_ECRYPTO;
-    goto cleanup_ctx;
-  }
-
- cleanup_ctx:
-  HMAC_CTX_cleanup(&ctx);
+  CHECK(cryptorv, AKV_ECRYPTO, "HMAC_Final");
+  
  out:
+  HMAC_CTX_cleanup(&ctx);
   return rv;
 }
-                                    
 
 static akv_err_t export_header(AkvpStorage__Header *h)
 {
@@ -86,10 +77,8 @@ static akv_err_t export_header(AkvpStorage__Header *h)
   char *audit_pub_key_pem;
 
   audited_err = audited_get_audit_server_pub_pem(&audit_pub_key_pem);
-  if(audited_err) {
-    rv=AKV_EAUDITED | (audited_err << 8);
-    goto out;
-  }
+  CHECK_RV(audited_err, AKV_EAUDITED | (audited_err << 8),
+           "audited_get_audit_server_pub_pem");
 
   *h = (AkvpStorage__Header) {
     .base = PROTOBUF_C_MESSAGE_INIT (&akvp_storage__header__descriptor),
@@ -110,11 +99,15 @@ static akv_err_t composite_hash_of_current_pcrs(TPM_COMPOSITE_HASH *composite_ha
   int i;
   uint8_t pcr[TPM_HASH_SIZE];
   int svcrv;
-  uint32_t sizeof_pcrs;
+  uint32_t sizeof_pcrs=0;
   uint32_t sizeof_pcrs_be;
+  int cryptorv;
+  akv_err_t rv=0;
 
   SHA1_Init(&sha_ctx);
-  SHA1_Update(&sha_ctx, pcr_selection, sizeof(*pcr_selection));
+
+  cryptorv=SHA1_Update(&sha_ctx, pcr_selection, sizeof(*pcr_selection));
+  CHECK(cryptorv, AKV_ECRYPTO, "SHA1_Update");
 
   for(i=0; i<TPM_PCR_NUM; i++) {
     if(utpm_pcr_is_selected(pcr_selection, i)) {
@@ -124,20 +117,24 @@ static akv_err_t composite_hash_of_current_pcrs(TPM_COMPOSITE_HASH *composite_ha
   assert((sizeof_pcrs & 0xff) == sizeof_pcrs);
   sizeof_pcrs_be = sizeof_pcrs << 24; /* XXX quick and dirty
                                          conversion to big endian */
-  SHA1_Update(&sha_ctx, &sizeof_pcrs_be, sizeof(sizeof_pcrs_be));
+  cryptorv=SHA1_Update(&sha_ctx, &sizeof_pcrs_be, sizeof(sizeof_pcrs_be));
+  CHECK(cryptorv, AKV_ECRYPTO, "SHA1_Update");
   
   for(i=0; i<TPM_PCR_NUM; i++) {
     if(utpm_pcr_is_selected(pcr_selection, i)) {
       svcrv = svc_utpm_pcr_read(i, &pcr[0]);
-      if(svcrv) {
-        return AKV_ESVC | (svcrv << 8);
-      }
-      SHA1_Update(&sha_ctx, &pcr[0], TPM_HASH_SIZE);
+      CHECK_RV(svcrv, AKV_ESVC | (svcrv << 8), "svc_utpm_pcr_read");
+
+      cryptorv = SHA1_Update(&sha_ctx, &pcr[0], TPM_HASH_SIZE);
+      CHECK(cryptorv, AKV_ECRYPTO, "SHA1_Update");
     }
   }
-  SHA1_Final(composite_hash->value, &sha_ctx);
 
-  return 0;
+  cryptorv = SHA1_Final(composite_hash->value, &sha_ctx);
+  CHECK(cryptorv, AKV_ECRYPTO, "SHA1_Final");
+
+ out:
+  return rv;
 }
 
 akv_err_t akvp_export(const void *req, AkvpStorage__Everything *res)
@@ -157,48 +154,33 @@ akv_err_t akvp_export(const void *req, AkvpStorage__Everything *res)
 
     /* select pcr0 */
     utpm_pcr_select_i(&pcr_info.pcrSelection, 0); /* seal to code identity */
+
     rv = composite_hash_of_current_pcrs(&pcr_info.digestAtRelease,
                                         &pcr_info.pcrSelection);
-    if(rv) {
-      goto out;
-    }
+    CHECK_RV(rv, rv, "composite_hash_of_current_pcrs");
     
     sealed_master_secret=malloc(sealed_master_secret_len);
-    if(!sealed_master_secret) {
-      rv = AKV_ENOMEM;
-      goto out;
-    }
+    CHECK_MEM(sealed_master_secret, AKV_ENOMEM);
+
     svcrv = svc_utpm_seal(&pcr_info,
                           akv_ctx.master_secret,
                           akv_ctx.master_secret_len,
                           sealed_master_secret,
                           &sealed_master_secret_len);
-    if(svcrv) {
-      rv = AKV_ESVC | (svcrv << 8);
-      goto out;
-    }
+    CHECK_RV(svcrv, AKV_ESVC | (svcrv << 8), "svc_utpm_seal");
   }
 
   header=malloc(sizeof(*header));
-  if(!header) {
-    rv=AKV_ENOMEM;
-    goto out;
-  }
+  CHECK_MEM(header, AKV_ENOMEM);
 
   rv = export_header(header);
-  if(rv) {
-    goto out;
-  }
+  CHECK_RV(rv, rv, "export_header");
 
-  header_mac=malloc(header_mac_len);
-  if(!header_mac) {
-    rv = AKV_ENOMEM;
-    goto out;
-  }
+  header_mac = malloc(header_mac_len);
+  CHECK_MEM(header_mac, AKV_ENOMEM);
+
   rv = compute_header_mac(header, header_mac, &header_mac_len);
-  if(rv) {
-    goto out;
-  }
+  CHECK_RV(rv, rv, "compute_header_mac");
 
   *res = (AkvpStorage__Everything) {
     .base =  PROTOBUF_C_MESSAGE_INIT (&akvp_storage__everything__descriptor),
@@ -225,7 +207,7 @@ void akvp_export_release_res(AkvpStorage__Everything *res)
 
 akv_err_t akvp_import(const AkvpStorage__Everything *req, void *res)
 {
-  uint32_t svcrv;
+  unsigned int svcrv;
   akv_err_t rv=0;
   TPM_DIGEST digest_at_creation;
   void *master_secret;
@@ -234,33 +216,26 @@ akv_err_t akvp_import(const AkvpStorage__Everything *req, void *res)
   size_t header_mac_len=SHA256_DIGEST_LENGTH;
 
   master_secret = malloc(req->sealed_master_secret.len);
-  if(!master_secret) {
-    rv = AKV_ENOMEM;
-    goto out;
-  }
+  CHECK_MEM(master_secret, AKV_ENOMEM);
+
   svcrv = svc_utpm_unseal(req->sealed_master_secret.data,
                           req->sealed_master_secret.len,
                           master_secret,
                           &master_secret_len,
                           &digest_at_creation);
-  if(svcrv) {
-    rv= AKV_ESVC | (svcrv << 8);
-    goto out;
-  }
+  CHECK_RV(svcrv, AKV_ESVC | (svcrv << 8), "svc_utpm_unseal");
+
   /* XXX check digest_at_creation */
 
   rv = akvp_init_priv(req->header->audit_pub_key_pem,
                       master_secret,
                       master_secret_len);
-  if(rv) {
-    goto out;
-  }
+  CHECK_RV(rv, rv, "akvp_init_priv");
 
   /* FIXME: should do this before full initialization, above */
   rv = compute_header_mac(req->header, header_mac, &header_mac_len);
-  if(rv) {
-    goto out;
-  }
+  CHECK_RV(rv, rv, "compute_header_mac");
+
   if(memcmp(header_mac, req->mac_of_header.data, header_mac_len)) {
     rv = AKV_EBADMAC;
     goto out;
