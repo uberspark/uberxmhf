ee5f9783a1ea699ff11b95065e57d2d0f8cb217c
Amit Vasudevan <amitvasudevan@acm.org>
2011-10-08 03:46:31 +0000

    test to see if RPB uart_config is being populated properly
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@2794 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/x86/runtime/runtime.c
+++ emhf/trunk/code/x86/runtime/runtime.c
@@ -50,10 +50,10 @@ void cstartup(void){
 	runtime_globals_init();
 
 	//setup debugging	
-/*#ifdef __DEBUG_SERIAL__
+#ifdef __DEBUG_SERIAL__
     g_uart_config = rpb->uart_config;
 	init_uart();
-#endif*/
+#endif
 	printf("\nruntime initializing...");
 
 	//check CPU type (Intel vs AMD)
