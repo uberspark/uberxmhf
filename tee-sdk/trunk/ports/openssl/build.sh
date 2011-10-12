4f8d1bae31d8741b0dab03f21691b9da94781397
Jonathan McCune <jonmccune@cmu.edu>
2011-10-12 19:42:52 +0000

    make install needs to run as root
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@2938 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tee-sdk/trunk/ports/openssl/build.sh
+++ tee-sdk/trunk/ports/openssl/build.sh
@@ -21,5 +21,5 @@ CFLAGS="-DOPENSSL_NO_DGRAM -DOPENSSL_NO_SOCK"
 cd openssl-1.0.0d
 ./Configure cc --prefix="$PREFIX" no-threads no-zlib no-shared no-sse2 no-dso no-hw $CFLAGS
 make
-make install
+sudo make install
 
