d2ad09d8bbd64c17a955a5984f0c99f222f4f8b2
Jonathan McCune <jonmccune@cmu.edu>
2011-10-29 05:40:29 +0000

    strip out definitions that duplicate libemhfc
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@3149 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/x86/include/_types.h
+++ emhf/trunk/code/x86/include/_types.h
@@ -41,65 +41,13 @@
 
 #ifndef __ASSEMBLY__
 
-#define INT_MAX         ((int)(~0U>>1))
-#define INT_MIN         (-INT_MAX - 1)
-#define UINT_MAX        (~0U)
-#define LONG_MAX        ((long)(~0UL>>1))
-#define LONG_MIN        (-LONG_MAX - 1)
-#define ULONG_MAX       (~0UL)
-
-
-#ifndef NULL
-#define NULL ((void*)0)
-#endif
-
-
-#define FILE			void
-
-typedef unsigned char u8;
-typedef signed char s8;
-typedef unsigned short u16;
-typedef signed short s16;
-typedef unsigned int u32;
-typedef signed int s32;
-typedef unsigned long long u64;
-typedef signed long long s64;
-typedef unsigned int size_t;
-
-typedef s32 int32_t;
-typedef u32 uint32_t;
-typedef u32 u_int32_t;
-typedef u16 uint16_t;
-typedef u16 u_int16_t;
-typedef u8  uint8_t;
-typedef u8  u_int8_t;
-typedef u64 uint64_t;
-
 typedef u32 paddr_t;
 typedef void * dma_addr_t;
-
-typedef unsigned int        uintptr_t;
 
 typedef void* hva_t; /* hypervisor virtual address */
 typedef u64 spa_t; /* system physical address */
 typedef u32 gva_t; /* guest virtual address. we only support 32-bit guests */
 typedef u64 gpa_t; /* guest physical address. can be 64-bit with PAE */
 #endif /*ifndef __ASSEMBLY__*/
-
-/* from BSD's stdbool.h */
-#define	__bool_true_false_are_defined	1
-
-#ifndef __cplusplus
-
-#define	false	0
-#define	true	1
-
-#define	bool	_Bool
-#if __STDC_VERSION__ < 199901L && __GNUC__ < 3
-typedef	int	_Bool;
-#endif
-
-#endif /* !__cplusplus */
-
 
 #endif /* __TYPES_H_ */
