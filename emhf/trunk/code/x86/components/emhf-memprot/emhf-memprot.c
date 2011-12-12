9fe21e06448de991961d2182c89be43226bf7f71
Amit Vasudevan <amitvasudevan@acm.org>
2011-12-12 20:53:49 +0000

    emhf-memprot: change emhf_memprot_get_current_root_pagemap_address return type to u64 from void *; keeps hpt abstraction happy
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@3815 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/x86/components/emhf-memprot/emhf-memprot.c
+++ emhf/trunk/code/x86/components/emhf-memprot/emhf-memprot.c
@@ -99,13 +99,13 @@ void * emhf_memprot_get_default_root_pagemap_address(VCPU *vcpu){
 } 
 
 //get current root page map address
-void * emhf_memprot_get_current_root_pagemap_address(VCPU *vcpu){
+u64 emhf_memprot_get_current_root_pagemap_address(VCPU *vcpu){
   ASSERT(vcpu->cpu_vendor == CPU_VENDOR_AMD || vcpu->cpu_vendor == CPU_VENDOR_INTEL);
   
 	if(vcpu->cpu_vendor == CPU_VENDOR_AMD)
-		return (void *) (u32)((struct vmcb_struct*)vcpu->vmcb_vaddr_ptr)->h_cr3;
+		return (u64) (u32)((struct vmcb_struct*)vcpu->vmcb_vaddr_ptr)->h_cr3;
 	else //CPU_VENDOR_INTEL
-		return (void *) (u32)vcpu->vmcs.control_EPT_pointer_full;
+		return (u64) (u32)vcpu->vmcs.control_EPT_pointer_full;
 }
 
 //set current root page map address
