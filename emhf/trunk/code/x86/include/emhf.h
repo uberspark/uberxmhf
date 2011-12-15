45bfba25afb493187c392cd93358248f1b237efe
Amit Vasudevan <amitvasudevan@acm.org>
2011-12-15 14:52:10 +0000

    emhf-smpguest: vmx/svm_do_wakeup are no longer in effect
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@3859 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/x86/include/emhf.h
+++ emhf/trunk/code/x86/include/emhf.h
@@ -629,7 +629,7 @@ void svm_initialize_vmcb_csrip(VCPU *vcpu, u16 cs_selector, u32 cs_base, u64 rip
 void svm_start_hvm(VCPU *vcpu);
 u32 svm_intercept_handler(VCPU *vcpu, struct regs *r);
 //void svm_do_quiesce(VCPU *vcpu);
-void svm_do_wakeup(VCPU *vcpu);
+//void svm_do_wakeup(VCPU *vcpu);
 void svm_setupvcpus(u32 cpu_vendor);
 
 //other SVM isolation layer global functions
@@ -649,7 +649,7 @@ void vmx_initialize_vmcs_csrip(VCPU *vcpu, u16 cs_selector, u32 cs_base, u64 rip
 void vmx_start_hvm(VCPU *vcpu);
 u32 vmx_intercept_handler(VCPU *vcpu, struct regs *r);
 //void vmx_do_quiesce(VCPU *vcpu);
-void vmx_do_wakeup(VCPU *vcpu);
+//void vmx_do_wakeup(VCPU *vcpu);
 void vmx_setupvcpus(u32 cpu_vendor);
 
 
