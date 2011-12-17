c0b53d7eb3de1ff1c75cdadaacca17b9dd597002
Amit Vasudevan <amitvasudevan@acm.org>
2011-12-17 02:53:19 +0000

    emhf-dmaprot: tie emhf-dmaprot.h to top-level emhf.h
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@3881 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/x86/include/emhf.h
+++ emhf/trunk/code/x86/include/emhf.h
@@ -443,6 +443,7 @@ typedef struct {
 //----------------------------------------------------------------------
 // component headers
 #include <emhf-memprot.h>	//EMHF memory protection component
+#include <emhf-dmaprot.h>	//EMHF DMA protection component
 #include <emhf-parteventhub.h>	//EMHF partition event-hub component
 #include <emhf-smpguest.h>		//EMHF SMP guest component
 
