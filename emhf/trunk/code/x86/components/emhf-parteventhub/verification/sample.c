530a88abb292e95aebd22d3fee5b9951d0767623
Amit Vasudevan <amitvasudevan@acm.org>
2011-12-27 04:34:40 +0000

    emhf-eventhub(verif.): sample.c, structure pointer typecast example now works with cbmc r816 - thanks daniel!
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@3971 1c601a1c-44f4-4d93-90ac-4b632a93592e

---
+++ emhf/trunk/code/x86/components/emhf-parteventhub/verification/sample.c
@@ -0,0 +1,65 @@
+/*
+ * @XMHF_LICENSE_HEADER_START@
+ *
+ * eXtensible, Modular Hypervisor Framework (XMHF)
+ * Copyright (c) 2009-2012 Carnegie Mellon University
+ * Copyright (c) 2010-2012 VDG Inc.
+ * All Rights Reserved.
+ *
+ * Developed by: XMHF Team
+ *               Carnegie Mellon University / CyLab
+ *               VDG Inc.
+ *               http://xmhf.org
+ *
+ * This file is part of the EMHF historical reference
+ * codebase, and is released under the terms of the
+ * GNU General Public License (GPL) version 2.
+ * Please see the LICENSE file for details.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * @XMHF_LICENSE_HEADER_END@
+ */
+
+typedef unsigned int u32;
+
+struct mystruct{
+	u32 a;
+	u32 b;
+	u32 c;
+};
+
+void *memcpy (char *destaddr, char *srcaddr, u32 len){
+  while (len-- > 0);
+    *destaddr++ = *srcaddr++;
+
+  return destaddr;
+}
+
+
+void main() {
+
+/*	struct mystruct *m;
+
+	m=(struct mystruct *)0x00001000;
+	assert((u32)m != 0);
+
+	m->a = 5;*/
+
+	memcpy((char *)0x2000, (char *)0x1000, 1);
+	
+	assert(0);
+}
+
