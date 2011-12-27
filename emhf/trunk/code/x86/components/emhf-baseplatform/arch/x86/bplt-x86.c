2aca7546e1798f977a81fe0ffc52b4c2708cb144
Amit Vasudevan <amitvasudevan@acm.org>
2011-12-27 04:38:23 +0000

    emhf-baseplatform: add emhf_arch_getcpuvendor x86 arch. backend definition
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@3986 1c601a1c-44f4-4d93-90ac-4b632a93592e

---
+++ emhf/trunk/code/x86/components/emhf-baseplatform/arch/x86/bplt-x86.c
@@ -0,0 +1,70 @@
+/*
+ * @XMHF_LICENSE_HEADER_START@
+ *
+ * eXtensible, Modular Hypervisor Framework (XMHF)
+ * Copyright (c) 2009-2012 Carnegie Mellon University
+ * Copyright (c) 2010-2012 VDG Inc.
+ * All Rights Reserved.
+ *
+ * Developed by: XMHF Team
+ *               Carnegie Mellon University / CyLab
+ *               VDG Inc.
+ *               http://xmhf.org
+ *
+ * This file is part of the EMHF historical reference
+ * codebase, and is released under the terms of the
+ * GNU General Public License (GPL) version 2.
+ * Please see the LICENSE file for details.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * @XMHF_LICENSE_HEADER_END@
+ */
+
+/*
+ * EMHF base platform component interface, x86 common backend
+ * author: amit vasudevan (amitvasudevan@acm.org)
+ */
+
+#include <emhf.h>
+
+//get CPU vendor
+u32 emhf_arch_getcpuvendor(void){
+	u32 vendor_dword1, vendor_dword2, vendor_dword3;
+	u32 cpu_vendor;
+	asm(	"xor	%%eax, %%eax \n"
+				  "cpuid \n"		
+				  "mov	%%ebx, %0 \n"
+				  "mov	%%edx, %1 \n"
+				  "mov	%%ecx, %2 \n"
+			     :	//no inputs
+					 : "m"(vendor_dword1), "m"(vendor_dword2), "m"(vendor_dword3)
+					 : "eax", "ebx", "ecx", "edx" );
+
+	if(vendor_dword1 == AMD_STRING_DWORD1 && vendor_dword2 == AMD_STRING_DWORD2
+			&& vendor_dword3 == AMD_STRING_DWORD3)
+		cpu_vendor = CPU_VENDOR_AMD;
+	else if(vendor_dword1 == INTEL_STRING_DWORD1 && vendor_dword2 == INTEL_STRING_DWORD2
+			&& vendor_dword3 == INTEL_STRING_DWORD3)
+		cpu_vendor = CPU_VENDOR_INTEL;
+	else{
+		printf("\n%s: unrecognized x86 CPU (0x%08x:0x%08x:0x%08x). HALT!",
+			__FUNCTION__, vendor_dword1, vendor_dword2, vendor_dword3);
+		HALT();
+	}   	 	
+
+	return cpu_vendor;
+}
+
