5dd6e975efcdc0d8b7e202f0e1ff15d7d2bc82ea
Amit Vasudevan <amitvasudevan@acm.org>
2011-12-27 04:40:30 +0000

    emhf-baseplatform: remove hvm_intercept_handler isolation layer abstraction
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4038 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/x86/include/emhf.h
+++ emhf/trunk/code/x86/include/emhf.h
@@ -623,7 +623,7 @@ struct isolation_layer {
 	void 	(*hvm_initialize_csrip)(VCPU *vcpu, u16 cs_selector, u32 cs_base, u64 rip);
 	//void 	(*hvm_apic_setup)(VCPU *vcpu);
 	void 	(*hvm_start)(VCPU *vcpu);
-	u32 	(*hvm_intercept_handler)(VCPU *vcpu, struct regs *r);
+	//u32 	(*hvm_intercept_handler)(VCPU *vcpu, struct regs *r);
 	
 	//void 	(*do_quiesce)(VCPU *vcpu);
 	//void 	(*do_wakeup)(VCPU *vcpu);
