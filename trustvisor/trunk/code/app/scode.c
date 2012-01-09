7916a95748e86e10255dcad70fe53c902712f8fa
James Newsome <jnewsome@cmu.edu>
2012-01-09 21:15:06 +0000

    reimplement shared sections, using scode_lend_section
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4186 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- trustvisor/trunk/code/app/scode.c
+++ trustvisor/trunk/code/app/scode.c
@@ -1428,156 +1428,51 @@ u32 hpt_scode_npf(VCPU * vcpu, u32 gpaddr, u64 errorcode)
 	return 1;
 }
 
-void scode_release_all_shared_pages(VCPU *vcpu, whitelist_entry_t* entry)
+void scode_release_all_shared_pages(VCPU *vcpu, whitelist_entry_t* wle)
 {
 	size_t i;
-	u32 shared_page_count=0;
-	const u32 scode_pages = entry->scode_size>>PAGE_SHIFT_4K;
-	u32 scode_pages_shared_start;
 
-	/* remove from section info, and count the number of shared pages */
-	for(i=(entry->scode_info.num_sections-1);
-			entry->scode_info.sections[i].type == TV_PAL_SECTION_SHARED;
-			i--) {
-		entry->scode_info.num_sections--;
-		shared_page_count += entry->scode_info.sections[i].page_num;
+	/* restore permissions for remapped sections */
+	/* assumes that SHARED sections are contiguous and on the end of the sections array */
+	for(i = wle->sections_num-1; wle->sections[i].section_type == TV_PAL_SECTION_SHARED; i--) {
+		scode_return_section(&g_reg_npmo_root, &wle->hpt_nested_walk_ctx,
+												 &wle->pal_npt_root, &wle->hpt_nested_walk_ctx,
+												 &wle->pal_gpt_root, &wle->hpt_guest_walk_ctx,
+												 &wle->sections[i]);
+		wle->sections_num--;
 	}
-	dprintf(LOG_TRACE, "scode_release_all_shared_pages: releasing %d shared pages\n",
-				 shared_page_count);
-	if(shared_page_count == 0) {
-		return;
-	}
-
-	ASSERT(scode_pages >= shared_page_count);
-	scode_pages_shared_start = scode_pages - shared_page_count;
-
-	/* exactly last shared_page_count pages of entry->scode_pages should be shared.
-		 check this assumption for debugging. */
-	ASSERT(scode_pages_shared_start == 0
-				 || (SCODE_PTE_TYPE_GET(entry->scode_pages[scode_pages_shared_start-1])
-						 != TV_PAL_SECTION_SHARED));
-	for(i=scode_pages_shared_start; i<scode_pages; i++) {
-		ASSERT(SCODE_PTE_TYPE_GET(entry->scode_pages[i]) == TV_PAL_SECTION_SHARED);
-	}
-
-	/* clear protections */
-	hpt_scode_clear_prot(vcpu,
-											 &entry->scode_pages[scode_pages_shared_start],
-											 shared_page_count<<PAGE_SHIFT_4K);
-
-	/* remove from pal's page tables */
-	hpt_walk_set_prots(&entry->hpt_nested_walk_ctx,
-										 entry->pal_npt_root.pm,
-										 hpt_root_lvl(entry->hpt_nested_walk_ctx.t),
-										 &entry->scode_pages[scode_pages_shared_start],
-										 shared_page_count,
-										 HPT_PROTS_NONE);
-
-	/* XXX Should also revoke access to corresponding guest page table
-	 * pages. Not doing it for now since won't normally hurt anything,
-	 * and likely to rewrite handling of guest page tables soon.
-	 */
-
-	/* remove from pte's */
-	entry->scode_size -= shared_page_count<<PAGE_SHIFT_4K;
+	/* flush TLB for page table modifications to take effect */
+	emhf_hwpgtbl_flushall(vcpu);
 }
 
-u32 scode_share_range(VCPU * vcpu, whitelist_entry_t *entry, u32 gva_base, u32 gva_len)
+/* note- caller is responsible for flushing page tables afterwards */
+u32 scode_share_range(VCPU * vcpu, whitelist_entry_t *wle, u32 gva_base, u32 gva_len)
 {
-	u32 gva_len_pages;
+	(void)vcpu; /* tell compiler to ignore unused param */
 
-	pte_t *new_scode_pages=NULL;
-	
-	/* for backout\error handling */
-	u32 section_num_added=0, scode_size_added=0;
-	bool did_set_prot=false;
-	u32 rv=0;
-
-	/* XXX locking? */
-
-	dprintf(LOG_TRACE, "[TV] scode_share: gva-base %08x, size %x\n", gva_base, gva_len);
-
-	if (!PAGE_ALIGNED_4K(gva_base) || !PAGE_ALIGNED_4K(gva_len)) {
-		dprintf(LOG_ERROR, "[TV] scode_share: addr %x or len %d not page aligned\n",
-					 gva_base, gva_len);
-		rv=1;
-		goto outerr;
-	}
-	gva_len_pages = gva_len >> PAGE_SHIFT_4K;
-
-	/* add the section entry info */
-	if (entry->scode_info.num_sections >= TV_MAX_SECTIONS) {
-		dprintf(LOG_ERROR, "[TV] scode_share: maximum number of sections %d exceeded\n", TV_MAX_SECTIONS);
-		rv=2;
-		goto outerr;
-	}
-	entry->scode_info.sections[entry->scode_info.num_sections] =
-		(struct tv_pal_section)
-		{
-			.type = TV_PAL_SECTION_SHARED,
-			.start_addr = gva_base,
-			.page_num = gva_len_pages,
-		};
-	entry->scode_info.num_sections++;
-	section_num_added++;
-
-	/* create pt entries */
-	{
-		u32 scode_num_pages = entry->scode_size >> PAGE_SHIFT_4K;
-		new_scode_pages = &entry->scode_pages[scode_num_pages];
-		if ((scode_num_pages+gva_len_pages) > MAX_REGPAGES_NUM) {
-			dprintf(LOG_ERROR, "[TV] scode_share registered-page-limit exceeded:"
-						 " reg'd:%d addtl:%d limit:%d\n",
-						 scode_num_pages, gva_len_pages, MAX_REGPAGES_NUM);
-		}
-		if (guest_pt_copy(vcpu,
-											new_scode_pages,
-											gva_base,
-											gva_len,
-											TV_PAL_SECTION_SHARED)) {
-			dprintf(LOG_ERROR, "[TV] scode_share registration Failed. Probably some pages not in memory yet\n");
-			rv=3;
-			goto outerr;
-		}
-		entry->scode_size += gva_len;
-		scode_size_added += gva_len;
+	if (wle->sections_num >= TV_MAX_SECTIONS) {
+		dprintf(LOG_ERROR, "[TV] scode_share_range: max sections (%d) limit exceeded\n", TV_MAX_SECTIONS);
+		return 1;
 	}
 
-	/* set up protection of newly registered pt entries */
-	if(hpt_scode_set_prot(vcpu, new_scode_pages, gva_len)) {
-		dprintf(LOG_ERROR, "[TV] SECURITY: Registration Failed. Probably some page has already been used by other sensitive code.\n");
-		rv=4;
-		goto outerr;
-	}
-	did_set_prot=true;
+	wle->sections[wle->sections_num] = (tv_pal_section_int_t) {
+		.reg_gva = gva_base,
+		.pal_gva = gva_base,
+		.size = gva_len,
+		.pal_prot = pal_prot_of_type(TV_PAL_SECTION_SHARED),
+		.reg_prot = reg_prot_of_type(TV_PAL_SECTION_SHARED),
+	};
 
-	/* add to pal's page tables */
-	{
-		hpt_insert_pal_pmes(vcpu, &entry->hpt_nested_walk_ctx,
-												entry->pal_npt_root.pm,
-												hpt_root_lvl(entry->hpt_nested_walk_ctx.t),
-												new_scode_pages,
-												gva_len_pages);
-		/* scode_expose_arch(vcpu, entry); */
-		hpt_insert_pal_pmes(vcpu, &entry->hpt_nested_walk_ctx,
-												entry->pal_npt_root.pm,
-												hpt_root_lvl(entry->hpt_nested_walk_ctx.t),
-												entry->pte_page,
-												entry->pte_size>>PAGE_SHIFT_4K);
-		/* scode_unexpose_arch(vcpu, entry); */
-	}
+	scode_lend_section(&g_reg_npmo_root, &wle->hpt_nested_walk_ctx,
+										 &wle->reg_gpt_root, &wle->hpt_guest_walk_ctx,
+										 &wle->pal_npt_root, &wle->hpt_nested_walk_ctx,
+										 &wle->pal_gpt_root, &wle->hpt_guest_walk_ctx,
+										 &wle->sections[wle->sections_num]);
 
-  return rv;
+	wle->sections_num++;
 
- outerr:
-	if (entry!=NULL) {
-		entry->scode_size -= scode_size_added;
-		entry->scode_info.num_sections -= section_num_added;
-	}
-	if (did_set_prot) {
-		hpt_scode_clear_prot(vcpu, new_scode_pages, gva_len_pages);
-	}
-	return rv;
+	return 0;
+
 }
 
 u32 scode_share_ranges(VCPU * vcpu, u32 scode_entry, u32 gva_base[], u32 gva_len[], u32 count)
@@ -1597,6 +1492,9 @@ u32 scode_share_ranges(VCPU * vcpu, u32 scode_entry, u32 gva_base[], u32 gva_len
 			return 1;
 		}
 	}
+
+	/* flush TLB for page table modifications to take effect */
+	emhf_hwpgtbl_flushall(vcpu);
 
 	return 0;
 }
