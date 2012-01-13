eea1ec470224754b8fe4af7bf115263a3cfc8b9f
Amit Vasudevan <amitvasudevan@acm.org>
2012-01-13 19:41:37 +0000

    use new emhf_memprot_flushmappings interface to flush h/w pagetable mappings
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4299 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- trustvisor/trunk/code/app/scode.c
+++ trustvisor/trunk/code/app/scode.c
@@ -745,7 +745,7 @@ u32 scode_register(VCPU *vcpu, u32 scode_info, u32 scode_pm, u32 gventry)
                                                hva2gpa(whitelist_new.pal_gpt_root.pm));
 
   /* flush TLB for page table modifications to take effect */
-  emhf_hwpgtbl_flushall(vcpu);
+  emhf_memprot_flushmappings(vcpu);
 
   /* initialize Micro-TPM instance */
   utpm_init_instance(&whitelist_new.utpm);
@@ -843,7 +843,7 @@ u32 scode_unregister(VCPU * vcpu, u32 gvaddr)
                          &whitelist[i].sections[j]);
   }
   /* flush TLB for page table modifications to take effect */
-  emhf_hwpgtbl_flushall(vcpu);
+  emhf_memprot_flushmappings(vcpu);
 
   /* delete entry from scode whitelist */
   /* CRITICAL SECTION in MP scenario: need to quiesce other CPUs or at least acquire spinlock */
@@ -1031,7 +1031,7 @@ u32 hpt_scode_switch_scode(VCPU * vcpu)
   dprintf(LOG_TRACE, "[TV] change NPT permission to run PAL!\n");
   hpt_emhf_set_root_pm(vcpu, whitelist[curr].pal_npt_root.pm);
   VCPU_gcr3_set(vcpu, whitelist[curr].pal_gcr3);
-  emhf_hwpgtbl_flushall(vcpu); /* XXX */
+  emhf_memprot_flushmappings(vcpu); /* XXX */
 
   /* disable interrupts */
   VCPU_grflags_set(vcpu, VCPU_grflags(vcpu) & ~EFLAGS_IF);
@@ -1168,7 +1168,7 @@ u32 hpt_scode_switch_regular(VCPU * vcpu)
   dprintf(LOG_TRACE, "[TV] change NPT permission to exit PAL!\n"); 
   hpt_emhf_set_root_pm(vcpu, g_reg_npmo_root.pm);
   VCPU_gcr3_set(vcpu, whitelist[curr].gcr3);
-  emhf_hwpgtbl_flushall(vcpu); /* XXX */
+  emhf_memprot_flushmappings(vcpu); /* XXX */
 
   /* switch back to regular stack */
   dprintf(LOG_TRACE, "[TV] switch from scode stack %#x back to regular stack %#x\n", (u32)VCPU_grsp(vcpu), (u32)whitelist[curr].grsp);
@@ -1381,8 +1381,8 @@ u32 scode_share_ranges(VCPU * vcpu, u32 scode_entry, u32 gva_base[], u32 gva_len
   }
 
   /* flush TLB for page table modifications to take effect */
-  emhf_hwpgtbl_flushall(vcpu);
-
+  emhf_memprot_flushmappings(vcpu);
+  
   return 0;
 }
 
