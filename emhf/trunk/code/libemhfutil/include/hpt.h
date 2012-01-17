393fc77f2eeea901c5dea4115eaf8555ecae299a
James Newsome <jnewsome@cmu.edu>
2012-01-17 23:32:02 +0000

    move page-table constants into a private header file to keep hpt.h tidier
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4329 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/libemhfutil/include/hpt.h
+++ emhf/trunk/code/libemhfutil/include/hpt.h
@@ -129,140 +129,8 @@ extern const u8 hpt_type_max_lvl[HPT_TYPE_NUM];
 
 size_t hpt_pm_size(hpt_type_t t, int lvl);
 
-/* static inline size_t hpt_pme_size(hpt_type_t t, int lvl) */
-/* { */
-/*   HPT_UNUSED_ARGUMENT(lvl); */
-/*   if(t == HPT_TYPE_EPT */
-/*      || t == HPT_TYPE_PAE */
-/*      || t == HPT_TYPE_LONG) { */
-/*     return 8; */
-/*   } else if (t == HPT_TYPE_NORM) { */
-/*     return 4; */
-/*   } */
-
-/*   assert(0); */
-/*   return 0; */
-/* } */
-
-/* HPT_<page table type>_<name>_L<applicable levels>_[M][P]_[LO|HI|BIT] */
-/* P if the mapping is valid for entries mapping a page.
- * M if the mapping is valid for entries mapping another map (table)
- * when there's no ambiguity, the same macro may include level 1 and 'M',
- * even though a level 1 entry is always a page. e.g., '...L21_MP...'
- * for a name that is valid for l2 maps, l2 pages, and l1 pages.
- *
- * Including in the name of each macro which levels and types it is
- * valid for is intended to help write error-free code below. e.g., it
- * is clear when using an 'L32_M' macro that one must first ensure that
- * the PTE being dealt with maps a map\table and is of level 3 or 2.
- * 
- */
-#define HPT_NORM_ADDR_L2_M_HI 31
-#define HPT_NORM_ADDR_L2_M_LO 12
-#define HPT_NORM_ADDR3122_L2_P_HI 31
-#define HPT_NORM_ADDR3122_L2_P_LO 22
-#define HPT_NORM_MBZ21_L2_P_BIT 21
-#define HPT_NORM_ADDR3932_L2_P_HI 20
-#define HPT_NORM_ADDR3932_L2_P_LO 13
-#define HPT_NORM_PAT_L2_P_BIT 12 /* page attribute table */
-#define HPT_NORM_ADDR_L1_P_HI 31
-#define HPT_NORM_ADDR_L1_P_LO 12
-#define HPT_NORM_AVL11_L21_MP_HI 11 /* available */
-#define HPT_NORM_AVL11_L21_MP_LO 9 
-#define HPT_NORM_G_L21_P_BIT 8 /* global */
-#define HPT_NORM_IGN8_L2_M_BIT 8
-#define HPT_NORM_PS_L2_MP_BIT 7 /* page size */
-#define HPT_NORM_PAT_L1_P_BIT 7 /* page attribute table */
-#define HPT_NORM_D_L21_P_BIT 6 /* dirty (valid for lowest lvl only) */
-#define HPT_NORM_IGN6_L2_M_BIT 6
-#define HPT_NORM_A_L21_MP_BIT 5 /* accessed */
-#define HPT_NORM_PCD_L21_MP_BIT 4 /* page cache disable */
-#define HPT_NORM_PWT_L21_MP_BIT 3 /* page write through */
-#define HPT_NORM_US_L21_MP_BIT 2 /* 0:supervisor 1:user */
-#define HPT_NORM_RW_L21_MP_BIT 1 /* 0:read-only 1:read\write */
-#define HPT_NORM_P_L21_MP_BIT 0 /* present */
-
-#define HPT_PAE_MBZ63_L3_MP_BIT 63
-#define HPT_PAE_NX_L21_MP_BIT 63
-#define HPT_PAE_MBZ62_L321_MP_HI 62
-#define HPT_PAE_MBZ62_L321_MP_LO 52
-#define HPT_PAE_ADDR_L321_M_HI 51 /* address bits */
-#define HPT_PAE_ADDR_L321_M_LO 12
-#define HPT_PAE_ADDR_L2_P_HI 51
-#define HPT_PAE_ADDR_L2_P_LO 13
-#define HPT_PAE_PAT_L2_P_BIT 12
-#define HPT_PAE_ADDR_L1_P_HI 51 /* address bits */
-#define HPT_PAE_ADDR_L1_P_LO 12
-#define HPT_PAE_AVL11_L321_MP_HI 11 /* available */
-#define HPT_PAE_AVL11_L321_MP_LO 9
-#define HPT_PAE_MBZ8_L3_MP_HI 8
-#define HPT_PAE_MBZ8_L3_MP_LO 5
-#define HPT_PAE_G_L21_P_BIT 8 /* global */
-#define HPT_PAE_IGN8_L2_M_BIT 8
-#define HPT_PAE_PS_L2_MP_BIT 7 /* page size */
-#define HPT_PAE_PAT_L1_P_BIT 7 /* page attribute table */
-#define HPT_PAE_D_L21_P_BIT 6 /* dirty */
-#define HPT_PAE_IGN6_L2_M_BIT 6
-#define HPT_PAE_A_L21_P_BIT 5 /* accessed */
-#define HPT_PAE_PCD_L321_MP_BIT 4 /* page cache disable */
-#define HPT_PAE_PWT_L321_MP_BIT 3 /* page write through */
-#define HPT_PAE_MBZ2_L3_M_HI 2
-#define HPT_PAE_MBZ2_L3_M_LO 1
-#define HPT_PAE_US_L21_MP_BIT 2 /* 0:supervisor 1:user */
-#define HPT_PAE_RW_L21_MP_BIT 1 /* 0:read-only 1:read\write */
-#define HPT_PAE_P_L321_MP_BIT 0 /* present */
-
-#define HPT_LONG_NX_L4321_MP_BIT 63
-#define HPT_LONG_AVL62_L4321_MP_HI 62
-#define HPT_LONG_AVL52_L4321_MP_LO 52
-#define HPT_LONG_ADDR_L4321_M_HI 51 /* address bits */
-#define HPT_LONG_ADDR_L4321_M_LO 12
-#define HPT_LONG_ADDR_L32_P_HI 51
-#define HPT_LONG_ADDR_L32_P_LO 13
-#define HPT_LONG_ADDR_L1_P_HI 51
-#define HPT_LONG_ADDR_L1_P_LO 12
-#define HPT_LONG_PAT_L32_P_BIT 12
-#define HPT_LONG_AVL11_L4321_MP_HI 11 /* available */
-#define HPT_LONG_AVL11_L4321_MP_LO 9
-#define HPT_LONG_MBZ8_L4_MP_BIT 8
-#define HPT_LONG_MBZ7_L4_MP_BIT 7
-#define HPT_LONG_G_L21_P_BIT 8 /* global */
-#define HPT_LONG_PS_L32_MP_BIT 7 /* page size */
-#define HPT_LONG_PAT_L1_P_BIT 7 /* page attribute table */
-#define HPT_LONG_IGN6_L4_M_BIT 6
-#define HPT_LONG_D_L321_P_BIT 6 /* dirty */
-#define HPT_LONG_A_L4321_MP_BIT 5 /* accessed */
-#define HPT_LONG_PCD_L4321_MP_BIT 4 /* page cache disable */
-#define HPT_LONG_PWT_L4321_MP_BIT 3 /* page write through */
-#define HPT_LONG_US_L4321_MP_BIT 2 /* 0:supervisor 1:user */
-#define HPT_LONG_RW_L4321_MP_BIT 1 /* 0:read-only 1:read\write */
-#define HPT_LONG_P_L4321_MP_BIT 0 /* present */
-
-#define HPT_EPT_MAXPHYADDR 52 /* FIXME, should find this dynamically
-                                 using cpuid, cf Intel manual 3A
-                                 4.1.4. using the max value of 52,
-                                 which could cause trouble if the
-                                 reserved bitfield above MAXPHYADDR
-                                 gets used for anything. */
-#define HPT_EPT_AVL63_L4321_MP_HI 63
-#define HPT_EPT_AVL52_L4321_MP_LO 52
-#define HPT_EPT_ADDR_L4321_MP_HI (HPT_EPT_MAXPHYADDR-1)
-#define HPT_EPT_ADDR_L4321_MP_LO 12
-#define HPT_EPT_AVL11_L4321_MP_HI 11
-#define HPT_EPT_AVL11_L4321_MP_LO 8
-#define HPT_EPT_MBZ7_L4_M_BIT 7
-#define HPT_EPT_PS_L32_MP_BIT 7
-#define HPT_EPT_AVL7_L1_P_BIT 7
-#define HPT_EPT_IPAT_L321_P_BIT 6
-#define HPT_EPT_MBZ6_L432_M_HI 6
-#define HPT_EPT_MBZ6_L432_M_LO 3
-#define HPT_EPT_MT_L321_P_HI 5
-#define HPT_EPT_MT_L321_P_LO 3
-#define HPT_EPT_X_L4321_MP_BIT 2
-#define HPT_EPT_W_L4321_MP_BIT 1
-#define HPT_EPT_R_L4321_MP_BIT 0
-#define HPT_EPT_PROT_L4321_MP_HI 2
-#define HPT_EPT_PROT_L4321_MP_LO 0
+#include <hpt_consts.h> /* XXX - remove this once fn bodies are moved
+                           into hpt.c */
 
 /* Abstract protection flags. */
 #define HPT_PROT_READ_BIT 0
@@ -280,21 +148,6 @@ size_t hpt_pm_size(hpt_type_t t, int lvl);
 #define HPT_PROTS_W (HPT_PROT_WRITE_MASK) /* never valid */
 #define HPT_PROTS_X (HPT_PROT_EXEC_MASK) /* never valid */
 #define HPT_PROTS_NONE (0)
-
-#define HPT_CR3_PML4_LONG_HI 51
-#define HPT_CR3_PML4_LONG_LO 12
-#define HPT_CR3_PML2_LONG_MBZ11_HI 11
-#define HPT_CR3_PML2_LONG_MBZ11_LO 5
-#define HPT_CR3_PML3_PAE_HI 31
-#define HPT_CR3_PML3_PAE_LO 5
-#define HPT_CR3_PML2_NORM_HI 31
-#define HPT_CR3_PML2_NORM_LO 12
-#define HPT_CR3_MBZ11_NORM_HI 11
-#define HPT_CR3_MBZ11_NORM_LO 5
-#define HPT_CR3_PCD_BIT 4
-#define HPT_CR3_PWT_BIT 3
-#define HPT_CR3_MBZ2_HI 2
-#define HPT_CR3_MBZ2_LO 0
 
 static inline u64 hpt_cr3_set_address(hpt_type_t t, u64 cr3, hpt_pa_t a)
 {
@@ -332,17 +185,6 @@ static inline hpt_pa_t hpt_cr3_get_address(hpt_type_t t, u64 cr3)
   assert(0); return (hpt_pa_t)0; /* unreachable; appeases compiler */
 }
 
-#define HPT_EPTP_MBZ63_H 63
-#define HPT_EPTP_MBZ63_L HPT_EPT_MAXPHYADDR
-#define HPT_EPTP_PML4_HI (HPT_EPT_MAXPHYADDR-1)
-#define HPT_EPTP_PML4_LO 12
-#define HPT_EPTP_MBZ11_HI 11
-#define HPT_EPTP_MBZ11_LO 6
-#define HPT_EPTP_PWLM1_HI 5
-#define HPT_EPTP_PWLM1_LO 3
-#define HPT_EPTP_PSMT_HI 2
-#define HPT_EPTP_PSMT_LO 0
-
 static inline u64 hpt_eptp_set_address(hpt_type_t t, u64 eptp, hpt_pa_t a)
 {
   assert(t == HPT_TYPE_EPT);
@@ -354,9 +196,6 @@ static inline hpt_pa_t hpt_eptp_get_address(hpt_type_t t, u64 eptp)
   assert(t == HPT_TYPE_EPT);
   return BR64_COPY_BITS_HL(0, eptp, HPT_EPTP_PML4_HI, HPT_EPTP_PML4_LO, 0);
 }
-
-#define HPT_CR4_PAE_BIT 5
-#define HPT_CR4_PSE_BIT 4
 
 #define MAX(x, y) (((y) > (x)) ? (y) : (x))
 #define MIN(x, y) (((y) < (x)) ? (y) : (x))
