c7b04f0d79290aaf93cfe231e196b96f72391cd6
Amit Vasudevan <amitvasudevan@acm.org>
2012-01-20 14:50:45 +0000

    emhf-runtime: cleanup and successful tests on both intel and amd with ubuntu smp linux kernel
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4370 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/emhfcore/components/emhf-runtime/runtime.c
+++ emhf/trunk/code/emhfcore/components/emhf-runtime/runtime.c
@@ -90,29 +90,16 @@ void emhf_runtime_entry(void){
 			u32 protectedbuffer_vaddr;
 			u32 protectedbuffer_size;
 			
-			/*if(cpu_vendor == CPU_VENDOR_AMD){
-				protectedbuffer_paddr = hva2spa(&g_svm_dev_bitmap);
-				protectedbuffer_vaddr = (u32)&g_svm_dev_bitmap;
-				protectedbuffer_size = 131072; //sizeof(g_svm_dev_bitmap) XXX: TODO remove hard-coded constant
-			}else{	//CPU_VENDOR_INTEL
-				protectedbuffer_paddr = hva2spa(&g_vmx_vtd_pdp_table);
-				protectedbuffer_vaddr = (u32)&g_vmx_vtd_pdp_table;
-				protectedbuffer_size = PAGE_SIZE_4K + (PAGE_SIZE_4K * PAE_PTRS_PER_PDPT) 
-					+ (PAGE_SIZE_4K * PAE_PTRS_PER_PDPT * PAE_PTRS_PER_PDT) + PAGE_SIZE_4K +
-					(PAGE_SIZE_4K * PCI_BUS_MAX);
-			}*/
 			protectedbuffer_paddr = hva2spa(&g_rntm_dmaprot_buffer);
 			protectedbuffer_vaddr = (u32)&g_rntm_dmaprot_buffer;
 			protectedbuffer_size = emhf_dmaprot_getbuffersize(ADDR_4GB);
 			ASSERT(protectedbuffer_size <= sizeof(g_rntm_dmaprot_buffer));
 			
-	
 			printf("\nRuntime: Re-initializing DMA protection...");
 			if(!emhf_dmaprot_initialize(protectedbuffer_paddr, protectedbuffer_vaddr, protectedbuffer_size)){
 				printf("\nRuntime: Unable to re-initialize DMA protection. HALT!");
 				HALT();
 			}
-
 
 			//protect SL and runtime memory regions
 			emhf_dmaprot_protect(rpb->XtVmmRuntimePhysBase - PAGE_SIZE_2M, rpb->XtVmmRuntimeSize+PAGE_SIZE_2M);
@@ -151,11 +138,11 @@ void emhf_runtime_main(VCPU *vcpu, u32 isEarlyInit){
   {
   	APP_PARAM_BLOCK appParamBlock;
   	
-		appParamBlock.bootsector_ptr = (u32)rpb->XtGuestOSBootModuleBase;
+	appParamBlock.bootsector_ptr = (u32)rpb->XtGuestOSBootModuleBase;
   	appParamBlock.bootsector_size = (u32)rpb->XtGuestOSBootModuleSize;
   	appParamBlock.optionalmodule_ptr = (u32)rpb->XtGuestOSBootModuleBaseSup1;
   	appParamBlock.optionalmodule_size = (u32)rpb->XtGuestOSBootModuleSizeSup1;
- 		appParamBlock.runtimephysmembase = (u32)rpb->XtVmmRuntimePhysBase;  
+	appParamBlock.runtimephysmembase = (u32)rpb->XtVmmRuntimePhysBase;  
 
   	//call app main
   	if(emhf_app_main(vcpu, &appParamBlock)){
