5cb8feba5636233351f50e4655e5478eeab74a22
James Newsome <jnewsome@cmu.edu>
2012-01-31 20:16:43 +0000

    define ssize_t
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4553 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/libemhfc/include/sys/types.h
+++ emhf/trunk/code/libemhfc/include/sys/types.h
@@ -142,6 +142,11 @@ typedef __size_t        size_t;
 #define _SIZE_T_DECLARED
 #endif
 
+#ifndef _SSIZE_T_DECLARED
+typedef __ssize_t        ssize_t;
+#define _SSIZE_T_DECLARED
+#endif
+
 typedef uint8_t         u8;
 typedef uint16_t        u16;
 typedef uint32_t        u32;
