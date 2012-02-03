98047842c3dc5e71ac8af3ae6e678839c38007f8
Amit Vasudevan <amitvasudevan@acm.org>
2012-02-03 15:23:38 +0000

    emhf-smpguest.h: split into arch. independent and arch. specific portions
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4594 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/emhfcore/include/emhf-smpguest.h
+++ emhf/trunk/code/emhfcore/include/emhf-smpguest.h
@@ -40,6 +40,9 @@
 #ifndef __EMHF_SMPGUEST_H__
 #define __EMHF_SMPGUEST_H__
 
+//bring in arch. specific declarations
+#include <arch/emhf-smpguest-arch.h>
+
 
 #ifndef __ASSEMBLY__
 
@@ -65,136 +68,6 @@ void emhf_smpguest_endquiesce(VCPU *vcpu);
 //note: returns 0xFFFFFFFF if there is no mapping
 u8 * emhf_smpguest_walk_pagetables(VCPU *vcpu, u32 vaddr);
 
-
-//----------------------------------------------------------------------
-//ARCH. BACKENDS
-//----------------------------------------------------------------------
-
-//initialize SMP guest logic
-void emhf_smpguest_arch_initialize(VCPU *vcpu);
-
-//quiesce interface to switch all guest cores into hypervisor mode
-void emhf_smpguest_arch_quiesce(VCPU *vcpu);
-
-//endquiesce interface to resume all guest cores after a quiesce
-void emhf_smpguest_arch_endquiesce(VCPU *vcpu);
-
-//walk guest page tables; returns pointer to corresponding guest physical address
-//note: returns 0xFFFFFFFF if there is no mapping
-u8 * emhf_smpguest_arch_walk_pagetables(VCPU *vcpu, u32 vaddr);
-
-
-//----------------------------------------------------------------------
-//x86 ARCH. INTERFACES
-//----------------------------------------------------------------------
-
-//perform required setup after a guest awakens a new CPU
-void emhf_smpguest_arch_x86_postCPUwakeup(VCPU *vcpu);
-
-//handle LAPIC access #DB (single-step) exception event
-void emhf_smpguest_arch_x86_eventhandler_dbexception(VCPU *vcpu, 
-	struct regs *r);
-
-//handle LAPIC access #NPF (nested page fault) event
-void emhf_smpguest_arch_x86_eventhandler_hwpgtblviolation(VCPU *vcpu, u32 gpa, u32 errorcode);
-
-//quiescing handler for #NMI (non-maskable interrupt) exception event
-void emhf_smpguest_arch_x86_eventhandler_nmiexception(VCPU *vcpu, struct regs *r);
-
-
-//----------------------------------------------------------------------
-//x86svm SUBARCH. INTERFACES
-//----------------------------------------------------------------------
-void emhf_smpguest_arch_x86svm_initialize(VCPU *vcpu);
-void emhf_smpguest_arch_x86svm_eventhandler_dbexception(VCPU *vcpu, 
-	struct regs *r);
-u32 emhf_smpguest_arch_x86svm_eventhandler_hwpgtblviolation(VCPU *vcpu, u32 paddr, u32 errorcode);
-//perform required setup after a guest awakens a new CPU
-void emhf_smpguest_arch_x86svm_postCPUwakeup(VCPU *vcpu);
-//walk guest page tables; returns pointer to corresponding guest physical address
-//note: returns 0xFFFFFFFF if there is no mapping
-u8 * emhf_smpguest_arch_x86svm_walk_pagetables(VCPU *vcpu, u32 vaddr);
-
-//the BSP LAPIC base address
-extern u32 g_svm_lapic_base __attribute__(( section(".data") ));
-
-//the quiesce counter, all CPUs except for the one requesting the
-//quiesce will increment this when they get their quiesce signal
-extern u32 g_svm_quiesce_counter __attribute__(( section(".data") ));
-
-//SMP lock to access the above variable
-extern u32 g_svm_lock_quiesce_counter __attribute__(( section(".data") )); 
-
-//resume counter to rally all CPUs after resumption from quiesce
-extern u32 g_svm_quiesce_resume_counter __attribute__(( section(".data") ));
-
-//SMP lock to access the above variable
-extern u32 g_svm_lock_quiesce_resume_counter __attribute__(( section(".data") )); 
-    
-//the "quiesce" variable, if 1, then we have a quiesce in process
-extern u32 g_svm_quiesce __attribute__(( section(".data") ));      
-
-//SMP lock to access the above variable
-extern u32 g_svm_lock_quiesce __attribute__(( section(".data") )); 
-    
-//resume signal, becomes 1 to signal resume after quiescing
-extern u32 g_svm_quiesce_resume_signal __attribute__(( section(".data") ));  
-
-//SMP lock to access the above variable
-extern u32 g_svm_lock_quiesce_resume_signal __attribute__(( section(".data") )); 
-
-//4k buffer which is the virtual LAPIC page that guest reads and writes from/to
-//during INIT-SIPI-SIPI emulation
-extern u8 g_svm_virtual_LAPIC_base[] __attribute__(( section(".palign_data") ));
-
-//SVM SIPI page buffers used for guest INIT-SIPI-SIPI emulation
-extern u8 g_svm_sipi_page_buffers[]__attribute__(( section(".palign_data") ));
-
-
-//----------------------------------------------------------------------
-//x86vmx SUBARCH. INTERFACES
-//----------------------------------------------------------------------
-void emhf_smpguest_arch_x86vmx_initialize(VCPU *vcpu);
-void emhf_smpguest_arch_x86vmx_eventhandler_dbexception(VCPU *vcpu, 
-	struct regs *r);
-u32 emhf_smpguest_arch_x86vmx_eventhandler_hwpgtblviolation(VCPU *vcpu, u32 paddr, u32 errorcode);
-//perform required setup after a guest awakens a new CPU
-void emhf_smpguest_arch_x86vmx_postCPUwakeup(VCPU *vcpu);
-//walk guest page tables; returns pointer to corresponding guest physical address
-//note: returns 0xFFFFFFFF if there is no mapping
-u8 * emhf_smpguest_arch_x86vmx_walk_pagetables(VCPU *vcpu, u32 vaddr);
-
-//the BSP LAPIC base address
-extern u32 g_vmx_lapic_base __attribute__(( section(".data") ));
-
-//4k buffer which is the virtual LAPIC page that guest reads and writes from/to
-//during INIT-SIPI-SIPI emulation
-extern u8 g_vmx_virtual_LAPIC_base[] __attribute__(( section(".palign_data") ));
-
-//the quiesce counter, all CPUs except for the one requesting the
-//quiesce will increment this when they get their quiesce signal
-extern u32 g_vmx_quiesce_counter __attribute__(( section(".data") ));
-
-//SMP lock to access the above variable
-extern u32 g_vmx_lock_quiesce_counter __attribute__(( section(".data") )); 
-
-//resume counter to rally all CPUs after resumption from quiesce
-extern u32 g_vmx_quiesce_resume_counter __attribute__(( section(".data") ));
-
-//SMP lock to access the above variable
-extern u32 g_vmx_lock_quiesce_resume_counter __attribute__(( section(".data") )); 
-    
-//the "quiesce" variable, if 1, then we have a quiesce in process
-extern u32 g_vmx_quiesce __attribute__(( section(".data") ));      
-
-//SMP lock to access the above variable
-extern u32 g_vmx_lock_quiesce __attribute__(( section(".data") )); 
-    
-//resume signal, becomes 1 to signal resume after quiescing
-extern u32 g_vmx_quiesce_resume_signal __attribute__(( section(".data") ));  
-
-//SMP lock to access the above variable
-extern u32 g_vmx_lock_quiesce_resume_signal __attribute__(( section(".data") )); 
 
 
 #endif	//__ASSEMBLY__
