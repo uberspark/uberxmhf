cd488dfc13938a822a10a37cb606ac6b75851fc8
James Newsome <jnewsome@cmu.edu>
2012-02-07 22:31:14 +0000

    use eu_ logging and error-checking fns in crypto_init.c
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4712 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- trustvisor/trunk/code/app/crypto_init.c
+++ trustvisor/trunk/code/app/crypto_init.c
@@ -53,6 +53,8 @@
 #include <hmac.h>
 #include <rsa.h>
 
+#include <tv_log.h>
+
 /* awesome trick from http://www.jaggersoft.com/pubs/CVu11_3.html */
 #define COMPILE_TIME_ASSERT(pred)               \
   switch(0){case 0:case pred:;}
@@ -80,24 +82,24 @@ void zeroize(uint8_t* _p, unsigned int len) {
  * violated and TrustVisor should HALT! */
 /* returns 0 on success. */
 int get_hw_tpm_entropy(uint8_t* buf, unsigned int requested_len /* bytes */) {
-  uint32_t rv;
+  uint32_t rv=1;
   unsigned int actual_len;
-		
-  if(!buf) { return 1; }
+
+  EU_CHK( buf);
 
   actual_len = requested_len;
-  rv = tpm_get_random(CRYPTO_INIT_LOCALITY, buf, &actual_len);
+  EU_CHKN( rv = tpm_get_random(CRYPTO_INIT_LOCALITY, buf, &actual_len));
 
-  if(actual_len != requested_len) {
-    dprintf(LOG_ERROR, "\nFATAL ERROR: Could not access TPM to initialize PRNG.\n");
-    /* TODO: Try a few more times before giving up. */
-    return 1;
-  }
+  /* TODO: Try a few more times before giving up. */
+  EU_CHK( actual_len == requested_len,
+          eu_err_e("FATAL ERROR: Could not access TPM to initialize PRNG."));
 
-  dprintf(LOG_TRACE, "\n[TV] Successfully received %d/%d bytes of entropy from HW TPM.",
-          actual_len, requested_len);
+  eu_trace("Successfully received %d/%d bytes of entropy from HW TPM.",
+           actual_len, requested_len);
 
-  return 0;
+  rv=0;
+ out:
+  return rv;
 }
 
 
@@ -105,30 +107,26 @@ int get_hw_tpm_entropy(uint8_t* buf, unsigned int requested_len /* bytes */) {
 static int master_prng_init(void) {
   uint8_t EntropyInput[CTR_DRBG_SEED_BITS/8];
   uint64_t Nonce;
-    
-  nist_ctr_initialize();
+  int rv=1;
+
+  EU_CHKN( nist_ctr_initialize());
 
   /* Get CTR_DRBG_SEED_BITS of entropy from the hardware TPM */
-  if(get_hw_tpm_entropy(EntropyInput, CTR_DRBG_SEED_BITS/8)) {
-    dprintf(LOG_ERROR, "\nFATAL ERROR: Could not access TPM to initialize PRNG.\n");
-    HALT();
-    return 1;
-  }
+  EU_VERIFYN( get_hw_tpm_entropy( EntropyInput, CTR_DRBG_SEED_BITS/8),
+              eu_err_e("FATAL ERROR: Could not access TPM to initialize PRNG."));
 
   /* Use rdtsc to get CTR_DRBG_NONCE_BITS of initialization nonce */
   COMPILE_TIME_ASSERT(CTR_DRBG_NONCE_BITS/8 == sizeof(Nonce));
   Nonce = rdtsc64();
 
-  if(0 != nist_ctr_drbg_instantiate(&g_drbg, EntropyInput, sizeof(EntropyInput),
-                                    &Nonce, sizeof(Nonce), NULL, 0)) {
-    dprintf(LOG_ERROR, "\nFATAL ERROR: nist_ctr_drbg_instantiate FAILED.\n");
-    HALT();
-    return 1;
-  }				
+  EU_VERIFYN( nist_ctr_drbg_instantiate(&g_drbg, EntropyInput, sizeof(EntropyInput),
+                                        &Nonce, sizeof(Nonce), NULL, 0),
+              eu_err_e("FATAL ERROR: nist_ctr_drbg_instantiate FAILED."));
 
-  return 0;
+  rv=0;
+ out:
+  return rv;
 }
-
 
 /**
  * Do a measurement of the "quick and dirty" RSA public key from the
@@ -141,15 +139,12 @@ static int master_prng_init(void) {
 #define SERIAL_BUFSIZE (TPM_RSA_KEY_LEN + 2*sizeof(uint32_t))
 
 static int trustvisor_measure_qnd_bridge_signing_pubkey(rsa_context *rsa) {
-  int rv;
+  int rv=1;
   uint8_t serial_pubkey[SERIAL_BUFSIZE];
   tpm_digest_t digest;
   tpm_pcr_value_t pcr_out;
-		
-  if(NULL == rsa) {
-    dprintf(LOG_ERROR, "\nERROR: NULL rsa context.\n");
-    return 1;
-  }
+
+  EU_CHK( rsa);
 
   /**
    * 1. Serialize RSA key into byte blob for purposes of measurement.
@@ -159,50 +154,40 @@ static int trustvisor_measure_qnd_bridge_signing_pubkey(rsa_context *rsa) {
   /* rsa->len */
   *((uint32_t*)&serial_pubkey[0]) = NIST_HTONL(rsa->len);
   /* rsa->E */
-  if(0 != mpi_write_binary(&rsa->E, &serial_pubkey[0]+sizeof(uint32_t), sizeof(uint32_t))) {
-    dprintf(LOG_ERROR, "\nERROR: mpi_write_binary FAILED with rsa->E.\n");
-    return 1;
-  }
+  EU_CHKN( mpi_write_binary( &rsa->E, &serial_pubkey[0]+sizeof(uint32_t), sizeof(uint32_t)));
+
   /* rsa->N */
-  if(rsa->len != TPM_RSA_KEY_LEN) {
-    dprintf(LOG_ERROR, "\nERROR: rsa->len != TPM_RSA_KEY_LEN.\n");
-    return 1;
-  }
-  if(0 != mpi_write_binary(&rsa->N, &serial_pubkey[0]+2*sizeof(uint32_t), rsa->len)) {
-    dprintf(LOG_ERROR, "\nERROR: mpi_write_binary FAILED with rsa->N.\n");
-    return 1;
-  }
-  print_hex("[TV] Serialized RSA key: ", serial_pubkey, SERIAL_BUFSIZE);
+  EU_CHK( rsa->len == TPM_RSA_KEY_LEN);
+
+  EU_CHKN( mpi_write_binary( &rsa->N, &serial_pubkey[0]+2*sizeof(uint32_t), rsa->len));
+  eu_trace("Serialized RSA key: %*D", SERIAL_BUFSIZE, serial_pubkey, " ");
 
   /**
    * 2. Hash serialized RSA key.
    */
-  if(0 != sha1_buffer(serial_pubkey, SERIAL_BUFSIZE, digest.digest)) {
-    return 1;
-  }
-  print_hex("Hashed serialized RSA key: ", digest.digest, TPM_HASH_SIZE);
+  EU_CHKN( sha1_buffer(serial_pubkey, SERIAL_BUFSIZE, digest.digest));
+  eu_trace("Hashed serialized RSA key: %*D", TPM_HASH_SIZE, digest.digest, " ");
 		
   /**
    * 3. Extend PCR with hash.
    */
-  rv = tpm_pcr_extend(CRYPTO_INIT_LOCALITY, QND_BRIDGE_PUBKEY_PCR,
-                      &digest, &pcr_out);
-  if(0 != rv) {
-    dprintf(LOG_ERROR, "\nFATAL ERROR: Could not extend HW TPM PCR %d.\n", QND_BRIDGE_PUBKEY_PCR);
-    return rv;
-  }
+  EU_CHKN( rv = tpm_pcr_extend(CRYPTO_INIT_LOCALITY, QND_BRIDGE_PUBKEY_PCR,
+                               &digest, &pcr_out),
+           eu_err_e("FATAL ERROR: Could not extend HW TPM PCR %d.", QND_BRIDGE_PUBKEY_PCR));
 
-  dprintf(LOG_TRACE, "\n[TV] Successfully extended HW TPM PCR %d.\n", QND_BRIDGE_PUBKEY_PCR);
-  print_hex("[TV] New PCR value: ", pcr_out.digest, TPM_HASH_SIZE);
+  eu_trace( "Successfully extended HW TPM PCR %d.", QND_BRIDGE_PUBKEY_PCR);
+  eu_trace( "New PCR value: ", TPM_HASH_SIZE, pcr_out.digest, " ");
 
-  return 0;
+  rv=0;
+ out:
+  return rv;
 }
 
 /* involves accessing TPM NV RAM. */
 /* depends on PRNG already being initialized. */
 /* returns 0 on success. */
 static int trustvisor_long_term_secret_init(void) {
-  int rv = 0;
+  int rv = 1;
   uint8_t mss[HW_TPM_MASTER_SEALING_SECRET_SIZE];
   /* The actual AES key is smaller than this, so we need a temporary
    * buffer. */
@@ -213,25 +198,23 @@ static int trustvisor_long_term_secret_init(void) {
   const uint8_t sealinghmac[11] = {0x73, 0x65, 0x61, 0x6c, 0x69, 0x6e,
                                    0x67,	0x68, 0x6d, 0x61, 0x63};
   rsa_context rsa;
-	
-  ASSERT(true == g_master_prng_init_completed);
+
+  EU_VERIFY( g_master_prng_init_completed);
 
   rv = trustvisor_nv_get_mss(CRYPTO_INIT_LOCALITY,
                              HW_TPM_MASTER_SEALING_SECRET_INDEX,
                              mss,
                              HW_TPM_MASTER_SEALING_SECRET_SIZE);
   if(0 != rv) {
-    dprintf(LOG_ERROR, "\nFATAL ERROR: %s FAILED (%d).\n",
-            __FUNCTION__, rv);
+    eu_err("FATAL ERROR: trustvisor_nv_get_mss FAILED (%d).\n",
+           rv);
     /* TODO: Support degraded operation during development. */
     /* XXX SECURITY XXX: continuing anyways for now */
     //ASSERT(false);
-    dprintf(LOG_ERROR, "\n[TV] %s: \n\n"
-            "VULNERABILITY:VULNERABILITY:VULNERABILITY:VULNERABILITY\n"
+    eu_err( "VULNERABILITY:VULNERABILITY:VULNERABILITY:VULNERABILITY\n"
             "   Reading / initializing master sealing secret FAILED!!\n"
             "   Continuing anyways...\n"
-            "VULNERABILITY:VULNERABILITY:VULNERABILITY:VULNERABILITY\n\n",
-            __FUNCTION__);
+            "VULNERABILITY:VULNERABILITY:VULNERABILITY:VULNERABILITY\n");
   }
 
   COMPILE_TIME_ASSERT(HW_TPM_MASTER_SEALING_SECRET_SIZE == SHA1_RESULTLEN);
@@ -243,51 +226,42 @@ static int trustvisor_long_term_secret_init(void) {
   HMAC_SHA1(mss, HW_TPM_MASTER_SEALING_SECRET_SIZE,
             sealinghmac, sizeof(sealinghmac),
             aeskey_temp);
-  ASSERT(TPM_AES_KEY_LEN_BYTES <= HW_TPM_MASTER_SEALING_SECRET_SIZE);
+  EU_VERIFY( TPM_AES_KEY_LEN_BYTES <= HW_TPM_MASTER_SEALING_SECRET_SIZE);
 	
-  dprintf(LOG_TRACE, "\n[TV] Sealing AES key derived from MSS.");
-  dprintf(LOG_TRACE, "\n[TV] Sealing HMAC key derived from MSS.");
+  eu_trace( "Sealing AES key derived from MSS.");
+  eu_trace( "Sealing HMAC key derived from MSS.");
 
   /* /\* SECURITY: Delete these print_hex()'s ASAP! *\/ */
   /* print_hex("XXX mss:       ", mss, 20); */
   /* print_hex("XXX g_aeskey:  ", g_aeskey, (TPM_AES_KEY_LEN>>3)); */
   /* print_hex("XXX g_hmackey: ", g_hmackey, 20); */
-
     
   /* Initialize RSA key required in uTPM Quote */
   /* FIXME: Having a single key here is a privacy-invading,
    * session-linkable, PAL-linkable hack to get things off the
    * ground. */
-  dprintf(LOG_TRACE, "\n[TV] Generating RSA keypair...");
+  eu_trace( "Generating RSA keypair...");
   rsa_init(&rsa, RSA_PKCS_V15, RSA_SHA1);
-  if(0 != rsa_gen_key(&rsa, (TPM_RSA_KEY_LEN<<3), 65537)) {
-    dprintf(LOG_ERROR, "FAILED!!!!! System halted.");
-    /* This is unrecoverable! */
-    HALT();
-    /* If someday, somebody decides to keep things going during
-       development or debugging, don't forget that we MUST zero
-       sensitive keys upon failure. */
-    rv = 1;
-    goto cleanup;
-  }
-  dprintf(LOG_TRACE, "\n[TV] RSA key pair generated!");
 
-  if(UTPM_SUCCESS != utpm_init_master_entropy(aeskey_temp, hmackey_temp, &rsa)) {
-    dprintf(LOG_ERROR, "utpm_init_master_entropy() FAILED!!!!! System halted.");
-    HALT();
-    /* same story as above. */
-    rv = 1;
-    goto cleanup;
-  }
+  /* If someday, somebody decides to keep things going during
+     development or debugging, don't forget that we MUST zero
+     sensitive keys upon failure. */
+  EU_VERIFYN( rsa_gen_key(&rsa, (TPM_RSA_KEY_LEN<<3), 65537));
 
- cleanup:
+  eu_trace("RSA key pair generated");
+
+  /* same story as above. */
+  EU_VERIFYN( utpm_init_master_entropy(aeskey_temp, hmackey_temp, &rsa));
+
+  rv = 0;
+ /* out: */
   memset(mss, 0, HW_TPM_MASTER_SEALING_SECRET_SIZE);
   memset(aeskey_temp, 0, HW_TPM_MASTER_SEALING_SECRET_SIZE);
   memset(hmackey_temp, 0, HW_TPM_MASTER_SEALING_SECRET_SIZE);
   /* Measure the Identity key used to sign Micro-TPM Quotes. */
   /* prefer not to depend on the globals */
   if(0 != (rv = trustvisor_measure_qnd_bridge_signing_pubkey(&rsa))) {
-    dprintf(LOG_ERROR, "\n[TV] trustvisor_long_term_secret_init FAILED with rv %d!!!!\n", rv);
+    eu_err("trustvisor_long_term_secret_init FAILED with rv %d!!!!", rv);
   }
   /* Intentionally not a proper free. Responsibility for this
    * structure has been transferred to the utpm implementation. */
@@ -299,61 +273,54 @@ static int trustvisor_long_term_secret_init(void) {
 /* returns 0 on success. */
 /* TODO: take ciphertext input, e.g., from a multiboot_t */
 int trustvisor_master_crypto_init(void) {
-  int rv;
+  int rv=1;
+  bool opened_tpm=false;
 
-  if(0 != (rv = emhf_tpm_open_locality(CRYPTO_INIT_LOCALITY))) {
-    dprintf(LOG_ERROR, "\nFATAL ERROR: Could not access HW TPM.\n");
-    return rv; /* no need to deactivate */
-  }
+  EU_CHKN( rv = emhf_tpm_open_locality(CRYPTO_INIT_LOCALITY),
+           eu_err_e( "FATAL ERROR: Could not access HW TPM."));
+  opened_tpm=true;
 		
   /* PRNG */
-  if(0 != (rv = master_prng_init())) {
-    dprintf(LOG_ERROR, "\n[TV] trustvisor_master_crypto_init: "
-            "AES-256 CTR_DRBG PRNG init FAILED with rv %d!!!!\n", rv);
-    goto out;
-  }
+  EU_CHKN( rv = master_prng_init(),
+           eu_err_e( "AES-256 CTR_DRBG PRNG init FAILED with rv %d!!!!\n", rv));
 		
   g_master_prng_init_completed = true;
 		
-  dprintf(LOG_TRACE, "\n[TV] trustvisor_master_crypto_init: "
-          "AES-256 CTR_DRBG PRNG successfully seeded with TPM RNG.");
+  eu_trace( "AES-256 CTR_DRBG PRNG successfully seeded with TPM RNG.");
         
   /* NV space used to support Micro-TPM Long-Term sealing */
-  if(0 != (rv = trustvisor_long_term_secret_init())) {
-    dprintf(LOG_ERROR, "\n[TV] trustvisor_long_term_secret_init FAILED with rv %d!!!!\n", rv);
-    goto out;
-  }
+  EU_CHKN( rv = trustvisor_long_term_secret_init());
 
-  dprintf(LOG_TRACE, "\n[TV] trustvisor_master_crypto_init: "
-          "long term secrets initialized successfully.");
+  eu_trace( "long term secrets initialized successfully.");
 
   /* Confirm that the NV space that will be used for anti-rollback
      by the NvMuxPal has appropriate controls in place. */
-  if(0 != (rv = validate_trustvisor_nv_region(
-                                              TRUSTVISOR_HWTPM_NV_LOCALITY,
-                                              HW_TPM_ROLLBACK_PROT_INDEX,
-                                              HW_TPM_ROLLBACK_PROT_SIZE))) {
-    dprintf(LOG_ERROR, "\n[TV] %s: ERROR: "
-            " validate_trustvisor_nv_region(%d, 0x%08x, %d)"
-            " FAILED with rv %d\n",	__FUNCTION__,
-            TRUSTVISOR_HWTPM_NV_LOCALITY,
-            HW_TPM_ROLLBACK_PROT_INDEX,
-            HW_TPM_ROLLBACK_PROT_SIZE,
-            rv);
-    goto out;
-  }
+  EU_CHKN( rv = validate_trustvisor_nv_region( TRUSTVISOR_HWTPM_NV_LOCALITY,
+                                               HW_TPM_ROLLBACK_PROT_INDEX,
+                                               HW_TPM_ROLLBACK_PROT_SIZE),
+           eu_err_e( "ERROR: "
+                     " validate_trustvisor_nv_region(%d, 0x%08x, %d)"
+                     " FAILED with rv %d\n",
+                     TRUSTVISOR_HWTPM_NV_LOCALITY,
+                     HW_TPM_ROLLBACK_PROT_INDEX,
+                     HW_TPM_ROLLBACK_PROT_SIZE,
+                     rv),
+           rv = 0); /* XXX maintaining old behavior here. is this really what we want? */
 
-  dprintf(LOG_TRACE, "\n[TV] %s: NvMuxPal anti-rollback NV Region"
-          "\n     Access Controls validated successfully.", __FUNCTION__);
+  eu_trace( "NvMuxPal anti-rollback NV Region: "
+            "Access Controls validated successfully.");
 		
   g_master_crypto_init_completed = true;
 
   /* We're now done with the TPM for a while. Make sure it is
    * available to the legacy OS. */
+  rv=0;
  out:
-  emhf_tpm_deactivate_all_localities();
+  if (opened_tpm) {
+    emhf_tpm_deactivate_all_localities();
+  }
 		
-  return 0;
+  return rv;
 }
 
 /* Local Variables: */
