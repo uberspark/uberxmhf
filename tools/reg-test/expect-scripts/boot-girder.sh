49ea3abed0bbf86f55ba589be5e42344f376d7af
Jonathan McCune <jonmccune@cmu.edu>
2012-02-07 21:52:41 +0000

    add a bunch of documentation and girder's hostname
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4686 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tools/reg-test/expect-scripts/boot-girder.sh
+++ tools/reg-test/expect-scripts/boot-girder.sh
@@ -5,7 +5,15 @@
 # variables are set before invoking an 'expect' script that can handle
 # the interactive session with grub via a serial port.
 
-# TODO: Cope with amtterm.
+# TODO: Cope with amtterm instead of a real serial port.
+
+# Values that are actually expected to change across test hosts:
+# 1. SINIT module is chipset-dependent on Intel hosts
+# 2. ISCSI_INITIATOR should be unique per test host (currently it is 
+#    unique because of a suffix containing that test host's ethernet MAC 
+#    address)
+# 3. hostname= variable (currently ignored by test Linux environment)
+# 4. The actual kernel version itself, if we want to test multiple versions
 
 export FIRST_ROOT='root (hd0,0)'
 export FIRST_KERNEL='kernel /boot/init-x86.bin serial=115200,8n1,0x3f8'
@@ -15,7 +23,7 @@ export FIRST_MOD2='modulenounzip (hd0)+1'
 export FIRST_MOD3='module /boot/i5_i7_DUAL_SINIT_18.BIN'
 
 export SECOND_ROOT='uuid e62ba4c2-87d2-4b42-b66d-dabf9af0c68c'
-export SECOND_KERNEL='kernel /boot/vmlinuz-2.6.32.26+drm33.12emhf-jm1 root=UUID=e62ba4c2-87d2-4b42-b66d-dabf9af0c68c ro ip=dhcp ISCSI_INITIATOR=iqn.2012-02.com.nfsec:01:643150805036 ISCSI_TARGET_NAME=iqn.2012-01.com.nfsec:lucid_rootfs ISCSI_TARGET_IP=10.0.0.1 ISCSI_TARGET_PORT=3260 aufs=tmpfs'
+export SECOND_KERNEL='kernel /boot/vmlinuz-2.6.32.26+drm33.12emhf-jm1 root=UUID=e62ba4c2-87d2-4b42-b66d-dabf9af0c68c ro ip=dhcp hostname=girder ISCSI_INITIATOR=iqn.2012-02.com.nfsec:01:643150805036 ISCSI_TARGET_NAME=iqn.2012-01.com.nfsec:lucid_rootfs ISCSI_TARGET_IP=10.0.0.1 ISCSI_TARGET_PORT=3260 aufs=tmpfs'
 export SECOND_MOD1='initrd /boot/initrd.img-2.6.32.26+drm33.12emhf-jm1'
 
 ./grub-generic.exp ttyS0
