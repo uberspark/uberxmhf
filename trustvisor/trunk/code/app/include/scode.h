21b16641d3d07422377e2034405e2ae6bafa40b7
James Newsome <jnewsome@cmu.edu>
2012-02-07 22:30:49 +0000

    remove unsafe get_16bit_aligned_value_from_guest
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4694 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- trustvisor/trunk/code/app/include/scode.h
+++ trustvisor/trunk/code/app/include/scode.h
@@ -152,7 +152,6 @@ hpt_prot_t pal_prot_of_type(int type);
 hpt_prot_t reg_prot_of_type(int type);
 
 /* several help functions to access guest address space */
-u16 get_16bit_aligned_value_from_guest(const hptw_ctx_t *ctx, const hpt_pmo_t *root, u32 gvaddr);
 u32 get_32bit_aligned_value_from_guest(const hptw_ctx_t *ctx, const hpt_pmo_t *root, u32 gvaddr);
 void put_32bit_aligned_value_to_guest(const hptw_ctx_t *ctx, const hpt_pmo_t *root, u32 gvaddr, u32 value);
 
