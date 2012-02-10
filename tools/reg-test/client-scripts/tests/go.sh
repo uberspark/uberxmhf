6971509bca4653ef8648883bfe01df37050853fc
Jonathan McCune <jonmccune@cmu.edu>
2012-02-10 21:20:21 +0000

    cleanup logging of command output
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4793 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tools/reg-test/client-scripts/tests/go.sh
+++ tools/reg-test/client-scripts/tests/go.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BASEDIR=$(dirname $0)
+LOGFILE=shell.log
 
 echo "$0 initializing" >&2
 
@@ -11,28 +12,27 @@ TESTDIR=$MACADDR/$TIMESTAMP
 mkdir -p $TESTDIR
 pushd $TESTDIR
 
-echo "$0 initializing" | tee shell.log
-echo "Host identifier: $MACADDR" | tee shell.log
-echo "Time stamp: $TIMESTAMP" | tee shell.log
+echo "$0 initializing" >> $LOGFILE
+echo "Host identifier: $MACADDR" >> $LOGFILE
+echo "Time stamp: $TIMESTAMP" >> $LOGFILE
 
-echo "Fetching http://10.0.0.1/test" | tee shell.log
+echo "Fetching http://10.0.0.1/test" >> $LOGFILE
 wget http://10.0.0.1/test
 chmod +x test
-echo "Invoking ./test" | tee shell.log
-./test 2>&1 | tee shell.log
+echo "Invoking ./test" >> $LOGFILE
+./test >> $LOGFILE 2>&1
 if [ $? -eq 0 ] ; then
-    echo "./test exited successfully with exit code $?"
+    echo "./test exited successfully with exit code $?" >> $LOGFILE
 else
-    echo "./test FAILED with exit code $?"
+    echo "./test FAILED with exit code $?" >> $LOGFILE
 fi
 
 popd
-scp -o StrictHostKeyChecking=no -r $MACADDR logger@10.0.0.1: \
-  | tee shell.log
+scp -o StrictHostKeyChecking=no -r $MACADDR logger@10.0.0.1: >> $LOGFILE 2>&1
 
-echo "... Success!"
+expect alldone.exp ttyS0 >> $LOGFILE 2>&1
 
-expect alldone.exp ttyS0
+echo "$0 ... Success!" >&2
 
 exit 0
 
