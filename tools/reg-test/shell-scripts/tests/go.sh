988898eb21986266e6c36a95ff60e921cd7b1945
Jonathan McCune <jonmccune@cmu.edu>
2012-02-10 20:27:20 +0000

    Actually fetch and run a test
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4787 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tools/reg-test/shell-scripts/tests/go.sh
+++ tools/reg-test/shell-scripts/tests/go.sh
@@ -15,7 +15,12 @@ echo "$0 initializing" | tee shell.log
 echo "Host identifier: $MACADDR" | tee shell.log
 echo "Time stamp: $TIMESTAMP" | tee shell.log
 
-echo "Dummy placeholder test" | tee shell.log
+echo "Fetching http://10.0.0.1/test" | tee shell.log
+wget http://10.0.0.1/test
+chmod +x test
+echo "Invoking ./test" | tee shell.log
+./test 2>&1 | tee shell.log
+
 
 popd
 scp -o StrictHostKeyChecking=no -r $MACADDR logger@10.0.0.1: \
