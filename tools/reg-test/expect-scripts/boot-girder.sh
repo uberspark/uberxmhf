50969257db60d26515d9799183c275e80feac251
Jonathan McCune <jonmccune@cmu.edu>
2012-02-10 20:45:19 +0000

    add support for oneiric and etherwake
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4789 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tools/reg-test/expect-scripts/boot-girder.sh
+++ tools/reg-test/expect-scripts/boot-girder.sh
@@ -22,9 +22,19 @@ export FIRST_MOD2='modulenounzip (hd0)+1'
 # On an AMD host such as girder this is a dummy module and is not used
 export FIRST_MOD3='module /boot/i5_i7_DUAL_SINIT_18.BIN'
 
-export SECOND_ROOT='uuid e62ba4c2-87d2-4b42-b66d-dabf9af0c68c'
-export SECOND_KERNEL='kernel /boot/vmlinuz-2.6.32.26+drm33.12emhf-jm1 root=UUID=e62ba4c2-87d2-4b42-b66d-dabf9af0c68c ro ip=dhcp hostname=girder ISCSI_INITIATOR=iqn.2012-02.com.nfsec:01:643150805036 ISCSI_TARGET_NAME=iqn.2012-01.com.nfsec:lucid_rootfs ISCSI_TARGET_IP=10.0.0.1 ISCSI_TARGET_PORT=3260 aufs=tmpfs'
-export SECOND_MOD1='initrd /boot/initrd.img-2.6.32.26+drm33.12emhf-jm1'
+# lucid (Ubuntu 10.04 LTS)
+#export SECOND_ROOT='uuid e62ba4c2-87d2-4b42-b66d-dabf9af0c68c'
+#export SECOND_KERNEL='kernel /boot/vmlinuz-2.6.32.26+drm33.12emhf-jm1 root=UUID=e62ba4c2-87d2-4b42-b66d-dabf9af0c68c ro ip=dhcp hostname=girder ISCSI_INITIATOR=iqn.2012-02.com.nfsec:01:643150805036 ISCSI_TARGET_NAME=iqn.2012-01.com.nfsec:lucid_rootfs ISCSI_TARGET_IP=10.0.0.1 ISCSI_TARGET_PORT=3260 aufs=tmpfs'
+#export SECOND_MOD1='initrd /boot/initrd.img-2.6.32.26+drm33.12emhf-jm1'
+
+# oneiric (Ubuntu 11.10)
+export SECOND_ROOT='uuid ad60f8ff-e0ca-4294-a7d4-9c07da18365b'
+export SECOND_KERNEL='kernel /boot/vmlinuz-3.0.4-jm2 root=UUID=ad60f8ff-e0ca-4294-a7d4-9c07da18365b ro ip=dhcp hostname=girder ISCSI_INITIATOR=iqn.2012-02.com.nfsec:01:643150805036 ISCSI_TARGET_NAME=iqn.2012-01.com.nfsec:oneiric_rootfs ISCSI_TARGET_IP=10.0.0.1 ISCSI_TARGET_PORT=3260 aufs=tmpfs'
+export SECOND_MOD1='initrd /boot/initrd.img-3.0.4-jm2'
+
+# If it has been power-cycled and we want to Wake-on-Lan:
+# (and actually, if the machine is already up, this is harmless)
+etherwake 64:31:50:80:50:36
 
 ./grub-generic.exp serial ttyS0
 
