15f6d716ed6abf2ba337d60ed2e67d245f7fa3ce
Jonathan McCune <jonmccune@cmu.edu>
2012-02-15 20:43:49 +0000

    tweak script to contain only the settings pertaining to test-host test-6555b
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4831 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tools/reg-test/driver-scripts/boot-test-6555b.sh
+++ tools/reg-test/driver-scripts/boot-test-6555b.sh
@@ -1,53 +1,31 @@
 #!/bin/bash
 
-# This script contains the necessary bootloader configuration for the
-# test host 'girder' (HP ProBook 6555b laptop with AMD CPU).  These
-# variables are set before invoking an 'expect' script that can handle
-# the interactive session with grub via a serial port.
+# test host 'girder' (HP ProBook 6555b laptop with AMD CPU)
 
-# TODO: Cope with amtterm instead of a real serial port.
+TEST_SLASHBOOT=/boot
+TEST_HOSTNAME=test-6555b
+TEST_MACADDR=64:31:50:80:40:b8
 
-# Values that are actually expected to change across test hosts:
-# 1. SINIT module is chipset-dependent on Intel hosts
-# 2. ISCSI_INITIATOR should be unique per test host (currently it is 
-#    unique because of a suffix containing that test host's ethernet MAC 
-#    address)
-# 3. hostname= variable (currently ignored by test Linux environment)
-# 4. The actual kernel version itself, if we want to test multiple versions
-
-export FIRST_ROOT='root (hd0,0)'
-export FIRST_KERNEL='kernel /boot/init-x86.bin serial=115200,8n1,0x3f8'
-export FIRST_MOD1='module /boot/hypervisor-x86.bin.gz'
-export FIRST_MOD2='modulenounzip (hd0)+1'
-# On an AMD host such as girder this is a dummy module and is not used
-export FIRST_MOD3='module /boot/i5_i7_DUAL_SINIT_18.BIN'
-
-# lucid (Ubuntu 10.04 LTS)
-#export SECOND_ROOT='uuid e62ba4c2-87d2-4b42-b66d-dabf9af0c68c'
-#export SECOND_KERNEL='kernel /boot/vmlinuz-2.6.32.26+drm33.12emhf-jm1 root=UUID=e62ba4c2-87d2-4b42-b66d-dabf9af0c68c ro ip=dhcp hostname=test-6555b ISCSI_INITIATOR=iqn.2012-02.com.nfsec:01:6431508040b8 ISCSI_TARGET_NAME=iqn.2012-01.com.nfsec:lucid_rootfs ISCSI_TARGET_IP=10.0.0.1 ISCSI_TARGET_PORT=3260 aufs=tmpfs'
-#export SECOND_MOD1='initrd /boot/initrd.img-2.6.32.26+drm33.12emhf-jm1'
-
-# oneiric (Ubuntu 11.10)
-export SECOND_ROOT='uuid ad60f8ff-e0ca-4294-a7d4-9c07da18365b'
-export SECOND_KERNEL='kernel /boot/vmlinuz-3.0.4-jm2 root=UUID=ad60f8ff-e0ca-4294-a7d4-9c07da18365b ro ip=dhcp hostname=test-6555b ISCSI_INITIATOR=iqn.2012-02.com.nfsec:01:6431508040b8 ISCSI_TARGET_NAME=iqn.2012-01.com.nfsec:oneiric_rootfs ISCSI_TARGET_IP=10.0.0.1 ISCSI_TARGET_PORT=3260 aufs=tmpfs'
-export SECOND_MOD1='initrd /boot/initrd.img-3.0.4-jm2'
-
-# This machine is plugged into port 2 of the StarTech device.
-# Turn its outlet off (just in case it's on), wait 1 second, then turn it on
-echo "Powering off outlet"
-./power-control.exp ttyS0 of 2
-sleep 2
-echo "Powering on outlet"
-./power-control.exp ttyS0 on 2
-
-# If it has been power-cycled then we want to Wake-on-Lan:
-# (and actually, if the machine is already up, this is harmless)
-echo "Sending wake-on-LAN packet"
-sleep 3
-etherwake 64:31:50:80:40:b8
-
-# See 8x serial cable number <-> ttySx mapping at
+# See 8x serial cable number <-> ttySx mapping,
+# and test host <-> power outlet mapping, at
 # https://plover.pdl.cmu.local/projects/emhf/wiki/Regression_Testing_Ideas
-echo "Starting grub-generic.exp"
-./grub-generic.exp serial ttyS5
+TEST_CONNECTION_SERIAL_PORT=ttyS5
+TEST_CONNECTION_OUTLET_NUMBER=2
+
+# # This machine is plugged into port 2 of the StarTech device.
+# # Turn its outlet off (just in case it's on), wait 1 second, then turn it on
+# echo "Powering off outlet"
+# ./power-control.exp ttyS0 of $TEST_CONNECTION_OUTLET_NUMBER
+# sleep 2
+# echo "Powering on outlet"
+# ./power-control.exp ttyS0 on $TEST_CONNECTION_OUTLET_NUMBER
+
+# # If it has been power-cycled then we want to Wake-on-Lan:
+# # (and actually, if the machine is already up, this is harmless)
+# echo "Sending wake-on-LAN packet"
+# sleep 3
+# etherwake $TEST_MACADDR
+
+# echo "Starting grub-generic.exp"
+# ./grub-generic.exp $TEST_CONNECTION $TEST_CONNECTION_SERIAL_PORT
 
