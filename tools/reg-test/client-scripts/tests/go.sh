a262adc57e9477d8b31c3e41a5617c493aeeb192
Jonathan McCune <jonmccune@cmu.edu>
2012-02-16 19:58:03 +0000

    announce completion on all serial ports, not just one; change in URL from which tests are pulled
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4858 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tools/reg-test/client-scripts/tests/go.sh
+++ tools/reg-test/client-scripts/tests/go.sh
@@ -16,8 +16,8 @@ echo "$0 initializing" >> $LOGFILE
 echo "Host identifier: $MACADDR" >> $LOGFILE
 echo "Time stamp: $TIMESTAMP" >> $LOGFILE
 
-echo "Fetching http://10.0.0.1/test" >> $LOGFILE
-wget http://10.0.0.1/test
+echo "Fetching http://10.0.0.1/~driver/test" >> $LOGFILE
+wget http://10.0.0.1/~driver/test
 chmod +x test
 echo "Invoking ./test" >> $LOGFILE
 ./test >> $LOGFILE 2>&1
@@ -30,7 +30,11 @@ fi
 popd
 scp -o StrictHostKeyChecking=no -r $MACADDR logger@10.0.0.1: >> $LOGFILE 2>&1
 
-expect alldone.exp ttyS0 >> $LOGFILE 2>&1
+# In lieu of a better option, send the "all done" message to all
+# serial ports.
+for i in `dmesg | perl -ne 'if(m/(ttyS\d+)/) { print "$1\n"; }' | sort | uniq`; do 
+    expect alldone.exp $i >> $LOGFILE 2>&1
+done
 
 echo "$0 ... Success!" >&2
 
