f8a2be7d3bcf8a92b578008d119fadffc5dcd839
Jonathan McCune <jonmccune@cmu.edu>
2012-02-16 20:34:27 +0000

    this will actually run the full set of tests on each host, one after the other
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4863 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tools/reg-test/driver-scripts/sequence-tests.sh
+++ tools/reg-test/driver-scripts/sequence-tests.sh
@@ -2,6 +2,31 @@
 
 HOSTS="test-e8100 pratt test-6555b creeper"
 
+LOGDIR_ROOT=/home/logger/public_html
+
 for i in $HOSTS; do
-    echo "Host $i"
+    export TIMESTAMP=`date --rfc-3339=seconds | tr ' ' - | cut -d - -f 1,2,3,4`
+    echo "################################################################################"
+    echo "########## STARTING tests on Host $i ########"
+    echo "################################################################################"
+    HOSTLOGDIR=$LOGDIR_ROOT/$i
+    
+    if [ ! -d $HOSTLOGDIR ]; then
+        echo "FATAL ERROR: $LOGDIR does not exist!"
+        exit
+    fi
+
+    THISLOGDIR=$HOSTLOGDIR/$TIMESTAMP
+    mkdir -p $THISLOGDIR
+    chown logger:logger $THISLOGDIR
+    echo "### Log directory: $THISLOGDIR"
+    echo "### TIMESTAMP:     $TIMESTAMP"
+
+    ./boot-test.sh $i 2>&1 | tee $THISLOGDIR/boot-test.log
+
+    echo "################################################################################"
+    echo "########## COMPLETED tests on Host $i ########"
+    echo "################################################################################"
+    echo ""
+    sleep 1
 done
