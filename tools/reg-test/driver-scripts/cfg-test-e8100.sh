b0b488cdfee3b70e81bd2b5b9ecbce0fdee53d48
Jonathan McCune <jonmccune@cmu.edu>
2012-02-16 14:56:42 +0000

    host test-e8100 uses amtterm for everything; add 8250.nr_uarts=0 to kernel command line so that Linux will not reinitialize the AMT serial port (which causes amtterm to die)
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4848 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tools/reg-test/driver-scripts/cfg-test-e8100.sh
+++ tools/reg-test/driver-scripts/cfg-test-e8100.sh
@@ -5,3 +5,9 @@ TEST_MACADDR=78:ac:c0:b1:83:aa
 TEST_CONNECTION=amtterm
 SERIAL_ADDR=0x1188
 BOOT_ROOT_UUID=c2a09e3b-08ba-409a-a896-bac38e8a8347
+
+# Setting nr_uarts=0 prevents the Linux kernel from doing any serial
+# port initialization.  The serial ports are thus exclusively
+# available to the hypervisor and for PAL debug output.  We do this to
+# increase the reliability of amtterm.
+ADDL_KERNEL_PARAM=8250.nr_uarts=0
