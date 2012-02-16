54a67f61c6cde9b0d736d01f6e93d436b8b2b538
Jonathan McCune <jonmccune@cmu.edu>
2012-02-16 16:05:05 +0000

    learned how to use expect_after to detect eof and re-launch amtterm From the comments now in grub-generic.exp:
     High tendency for amtterm to crap out in the second while loop below
     (which looks for serial output after Linux is booted on top of
     EMHF); it's related to the the linux kernel initializing serial
     ports for its own use. There are two remediation options:
    
     1. Use linux kernel command line option 8250.nr_uarts=0 to
        completely disable the Linux kernel's support for serial I/O.
    
     2. Detect the "eof" that is produced when amtterm dies, and
        automatically restart it. "expect_after" is perfect for this,
        since it is applied to all subsecquent expect blocks if they fail
        to match anything.  Thus, we don't need to litter eof detection
        conditions throughout this entire script..
    
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@4850 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- tools/reg-test/driver-scripts/cfg-test-e8100.sh
+++ tools/reg-test/driver-scripts/cfg-test-e8100.sh
@@ -6,8 +6,12 @@ TEST_CONNECTION=amtterm
 SERIAL_ADDR=0x1188
 BOOT_ROOT_UUID=c2a09e3b-08ba-409a-a896-bac38e8a8347
 
+# The nr_uarts=0 work-around below is DISABLED because
+# grub-generic.exp has been taught how to detect the death of amtterm
+# and automatically restart it.
+
 # Setting nr_uarts=0 prevents the Linux kernel from doing any serial
 # port initialization.  The serial ports are thus exclusively
 # available to the hypervisor and for PAL debug output.  We do this to
 # increase the reliability of amtterm.
-ADDL_KERNEL_PARAM=8250.nr_uarts=0
+#ADDL_KERNEL_PARAM=8250.nr_uarts=0
