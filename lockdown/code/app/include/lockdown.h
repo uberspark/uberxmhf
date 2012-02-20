67c403dd691722a92a4af613b019316b3a3eeb72
Amit Vasudevan <amitvasudevan@acm.org>
2012-02-20 15:40:29 +0000

    hyperpartitioning: seperate out hp6555b and hp8540p configuration definitions. add extended partition control sectors to LDN_ALLOWED_SECTORS
    git-svn-id: https://sparrow.ece.cmu.edu/svn/amit/projects/lockdown@1773 f23f674a-de99-4fcc-816c-cbdf24235276

--- lockdown/code/app/include/lockdown.h
+++ lockdown/code/app/include/lockdown.h
@@ -16,17 +16,25 @@
 //#define LDN_MACHINE_NETWORKDEVICES {1,0,0},{2,0,0}
 //#define LDN_OUTOFBOUNDS_CHECK	(((u64)lbaaddr >= (u64)LDN_ENV_TRUSTED_STARTSECTOR) && ((u64)lbaaddr <= (u64)LDN_ENV_TRUSTED_ENDSECTOR)) || ((u64)lbaaddr < 63ULL) 
 
-//specific defines for HP 8540p demo laptop
-
-//hyper-partitioning configuration (specific to the platform/installation)
+/*//hyper-partitioning configuration (specific to the platform/installation)
+//hp6555b
 #define LDN_ENV_TRUSTED_STARTSECTOR  	(159396993)
 #define LDN_ENV_TRUSTED_ENDSECTOR  		(222291404)
 #define LDN_NULL_SECTOR  				(620000000)
-//#define LDN_IDE_BUS   					0x6010
-#define LDN_IDE_BUS   					0x1F0
+#define LDN_IDE_BUS   					0x6010
 #define LDN_ALLOWED_SECTORS 			159396930
-//#define LDN_OUTOFBOUNDS_CHECK			(((u64)lbaaddr >= (u64)LDN_ENV_TRUSTED_STARTSECTOR) && ((u64)lbaaddr <= (u64)LDN_ENV_TRUSTED_ENDSECTOR)) || ((u64)lbaaddr < 63ULL) || ((u64)lbaaddr >= 63ULL && (u64)lbaaddr <= 33554494ULL)
-#define LDN_OUTOFBOUNDS_CHECK			(((u64)lbaaddr >= (u64)LDN_ENV_TRUSTED_STARTSECTOR) && ((u64)lbaaddr <= (u64)LDN_ENV_TRUSTED_ENDSECTOR)) || ((u64)lbaaddr < 63ULL) 
+#define LDN_OUTOFBOUNDS_CHECK			(((u64)lbaaddr >= (u64)LDN_ENV_TRUSTED_STARTSECTOR) && ((u64)lbaaddr <= (u64)LDN_ENV_TRUSTED_ENDSECTOR)) || ((u64)lbaaddr < 63ULL) || ((u64)lbaaddr >= 63ULL && (u64)lbaaddr <= 33554494ULL)
+*/
+
+
+//hyper-partitioning configuration (specific to the platform/installation)
+//hp8450p
+#define LDN_ENV_TRUSTED_STARTSECTOR  	(159396993)
+#define LDN_ENV_TRUSTED_ENDSECTOR  		(222291404)
+#define LDN_NULL_SECTOR  				(620000000)
+#define LDN_IDE_BUS   					0x1F0
+#define LDN_ALLOWED_SECTORS 			33554495, 96470325, 159396991
+#define LDN_OUTOFBOUNDS_CHECK			(((u64)lbaaddr >= (u64)LDN_ENV_TRUSTED_STARTSECTOR) && ((u64)lbaaddr <= (u64)LDN_ENV_TRUSTED_ENDSECTOR)) || ((u64)lbaaddr < 63ULL) || ((u64)lbaaddr >= 63ULL && (u64)lbaaddr <= 33554494ULL)
 
 #define LDN_MACHINE_NETWORKDEVICES {0x44,0,0},{0x46,0x06,0x00}
 
