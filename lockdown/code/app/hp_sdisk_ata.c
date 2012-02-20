71c467cf926856f031cb29aca1143dd211baef26
Amit Vasudevan <amitvasudevan@acm.org>
2012-02-20 15:40:17 +0000

    hyperpartitioning: be a little verbose about what ATA commands are being handled
    git-svn-id: https://sparrow.ece.cmu.edu/svn/amit/projects/lockdown@1767 f23f674a-de99-4fcc-816c-cbdf24235276

--- lockdown/code/app/hp_sdisk_ata.c
+++ lockdown/code/app/hp_sdisk_ata.c
@@ -168,10 +168,10 @@ u32 hp(VCPU *vcpu, struct regs *r, u32 portnum, u32 access_type, u32 access_size
 					ata_lbamid_buf[1], ata_lbalow_buf[1]);
 
 				//[DBG]
-				//printf("\nATA R/W DMA EXT: 0x%02x (count=%02x%02x, lba=%02x%02x%02x%02x%02x%02x)", 
-				//command, ata_sector_count_buf[0], ata_sector_count_buf[1],
-				//	ata_lbahigh_buf[0], ata_lbamid_buf[0], ata_lbalow_buf[0],
-				//	ata_lbahigh_buf[1], ata_lbamid_buf[1], ata_lbalow_buf[1]);
+				printf("\nATA R/W DMA EXT: 0x%02x (count=%02x%02x, lba=%02x%02x%02x%02x%02x%02x)", 
+				command, ata_sector_count_buf[0], ata_sector_count_buf[1],
+					ata_lbahigh_buf[0], ata_lbamid_buf[0], ata_lbalow_buf[0],
+					ata_lbahigh_buf[1], ata_lbamid_buf[1], ata_lbalow_buf[1]);
 
 				//check if we are out of bounds
 				if(check_if_LBA_outofbounds(lba48addr)){
@@ -216,8 +216,8 @@ u32 hp(VCPU *vcpu, struct regs *r, u32 portnum, u32 access_type, u32 access_size
 				lba28addr = LBA28_TO_CPU32(t3, t2, t1, t0);
 
 				//[DBG]
-				//printf("\nATA R/W DMA: 0x%02x (count=%02x, lba=(%02x%02x%02x%02x)", 
-				//command, count, t3, t2, t1, t0);
+				printf("\nATA R/W DMA: 0x%02x (count=%02x, lba=(%02x%02x%02x%02x)", 
+				command, count, t3, t2, t1, t0);
 				
 				//check if LBA is out of bounds
 				if(check_if_LBA_outofbounds((u64)lba28addr)){
@@ -246,7 +246,7 @@ u32 hp(VCPU *vcpu, struct regs *r, u32 portnum, u32 access_type, u32 access_size
 				ata_lbahigh_buf[0] = ata_lbahigh_buf[1] = 0;
 				
 			}else {
-				//printf("\nATA command: 0x%02x",	command);
+				printf("\nATA command: 0x%02x",	command);
 			}
 			
 			ata_sector_count_index=0;
