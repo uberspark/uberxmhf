3d458e7744e08ae46c80149d42373fa65862b333
Amit Vasudevan <amitvasudevan@acm.org>
2012-02-20 15:40:15 +0000

    hyperpartitioning: blacklist bootpartition range to check if hyperpartitioning is working correctly...
    git-svn-id: https://sparrow.ece.cmu.edu/svn/amit/projects/lockdown@1766 f23f674a-de99-4fcc-816c-cbdf24235276

--- lockdown/code/app/include/lockdown.h
+++ lockdown/code/app/include/lockdown.h
@@ -24,8 +24,8 @@
 #define LDN_NULL_SECTOR  				(620000000)
 #define LDN_IDE_BUS   					0x6010
 #define LDN_ALLOWED_SECTORS 			159396930
-#define LDN_OUTOFBOUNDS_CHECK			(((u64)lbaaddr >= (u64)LDN_ENV_TRUSTED_STARTSECTOR) && ((u64)lbaaddr <= (u64)LDN_ENV_TRUSTED_ENDSECTOR)) || ((u64)lbaaddr < 63ULL) || ((u64)lbaaddr >= 63ULL && (u64)lbaaddr <= 33554494ULL)
-
+//#define LDN_OUTOFBOUNDS_CHECK			(((u64)lbaaddr >= (u64)LDN_ENV_TRUSTED_STARTSECTOR) && ((u64)lbaaddr <= (u64)LDN_ENV_TRUSTED_ENDSECTOR)) || ((u64)lbaaddr < 63ULL) || ((u64)lbaaddr >= 63ULL && (u64)lbaaddr <= 33554494ULL)
+#define LDN_OUTOFBOUNDS_CHECK			(((u64)lbaaddr >= (u64)LDN_ENV_TRUSTED_STARTSECTOR) && ((u64)lbaaddr <= (u64)LDN_ENV_TRUSTED_ENDSECTOR)) || ((u64)lbaaddr < 63ULL) 
 
 #define LDN_MACHINE_NETWORKDEVICES {0x44,0,0},{0x46,0x06,0x00}
 
