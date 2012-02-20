c5fed68308c15f39b4eb3af126c6c80dd619de11
Amit Vasudevan <amitvasudevan@acm.org>
2012-02-20 15:40:23 +0000

    hyperpartitioning: add (OOB) suffix to debug messages indicating partition out-of-bound disk accessess
    git-svn-id: https://sparrow.ece.cmu.edu/svn/amit/projects/lockdown@1770 f23f674a-de99-4fcc-816c-cbdf24235276

--- lockdown/code/app/hp_sdisk_ata.c
+++ lockdown/code/app/hp_sdisk_ata.c
@@ -182,7 +182,7 @@ u32 hp(VCPU *vcpu, struct regs *r, u32 portnum, u32 access_type, u32 access_size
 
 				//check if we are out of bounds
 				if(check_if_LBA_outofbounds(lba48addr)){
-					printf("\nATA R/W DMA EXT: 0x%02x (count=%02x%02x, lba=%02x%02x%02x%02x%02x%02x)", 
+					printf("\nATA R/W DMA EXT (OOB): 0x%02x (count=%02x%02x, lba=%02x%02x%02x%02x%02x%02x)", 
 						command, ata_sector_count_buf[0], ata_sector_count_buf[1],
 						ata_lbahigh_buf[0], ata_lbamid_buf[0], ata_lbalow_buf[0],
 						ata_lbahigh_buf[1], ata_lbamid_buf[1], ata_lbalow_buf[1]);
@@ -228,7 +228,7 @@ u32 hp(VCPU *vcpu, struct regs *r, u32 portnum, u32 access_type, u32 access_size
 				
 				//check if LBA is out of bounds
 				if(check_if_LBA_outofbounds((u64)lba28addr)){
-					printf("\nATA R/W DMA: 0x%02x (count=%02x, lba=(%02x%02x%02x%02x)", 
+					printf("\nATA R/W DMA (OOB): 0x%02x (count=%02x, lba=(%02x%02x%02x%02x)", 
 					command, count, t3, t2, t1, t0);
 					//convert the access to a "null" sector
 					CPU32_TO_LBA28((u32)LDN_NULL_SECTOR, &t3, &t2, &t1, &t0);
