006758062ed2066dafff4327716ac85fe521879d
Amit Vasudevan <amitvasudevan@acm.org>
2012-02-20 15:40:25 +0000

    hyperpartitioning: print 28 or 48-bit LBA addresses for R/W accesses
    git-svn-id: https://sparrow.ece.cmu.edu/svn/amit/projects/lockdown@1771 f23f674a-de99-4fcc-816c-cbdf24235276

--- lockdown/code/app/hp_sdisk_ata.c
+++ lockdown/code/app/hp_sdisk_ata.c
@@ -175,10 +175,9 @@ u32 hp(VCPU *vcpu, struct regs *r, u32 portnum, u32 access_type, u32 access_size
 					ata_lbamid_buf[1], ata_lbalow_buf[1]);
 
 				//[DBG]
-				printf("\nATA R/W DMA EXT: 0x%02x (count=%02x%02x, lba=%02x%02x%02x%02x%02x%02x)", 
+				printf("\nATA R/W DMA EXT: 0x%02x (count=%02x%02x, lba=%u)", 
 				command, ata_sector_count_buf[0], ata_sector_count_buf[1],
-					ata_lbahigh_buf[0], ata_lbamid_buf[0], ata_lbalow_buf[0],
-					ata_lbahigh_buf[1], ata_lbamid_buf[1], ata_lbalow_buf[1]);
+					(u32)lba48addr);
 
 				//check if we are out of bounds
 				if(check_if_LBA_outofbounds(lba48addr)){
@@ -223,8 +222,8 @@ u32 hp(VCPU *vcpu, struct regs *r, u32 portnum, u32 access_type, u32 access_size
 				lba28addr = LBA28_TO_CPU32(t3, t2, t1, t0);
 
 				//[DBG]
-				printf("\nATA R/W DMA: 0x%02x (count=%02x, lba=(%02x%02x%02x%02x)", 
-				command, count, t3, t2, t1, t0);
+				printf("\nATA R/W DMA: 0x%02x (count=%02x, lba=%u", 
+				command, count, lba28addr);
 				
 				//check if LBA is out of bounds
 				if(check_if_LBA_outofbounds((u64)lba28addr)){
