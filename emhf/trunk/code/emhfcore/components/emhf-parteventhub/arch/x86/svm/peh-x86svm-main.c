0bd017f5f7a291d160363216b836ad1ff85374ac
Amit Vasudevan <amitvasudevan@acm.org>
2012-03-01 15:49:35 +0000

    emhf-parteventhub: get rid of unused variable "gva"
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@5057 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/emhfcore/components/emhf-parteventhub/arch/x86/svm/peh-x86svm-main.c
+++ emhf/trunk/code/emhfcore/components/emhf-parteventhub/arch/x86/svm/peh-x86svm-main.c
@@ -136,7 +136,6 @@ static void _svm_handle_npf(VCPU *vcpu, struct regs *r){
     emhf_smpguest_arch_x86_eventhandler_hwpgtblviolation(vcpu, gpa, errorcode);
     //HALT();
   } else {
-	u32 gva;
 	//note: AMD does not provide guest virtual address on a #NPF
 	//so we pass zero always
 	// call EMHF app hook
