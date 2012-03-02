698696a51178e9b5ba778f2463561d27c5d635b2
Amit Vasudevan <amitvasudevan@acm.org>
2012-03-02 18:28:13 +0000

    emhf-runtime.h: external decl. of array "with" exact size causes CBMC to allocate/track the entire memory region allocated to the array which causes it to die; so we just extern the decl. "without" size.
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@5082 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/emhfcore/include/emhf-runtime.h
+++ emhf/trunk/code/emhfcore/include/emhf-runtime.h
@@ -54,9 +54,7 @@ extern RPB arch_rpb;
 extern RPB *rpb __attribute__(( section(".data") )); 
 
 //runtime DMA protection buffer
-extern u8 g_rntm_dmaprot_buffer[(PAGE_SIZE_4K + (PAGE_SIZE_4K * PAE_PTRS_PER_PDPT) 
-					+ (PAGE_SIZE_4K * PAE_PTRS_PER_PDPT * PAE_PTRS_PER_PDT) + PAGE_SIZE_4K +
-					(PAGE_SIZE_4K * PCI_BUS_MAX))] __attribute__(( section(".palign_data") ));
+extern u8 g_rntm_dmaprot_buffer[] __attribute__(( section(".palign_data") ));
 
 //variable that is incremented by 1 by all cores that cycle through appmain
 //successfully, this should be finally equal to g_midtable_numentries at
