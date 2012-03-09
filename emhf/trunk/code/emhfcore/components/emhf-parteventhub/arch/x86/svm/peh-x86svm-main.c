7b75bbf5d910cd15a22f758ca93d10105353de19
Amit Vasudevan <amitvasudevan@acm.org>
2012-03-09 16:04:30 +0000

    emhf-parteventhub(verif.): CBMC reports successful verification with bare minimum partition event hub
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@5165 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/emhfcore/components/emhf-parteventhub/arch/x86/svm/peh-x86svm-main.c
+++ emhf/trunk/code/emhfcore/components/emhf-parteventhub/arch/x86/svm/peh-x86svm-main.c
@@ -292,7 +292,6 @@ u32 emhf_parteventhub_arch_x86svm_intercept_handler(VCPU *vcpu, struct regs *r){
   
   vmcb->tlb_control = TLB_CONTROL_NOTHING;
 
-#if 0
   //SVM stores guest EAX in VMCB, so move that into struct regs r->eax 
   //to reflect true guest EAX value
   //r->eax = (u32)vmcb->rax;
@@ -377,8 +376,6 @@ u32 emhf_parteventhub_arch_x86svm_intercept_handler(VCPU *vcpu, struct regs *r){
 	//SVM stores guest EAX in VMCB, so move struct regs r->eax 
 	//to reflect true guest EAX value
 	//vmcb->rax = r->eax;
-
-#endif
 
 	return 0;
 }
