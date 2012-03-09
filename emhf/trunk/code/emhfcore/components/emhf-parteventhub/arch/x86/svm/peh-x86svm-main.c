e97608bbcd65be7d0f8060077446e91b9cf5a03d
Amit Vasudevan <amitvasudevan@acm.org>
2012-03-09 16:06:53 +0000

    emhfcore(verif.): including dbexception intercept logic in hub; CBMC reports error in dereferencing
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@5211 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/emhfcore/components/emhf-parteventhub/arch/x86/svm/peh-x86svm-main.c
+++ emhf/trunk/code/emhfcore/components/emhf-parteventhub/arch/x86/svm/peh-x86svm-main.c
@@ -374,6 +374,8 @@ u32 emhf_parteventhub_arch_x86svm_intercept_handler(VCPU *vcpu, struct regs *r){
 		}
 		break;
 
+#endif
+
 		case VMEXIT_EXCEPTION_DB:{
 			if(vcpu->isbsp == 1){											//LAPIC SIPI detection only happens on BSP
 				emhf_smpguest_arch_x86_eventhandler_dbexception(vcpu, r);
@@ -385,7 +387,6 @@ u32 emhf_parteventhub_arch_x86svm_intercept_handler(VCPU *vcpu, struct regs *r){
 		}
 		break;
 
-#endif
 
 		case VMEXIT_INIT:{
 		printf("\nCPU(0x%02x): INIT intercepted, halting.", vcpu->id);
