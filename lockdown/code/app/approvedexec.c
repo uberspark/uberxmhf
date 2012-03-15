42b5fdc6ae5d12e50f07d61d33c092e580873a0f
Amit Vasudevan <amitvasudevan@acm.org>
2012-03-15 18:14:16 +0000

    approved execution: get rid of debug verbosity
    git-svn-id: https://sparrow.ece.cmu.edu/svn/amit/projects/lockdown@1836 f23f674a-de99-4fcc-816c-cbdf24235276

--- lockdown/code/app/approvedexec.c
+++ lockdown/code/app/approvedexec.c
@@ -177,15 +177,15 @@ u32 approvedexec_handleevent(VCPU *vcpu, struct regs *r,
 
   }else{
     //we have a write fault, check if it is cmd on same page
-    printf("\n%s: CPU(0x%02x) PF-W, p=0x%08x, v=0x%08x, pcp=0x%08x, pcv=0x%08x",
-		__FUNCTION__, vcpu->id, (u32)gpa, (u32)gva, 
-		approvedexec_getguestpcpaddr(vcpu), 
-		approvedexec_getguestpcvaddr(vcpu));
+    //printf("\n%s: CPU(0x%02x) PF-W, p=0x%08x, v=0x%08x, pcp=0x%08x, pcv=0x%08x",
+		//__FUNCTION__, vcpu->id, (u32)gpa, (u32)gva, 
+		//approvedexec_getguestpcpaddr(vcpu), 
+		//approvedexec_getguestpcvaddr(vcpu));
 
     if(approvedexec_iscmdonsamepage(vcpu, gpa, gva)){
       //TODO: we will need to single-step or emulate instructions on this page  
       //for now give all permissions and move on...
-      printf("\n  CPU(0x%02x): C-M-D on same page", vcpu->id);
+      //printf("\n  CPU(0x%02x): C-M-D on same page", vcpu->id);
       emhf_memprot_setprot(vcpu, gpa, MEMP_PROT_PRESENT | MEMP_PROT_READWRITE | MEMP_PROT_EXECUTE);
     }else{
       //make page read-write and remove execute permission
