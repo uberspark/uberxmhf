4226db2fcac2854db2a12157aeb6ba4556a65d29
Amit Vasudevan <amitvasudevan@acm.org>
2012-03-15 18:14:12 +0000

    debugging approved execution: start with handling on NX faults
    git-svn-id: https://sparrow.ece.cmu.edu/svn/amit/projects/lockdown@1834 f23f674a-de99-4fcc-816c-cbdf24235276

--- lockdown/code/app/approvedexec.c
+++ lockdown/code/app/approvedexec.c
@@ -155,10 +155,10 @@ u32 approvedexec_handleevent(VCPU *vcpu, struct regs *r,
   u64 gpa, u64 gva, u64 violationcode){
   (void)r;
 
-  printf("\n%s: CPU(0x%02x) PF, p=0x%08x, v=0x%08x, pcp=0x%08x, pcv=0x%08x",
-	__FUNCTION__, vcpu->id, (u32)gpa, (u32)gva, 
-	approvedexec_getguestpcpaddr(vcpu), 
-	approvedexec_getguestpcvaddr(vcpu));
+  //printf("\n%s: CPU(0x%02x) PF, p=0x%08x, v=0x%08x, pcp=0x%08x, pcv=0x%08x",
+	//__FUNCTION__, vcpu->id, (u32)gpa, (u32)gva, 
+	//approvedexec_getguestpcpaddr(vcpu), 
+	//approvedexec_getguestpcvaddr(vcpu));
 
   
   if(ispfduetoexec(vcpu, violationcode)){
@@ -171,7 +171,7 @@ u32 approvedexec_handleevent(VCPU *vcpu, struct regs *r,
     //verify integrity of code page
     //windows_verifycodeintegrity(vcpu, (u32)gpa, (u32)gva);
     //give page execute permissions but prevent further writes
-    emhf_memprot_setprot(vcpu, gpa, MEMP_PROT_PRESENT | MEMP_PROT_READONLY | MEMP_PROT_EXECUTE);
+    emhf_memprot_setprot(vcpu, gpa, MEMP_PROT_PRESENT | MEMP_PROT_READWRITE | MEMP_PROT_EXECUTE);
   }else{
     //we have a write fault, check if it is cmd on same page
     printf("\n%s: CPU(0x%02x) PF-W, p=0x%08x, v=0x%08x, pcp=0x%08x, pcv=0x%08x",
