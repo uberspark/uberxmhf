484c408587e9dfdd56c6a923d15331990c8d31b7
James Newsome <jnewsome@cmu.edu>
2012-03-27 19:42:34 +0000

    header file for trustvisor's emhf callbacks. Another app that wants to use the trustvisor lib should call these functions.
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@5387 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- trustvisor/trunk/code/app/include/tv_emhf.h
+++ trustvisor/trunk/code/app/include/tv_emhf.h
@@ -33,32 +33,17 @@
  * @XMHF_LICENSE_HEADER_END@
  */
 
+#ifndef TV_EMHF_H
+#define TV_EMHF_H
+
 #include <emhf.h>
-#include <tv_emhf.h>
 
-u32 emhf_app_main(VCPU *vcpu, APP_PARAM_BLOCK *apb)
-{
-  return tv_app_main(vcpu, apb);
-}
+u32 tv_app_main(VCPU *vcpu, APP_PARAM_BLOCK *apb);
+u32 tv_app_handlehypercall(VCPU *vcpu, struct regs *r);
+u32 tv_app_handleintercept_hwpgtblviolation(VCPU *vcpu,
+                                            struct regs *r, u64 gpa, u64 gva, u64 violationcode);
+u32 tv_app_handleintercept_portaccess(VCPU *vcpu, struct regs *r, 
+                                      u32 portnum, u32 access_type, u32 access_size);
+void tv_app_handleshutdown(VCPU *vcpu, struct regs *r);
 
-u32 emhf_app_handlehypercall(VCPU *vcpu, struct regs *r)
-{
-  return tv_app_handlehypercall(vcpu, r);
-}
-
-u32 emhf_app_handleintercept_hwpgtblviolation(VCPU *vcpu,
-                                              struct regs *r, u64 gpa, u64 gva, u64 violationcode)
-{
-  return tv_app_handleintercept_hwpgtblviolation(vcpu, r, gpa, gva, violationcode);
-}
-
-u32 emhf_app_handleintercept_portaccess(VCPU *vcpu, struct regs *r, 
-                                        u32 portnum, u32 access_type, u32 access_size)
-{
-  return tv_app_handleintercept_portaccess(vcpu, r, portnum, access_type, access_size);
-}
-
-void emhf_app_handleshutdown(VCPU *vcpu, struct regs *r)
-{
-  tv_app_handleshutdown(vcpu, r);
-}
+#endif
