bc68aca03e317f66391610442586c81b9639b623
Amit Vasudevan <amitvasudevan@acm.org>
2012-03-28 18:30:36 +0000

    genldnpb: minor cleanup and commenting
    git-svn-id: https://sparrow.ece.cmu.edu/svn/amit/projects/lockdown@1897 f23f674a-de99-4fcc-816c-cbdf24235276

--- lockdown/code/guestos/windows/genldnpb/generate-ldnpb.pl
+++ lockdown/code/guestos/windows/genldnpb/generate-ldnpb.pl
@@ -8,15 +8,19 @@
 use lib '..';
 use bignum;
 
+# if we are called with insufficient command line parameters, say
+# what we should be called with
 if ($#ARGV != 1 ) {
 	print "usage: generate-ldnpb.pl full_hashlist partial_hashlist\n";
 	exit;
 }
 
-
+# grab the full and partial hash list file names
 $full_hashlist=$ARGV[0];
 $partial_hashlist=$ARGV[1];
 
+# get the total number of full and partial code page hashes
+# we just use wc for this
 $full_hashlist_count = `wc -l < $full_hashlist`;
 die "wc failed: $?" if $?;
 chomp($full_hashlist_count);
@@ -25,24 +29,22 @@ $partial_hashlist_count = `wc -l < $partial_hashlist`;
 die "wc failed: $?" if $?;
 chomp($partial_hashlist_count);
 
+# dump out some info on the full and partial hash list files
 print "full hash list file: $full_hashlist, $full_hashlist_count hashes\n";
 print "partial hash list file: $partial_hashlist, $partial_hashlist_count hashes\n";
 
+# our TE parameter block binary blob file name
 $ldnpbfilename = "ldnpb_te.bin";
 
-$full_hashlist_count = `wc -l < $full_hashlist`;
-die "wc failed: $?" if $?;
-chomp($full_hashlist_count);
-
-
+# open our TE parameter block file
 open LDNPBBIN, ">", $ldnpbfilename
 or die "\nCan't open $ldnpbfilename for writing: $!\n";
+
+# switch to binary mode
 binmode( LDNPBBIN );
 
-#print( LDNPBBIN pack("H8", "c8403020") );
 
-
-# open full hashlist file and iterate through all the hashes there
+# open full hash list file and iterate through all the hashes there
 $full_hashlist_totalelements = 0;
 
 open FHASHFULL, $full_hashlist
