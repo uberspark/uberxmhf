bb03d4df6331c190954c0da99f69bd24f9379abc
Amit Vasudevan <amitvasudevan@acm.org>
2012-03-28 18:30:05 +0000

    script to generate Lockdown parameter block; for the trusted environment this is a signature followed by the full and partial code page hashes that are passed to the Lockdown hypervisor at boot
    git-svn-id: https://sparrow.ece.cmu.edu/svn/amit/projects/lockdown@1883 f23f674a-de99-4fcc-816c-cbdf24235276

---
+++ lockdown/code/guestos/windows/genldnpb/generate-ldnpb.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+# generate-ldnpb.pl
+# author(s): amit vasudevan (amitvasudevan@acm.org) 
+# this script will generate Lockdown parameter block for the 
+# trusted environment (TE) which includes the TE signature and full and
+# partial code page hashes
+
