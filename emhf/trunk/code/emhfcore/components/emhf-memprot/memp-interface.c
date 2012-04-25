e69d6983552ddbbee3614ae553c6d6705430ae72
Amit Vasudevan <amitvasudevan@acm.org>
2012-04-25 15:46:43 +0000

    remove emhf verification: emhf-memprot: remove function contracts from emhf_memprot_setprot
    git-svn-id: https://sparrow.ece.cmu.edu/svn/hyp@5595 1c601a1c-44f4-4d93-90ac-4b632a93592e

--- emhf/trunk/code/emhfcore/components/emhf-memprot/memp-interface.c
+++ emhf/trunk/code/emhfcore/components/emhf-memprot/memp-interface.c
@@ -78,21 +78,6 @@ void emhf_memprot_flushmappings(VCPU *vcpu){
 
 //set protection for a given physical memory address
 void emhf_memprot_setprot(VCPU *vcpu, u64 gpa, u32 prottype){
-	EV_FNCONTRACT_DOMAIN ( (vcpu != NULL) );
-	EV_FNCONTRACT_DOMAIN ( ( (gpa < rpb->XtVmmRuntimePhysBase) || 
-							 (gpa >= (rpb->XtVmmRuntimePhysBase + rpb->XtVmmRuntimeSize)) 
-						   ) );
-	EV_FNCONTRACT_DOMAIN ( ( (prottype > 0)	&& 
-	                         (prottype <= MEMP_PROT_MAXVALUE) 
-	                       ) );						
-	EV_FNCONTRACT_DOMAIN(
-	 (prottype == MEMP_PROT_NOTPRESENT) ||
-	 ((prottype & MEMP_PROT_PRESENT) && (prottype & MEMP_PROT_READONLY) && (prottype & MEMP_PROT_EXECUTE)) ||
-	 ((prottype & MEMP_PROT_PRESENT) && (prottype & MEMP_PROT_READWRITE) && (prottype & MEMP_PROT_EXECUTE)) ||
-	 ((prottype & MEMP_PROT_PRESENT) && (prottype & MEMP_PROT_READONLY) && (prottype & MEMP_PROT_NOEXECUTE)) ||
-	 ((prottype & MEMP_PROT_PRESENT) && (prottype & MEMP_PROT_READWRITE) && (prottype & MEMP_PROT_NOEXECUTE)) 
-	);
-
 	emhf_memprot_arch_setprot(vcpu, gpa, prottype);
 }
 
