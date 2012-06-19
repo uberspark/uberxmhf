7979f1fc71f57bcb2fd18598d28f116f9f558aa8
Jonathan McCune <jonmccune@cmu.edu>
2012-06-19 16:33:13 -0400

    relicense code Copyright CMU / VDG to BSD-style in emhf/.../emhfcore

--- emhf/trunk/code/emhfcore/components/emhf-baseplatform/arch/x86/vmx/bplt-x86vmx.c
+++ emhf/trunk/code/emhfcore/components/emhf-baseplatform/arch/x86/vmx/bplt-x86vmx.c
@@ -11,10 +11,21 @@
  *               VDG Inc.
  *               http://xmhf.org
  *
- * This file is part of the EMHF historical reference
- * codebase, and is released under the terms of the
- * GNU General Public License (GPL) version 2.
- * Please see the LICENSE file for details.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * Neither the names of Carnegie Mellon or VDG Inc, nor the names of
+ * its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
  * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
@@ -66,5 +77,3 @@ void emhf_baseplatform_arch_x86vmx_cpuinitialize(void){
         }
         restore_mtrrs(&(os_mle_data->saved_mtrr_state));
 }
-
-
