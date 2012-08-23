23913eb24e9a00fd27daa4bb4fa6986926981c07
Jonathan McCune <jonmccune@cmu.edu>
2012-08-23 15:58:17 -0400

    only operate on a single test host's /boot partition, but copy multiple files there

--- tools/reg-test/driver-scripts/initialize_slashboot.sh
+++ tools/reg-test/driver-scripts/initialize_slashboot.sh
@@ -2,13 +2,24 @@
 
 set -e
 
-# This script copies one file to all of the different /boot partitions for test hosts
+# This script copies files to the LVM /boot partition for the
+# specified test host
+
+if [ -z "$1" -o -z "$2" ] ; then
+    echo "Usage: $0 hostname file1 file2 .. filen" 1>&2
+    exit 1
+fi
+
 MAPPER_MOUNTS=`mount | grep mapper | wc -l`
 
 if [ $MAPPER_MOUNTS -gt 0 ]; then
     echo "ERROR: Looks like some LVM filesystems are already mounted" 1>&2
     exit 1
 fi
+
+TESTHOSTNAME="$1"
+# Drop hostname from arglist
+shift
 
 for file in "$@"; do
     if [ ! -e ${file} ]; then
@@ -20,29 +31,12 @@ done
 TARGET=/mnt
 
 echo "$0: Source files exist. Here goes!"
-for i in /dev/vg0/iscsi.slashboot.* ; do 
-    echo "*** Copying $1 to $i ***"
+$DRYRUN losetup /dev/loop0 /dev/vg0/iscsi.slashboot.$TESTHOSTNAME
+$DRYRUN kpartx -av /dev/loop0
+$DRYRUN mount /dev/mapper/loop0p1 $TARGET
+$DRYRUN cp -v $@ $TARGET
+$DRYRUN sync
+$DRYRUN umount $TARGET
+$DRYRUN kpartx -dv /dev/loop0
+$DRYRUN losetup -d /dev/loop0
 
-    $DRYRUN losetup /dev/loop0 $i
-    $DRYRUN kpartx -av /dev/loop0
-    $DRYRUN mount /dev/mapper/loop0p1 $TARGET
-    $DRYRUN cp -v $@ $TARGET
-    $DRYRUN sync
-    $DRYRUN umount $TARGET
-    $DRYRUN kpartx -dv /dev/loop0
-    $DRYRUN losetup -d /dev/loop0
-    #break
-done
-
-for i in /dev/vg0/oneiric_rootfs /dev/vg0/lucid_rootfs /dev/vg0/precise_rootfs; do
-    echo "*** Copying $1 to $i ***"
-    #break
-    $DRYRUN losetup /dev/loop0 $i
-    $DRYRUN kpartx -av /dev/loop0
-    $DRYRUN mount /dev/mapper/loop0p1 $TARGET
-    $DRYRUN cp -v $@ $TARGET/boot
-    $DRYRUN sync
-    $DRYRUN umount $TARGET
-    $DRYRUN kpartx -dv /dev/loop0
-    $DRYRUN losetup -d /dev/loop0
-done
