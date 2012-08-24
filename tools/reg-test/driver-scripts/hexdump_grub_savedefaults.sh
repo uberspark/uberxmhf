2f1da25b4872bbd4f90e65a0c189116581b24018
Jonathan McCune <jonmccune@cmu.edu>
2012-08-24 11:56:20 -0400

    useful script to dump 'default' file updated by grub when it does 'savedefault'; suspect corruption

---
+++ tools/reg-test/driver-scripts/hexdump_grub_savedefaults.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export TARGET=/mnt
+
+for i in test-6555b test-e8100 creeper pratt; do 
+    losetup /dev/loop0 /dev/vg0/iscsi.slashboot.$i
+    kpartx -av /dev/loop0
+    mount /dev/mapper/loop0p1 $TARGET
+
+    echo
+    echo "HOST $i grub/default file"
+    hd $TARGET/grub/default
+
+    umount $TARGET
+    kpartx -dv /dev/loop0
+    losetup -d /dev/loop0
+done
