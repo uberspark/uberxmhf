22cc1d8002e7400d803d43e05d717194fa6a385e
Amit Vasudevan <amitvasudevan@acm.org>
2012-11-13 11:25:48 -0500

    xmhf-core: verification: check-maca.sh: capture status of svm NPT variables as a boolean value

--- xmhf/src/xmhf-core/verification/check-maca.sh
+++ xmhf/src/xmhf-core/verification/check-maca.sh
@@ -3,6 +3,7 @@
 # XMHF MAC(a) verification 
 # i.e., ensure that NPT structures are always in the data section
 # they are supposed to be in
+# author: amit vasudevan (amitvasudevan@acm.org)
 
 # bail out on any errors
 set -e
@@ -10,4 +11,11 @@ set -e
 echo -------------------------------------------------------------------
 echo Checking x86svm HPT data structure locations...
 echo -------------------------------------------------------------------
+
+pdpt_success=$(objdump --syms ../components/xmhf-runtime/runtime.exe | awk '{print $4,$6}' | grep ".palign_data g_svm_npt_pdpt_buffers" | wc -l)
+pdts_success=$(objdump --syms ../components/xmhf-runtime/runtime.exe | awk '{print $4,$6}' | grep ".palign_data g_svm_npt_pdts_buffers" | wc -l)
+pts_success=$(objdump --syms ../components/xmhf-runtime/runtime.exe | awk '{print $4,$6}' | grep ".palign_data g_svm_npt_pts_buffers" | wc -l)
+
+# echo Return value: $pdpt_success, $pdts_success, $pts_success
+ 
 
