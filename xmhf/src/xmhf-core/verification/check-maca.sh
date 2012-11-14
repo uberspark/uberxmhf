82c80761233cfefc6f1f3593f6317940c53e387e
Amit Vasudevan <amitvasudevan@acm.org>
2012-11-14 10:32:06 -0500

    xmhf-core: verification: x86vmx: add MAC(a) verification logic

--- xmhf/src/xmhf-core/verification/check-maca.sh
+++ xmhf/src/xmhf-core/verification/check-maca.sh
@@ -17,14 +17,41 @@ pdts_success=$(objdump --syms ../components/xmhf-runtime/runtime.exe | awk '{pri
 pts_success=$(objdump --syms ../components/xmhf-runtime/runtime.exe | awk '{print $4,$6}' | grep ".palign_data g_svm_npt_pts_buffers" | wc -l)
 
 # echo Return value: $pdpt_success, $pdts_success, $pts_success
+svm_success=0;
 
 if [ $pdpt_success -eq 1 ] && [ $pdts_success -eq 1 ] && [ $pts_success -eq 1 ]
 then
-	echo "VERIFICATION SUCCESS: NPT data structures are where they should be in the executable!"
+	svm_success=1
+else
+	svm_success=0;
+fi
+
+echo -------------------------------------------------------------------
+echo Checking x86vmx HPT data structure locations...
+echo -------------------------------------------------------------------
+
+vmx_pml4_success=$(objdump --syms ../components/xmhf-runtime/runtime.exe | awk '{print $4,$6}' | grep ".palign_data g_vmx_ept_pml4_table_buffers" | wc -l)
+vmx_pdpt_success=$(objdump --syms ../components/xmhf-runtime/runtime.exe | awk '{print $4,$6}' | grep ".palign_data g_vmx_ept_pdp_table_buffers" | wc -l)
+vmx_pdt_success=$(objdump --syms ../components/xmhf-runtime/runtime.exe | awk '{print $4,$6}' | grep ".palign_data g_vmx_ept_pd_table_buffers" | wc -l)
+vmx_pt_success=$(objdump --syms ../components/xmhf-runtime/runtime.exe | awk '{print $4,$6}' | grep ".palign_data g_vmx_ept_p_table_buffers" | wc -l)
+
+vmx_success=0;
+
+if [ $vmx_pml4_success -eq 1 ] && [ $vmx_pdpt_success -eq 1 ] && [ $vmx_pdt_success -eq 1 ] && [ $vmx_pt_success -eq 1 ]
+then
+	vmx_success=1
+else
+	vmx_success=0;
+fi
+
+
+if [ $svm_success -eq 1 ] && [ $vmx_success -eq 1 ]
+then
+	echo "VERIFICATION SUCCESS: NPT/EPT data structures are where they should be in the executable!"
 	# success
 	exit 0
 else
-	echo "VERIFICATION FAIL: NPT data structures are not in the required section!"
+	echo "VERIFICATION FAIL: NPT/EPT data structures are not in the required section!"
 	# fail
 	exit 1
 fi 
