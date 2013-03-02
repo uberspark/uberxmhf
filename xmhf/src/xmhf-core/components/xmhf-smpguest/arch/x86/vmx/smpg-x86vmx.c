9a1ca6da03dada22550ac91f702090e08ba90737
Amit Vasudevan <amitvasudevan@acm.org>
2013-03-01 21:59:54 -0500

    xmhf-verification: MED-SMP(x86vmx): seed error corresponding to allowing the LAPIC ICR write to go through on DB exception; CBMC reports failure at the correct assertion

--- xmhf/src/xmhf-core/components/xmhf-smpguest/arch/x86/vmx/smpg-x86vmx.c
+++ xmhf/src/xmhf-core/components/xmhf-smpguest/arch/x86/vmx/smpg-x86vmx.c
@@ -321,7 +321,7 @@ u32 emhf_smpguest_arch_x86vmx_eventhandler_hwpgtblviolation(VCPU *vcpu, u32 padd
 	vcpu->vmcs.guest_RFLAGS |= EFLAGS_TF;
 
 	#ifdef __XMHF_VERIFICATION__
-	g_vmx_lapic_npf_verification_guesttrapping = false;
+	g_vmx_lapic_npf_verification_guesttrapping = true;
 	#endif
 	
   //disable interrupts by clearing guest IF on this CPU until we get 
@@ -410,7 +410,7 @@ void emhf_smpguest_arch_x86vmx_eventhandler_dbexception(VCPU *vcpu, struct regs 
           (u16)vcpu->vmcs.guest_CS_selector, (u32)vcpu->vmcs.guest_RIP, g_vmx_lapic_reg, value_tobe_written);        
 		
 		#ifdef __XMHF_VERIFICATION__
-			g_vmx_lapic_db_verification_coreprotected = true;
+			g_vmx_lapic_db_verification_coreprotected = false;
 		#else
 			delink_lapic_interception=processSIPI(vcpu, value_tobe_written, icr_value_high);
 		#endif
