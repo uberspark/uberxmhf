3748af994aa54ff811262afe875a64cbca9679a9
Amit Vasudevan <amitvasudevan@acm.org>
2013-03-01 20:15:48 -0500

    xmhf-verification: MED(x86svm): verify that NPTs point to correct table and NPT is enabled; CBMC reports success

--- xmhf/src/xmhf-core/components/xmhf-partition/arch/x86/svm/part-x86svm.c
+++ xmhf/src/xmhf-core/components/xmhf-partition/arch/x86/svm/part-x86svm.c
@@ -312,7 +312,8 @@ void emhf_partition_arch_x86svm_start(VCPU *vcpu){
 #ifdef __XMHF_VERIFICATION__
 	//ensure that whenever a partition is started on a vcpu, we have nested paging
 	//enabled and that the base points to the nested page tables we have initialized
-	assert( (vmcb->np_enable == 1) && (vmcb->n_cr3 == vcpu->npt_vaddr_ptr) );
+	assert( (vmcb->np_enable == 1) );
+	assert( (vmcb->n_cr3 == vcpu->npt_vaddr_ptr) );
 #endif
 
 #ifndef __XMHF_VERIFICATION__
