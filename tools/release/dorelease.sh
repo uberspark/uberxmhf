26149641c63b5279d42d9b389a8e0991ec7912d8
Amit Vasudevan <amitvasudevan@acm.org>
2013-09-04 09:56:09 -0700

    xmhf: tools: add home for release scripts

---
+++ tools/release/dorelease.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# dorelease.sh
+# helper script to handle all the (mundane) steps required for a
+# release
+
+# author: amit vasudevan (amitvasudevan@acm.org)
+
+# Immediately bail out if any errors are detected (non-zero exit
+# status from a child process)
+set -e
+
