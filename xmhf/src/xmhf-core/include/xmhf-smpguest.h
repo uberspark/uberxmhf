cde7e15824c34f6b257d6e56c44195c612733cbf
Amit Vasudevan <amitvasudevan@acm.org>
2014-04-01 22:03:55 -0500

    xmhf-core: xmhf-smpguest.h: add rich guest memory access functions

--- xmhf/src/xmhf-core/include/xmhf-smpguest.h
+++ xmhf/src/xmhf-core/include/xmhf-smpguest.h
@@ -66,6 +66,45 @@
 //exported FUNCTIONS 
 //----------------------------------------------------------------------
 
+//----------------------------------------------------------------------
+//rich guest memory functions
+
+static inline bool xmhf_smpguest_readu16(context_desc_t context_desc, const void *guestaddress, u16 *valueptr){
+		u16 *tmp = (u16 *)xmhf_smpguest_arch_walk_pagetables(context_desc, guestaddress);
+		if((u32)tmp == 0xFFFFFFFFUL || valueptr == NULL)
+			return false;
+		*valueptr = *tmp;
+		return true;
+}
+
+static inline bool xmhf_smpguest_writeu16(context_desc_t context_desc, const void *guestaddress, u16 value){
+		u16 *tmp = (u16 *)xmhf_smpguest_arch_walk_pagetables(context_desc, guestaddress);
+		if((u32)tmp == 0xFFFFFFFFUL || 
+			( ((u32)tmp >= rpb->XtVmmRuntimePhysBase) && ((u32)tmp <= (rpb->XtVmmRuntimePhysBase+rpb->XtVmmRuntimeSize)) ) 
+		  )
+			return false;
+		*tmp = value;
+		return true;
+}
+
+static inline bool xmhf_smpguest_memcpyfrom(context_desc_t context_desc, void *buffer, const void *guestaddress, size_t numbytes){
+	u8 *guestbuffer = (u8 *)xmhf_smpguest_arch_walk_pagetables(context_desc, guestaddress);
+	if((u32)guestbuffer == 0xFFFFFFFFUL)
+		return false;
+	memcpy(buffer, gpa2hva(guestbuffer), numbytes);
+}
+
+static inline bool xmhf_smpguest_memcpyto(context_desc_t context_desc, void *guestaddress, const void *buffer, size_t numbytes){
+	u8 *guestbuffer = (u8 *)xmhf_smpguest_arch_walk_pagetables(context_desc, guestaddress);
+	if((u32)guestbuffer == 0xFFFFFFFFUL || 
+		( ((u32)guestbuffer >= rpb->XtVmmRuntimePhysBase) && ((u32)guestbuffer <= (rpb->XtVmmRuntimePhysBase+rpb->XtVmmRuntimeSize)) ) 
+	  )
+		return false;
+	memcpy(gpa2hva(guestbuffer), buffer, numbytes);
+}
+
+
+
 //initialize SMP guest logic
 //void xmhf_smpguest_initialize(VCPU *vcpu);
 void xmhf_smpguest_initialize(context_desc_t context_desc);
