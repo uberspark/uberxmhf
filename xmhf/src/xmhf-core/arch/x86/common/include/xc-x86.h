a6e5e7f58dee195f6f58ffc963db5adcd9386c93
Amit Vasudevan <amitvasudevan@acm.org>
2014-04-18 13:26:56 -0500

    xmhf-core: arch/x86/common/xc-x86.h: remove vmx_guest_unrestricted field from VCPU and make related changes; unrestricted guest support is a requirement and not an option

--- xmhf/src/xmhf-core/arch/x86/common/include/xc-x86.h
+++ xmhf/src/xmhf-core/arch/x86/common/include/xc-x86.h
@@ -168,7 +168,7 @@ typedef struct _vcpu {
   //guest state fields
   //u32 vmx_guest_currentstate;		//current operating mode of guest
   //u32 vmx_guest_nextstate;		  //next operating mode of guest
-	u32 vmx_guest_unrestricted;		//this is 1 if the CPU VMX implementation supports unrestricted guest execution
+	//u32 vmx_guest_unrestricted;		//this is 1 if the CPU VMX implementation supports unrestricted guest execution
   //struct _vmx_vmcsfields vmcs;   //the VMCS fields
 
 } __attribute__((packed)) VCPU;
