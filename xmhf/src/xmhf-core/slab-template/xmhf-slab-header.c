eb6de393d755f9a2266c1910118f8eb05f0105c6
Amit Vasudevan <amitvasudevan@acm.org>
2014-05-28 17:20:42 -0500

    xmhf-core: define type slab_entrystub_t and add new field entry_cr3 to type slab_header_t; field holds the entry stub address for CR3-based privileged slab transitions

--- xmhf/src/xmhf-core/slab-template/xmhf-slab-header.c
+++ xmhf/src/xmhf-core/slab-template/xmhf-slab-header.c
@@ -52,6 +52,8 @@
  * author: amit vasudevan (amitvasudevan@acm.org)
  */
 
+extern void entry_cr3(void);
+
 // this is in xmhf-slab.lscript
 extern u8 slab_rodata_start[];
 extern u8 slab_rodata_end[];
@@ -78,6 +80,7 @@ __attribute__ ((section(".slabrodata"))) slab_header_t slab_header = {
 	.slab_code.end = (u32)&slab_code_end,
 	.slab_stack.start = (u32)&slab_stack_start,
 	.slab_stack.end = (u32)&slab_stack_end,
+	.entry_cr3 = &entry_cr3,
 };
 
 
