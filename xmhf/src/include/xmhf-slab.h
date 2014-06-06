303ca6e628aefe18d8a39ab07580a30661e5d033
Amit Vasudevan <amitvasudevan@acm.org>
2014-06-06 12:32:34 -0500

    xmhf: include/xmhf-slab.h: define XMHF_SLAB_DEFINTERFACE to define slab interface and XMHF_SLAB_DEFEXPORTFN to define slab C export function for an interface

--- xmhf/src/include/xmhf-slab.h
+++ xmhf/src/include/xmhf-slab.h
@@ -53,6 +53,56 @@
 
 #ifndef __ASSEMBLY__
 
+#define XMHF_SLAB_DEFEXPORTFN(fn_name, fn_num, fn_aggregateret)		\
+			"1:\r\n"								\
+			"cmpl $"#fn_num", %%ebx \r\n"			\
+			"jne 1f \r\n"							\
+			"call "#fn_name" \r\n"					\//call function
+			"subl $"#fn_aggregateret", %%ebp \r\n"	\
+			"jmp endswitch \r\n"					\
+
+
+//setup
+//esi = base address of input parameter frame on stack (excluding return address)
+//edi = return address
+//ebx = function number
+//ecx = number of 32-bit dwords comprising the parameters (excluding return address)
+							
+#define XMHF_SLAB_DEFINTERFACE(...) __attribute__((naked)) void _slab_interface(void){	\
+	asm volatile (							\
+			"pushl %%edi \r\n" 				\//save return address
+											\
+			"xorl %%ebp, %%ebp \r\n"		\//ebp = 0
+			"cmpl $0, %%edx \r\n"			\
+			"je 1f \r\n"					\//check to see if we need to allocate space for aggregate return value, if not just proceed
+			"movl %%edx, %%ebp \r\n"		\//
+			"movl %%esp, %%edx \r\n"		\//
+			"subl %%ebp, %%edx \r\n"		\//edx = address where aggregate return value begins on stack
+			"1:\r\n"						\
+			"addl %%ecx, %%ebp \r\n"		\//ebp = total bytes that we need to allocate for aggregate return value + input parameters
+			"subl %%ebp, %%esp 	\r\n"		\//esp = new empty tos to house input parameters
+			"movl %%esp, %%edi \r\n"		\//edi = esp
+			"cld \r\n"						\
+			"rep movsb \r\n"				\
+			"cmpl $0, %%edx \r\n"			\
+			"je 1f \r\n"					\
+			"addl $4, %%esp \r\n"			\//pop out old aggregate return value address
+			"pushl %%edx \r\n"				\//store local aggregate return value address
+			"movl %%edx, %%esi \r\n"		\
+											\
+			__VA_ARGS__ 					\
+											\
+			"1:\r\n"						\
+			"endswitch:\r\n"				\
+			"addl %%ebp, %%esp \r\n"		\//remove parameters from stack
+			"popl %%edi \r\n"				\
+			"jmpl *%%edi \r\n"				\//return back to caller slab
+			:								\
+			:								\
+			:								\
+		);									\
+}											\
+
 
 #endif //__ASSEMBLY__
 
