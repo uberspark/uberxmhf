d38bda50aceb145b21b7999a27d4fd2525547bad
Amit Vasudevan <amitvasudevan@acm.org>
2014-06-25 13:24:10 -0500

    xmhf-core: initbs: fix typo to build successfully; tested and works with hyperdep hypapp

--- xmhf/src/xmhf-core/initbs/xc-initbs-code.c
+++ xmhf/src/xmhf-core/initbs/xc-initbs-code.c
@@ -52,7 +52,7 @@
 
 #include <xc-initbs.h>
 
-#define __XMHF_SLAB_CALLER_INDEX__	XMHF_SLAB_INDEX_INITBS
+#define __XMHF_SLAB_CALLER_INDEX__	XMHF_SLAB_INITBS_INDEX
 #include <xmhf-slab-implib.h>
 #include <xc-init.h>
 #undef __XMHF_SLAB_CALLER_INDEX__
