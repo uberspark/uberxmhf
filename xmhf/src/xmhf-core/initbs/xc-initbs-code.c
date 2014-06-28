a41af02edd72aa7b1fd940772429561625211539
Amit Vasudevan <amitvasudevan@acm.org>
2014-06-28 09:43:37 -0500

    xmhf-core: xcinitbs: test exception trigger *after* paging is enabled; works as expected

--- xmhf/src/xmhf-core/initbs/xc-initbs-code.c
+++ xmhf/src/xmhf-core/initbs/xc-initbs-code.c
@@ -111,10 +111,10 @@ void xmhf_runtime_entry(void){
 	}
 
 
-	asm volatile("int $0x03 \r\n");
-
 	//setup slab page tables and turn on paging
 	xmhf_apihub_initialize();
+
+	asm volatile("int $0x03 \r\n");
 
 	//[test] slab
 	{
