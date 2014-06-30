02fe765ea8d88736c67adea11810989ba16f274b
Amit Vasudevan <amitvasudevan@acm.org>
2014-06-30 13:28:04 -0500

    xmhf-core: xcexhub: add arch/x86/common backend module and move all exception handling related logic from xcinitbs in there.

--- xmhf/src/xmhf-core/initbs/arch/x86/common/xc-initbs-x86-code.c
+++ xmhf/src/xmhf-core/initbs/arch/x86/common/xc-initbs-x86-code.c
@@ -67,21 +67,10 @@
 //----------------------------------------------------------------------
 // local variables
 
-//core IDT
-static u64 _idt_start[EMHF_XCPHANDLER_MAXEXCEPTIONS] __attribute__(( aligned(16) ));
-
-//core IDT descriptor
-arch_x86_idtdesc_t _idt __attribute__(( aligned(16) )) = {
-	.size=sizeof(_idt_start)-1,
-	.base=(u32)&_idt_start,
-};
 
 //runtime TSS
 u8 _tss[PAGE_SIZE_4K] = { 0 };
 
-//exclusive exception handling stack, we switch to this stack if there
-//are any exceptions during hypapp execution
-static u8 _exceptionstack[PAGE_SIZE_4K] __attribute__((section(".stack")));
 
 
 
@@ -208,17 +197,6 @@ void xmhf_baseplatform_arch_x86_initializeIOPL(void){
 	
 }
 
-//initialize IDT
-void xmhf_baseplatform_arch_x86_initializeIDT(void){
-
-	asm volatile(
-		"lidt  %0 \r\n"
-		: //no outputs
-		: "m" (_idt)
-		: //no clobber
-	);
-	
-}
 
 
 //initialize TR/TSS
@@ -644,171 +622,3 @@ void xmhf_apihub_arch_initialize (void){
 
 
 
-/* originally within xc-xcphandler-x86.c */
-
-#define XMHF_EXCEPTION_HANDLER_DEFINE(vector) 												\
-	static void __xmhf_exception_handler_##vector(void) __attribute__((naked)) { 					\
-		asm volatile(	"pushal	\r\n"								\
-						"movw	%0, %%ax\r\n"						\
-						"movw	%%ax, %%ds\r\n"						\
-						"movl 	%%esp, %%eax\r\n"					\
-						"pushl 	%%eax\r\n"							\
-						"pushl	%1\r\n" 							\
-						"call	xmhf_xcphandler_arch_hub\r\n"			\
-						"addl  	$0x08, %%esp\r\n"					\
-						"popal	 \r\n"								\
-						"iretl\r\n"									\
-					:												\
-					:	"i" (__DS_CPL0), "i" (vector)				\
-		);															\
-	}\
-
-#define XMHF_EXCEPTION_HANDLER_ADDROF(vector) &__xmhf_exception_handler_##vector
-
-XMHF_EXCEPTION_HANDLER_DEFINE(0)
-XMHF_EXCEPTION_HANDLER_DEFINE(1)
-XMHF_EXCEPTION_HANDLER_DEFINE(2)
-XMHF_EXCEPTION_HANDLER_DEFINE(3)
-XMHF_EXCEPTION_HANDLER_DEFINE(4)
-XMHF_EXCEPTION_HANDLER_DEFINE(5)
-XMHF_EXCEPTION_HANDLER_DEFINE(6)
-XMHF_EXCEPTION_HANDLER_DEFINE(7)
-XMHF_EXCEPTION_HANDLER_DEFINE(8)
-XMHF_EXCEPTION_HANDLER_DEFINE(9)
-XMHF_EXCEPTION_HANDLER_DEFINE(10)
-XMHF_EXCEPTION_HANDLER_DEFINE(11)
-XMHF_EXCEPTION_HANDLER_DEFINE(12)
-XMHF_EXCEPTION_HANDLER_DEFINE(13)
-XMHF_EXCEPTION_HANDLER_DEFINE(14)
-XMHF_EXCEPTION_HANDLER_DEFINE(15)
-XMHF_EXCEPTION_HANDLER_DEFINE(16)
-XMHF_EXCEPTION_HANDLER_DEFINE(17)
-XMHF_EXCEPTION_HANDLER_DEFINE(18)
-XMHF_EXCEPTION_HANDLER_DEFINE(19)
-XMHF_EXCEPTION_HANDLER_DEFINE(20)
-XMHF_EXCEPTION_HANDLER_DEFINE(21)
-XMHF_EXCEPTION_HANDLER_DEFINE(22)
-XMHF_EXCEPTION_HANDLER_DEFINE(23)
-XMHF_EXCEPTION_HANDLER_DEFINE(24)
-XMHF_EXCEPTION_HANDLER_DEFINE(25)
-XMHF_EXCEPTION_HANDLER_DEFINE(26)
-XMHF_EXCEPTION_HANDLER_DEFINE(27)
-XMHF_EXCEPTION_HANDLER_DEFINE(28)
-XMHF_EXCEPTION_HANDLER_DEFINE(29)
-XMHF_EXCEPTION_HANDLER_DEFINE(30)
-XMHF_EXCEPTION_HANDLER_DEFINE(31)
-	
-static u32 exceptionstubs[] = { 	XMHF_EXCEPTION_HANDLER_ADDROF(0),
-							XMHF_EXCEPTION_HANDLER_ADDROF(1),
-							XMHF_EXCEPTION_HANDLER_ADDROF(2),
-							XMHF_EXCEPTION_HANDLER_ADDROF(3),
-							XMHF_EXCEPTION_HANDLER_ADDROF(4),
-							XMHF_EXCEPTION_HANDLER_ADDROF(5),
-							XMHF_EXCEPTION_HANDLER_ADDROF(6),
-							XMHF_EXCEPTION_HANDLER_ADDROF(7),
-							XMHF_EXCEPTION_HANDLER_ADDROF(8),
-							XMHF_EXCEPTION_HANDLER_ADDROF(9),
-							XMHF_EXCEPTION_HANDLER_ADDROF(10),
-							XMHF_EXCEPTION_HANDLER_ADDROF(11),
-							XMHF_EXCEPTION_HANDLER_ADDROF(12),
-							XMHF_EXCEPTION_HANDLER_ADDROF(13),
-							XMHF_EXCEPTION_HANDLER_ADDROF(14),
-							XMHF_EXCEPTION_HANDLER_ADDROF(15),
-							XMHF_EXCEPTION_HANDLER_ADDROF(16),
-							XMHF_EXCEPTION_HANDLER_ADDROF(17),
-							XMHF_EXCEPTION_HANDLER_ADDROF(18),
-							XMHF_EXCEPTION_HANDLER_ADDROF(19),
-							XMHF_EXCEPTION_HANDLER_ADDROF(20),
-							XMHF_EXCEPTION_HANDLER_ADDROF(21),
-							XMHF_EXCEPTION_HANDLER_ADDROF(22),
-							XMHF_EXCEPTION_HANDLER_ADDROF(23),
-							XMHF_EXCEPTION_HANDLER_ADDROF(24),
-							XMHF_EXCEPTION_HANDLER_ADDROF(25),
-							XMHF_EXCEPTION_HANDLER_ADDROF(26),
-							XMHF_EXCEPTION_HANDLER_ADDROF(27),
-							XMHF_EXCEPTION_HANDLER_ADDROF(28),
-							XMHF_EXCEPTION_HANDLER_ADDROF(29),
-							XMHF_EXCEPTION_HANDLER_ADDROF(30),
-							XMHF_EXCEPTION_HANDLER_ADDROF(31),
-};
-						
-
-//initialize EMHF core exception handlers
-void xmhf_xcphandler_arch_initialize(void){
-	u32 *pexceptionstubs;
-	u32 i;
-
-	printf("\n%s: setting up runtime IDT...", __FUNCTION__);
-	
-	for(i=0; i < EMHF_XCPHANDLER_MAXEXCEPTIONS; i++){
-		idtentry_t *idtentry=(idtentry_t *)((u32)xmhf_baseplatform_arch_x86_getidtbase()+ (i*8));
-		idtentry->isrLow= (u16)exceptionstubs[i];
-		idtentry->isrHigh= (u16) ( (u32)exceptionstubs[i] >> 16 );
-		idtentry->isrSelector = __CS_CPL0;
-		idtentry->count=0x0;
-		idtentry->type=0xEE;	//32-bit interrupt gate
-								//present=1, DPL=11b, system=0, type=1110b
-	}
-	
-	printf("\n%s: IDT setup done.", __FUNCTION__);
-}
-
-
-/* originally in xc-ihub-xcphandler-x86.c */
-
-static void xmhf_xcphandler_arch_unhandled(u32 vector, struct regs *r){
-	u32 exception_cs, exception_eip, exception_eflags, errorcode=0;
-
-	if(vector == CPU_EXCEPTION_DF ||
-		vector == CPU_EXCEPTION_TS ||
-		vector == CPU_EXCEPTION_NP ||
-		vector == CPU_EXCEPTION_SS ||
-		vector == CPU_EXCEPTION_GP ||
-		vector == CPU_EXCEPTION_PF ||
-		vector == CPU_EXCEPTION_AC){
-		errorcode = *(uint32_t *)(r->esp+0);
-		r->esp += sizeof(uint32_t);	//skip error code on stack if applicable
-	}
-
-	exception_eip = *(uint32_t *)(r->esp+0);
-	exception_cs = *(uint32_t *)(r->esp+sizeof(uint32_t));
-	exception_eflags = *(uint32_t *)(r->esp+(2*sizeof(uint32_t)));
-
-	printf("\nunhandled exception %x, halting!", vector);
-	printf("\nstate dump follows...");
-	//things to dump
-	printf("\nCS:EIP 0x%04x:0x%08x with EFLAGS=0x%08x, errorcode=%08x", (u16)exception_cs, exception_eip, exception_eflags, errorcode);
-	printf("\nCR0=%08x, CR2=%08x, CR3=%08x, CR4=%08x", read_cr0(), read_cr2(), read_cr3(), read_cr4());
-	printf("\nEAX=0x%08x EBX=0x%08x ECX=0x%08x EDX=0x%08x", r->eax, r->ebx, r->ecx, r->edx);
-	printf("\nESI=0x%08x EDI=0x%08x EBP=0x%08x ESP=0x%08x", r->esi, r->edi, r->ebp, r->esp);
-	printf("\nCS=0x%04x, DS=0x%04x, ES=0x%04x, SS=0x%04x", (u16)read_segreg_cs(), (u16)read_segreg_ds(), (u16)read_segreg_es(), (u16)read_segreg_ss());
-	printf("\nFS=0x%04x, GS=0x%04x", (u16)read_segreg_fs(), (u16)read_segreg_gs());
-	printf("\nTR=0x%04x", (u16)read_tr_sel());
-
-	//do a stack dump in the hopes of getting more info.
-	{
-		uint32_t i;
-		uint32_t stack_start = r->esp;
-		printf("\n-----stack dump (16 entries)-----");
-		for(i=stack_start; i < stack_start+(16*sizeof(uint32_t)); i+=sizeof(uint32_t)){
-			printf("\nStack(0x%08x) -> 0x%08x", i, *(uint32_t *)i);
-		}
-		printf("\n-----end------------");
-	}
-	HALT();
-}
-
-//exception handler hub
-void xmhf_xcphandler_arch_hub(u32 vector, struct regs *r){
-	switch(vector){
-			case CPU_EXCEPTION_NMI:{
-				xc_coreapi_arch_eventhandler_nmiexception(r);
-				}
-				break;
-
-			default:{
-				xmhf_xcphandler_arch_unhandled(vector, r);
-				//we will never get here
-			}
-	}
-}
