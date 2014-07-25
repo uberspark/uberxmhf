438aca1ba318b341e0726936d2f380868e6f98d5
Amit Vasudevan <amitvasudevan@acm.org>
2014-07-25 12:18:21 -0500

    xmhf-core: move PCID check, WP and NX bit enabling within libxmhfhw:: xmhf_baseplatform_arch_x86_cpu_initialize. tested and works with hyperdep hypapp.

--- xmhf/src/libxmhfhw/cpu/x86/common/bplt-x86-cpu.c
+++ xmhf/src/libxmhfhw/cpu/x86/common/bplt-x86-cpu.c
@@ -218,16 +218,38 @@ void xmhf_baseplatform_arch_x86_cpu_initialize(void){
 		write_cr4(t_cr4);
 	}
 
-	//turn on NX protections via MSR EFER
-	//note: on BSP NX protections are turned on much before we get here, but on APs this is the place we
-	//set NX protections on
+	//check for PCID support (if present)
+	{
+			u32 eax, ebx, ecx, edx;
+			
+			cpuid(0x1, &eax, &ebx, &ecx, &edx);
+			
+			if( ecx & (1UL << 17) )
+				printf("\n%s: PCID supported", __FUNCTION__);
+			else
+				printf("\n%s: PCID not supported", __FUNCTION__);
+	}
+
+	//turn on WP bit in CR0 register for supervisor mode read-only permission support
+	{
+		u32 cr0;
+		cr0=read_cr0();
+		printf("\n%s: CR0=%08x", __FUNCTION__, cr0);
+		cr0 |= CR0_WP;
+		printf("\n%s: attempting to change CR0 to %08x", __FUNCTION__, cr0);
+		write_cr0(cr0);
+		cr0 = read_cr0();
+		printf("\n%s: CR0 changed to %08x", __FUNCTION__, cr0);
+	}
+
+	//turn on NX protections
 	{
 		u32 eax, edx;
 		rdmsr(MSR_EFER, &eax, &edx);
 		eax |= (1 << EFER_NXE);
 		wrmsr(MSR_EFER, eax, edx);
 		printf("\n%s: NX protections enabled: MSR_EFER=%08x%08x", __FUNCTION__, edx, eax);
-	}	
+	}
 
 }
 
