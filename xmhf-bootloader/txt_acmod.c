88975a300fe6321060d46f3440e3815ce620a20a
Amit Vasudevan <amitvasudevan@acm.org>
2014-08-04 14:12:45 -0500

    xmhf: xmhf-libs/libxmhfhw: bring in all headers under include/ with sub-namespaces cpu/, container/ and platform/. refactor existing sources to cope with this change. tested and works with hyperdep hypapp.

--- xmhf-bootloader/txt_acmod.c
+++ xmhf-bootloader/txt_acmod.c
@@ -87,15 +87,6 @@
 #include <xmhf.h> 
 #include <xmhf-debug.h>
 
-#include "platform/x86pc/include/common/_multiboot.h"		//multiboot
-#include "cpu/x86/include/common/_processor.h"  	//CPU
-#include "cpu/x86/include/common/_msr.h"        	//model specific registers
-#include "cpu/x86/include/common/_paging.h"     	//MMU
-//#include "cpu/x86/include/common/_io.h"         	//legacy I/O
-#include "cpu/x86/include/intel/txt/_txt.h"			//Trusted eXecution Technology (SENTER support)
-//#include "platform/x86pc/include/common/_acpi.h"			//ACPI glue
-//#include "platform/x86pc/include/common/_memaccess.h"	//platform memory access
-
 static acm_info_table_t *get_acmod_info_table(acm_hdr_t* hdr)
 {
     uint32_t user_area_off;
