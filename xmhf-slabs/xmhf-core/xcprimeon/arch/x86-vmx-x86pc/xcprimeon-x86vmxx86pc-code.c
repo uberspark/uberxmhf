309fa1c7140699ea7fb32c7a7377d4efb4c477bf
Amit Vasudevan <amitvasudevan@acm.org>
2014-08-15 09:24:36 -0500

    xmhf: xmhf-core/xcprimeon: fix XCSMP slab index

--- xmhf-slabs/xmhf-core/xcprimeon/arch/x86-vmx-x86pc/xcprimeon-x86vmxx86pc-code.c
+++ xmhf-slabs/xmhf-core/xcprimeon/arch/x86-vmx-x86pc/xcprimeon-x86vmxx86pc-code.c
@@ -452,7 +452,7 @@ static u32 _xcprimeon_slab_getspatype(u32 slab_index, u32 spa){
 	//slab shared data region 
 	//TODO: add per shared data variable access policy rather than entire section
 	if(spa >= (u32)_slab_shareddata_memregion_start && spa < (u32)_slab_shareddata_memregion_end){
-			if (slab_index == XMHF_SLAB_XCPRIMEON_INDEX || slab_index == XMHF_SLAB_INITBS_INDEX || slab_index == XMHF_SLAB_XCEXHUB_INDEX)
+			if (slab_index == XMHF_SLAB_XCPRIMEON_INDEX || slab_index == XMHF_SLAB_XCSMP_INDEX || slab_index == XMHF_SLAB_XCEXHUB_INDEX)
 				return _SLAB_SPATYPE_SLAB_RWDATA; //map read-write in initbs (GDT,TSS setup) and xcexhub (IDT setup)
 			else
 				return _SLAB_SPATYPE_SLAB_RODATA; //map read-only in all other slabs 
