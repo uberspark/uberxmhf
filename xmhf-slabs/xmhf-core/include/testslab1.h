d606e54603ca0482e4b1e8e8e0e7f129665a5736
Amit Vasudevan <amitvasudevan@acm.org>
2014-08-15 19:09:25 -0500

    xmhf: testslab1: delink entry_0 function from the subinterface list

--- xmhf-slabs/xmhf-core/include/testslab1.h
+++ xmhf-slabs/xmhf-core/include/testslab1.h
@@ -60,7 +60,7 @@
 
 #ifdef __XMHF_SLAB_CALLER_INDEX__ 
 
-XMHF_SLAB_DEFIMPORTFN(slab_retval_t, entry_0, (void), XMHF_SLAB_DEFIMPORTFNSTUB(__XMHF_SLAB_CALLER_INDEX__, XMHF_SLAB_TESTSLAB1_INDEX, XMHF_SLAB_TESTSLAB1_FNENTRY0, (sizeof(u32)), sizeof(slab_retval_t), XMHF_SLAB_FN_RETTYPE_AGGREGATE))
+//XMHF_SLAB_DEFIMPORTFN(slab_retval_t, entry_0, (void), XMHF_SLAB_DEFIMPORTFNSTUB(__XMHF_SLAB_CALLER_INDEX__, XMHF_SLAB_TESTSLAB1_INDEX, XMHF_SLAB_TESTSLAB1_FNENTRY0, (sizeof(u32)), sizeof(slab_retval_t), XMHF_SLAB_FN_RETTYPE_AGGREGATE))
 XMHF_SLAB_DEFIMPORTFN(slab_retval_t, entry_1, (u32 param1, u32 param2), XMHF_SLAB_DEFIMPORTFNSTUB(__XMHF_SLAB_CALLER_INDEX__, XMHF_SLAB_TESTSLAB1_INDEX, XMHF_SLAB_TESTSLAB1_FNENTRY1, (sizeof(u32)+sizeof(u32)+sizeof(u32)), sizeof(slab_retval_t), XMHF_SLAB_FN_RETTYPE_AGGREGATE))
 XMHF_SLAB_DEFIMPORTFN(slab_retval_t, entry_2, (u32 cpu_index, bool isbsp, u32 partition_index), XMHF_SLAB_DEFIMPORTFNSTUB(__XMHF_SLAB_CALLER_INDEX__, XMHF_SLAB_TESTSLAB1_INDEX, XMHF_SLAB_TESTSLAB1_FNENTRY2, (sizeof(u32)+sizeof(bool)+sizeof(u32)+sizeof(u32)), sizeof(slab_retval_t), XMHF_SLAB_FN_RETTYPE_AGGREGATE) )
 XMHF_SLAB_DEFIMPORTFN(slab_retval_t, entry_3, (context_desc_t context_desc, xc_hypapp_arch_param_t archparam), XMHF_SLAB_DEFIMPORTFNSTUB(__XMHF_SLAB_CALLER_INDEX__, XMHF_SLAB_TESTSLAB1_INDEX, XMHF_SLAB_TESTSLAB1_FNENTRY3, (sizeof(context_desc_t)+sizeof(xc_hypapp_arch_param_t)+sizeof(u32)), sizeof(slab_retval_t), XMHF_SLAB_FN_RETTYPE_AGGREGATE))
