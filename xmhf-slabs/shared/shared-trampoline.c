79e9a3181d7ba30d61cda7684cef31eace8a6f07
Amit Vasudevan <amitvasudevan@acm.org>
2014-08-20 17:11:33 -0500

    xmhf: allow both XMHF_SLAB_CALL (old) and XMHF_SLAB_CALL_P2P (new) to co-exist; this is in preparation to migrate everything to the new P2P calling interface. tested and works with hyperdep hypapp.

--- xmhf-slabs/shared/shared-trampoline.c
+++ xmhf-slabs/shared/shared-trampoline.c
@@ -188,7 +188,7 @@ __attribute__((fastcall)) __attribute (( section(".slabtrampoline") )) void _sla
 		"call *%%eax \r\n"
 		"movl %%esi, %0 \r\n"
 		: "=S" (aggrettypeptr)
-		: "g" (&tframe->src_slabid), "g" (framesize_op), "m" (_slab_table[tframe->dst_slabid].entry_cr3)
+		: "g" (&tframe->src_slabid), "g" (framesize_op), "m" (_slab_table[tframe->dst_slabid].entry_cr3_new)
 		: "esi", "ecx"	 							
 	);
 
