29b1d2ec3962defe465c56f04fb19ba174866092
Amit Vasudevan <amitvasudevan@acm.org>
2014-08-25 23:39:15 -0500

    xmhf: convert xc_api_partition_xxx (partition APIs) to XMHF_SLAB_CALL_P2P interface. tested and works with hyperdep hypapp.

--- xmhf-slabs/xmhf-core/xcrichguest/xcrichguest-code.c
+++ xmhf-slabs/xmhf-core/xcrichguest/xcrichguest-code.c
@@ -62,9 +62,9 @@
 slab_retval_t xcrichguest_interface(u32 src_slabid, u32 dst_slabid, u32 fn_id, u32 fn_paramsize, ...){
 	slab_retval_t srval;
 	va_list args;
-	
+
 	_XDPRINTF_("%s: Got control: src_slabid=%u, dst_slabid=%u, fn_id=%u, fn_paramsize=%u\n", __FUNCTION__, src_slabid, dst_slabid, fn_id, fn_paramsize);
-	
+
 	switch(fn_id){
 			case XMHF_SLAB_XCRICHGUEST_FNENTRY:{
 				u32 cpuid;
@@ -76,7 +76,7 @@ slab_retval_t xcrichguest_interface(u32 src_slabid, u32 dst_slabid, u32 fn_id, u
 				va_end(args);
 			}
 			break;
-	
+
 			case XMHF_SLAB_XCRICHGUEST_FNGUESTMEMORYREPORTING:{
 				context_desc_t context_desc;
 				struct regs r;
@@ -87,13 +87,13 @@ slab_retval_t xcrichguest_interface(u32 src_slabid, u32 dst_slabid, u32 fn_id, u
 				va_end(args);
 			}
 			break;
-				
+
 			default:
 				_XDPRINTF_("%s: unhandled subinterface %u. Halting\n", __FUNCTION__, fn_id);
 				HALT();
 	}
-	
-	return srval;	
+
+	return srval;
 }
 
 
@@ -102,10 +102,12 @@ slab_retval_t xcrichguest_interface(u32 src_slabid, u32 dst_slabid, u32 fn_id, u
 //add given cpu to the rich guest partition
 static context_desc_t _xcrichguest_setup(u32 partition_index, u32 cpuid, bool is_bsp){
 	context_desc_t context_desc;
+    slab_retval_t srval;
 
 	//add cpu to the richguest partition
-	context_desc = XMHF_SLAB_CALL(xc_api_partition_addcpu(partition_index, cpuid, is_bsp));
-
+	//context_desc = XMHF_SLAB_CALL(xc_api_partition_addcpu(partition_index, cpuid, is_bsp));
+    srval = XMHF_SLAB_CALL_P2P(xcapi, XMHF_SLAB_XCRICHGUEST_INDEX, XMHF_SLAB_XCAPI_INDEX, XMHF_SLAB_XCAPI_FNXCAPIPARTITIONADDCPU, XMHF_SLAB_XCAPI_FNXCAPIPARTITIONADDCPU_SIZE, partition_index, cpuid, is_bsp);
+    context_desc = srval.retval_context_desc;
 	//bail out if we could not add cpu to the rich guest partition
 	if(context_desc.cpu_desc.cpu_index == XC_PARTITION_INDEX_INVALID || context_desc.partition_desc.partition_index == XC_PARTITION_INDEX_INVALID){
 		_XDPRINTF_("\n%s: could not add cpu to rich guest partition. Halting!", __FUNCTION__);
@@ -119,30 +121,32 @@ static context_desc_t _xcrichguest_setup(u32 partition_index, u32 cpuid, bool is
 }
 
 
-//we get control here in the context of *each* physical CPU core 
+//we get control here in the context of *each* physical CPU core
 bool xcrichguest_entry(u32 cpuid, bool is_bsp){
-	static u32 _xc_startup_hypappmain_counter = 0; 
-	static u32 _xc_startup_hypappmain_counter_lock = 1; 
+	static u32 _xc_startup_hypappmain_counter = 0;
+	static u32 _xc_startup_hypappmain_counter_lock = 1;
 	context_desc_t context_desc;
 	static volatile bool bsp_done=false;
 	static u32 xc_richguest_partition_index=XC_PARTITION_INDEX_INVALID;
+    slab_retval_t srval;
 
 	_XDPRINTF_("\n%s: cpu=%x, is_bsp=%u. Halting\n", __FUNCTION__, cpuid, is_bsp);
-	
+
 	//ensure BSP is the first to grab the lock below
 	if(!is_bsp)
 		while(!bsp_done);
-	
+
 	//serialize execution
     spin_lock(&_xc_startup_hypappmain_counter_lock);
 
 	//[debug]
 	_XDPRINTF_("\n%s: cpuid=%08x, is_bsp=%u...\n", __FUNCTION__, cpuid, is_bsp);
-	
+
 	//create rich guest partition if we are the BSP
 	if(is_bsp){
 		_XDPRINTF_("\n%s: proceeding to create rich guest partition (esp=%x)\n", __FUNCTION__, read_esp());
-		xc_richguest_partition_index = XMHF_SLAB_CALL(xc_api_partition_create(XC_PARTITION_PRIMARY));
+		srval = XMHF_SLAB_CALL_P2P(xcapi, XMHF_SLAB_XCRICHGUEST_INDEX, XMHF_SLAB_XCAPI_INDEX, XMHF_SLAB_XCAPI_FNXCAPIPARTITIONCREATE, XMHF_SLAB_XCAPI_FNXCAPIPARTITIONCREATE_SIZE, XC_PARTITION_PRIMARY);
+        xc_richguest_partition_index = srval.retval_u32;
 		if(xc_richguest_partition_index == XC_PARTITION_INDEX_INVALID){
 			_XDPRINTF_("\n%s: Fatal error, could not create rich guest partition!", __FUNCTION__);
 			HALT();
@@ -151,33 +155,33 @@ bool xcrichguest_entry(u32 cpuid, bool is_bsp){
 		xcrichguest_arch_initialize(xc_richguest_partition_index);
 		_XDPRINTF_("\n%s: BSP: initialized rich guest partition %u", __FUNCTION__, xc_richguest_partition_index);
 	}
-	
+
 	//add cpu to rich guest partition
 	//TODO: check if this CPU is allocated to the "rich" guest. if so, pass it on to
 	//the rich guest initialization procedure. if the CPU is not allocated to the
 	//rich guest, enter it into a CPU pool for use by other partitions
 	context_desc=_xcrichguest_setup(xc_richguest_partition_index, cpuid, is_bsp);
-	
+
 	if(context_desc.cpu_desc.cpu_index == XC_PARTITION_INDEX_INVALID || context_desc.partition_desc.partition_index == XC_PARTITION_INDEX_INVALID){
 		_XDPRINTF_("\n%s: Fatal error, could not add cpu to rich guest. Halting!", __FUNCTION__);
 		HALT();
 	}
-	
+
 	//call hypapp main function
 	{
 		hypapp_env_block_t hypappenvb;
-		
-		hypappenvb.runtimephysmembase = (u32)xcbootinfo->physmem_base;  
+
+		hypappenvb.runtimephysmembase = (u32)xcbootinfo->physmem_base;
 		hypappenvb.runtimesize = (u32)xcbootinfo->size;
-	
+
 		//call app main
 		_XDPRINTF_("\n%s: proceeding to call xmhfhypapp_main on BSP", __FUNCTION__);
 		XMHF_SLAB_CALL(xmhf_hypapp_initialization(context_desc, hypappenvb));
 		_XDPRINTF_("\n%s: came back into core", __FUNCTION__);
-	}   	
-    
+	}
+
     _xc_startup_hypappmain_counter++;
-    
+
     //end serialized execution
     spin_unlock(&_xc_startup_hypappmain_counter_lock);
 
@@ -187,15 +191,17 @@ bool xcrichguest_entry(u32 cpuid, bool is_bsp){
 
 	_XDPRINTF_("\n%s: cpu %x, isbsp=%u, Waiting for all cpus fo cycle through hypapp main", __FUNCTION__, cpuid, is_bsp);
 	_XDPRINTF_("\n\n");
-	
+
 	//wait for hypapp main to execute on all the cpus
 	while(_xc_startup_hypappmain_counter < xcbootinfo->cpuinfo_numentries);
-	
+
 	//start cpu in corresponding partition
 	_XDPRINTF_("\n%s[%u]: starting in partition...\n", __FUNCTION__, context_desc.cpu_desc.cpu_index);
-	
+
 	//xmhf_partition_start(context_desc.cpu_desc.cpu_index);
-	if(!XMHF_SLAB_CALL(xc_api_partition_startcpu(context_desc))){
+	//if(!XMHF_SLAB_CALL(xc_api_partition_startcpu(context_desc))){
+	srval = XMHF_SLAB_CALL_P2P(xcapi, XMHF_SLAB_XCRICHGUEST_INDEX, XMHF_SLAB_XCAPI_INDEX, XMHF_SLAB_XCAPI_FNXCAPIPARTITIONSTARTCPU, XMHF_SLAB_XCAPI_FNXCAPIPARTITIONSTARTCPU_SIZE, context_desc);
+    if(!srval.retval_bool){
 		_XDPRINTF_("\n%s: should not be here. HALTING!", __FUNCTION__);
 		HALT();
 	}
