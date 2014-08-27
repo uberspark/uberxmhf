06bc25da130c5eaadca4c743b09979c60c473c6f
Amit Vasudevan <amitvasudevan@acm.org>
2014-08-26 23:32:54 -0500

    xmhf: include/xmhf-slab.h: cleanup old and now defunct slab calling interface; tested and works with hyperdep hypapp

--- include/xmhf-slab.h
+++ include/xmhf-slab.h
@@ -51,10 +51,6 @@
 #ifndef __XMHF_SLAB_H__
 #define __XMHF_SLAB_H__
 
-#define	XMHF_SLAB_FN_RETTYPE_NORMAL		0x0
-#define	XMHF_SLAB_FN_RETTYPE_AGGREGATE	0x4
-
-
 #define XMHF_SLAB_CALLP2P					0xB0
 
 
@@ -86,217 +82,8 @@ typedef union {
 		xc_hypapp_arch_param_t retval_xc_hypapp_arch_param;
 } slab_retval_t;
 
-//extern slab_header_t _slab_table[];
-
 extern __attribute__(( section(".sharedro_xcbootinfoptr") )) XMHF_BOOTINFO *xcbootinfo;
 extern __attribute__ ((section(".sharedro_slab_table"))) slab_header_t _slab_table[XMHF_SLAB_NUMBEROFSLABS];
-
-/*#define _XMHF_SLAB_DEFEXPORTFN(fn_name, fn_num, fn_aggregateret)		\
-			"1:\r\n"								\
-			"cmpl $"#fn_num", %%ebx \r\n"			\
-			"jne 1f \r\n"							\
-			"call "#fn_name" \r\n"					\
-			"subl $"#fn_aggregateret", %%ebp \r\n"	\
-			"jmp endswitch \r\n"					\
-*/
-
-#define _XMHF_SLAB_DEFEXPORTFN(fn_name, fn_num, fn_aggregateret)		\
-			"cmpl $"#fn_num", %%ebx \r\n"			\
-			"jne 1f \r\n"							\
-			"jmp "#fn_name" \r\n"					\
-			"1:\r\n"								\
-
-
-#define XMHF_SLAB_DEFEXPORTFN(fn_name, fn_num, fn_aggregateret)		_XMHF_SLAB_DEFEXPORTFN(fn_name, fn_num, fn_aggregateret)
-
-//setup
-//esi = base address of input parameter frame on stack (excluding return address)
-//edi = return address
-//ebx = function number
-//ecx = number of 32-bit dwords comprising the parameters (excluding return address)
-/*							
-#define XMHF_SLAB_DEFINTERFACE(...) __attribute__((naked)) __attribute__ ((section(".slab_entrystub"))) void _slab_interface(void){	\
-	asm volatile (							\
-			"pushl %%edi \r\n" 				\
-											\
-			"xorl %%ebp, %%ebp \r\n"		\
-			"cmpl $0, %%edx \r\n"			\
-			"je 1f \r\n"					\
-			"movl %%edx, %%ebp \r\n"		\
-			"movl %%esp, %%edx \r\n"		\
-			"subl %%ebp, %%edx \r\n"		\
-			"1:\r\n"						\
-			"addl %%ecx, %%ebp \r\n"		\
-			"subl %%ebp, %%esp 	\r\n"		\
-			"movl %%esp, %%edi \r\n"		\
-			"cld \r\n"						\
-			"rep movsb \r\n"				\
-			"cmpl $0, %%edx \r\n"			\
-			"je 1f \r\n"					\
-			"addl $4, %%esp \r\n"			\
-			"pushl %%edx \r\n"				\
-			"movl %%edx, %%esi \r\n"		\
-											\
-			__VA_ARGS__ 					\
-											\
-			"1:\r\n"						\
-			"endswitch:\r\n"				\
-			"addl %%ebp, %%esp \r\n"		\
-			"popl %%edi \r\n"				\
-			"jmpl *%%edi \r\n"				\
-			:								\
-			:								\
-			:								\
-		);									\
-}*/											\
-
-
-#define XMHF_SLAB_DEFINTERFACE(...) __attribute__((naked)) __attribute__ ((section(".slab_entrystub"))) void _slab_interface(void){	\
-	asm volatile (							\
-											\
-			__VA_ARGS__ 					\
-											\
-			:								\
-			:								\
-			:								\
-		);									\
-}											\
-
-
-#define XMHF_SLAB_DEFINTERFACEBARE(fn) __attribute__((naked)) __attribute__ ((section(".slab_entrystub"))) void _slab_interface(void){	\
-	asm volatile (							\
-			"jmp "#fn" \r\n" 				\
-			:								\
-			:								\
-			:								\
-		);									\
-}											\
-
-/*
-#define _XMHF_SLAB_DEFIMPORTFNSTUB(src_slab_index, dest_slab_index, fnnum, fn_paramsize, fn_retsize, fn_aggregateret) asm volatile(	\
-						"pushl %%edi \r\n"				\
-						"pushl %%esi \r\n"				\
-						"pushl %%ebp \r\n"				\
-						"pushl %%ecx \r\n"				\
-						"pushl %%ebx \r\n"				\
-														\
-						"leal 24(%%esp), %%esi \r\n"	\
-						"movl $1f, %%edi \r\n"			\
-						"movl %0, %%ebx \r\n"			\
-						"movl %1, %%ecx \r\n"			\
-						"movl %3, %%edx \r\n"			\
-						"movl %5, %%eax \r\n"			\
-						"movl %%eax, %%cr3 \r\n"		\
-						"movl %2, %%eax \r\n"			\
-						"jmpl *%%eax	\r\n"			\
-														\
-						"1: \r\n"						\
-						"movl %4, %%ecx \r\n"			\
-						"movl %%ecx, %%cr3 \r\n"		\
-						"movl %3, %%ecx \r\n"			\
-						"movl 24(%%esp), %%edi \r\n"	\
-						"cld \r\n"						\
-						"rep movsb \r\n"				\
-						"popl %%ebx \r\n"				\
-						"popl %%ecx \r\n"				\
-						"popl %%ebp \r\n"				\
-						"popl %%esi \r\n"				\
-						"popl %%edi \r\n"				\
-						"ret $"#fn_aggregateret" \r\n"					\
-						: 								\
-						: "i" (fnnum), "i" (fn_paramsize), "m" (_slab_table[dest_slab_index].entry_cr3), "i" (fn_retsize), "m" (_slab_table[src_slab_index].slab_macmid), "m" (_slab_table[dest_slab_index].slab_macmid)	\
-						:	 							\
-						);								\
-*/
-
-// esi = 32-bit address of input parameter base
-// edi = 32-bit address of return from slab call
-// ebp = 32-bit address of destination slab entry point
-// ecx = top 16-bits = size of result dwords
-//		 bottom 16-bits = size of parameter dwords
-// ebx = 32-bit function number
-// eax = 32-bit src slab macmid
-// edx = 32-bit dst slab macmid
-
-#define _XMHF_SLAB_DEFIMPORTFNSTUB(src_slab_index, dest_slab_index, fnnum, fn_paramsize, fn_retsize, fn_aggregateret) asm volatile(	\
-						"pushl %%edi \r\n"				\
-						"pushl %%esi \r\n"				\
-						"pushl %%ebp \r\n"				\
-						"pushl %%ecx \r\n"				\
-						"pushl %%ebx \r\n"				\
-														\
-						"leal 24(%%esp), %%esi \r\n"	\
-						"movl $1f, %%edi \r\n"			\
-						"movl %0, %%ebp \r\n"			\
-						"movw %1, %%cx \r\n"			\
-						"rol $16, %%ecx \r\n"			\
-						"movw %2, %%cx \r\n"			\
-						"movl %3, %%ebx \r\n"			\
-						"movl %4, %%eax \r\n"			\
-						"movl %5, %%edx \r\n"			\
-														\
-						"jmp _slab_trampoline \r\n"		\
-														\
-						"1: \r\n"						\
-						"popl %%ebx \r\n"				\
-						"popl %%ecx \r\n"				\
-						"popl %%ebp \r\n"				\
-						"popl %%esi \r\n"				\
-						"popl %%edi \r\n"				\
-						"ret $"#fn_aggregateret" \r\n"					\
-						: 								\
-						: "m" (_slab_table[dest_slab_index].entry_cr3), "i" (fn_retsize), "i" (fn_paramsize), "i" (fnnum), "m" (_slab_table[src_slab_index].slab_macmid), "m" (_slab_table[dest_slab_index].slab_macmid)	\
-						:	 							\
-						);								\
-
-#define XMHF_SLAB_DEFIMPORTFNSTUB(src_slab_index, dest_slab_index, fnnum, fn_paramsize, fn_retsize, fn_aggregateret) _XMHF_SLAB_DEFIMPORTFNSTUB(src_slab_index, dest_slab_index, fnnum, fn_paramsize, fn_retsize, fn_aggregateret)
-
-
-//#define XMHF_SLAB_DEFIMPORTFN(fn_decl, fn_stub)	__attribute__ ((section(".slab_trampoline"))) __attribute__((naked)) __attribute__ ((noinline)) static inline fn_decl { fn_stub }
-
-#define XMHF_SLAB_DEFIMPORTFN(fn_rettype, fn_name, fn_decl, fn_stub)	\
-	__attribute__ ((section(".slab_trampoline"))) __attribute__((naked)) __attribute__ ((noinline)) static inline fn_rettype __impslab__##fn_name fn_decl { fn_stub }	\
-
-#define XMHF_SLAB_CALL(slabfn) __impslab__##slabfn
-
-
-/*
-#define XMHF_SLAB(slab_name)	\
-	extern void _slab_interface(void);												\
-	extern u8 slab_rodata_start[];													\
-	extern u8 slab_rodata_end[];													\
-	extern u8 slab_rwdata_start[];													\
-	extern u8 slab_rwdata_end[];													\
-	extern u8 slab_code_start[];													\
-	extern u8 slab_code_end[];														\
-	extern u8 slab_stack_start[];													\
-	extern u8 slab_stack_end[];														\
-																					\
-	__attribute__ ((section(".stack"))) static u8 _slab_stack[XMHF_SLAB_STACKSIZE];	\
-																					\
-	__attribute__ ((section(".slabrodata"))) slab_header_t slab_header = {			\
-		.slab_index = 0,															\
-		.slab_macmid = 0,															\
-		.slab_privilegemask = 0,													\
-		.slab_tos = ((u32)(&_slab_stack) + sizeof(_slab_stack)), 					\
-		.slab_rodata.start = &slab_rodata_start,									\
-		.slab_rodata.end = (u32)&slab_rodata_end,									\
-		.slab_rwdata.start = &slab_rwdata_start,									\
-		.slab_rwdata.end = (u32)&slab_rwdata_end,									\
-		.slab_code.start = (u32)&slab_code_start,									\
-		.slab_code.end = (u32)&slab_code_end,										\
-		.slab_stack.start = (u32)&slab_stack_start,									\
-		.slab_stack.end = (u32)&slab_stack_end,										\
-		.entry_cr3 = &_slab_interface,												\
-	};																				\
-*/
-
-
-#define XMHF_SLAB(slab_name)	\
-	__attribute__ ((section(".stack"))) u8 _slab_stack[XMHF_SLAB_STACKSIZE];	\
-
-//	__attribute__ ((section(".slab_trampoline"))) u8 _slab_trampoline_peg[1];	\
-
 
 typedef struct {
 	u32 returnaddress;
@@ -306,145 +93,46 @@ typedef struct {
 	u32 fn_id;
 	u8 params[1];
 } __attribute__((packed)) slab_trampoline_frame_t;
-	
 
-
-//----------------------------------------------------------------------------------
-
-// ecx = 32-bit address of input parameter base
-//	   = <slab return address, edi,esi,ecx,ebp, import return address, import aggregate return, src_slabid, dst_slabid, fn_id, ...>
-// edx = lower 16-bits = operation (XMHF_SLAB_CALLP2P, ...)
-//	   = upper 16-bits = size of input parameters (in bytes) at ebp	   
-
-/*#define _XMHF_SLAB_P2P_DEFIMPORTFNSTUB(iface_paramsize) asm volatile(	\
-						"pushl %%ebp \r\n"					\
-						"pushl %%ecx \r\n"					\
-						"pushl %%esi \r\n"					\
-						"pushl %%edi \r\n"					\
-						"pushl $1f	\r\n"					\
-															\
-						"movl %%esp, %%ecx \r\n"			\
-						"movw %0, %%dx \r\n"				\
-						"rol $16, %%edx \r\n"				\
-						"movw %1, %%dx \r\n"				\
-															\
-						"jmp _slab_trampolinenew \r\n"		\
-															\
-						"1: \r\n"							\
-						"movl %2, %%ecx \r\n"				\
-						"lea 24(%%esp), %%edi \r\n"			\
-						"cld \r\n"							\
-						"rep movsb \r\n"					\
-															\
-						"addl $4, %%esp \r\n"				\
-						"popl %%edi \r\n"					\
-						"popl %%esi \r\n"					\
-						"popl %%ecx \r\n"					\
-						"popl %%ebp \r\n"					\
-															\
-						"ret $0x4 \r\n"						\
-						: 									\
-						: "i" (iface_paramsize), "i" (XMHF_SLAB_CALLP2P), "i" (sizeof(slab_retval_t))	\
-						:	 							\
-						);								\
-
-
-#define XMHF_SLAB_P2P_DEFIMPORTFNSTUB(iface_paramsize) _XMHF_SLAB_P2P_DEFIMPORTFNSTUB(iface_paramsize)
-
-
-#define XMHF_SLAB_P2P_DEFIMPORTFN(fn_rettype, fn_name, fn_decl, fn_stub)	\
-	__attribute__ ((section(".slab_trampoline"))) __attribute__((naked)) __attribute__ ((noinline)) static inline fn_rettype __impslab_p2p_##fn_name fn_decl { fn_stub }	\
-*/
 
 __attribute__ ((section(".slab_trampoline"))) __attribute__((naked)) __attribute__ ((noinline)) static inline slab_retval_t __xmhf_impslab_p2p(u32 src_slabid, u32 dst_slabid, u32 iface_subid, u32 iface_subparamsize, ...){
-	asm volatile(		
-						"pushl %%ebp \r\n"					
-						"pushl %%ecx \r\n"					
-						"pushl %%esi \r\n"					
-						"pushl %%edi \r\n"					
-						"pushl $1f	\r\n"					
-															
-						"movl %%esp, %%ecx \r\n"			
+	asm volatile(
+						"pushl %%ebp \r\n"
+						"pushl %%ecx \r\n"
+						"pushl %%esi \r\n"
+						"pushl %%edi \r\n"
+						"pushl $1f	\r\n"
+
+						"movl %%esp, %%ecx \r\n"
 						"movl 40(%%esp), %%edx \r\n"
-						"addl %0, %%edx \r\n"				
-						"rol $16, %%edx \r\n"				
-						"movw %1, %%dx \r\n"				
-						
-						"jmp _slab_trampolinenew \r\n"		
-															
-						"1: \r\n"							
-						"movl %2, %%ecx \r\n"				
-						"movl 24(%%esp), %%edi \r\n"			
-						"cld \r\n"							
-						"rep movsb \r\n"					
-															
-						"addl $4, %%esp \r\n"				
-						"popl %%edi \r\n"					
-						"popl %%esi \r\n"					
-						"popl %%ecx \r\n"					
-						"popl %%ebp \r\n"					
-															
-						"ret $0x4 \r\n"						
-						: 									
+						"addl %0, %%edx \r\n"
+						"rol $16, %%edx \r\n"
+						"movw %1, %%dx \r\n"
+
+						"jmp _slab_trampolinenew \r\n"
+
+						"1: \r\n"
+						"movl %2, %%ecx \r\n"
+						"movl 24(%%esp), %%edi \r\n"
+						"cld \r\n"
+						"rep movsb \r\n"
+
+						"addl $4, %%esp \r\n"
+						"popl %%edi \r\n"
+						"popl %%esi \r\n"
+						"popl %%ecx \r\n"
+						"popl %%ebp \r\n"
+
+						"ret $0x4 \r\n"
+						:
 						: "i"(4*sizeof(u32)), "i" (XMHF_SLAB_CALLP2P), "i" (sizeof(slab_retval_t))	\
-						:	 							
-	);									
+						:
+	);
 
 }
 
-
 //privilege-to-privilege (P2P) slab call macro
 #define XMHF_SLAB_CALL_P2P(slab_name, src_slabid, dst_slabid, iface_subid, ...) __xmhf_impslab_p2p(src_slabid, dst_slabid, iface_subid, __VA_ARGS__)
-
-/*
-// esi = interface parameter base
-// ecx = top 16-bits = parameter size
-//	   = low 16-bits = operation
-#define XMHF_SLAB_DEFINTERFACE_NEW(...) __attribute__((naked)) __attribute__ ((section(".slab_entrystub"))) void _slab_interface(void){	\
-	asm volatile (							\
-			"pushl %%ebp \r\n"				\
-			"movl %%esp, %%ebp \r\n"		\
-											\
-			"movl %%ecx, %%ebx	\r\n"		\
-			"shr $16, %%ecx \r\n"			\
-			"subl %0, %%esp \r\n"			\ 
-			"movl %%esp, %%eax \r\n"		\
-			"subl %%ecx, %%esp \r\n"		\
-			"movl %%esp, %%edi \r\n"		\
-			"cld \r\n"						\
-			"rep movsb \r\n"				\
-											\
-			"cmpw %1, %%bx \r\n"			\
-			"jne 3f \r\n"					\
-											\
-			__VA_ARGS__ 					\
-											\
-			"2:\r\n"						\
-			"movl %%edi, %%esi \r\n"		\
-			"movl %%ebp, %%esp \r\n"		\
-			"movl (%%esp), %%ebp \r\n"		\
-			"addl $4, %%esp \r\n"			\
-			"ret \r\n"						\
-											\
-			"3: \r\n"						\
-			"int $0x03 \r\n"				\
-			"hlt \r\n"						\
-			:								\
-			: "i" (sizeof(slab_retval_t)), "i" (XMHF_SLAB_CALLP2P)		\
-			:								\
-		);									\
-}											\
-
-#define _XMHF_SLAB_P2P_DEFEXPORTFN(fn_name, fn_num)		\
-			"cmpl $"#fn_num", 8(%%esp) \r\n"			\
-			"jne 1f \r\n"							\
-			"pushl %%eax \r\n"						\
-			"call "#fn_name" \r\n"					\
-			"jmp 2f \r\n"							\
-			"1:\r\n"								\
-
-#define XMHF_SLAB_P2P_DEFEXPORTFN(fn_name, fn_num)		_XMHF_SLAB_P2P_DEFEXPORTFN(fn_name, fn_num)
-*/
 
 #define XMHF_SLAB_DEF(slab_name)	\
 	__attribute__ ((section(".stack"))) u8 _slab_stack[XMHF_SLAB_STACKSIZE];	\
@@ -456,7 +144,7 @@ __attribute__ ((section(".slab_trampoline"))) __attribute__((naked)) __attribute
 											\
 			"movl %%ecx, %%ebx	\r\n"		\
 			"shr $16, %%ecx \r\n"			\
-			"subl %0, %%esp \r\n"			\ 
+			"subl %0, %%esp \r\n"			\
 			"movl %%esp, %%eax \r\n"		\
 			"subl %%ecx, %%esp \r\n"		\
 			"movl %%esp, %%edi \r\n"		\
@@ -483,9 +171,7 @@ __attribute__ ((section(".slab_trampoline"))) __attribute__((naked)) __attribute
 			: "i" (sizeof(slab_retval_t)), "i" (XMHF_SLAB_CALLP2P)		\
 			:								\
 		);									\
-}											
-
-
+}
 
 #define XMHF_SLAB_DEF_BARE(slab_name)	\
 	__attribute__ ((section(".stack"))) u8 _slab_stack[XMHF_SLAB_STACKSIZE];	\
@@ -497,7 +183,7 @@ __attribute__ ((section(".slab_trampoline"))) __attribute__((naked)) __attribute
 			: 								\
 			:								\
 		);									\
-}											
+}
 
 
 #endif //__ASSEMBLY__
