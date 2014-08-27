339ab5b93d1d046a92e2ee0e327f377e0aa9491a
Amit Vasudevan <amitvasudevan@acm.org>
2014-08-26 23:26:32 -0500

    xmhf: xmhf-core/include: cleanup slab headers. tested and works with hyperdep hypapp.

--- xmhf-slabs/xmhf-core/include/xcexhub.h
+++ xmhf-slabs/xmhf-core/include/xcexhub.h
@@ -46,32 +46,19 @@
 
 
 /*
- * 
- *  XMHF core exception handling slab 
- * 
+ *
+ *  XMHF core exception handling slab
+ *
  *  author: amit vasudevan (amitvasudevan@acm.org)
  */
 
 #ifndef __XCEXHUB_H__
 #define __XCEXHUB_H__
 
-//#define 	XMHF_SLAB_XCEXHUB_FNXCEXHUBINITIALIZE	0
 
 #ifndef __ASSEMBLY__
 
 slab_retval_t xcexhub_interface(u32 src_slabid, u32 dst_slabid, u32 fn_id, u32 fn_paramsize, ...);
-
-//#ifdef __XMHF_SLAB_CALLER_INDEX__ 
-
-//XMHF_SLAB_DEFIMPORTFN(arch_x86_idtdesc_t xcexhub_initialize(void),	XMHF_SLAB_DEFIMPORTFNSTUB(__XMHF_SLAB_CALLER_INDEX__, XMHF_SLAB_XCEXHUB_INDEX,	XMHF_SLAB_XCEXHUB_FNXCEXHUBINITIALIZE, (0)			, sizeof(arch_x86_idtdesc_t), XMHF_SLAB_FN_RETTYPE_AGGREGATE)								)
-//XMHF_SLAB_DEFIMPORTFN(void, xcexhub_initialize, (void),	XMHF_SLAB_DEFIMPORTFNSTUB(__XMHF_SLAB_CALLER_INDEX__, XMHF_SLAB_XCEXHUB_INDEX,	XMHF_SLAB_XCEXHUB_FNXCEXHUBINITIALIZE, (0)			, (0), XMHF_SLAB_FN_RETTYPE_NORMAL)								)
-
-//#else 	//!__XMHF_SLAB_CALLER_INDEX__
-
-//arch_x86_idtdesc_t xcexhub_initialize(void);
-//void xcexhub_initialize(void);
-
-//#endif	//__XMHF_SLAB_CALLER_INDEX__
 
 //----------------------------------------------------------------------
 //ARCH. BACKENDS
--- xmhf-slabs/xmhf-core/include/xcihub.h
+++ xmhf-slabs/xmhf-core/include/xcihub.h
@@ -50,25 +50,10 @@
 #ifndef __XCIHUB_H__
 #define __XCIHUB_H__
 
-//#define	XMHF_SLAB_XCIHUB_FNARCHENTRY	0
 
 #ifndef __ASSEMBLY__
 
 void xcihub_interface(void);
-//#ifdef __XMHF_SLAB_CALLER_INDEX__ 
-
-//XMHF_SLAB_DEFIMPORTFN(void, xcihub_arch_entry, (void), XMHF_SLAB_DEFIMPORTFNSTUB(__XMHF_SLAB_CALLER_INDEX__, XMHF_SLAB_XCIHUB_INDEX, XMHF_SLAB_XCIHUB_FNARCHENTRY, (0), 0, XMHF_SLAB_FN_RETTYPE_NORMAL))
-
-//#else 	//!__XMHF_SLAB_CALLER_INDEX__
-
-
-//----------------------------------------------------------------------
-//ARCH. BACKENDS
-//----------------------------------------------------------------------
-//void xcihub_arch_entry(void);
-
-
-//#endif	//__XMHF_SLAB_CALLER_INDEX__
 
 #endif //__ASSEMBLY__
 
