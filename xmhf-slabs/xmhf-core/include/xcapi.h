f10fc290c51de9214d7069dcb7112d27819cfa03
Amit Vasudevan <amitvasudevan@acm.org>
2014-09-19 10:57:06 -0500

    xmhf: xmhf-core/xcapi: add xc_api_platform_arch_enumeratedevices function in preparation to move the PCI bus enumeration logic...

--- xmhf-slabs/xmhf-core/include/xcapi.h
+++ xmhf-slabs/xmhf-core/include/xcapi.h
@@ -120,7 +120,9 @@ bool xc_api_partition_arch_addcpu(u32 partition_index, u32 cpu_index);
 void xc_api_platform_shutdown(context_desc_t context_desc);
 void xc_api_platform_arch_shutdown(context_desc_t context_desc);
 
-xc_platformdevice_desc_t xc_xc_api_platform_enumeratedevices(context_desc_t context_desc);
+xc_platformdevice_desc_t xc_api_platform_enumeratedevices(context_desc_t context_desc);
+xc_platformdevice_desc_t xc_api_platform_arch_enumeratedevices(context_desc_t context_desc);
+
 bool xc_api_platform_allocdevices_to_partition(context_desc_t context_desc, xc_platformdevice_desc_t device_descs);
 bool xc_api_platform_deallocdevices_from_partition(context_desc_t context_desc, xc_platformdevice_desc_t device_descs);
 
