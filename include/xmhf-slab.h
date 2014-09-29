745b75a0a08569bb396f4c79a5801ec1f653cf03
Amit Vasudevan <amitvasudevan@acm.org>
2014-09-29 11:34:22 -0500

    xmhf: xmhf-slab.h: refine XMHF_SLAB_DEFENTRYSTUB[BARE] macro definitions to define slab stack pointer variable and point it to the top of the slab stack to start with.

--- include/xmhf-slab.h
+++ include/xmhf-slab.h
@@ -301,14 +301,16 @@ __attribute__ ((section(".slab_trampoline"))) __attribute__((naked)) __attribute
 
 
 #define XMHF_SLAB_DEFENTRYSTUBBARE(slab_name)	\
-	__attribute__ ((section(".stack"))) u8 slab_name##_slab_stack[XMHF_SLAB_STACKSIZE];	\
+	__attribute__ ((section(".stack"))) u8 slab_name##_slab_stack[1][XMHF_SLAB_STACKSIZE];	\
+	__attribute__ ((section(".stack"))) u64 slab_name##_slab_tos[1]= { ((u64)&slab_name##_slab_stack[0] + XMHF_SLAB_STACKSIZE)  };	\
 																				\
 	__attribute__((naked)) __attribute__ ((section(".slab_entrystubnew"))) __attribute__((align(1))) void _interfacestub_##slab_name(void){	\
 }
 
 
 #define XMHF_SLAB_DEFENTRYSTUB(slab_name)	\
-	__attribute__ ((section(".stack"))) u8 slab_name##_slab_stack[XMHF_SLAB_STACKSIZE];	\
+	__attribute__ ((section(".stack"))) u8 slab_name##_slab_stack[1][XMHF_SLAB_STACKSIZE];	\
+	__attribute__ ((section(".stack"))) u64 slab_name##_slab_tos[1]= { ((u64)&slab_name##_slab_stack[0] + XMHF_SLAB_STACKSIZE)  };	\
 																				\
 	__attribute__((naked)) __attribute__ ((section(".slab_entrystubnew"))) __attribute__((align(1))) void _entrystub_##slab_name(void){	\
 	asm volatile (							\
