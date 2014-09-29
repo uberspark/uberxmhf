cc4c42db56927bc99878c29e25b8f106466fcf15
Amit Vasudevan <amitvasudevan@acm.org>
2014-09-29 15:39:33 -0500

    xmhf: shared/shared-trampoline: fix trampoline to preserve rbp; tested and works with testslab1 invocation. we get the required return results.

--- include/xmhf-slab.h
+++ include/xmhf-slab.h
@@ -290,10 +290,13 @@ __attribute__ ((section(".slab_trampoline"))) __attribute__((naked)) __attribute
             "movq $1f, %%r8 \r\n" \
             "leaq 8(%%rsp), %%r9 \r\n" \
             "movq %%rsp, "#slab_name"_slab_tos \r\n"  \
+            "int $0x03 \r\n" \
             "jmp _slab_trampolinenew \r\n" \
                         \
             "1:\r\n" \
+            "int $0x03 \r\n" \
             "movq "#slab_name"_slab_tos, %%rsp \r\n" \
+            "int $0x03 \r\n" \
             "movq %%rdi, %%rax \r\n" \
             "retq \r\n" \
             : \
