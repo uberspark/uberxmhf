32adad15860740560ab2a504010e55631b853b62
Amit Vasudevan <amitvasudevan@acm.org>
2014-10-08 14:13:46 -0500

    xmhf: xmhf-hic: prepare to initialize TSS descriptors within GDT for all the physical CPUs

--- include/arch/x86-vmx-x86pc/xmhf-x86-vmx-x86pc.h
+++ include/arch/x86-vmx-x86pc/xmhf-x86-vmx-x86pc.h
@@ -154,7 +154,7 @@ struct _memorytype {
 #define		__CS_CPL3	    0x001b	//CPL-3 code segment selector
 #define		__DS_CPL3	    0x0023  //CPL-3 data segment selector
 #define		__CS_CPL3_SE	0x002b	//CPL-3 code segment selector
-#define		__DS_CPL3_SSE	0x0033  //CPL-3 data segment selector
+#define		__DS_CPL3_SE	0x0033  //CPL-3 data segment selector
 #define 	__TRSEL 	    0x0038  //TSS (task) selector
 
 
