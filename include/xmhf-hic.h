1956fe593006578e3a6b78b62981ae7ce8f82f96
Amit Vasudevan <amitvasudevan@acm.org>
2014-10-09 15:31:54 -0500

    xmhf: xmhf-hic: add xmhfhic_arch_relinquish_control_to_init_slab in preparation to call the init slab; put BSP in ring-3 -- works.

--- include/xmhf-hic.h
+++ include/xmhf-hic.h
@@ -93,6 +93,7 @@ void xmhfhic_arch_switch_to_smp(void);
 void xmhfhic_arch_setup_base_cpu_data_structures(void);
 void xmhf_hic_arch_setup_cpu_state(u64 cpuid);
 void xmhfhic_smp_entry(u64 cpuid);
+void xmhfhic_arch_relinquish_control_to_init_slab(u64 cpuid);
 
 
 
