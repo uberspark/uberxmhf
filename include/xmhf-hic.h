2a8c54d30cb344ac1694a905bbe956e67e561fa1
Amit Vasudevan <amitvasudevan@acm.org>
2014-10-21 15:25:21 -0500

    xmhf: xmhf-hic: setup base guest state within VMCS for guest slab execution; populate host RIP to point to trampoline intercept stub.

--- include/xmhf-hic.h
+++ include/xmhf-hic.h
@@ -174,6 +174,7 @@ void __xmhfhic_safepop(u64 cpuid, u64 *src_slabid, u64 *dst_slabid, u64 *hic_cal
                        slab_output_params_t **oparams, slab_output_params_t **newoparams, u64 *oparams_size, u64 *iparams_size);
 
 
+__attribute__((naked)) void __xmhfhic_rtm_intercept_stub(void);
 __attribute__((naked)) void __xmhfhic_rtm_trampoline_stub(void);
 void __xmhfhic_rtm_trampoline(u64 hic_calltype, slab_input_params_t *iparams, u64 iparams_size, slab_output_params_t *oparams, u64 oparams_size, u64 dst_slabid, u64 src_slabid, u64 cpuid, u64 return_address, u64 return_rsp);
 
