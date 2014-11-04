209388ddbc30e8af4094b63790d7102944f0346f
Amit Vasudevan <amitvasudevan@acm.org>
2014-11-04 17:30:07 -0600

    xmhf: xmhf-verification: include assertion for approvexec hypapp within UAPI setentry; CBMC now reports verification failure indicating hyperdep and approvexec cannot be composed...

--- xmhf-verification/xmhf-hic-rtmcode-uapi-verifstubs.c
+++ xmhf-verification/xmhf-hic-rtmcode-uapi-verifstubs.c
@@ -57,9 +57,11 @@
 
 //////
 // hypapp metadata externs
-extern bool hd_activated;
+bool hd_activated=false;
 
 
+
+bool ae_activated=false;
 
 
 //extern x_slab_info_t _x_xmhfhic_common_slab_info_table[XMHF_HIC_MAX_SLABS];
@@ -745,6 +747,12 @@ static void __xmhfhic_rtm_uapihandler_mempgtbl(u64 uapicall_subnum, u64 iparams,
                 if(!hd_activated){
                     assert(!(entry & 0x4));
                 }
+
+                //approvexec invariant
+                if(!ae_activated){
+                    assert( (entry & 0x7) == 0x5 );
+                }
+
 
 
             }
