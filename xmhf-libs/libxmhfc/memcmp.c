3bb5b53d6503e69443e28111767ecdce49fb651a
Amit Vasudevan <amitvasudevan@acm.org>
2015-02-03 16:50:29 -0600

    xmhf-libs: libxmhfc: split string.c into individual function modules; tested with ssteptrace hypapp.

--- xmhf-libs/libxmhfc/memcmp.c
+++ xmhf-libs/libxmhfc/memcmp.c
@@ -45,54 +45,8 @@
  */
 
 #include <stdint.h>
-#include <string.h> 
+#include <string.h>
 
-void *memmove(void *dst_void, const void *src_void, u32 length){
-  char *dst = dst_void;
-  const char *src = src_void;
-
-  if (src < dst && dst < src + length){
-      // Have to copy backwards 
-      src += length;
-      dst += length;
-      while (length--){
-	     *--dst = *--src;
-	     }
-  }else{
-      while (length--){
-	     *dst++ = *src++;
-	    }
-  }
-
-  return dst_void;
-}
-
-
-u32 strnlen(const char * s, u32 count){
-	const char *sc;
-
-	for (sc = s; count-- && *sc != '\0'; ++sc);
-	return (u32)(sc - s);
-}
-
-void *memcpy(void * to, const void * from, u32 n)
-{
-  size_t i;
-  for(i=0; i<n; i++) {
-    ((uint8_t*)to)[i] = ((const uint8_t*)from)[i];
-  }
-  return to;
-}
-
-void *memset (void *str, int c, size_t len) {
-  register u8 *st = str;
-
-  while (len-- > 0)
-    *st++ = (u8)c;
-  return str;
-}
-
-#ifndef HAVE_MEMCMP
 int
 memcmp(const void *s1, const void *s2, size_t n)
 {
@@ -106,4 +60,3 @@ memcmp(const void *s1, const void *s2, size_t n)
     }
     return (0);
 }
-#endif /* HAVE_MEMCMP */
