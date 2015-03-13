d372436d1f40e6f24f375a1bc0f8fc59e2919052
Amit Vasudevan <amitvasudevan@acm.org>
2015-03-13 10:38:12 -0500

    xmhf: remove debug halt harness within prime; viola! successful execution with syscalllog hypapp

--- xmhf-hic/xmhf-hic-prime.c
+++ xmhf-hic/xmhf-hic-prime.c
@@ -160,8 +160,6 @@ void slab_main(slab_params_t *sp){
   	}
 #endif
 
-    HALT();
-
     _XDPRINTF_("Proceeding to setup init pagetables...\n");
     xmhfhic_setupinitpgtables();
     _XDPRINTF_("Init page table setup.\n");
