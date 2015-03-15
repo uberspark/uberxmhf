c45290e94873b4fbd43ae0bca8b519533cc3a1a9
Amit Vasudevan <amitvasudevan@acm.org>
2015-03-14 22:26:54 -0500

    xmhf: libxmhfhw: _vtd_reg --> _vtd_reg_read and _vtd_reg_write; tested with syscalllog hypapp.

--- xmhf-libs/libxmhfhw/include/xmhfhw.h
+++ xmhf-libs/libxmhfhw/include/xmhfhw.h
@@ -340,7 +340,6 @@ typedef struct {
 }__attribute__((packed)) vtd_slpgtbl_handle_t;
 
 //vt-d register access function
-void _vtd_reg(VTD_DRHD *dmardevice, u32 access, u32 reg, void *value);
 u64 _vtd_reg_read(VTD_DRHD *dmardevice, u32 reg);
 void _vtd_reg_write(VTD_DRHD *dmardevice, u32 reg, u64 value);
 VTD_DRHD *_vtd_get_drhd_struct(vtd_drhd_handle_t drhd_handle);
