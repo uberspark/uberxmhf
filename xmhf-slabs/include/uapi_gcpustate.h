ca6416cb003c2cdd96a8f8c5a178951a8f10b76a
Amit Vasudevan <amitvasudevan@acm.org>
2015-04-01 17:49:02 -0500

    xmhf: xc.h/xmhf_uapi_params_t --> xmfhicslab.h/xmhf_uapi_params_hdr_t; update uapi_gcpustate and xcihub slab accordingly. tested with approvexec.

--- xmhf-slabs/include/uapi_gcpustate.h
+++ xmhf-slabs/include/uapi_gcpustate.h
@@ -60,14 +60,14 @@
 
 
 typedef struct {
-    xmhf_uapi_params_t uapip;
+    xmhf_uapi_params_hdr_t uapiphdr;
     u64 encoding;
     u64 value;
 }__attribute__((packed)) xmhf_uapi_gcpustate_vmrw_params_t;
 
 
 typedef struct {
-    xmhf_uapi_params_t uapip;
+    xmhf_uapi_params_hdr_t uapiphdr;
     x86regs_t gprs;
 }__attribute__((packed)) xmhf_uapi_gcpustate_gprs_params_t;
 
