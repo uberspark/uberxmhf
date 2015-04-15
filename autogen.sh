7bfd9dd0daac3195e3651368e6fb6aa49f64f033
Amit Vasudevan <amitvasudevan@acm.org>
2015-04-15 00:08:44 -0500

    xmhf: switch to consolidated slab build harness; successful build and runtime test!

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-autoreconf --install
-automake --add-missing --copy >/dev/null 2>&1
+#autoreconf --install
+#automake --add-missing --copy >/dev/null 2>&1
+
+autoconf
+autoconf --output=configure_slab configure_slab.ac
 
 exit 0
