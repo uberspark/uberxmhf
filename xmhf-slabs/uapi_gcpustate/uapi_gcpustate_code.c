8f30907dffca823ec2881185a091a3fb787e5d0f
Amit Vasudevan <amitvasudevan@acm.org>
2015-04-21 14:25:53 -0500

    xmhf: for uvt_uvu_prog-->vft_prog store callee saved registers within stub since sysenter stack is volatile; regression test succesful.

--- xmhf-slabs/uapi_gcpustate/uapi_gcpustate_code.c
+++ xmhf-slabs/uapi_gcpustate/uapi_gcpustate_code.c
@@ -85,9 +85,11 @@ static bool _uapicheck_encoding_used_by_hic(u64 encoding){
 void slab_main(slab_params_t *sp){
     xmhf_uapi_params_hdr_t *uapiphdr = (xmhf_uapi_params_hdr_t *)sp->in_out_params;
 
+#if 0
     _XDPRINTF_("UAPI_GCPUSTATE: esp=%x, src=%u, dst=%u\n",
                CASM_FUNCCALL(read_esp, CASM_NOPARAM), sp->src_slabid, sp->dst_slabid);
     HALT();
+#endif // 0
 
     switch(uapiphdr->uapifn){
         case XMHF_HIC_UAPI_CPUSTATE_VMREAD:{
