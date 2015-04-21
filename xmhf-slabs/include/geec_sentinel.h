9718294e65e56e27f0c06be14f5a255e4678419d
Amit Vasudevan <amitvasudevan@acm.org>
2015-04-21 17:17:54 -0500

    xmhf: geec_sentinel: finish full RTT for uvt_uvu_prog --> vft_prog transition; successful test.

--- xmhf-slabs/include/geec_sentinel.h
+++ xmhf-slabs/include/geec_sentinel.h
@@ -86,6 +86,7 @@ CASM_FUNCDECL(void _geec_sentinel_xfer_ret_from_intercept(x86regs_t *r));
 CASM_FUNCDECL(void _geec_sentinel_xfer_vft_prog_to_uvt_uvu_prog(u32 entry_point, void *callee_stack_frame));
 CASM_FUNCDECL(void _geec_sentinel_xfer_ret_vft_prog_to_uvt_uvu_prog(void *caller_stack_frame));
 CASM_FUNCDECL(void _geec_sentinel_xfer_call_uvt_uvu_prog_to_vft_prog(u32 entry_point, void *callee_stack_frame));
+CASM_FUNCDECL(void _geec_sentinel_xfer_ret_uvt_uvu_prog_to_vft_prog(void *caller_stack_frame));
 
 
 #endif // __ASSEMBLY__
