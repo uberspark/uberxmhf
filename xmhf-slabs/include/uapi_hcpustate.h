29e0e6c654dba88d44e25e768b9cf6d64709a440
Amit Vasudevan <amitvasudevan@acm.org>
2015-04-27 09:14:24 -0500

    xmhf: populate slab gsm's with UAPI dependencies

--- xmhf-slabs/include/uapi_hcpustate.h
+++ xmhf-slabs/include/uapi_hcpustate.h
@@ -55,8 +55,8 @@
 #ifndef __UAPI_HCPUSTATE_H__
 #define __UAPI_HCPUSTATE_H__
 
-#define XMHF_HIC_UAPI_CPUSTATE_WRMSR            (0)
-#define XMHF_HIC_UAPI_CPUSTATE_RDMSR            (1)
+#define XMHF_HIC_UAPI_CPUSTATE_WRMSR            0
+#define XMHF_HIC_UAPI_CPUSTATE_RDMSR            1
 
 
 #ifndef __ASSEMBLY__
