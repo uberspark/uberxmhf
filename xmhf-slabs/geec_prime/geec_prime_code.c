c49a1b608989727104f7ebbf53c86c4eccfc5a3e
Amit Vasudevan <amitvasudevan@acm.org>
2015-04-29 11:54:46 -0500

    xmhf: geec_prime: cleanup

--- xmhf-slabs/geec_prime/geec_prime_code.c
+++ xmhf-slabs/geec_prime/geec_prime_code.c
@@ -879,40 +879,6 @@ void xmhfhic_arch_setup_slab_device_allocation(void){
     }
 
 
-#if 0
-    //enumerate PCI bus to find out all the devices
-	//bus numbers range from 0-255, device from 0-31 and function from 0-7
-	for(b=0; b < PCI_BUS_MAX; b++){
-		for(d=0; d < PCI_DEVICE_MAX; d++){
-			for(f=0; f < PCI_FUNCTION_MAX; f++){
-				u32 vendor_id, device_id;
-
-				//read device and vendor ids, if no device then both will be 0xFFFF
-				xmhf_baseplatform_arch_x86_pci_type1_read(b, d, f, PCI_CONF_HDR_IDX_VENDOR_ID, sizeof(u16), &vendor_id);
-				xmhf_baseplatform_arch_x86_pci_type1_read(b, d, f, PCI_CONF_HDR_IDX_DEVICE_ID, sizeof(u16), &device_id);
-				if(vendor_id == 0xFFFF && device_id == 0xFFFF)
-					break;
-
-                spl.dst_uapifn = XMHFGEEC_UAPI_SDEVPGTBL_BINDDEVICE;
-                binddevicep->dst_slabid = _geec_prime_getslabfordevice(b, d, f);
-                if(binddevicep->dst_slabid == 0xFFFFFFFFUL){
-                    _XDPRINTF_("%s: Warning, could not find slab for device, skipping\n", __func__);
-                    //HALT();
-                }else{
-                    binddevicep->bus = b;
-                    binddevicep->dev = d;
-                    binddevicep->func = f;
-                    binddevicep->pagewalk_level = vtd_pagewalk_level;
-                    XMHF_SLAB_CALLNEW(&spl);
-                    _XDPRINTF_("  Allocated device %x:%x:%x(%x:%x) to slab %u\n",
-                           b, d, f, vendor_id, device_id, binddevicep->dst_slabid);
-                }
-			}
-		}
-	}
-#endif // 0
-
-
 }
 
 
