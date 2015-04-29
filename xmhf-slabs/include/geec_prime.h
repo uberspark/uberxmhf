03a9d675e9c563efd731eafd9d8fe379e1029d04
Amit Vasudevan <amitvasudevan@acm.org>
2015-04-29 10:34:29 -0500

    xmhf: geec_prime: add typedef sysdev_memioregions_t to hold system device (MM)IO extents

--- xmhf-slabs/include/geec_prime.h
+++ xmhf-slabs/include/geec_prime.h
@@ -72,18 +72,31 @@ typedef struct {
 #define MAX_SLAB_MEMIOREGIONS_MEMEXTENTS (PCI_CONF_MAX_BARS * MAX_PLATFORM_DEVICES)
 #define MAX_SLAB_MEMIOREGIONS_IOEXTENTS (PCI_CONF_MAX_BARS * MAX_PLATFORM_DEVICES)
 
+#define _MEMIOREGIONS_EXTENTS_TYPE_MEM  0
+#define _MEMIOREGIONS_EXTENTS_TYPE_IO   1
+#define _MEMIOREGIONS_EXTENTS_TYPE_NONE 3
 
 typedef struct {
+    u32 extent_type;
     u32 addr_start;
     u32 addr_end;
-} __attribute__((packed)) _slab_memioregions_extents_t;
+} __attribute__((packed)) _memioregions_extents_t;
 
 typedef struct {
     u32 num_memextents;
     u32 num_ioextents;
-    _slab_memioregions_extents_t memextents[MAX_SLAB_MEMIOREGIONS_MEMEXTENTS];
-    _slab_memioregions_extents_t ioextents[MAX_SLAB_MEMIOREGIONS_IOEXTENTS];
+    _memioregions_extents_t memextents[MAX_SLAB_MEMIOREGIONS_MEMEXTENTS];
+    _memioregions_extents_t ioextents[MAX_SLAB_MEMIOREGIONS_IOEXTENTS];
 } __attribute__((packed)) slab_memioregions_t;
+
+
+typedef struct {
+    u32 b;
+    u32 d;
+    u32 f;
+    _memioregions_extents_t memioextents[PCI_CONF_MAX_BARS];
+} __attribute__((packed)) sysdev_memioregions_t;
+
 
 extern __attribute__(( section(".data") )) XMHF_BOOTINFO *xcbootinfo;
 
