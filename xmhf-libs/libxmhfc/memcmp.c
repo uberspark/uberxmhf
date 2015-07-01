b5c3e87304881bf4db29bf8274ba78da2275876e
Amit Vasudevan <amitvasudevan@acm.org>
2015-07-01 09:05:51 -0500

    libxmhfc: plug in ACSL annotated version of memcmp.c; successful build and test.

--- xmhf-libs/libxmhfc/memcmp.c
+++ xmhf-libs/libxmhfc/memcmp.c
@@ -47,6 +47,7 @@
 #include <stdint.h>
 #include <string.h>
 
+#if 0
 int
 memcmp(const void *s1, const void *s2, size_t n)
 {
@@ -60,3 +61,51 @@ memcmp(const void *s1, const void *s2, size_t n)
     }
     return (0);
 }
+#endif // 0
+
+
+
+/*@
+  requires n >= 0;
+  requires \valid(((char*)s1)+(0..n-1));
+  requires \valid(((char*)s2)+(0..n-1));
+  requires \separated(((char*)s1)+(0..n-1), ((char*)s2)+(0..n-1));
+  assigns \nothing;
+  behavior eq:
+    assumes n >= 0;
+    assumes \forall integer i; 0 <= i < n ==> ((unsigned char*)s1)[i] == ((unsigned char*)s2)[i];
+    ensures \result == 0;
+  behavior not_eq:
+    assumes n > 0;
+    assumes \exists integer i; 0 <= i < n && ((unsigned char*)s1)[i] != ((unsigned char*)s2)[i];
+    ensures \result != 0;
+  complete behaviors;
+  disjoint behaviors;
+@*/
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+  const unsigned char *c1 = s1, *c2 = s2;
+  int d = 0;
+
+  /*@
+    loop invariant N_RANGE: 0 <= n <= \at(n, Pre);
+    loop invariant C1_RANGE: c1 == (unsigned char*)s1+(\at(n,Pre) - n);
+    loop invariant C2_RANGE: c2 == (unsigned char*)s2+(\at(n,Pre) - n);
+    loop invariant COMPARE: \forall integer i; 0 <= i < (\at(n, Pre) - n) ==> ((unsigned char*)s1)[i] == ((unsigned char*)s2)[i];
+    loop invariant D_ZERO: d == 0;
+    loop assigns n, d, c1, c2;
+    loop variant n;
+  @*/
+  while (/*n--*/ n) {
+    d = (int)*c1++ - (int)*c2++;
+    if (d)
+      break;
+
+    n--; //inserted code
+  }
+
+  return d;
+}
+
+
+
