40730cd44415bbfb1cd4c4fc8adfa4941759f0b6
Amit Vasudevan <amitvasudevan@acm.org>
2015-08-11 11:49:55 -0500

    libxmhfhw: verify memory-safety of txtreadprivcr

--- xmhf-libs/libxmhfhw/xmhfhw_cpu_txtreadprivcr.c
+++ xmhf-libs/libxmhfhw/xmhfhw_cpu_txtreadprivcr.c
@@ -44,7 +44,6 @@
  * @XMHF_LICENSE_HEADER_END@
  */
 
-//xmhfhw_cpu - base CPU functions
 //author: amit vasudevan (amitvasudevan@acm.org)
 
 #include <xmhf.h>
@@ -52,6 +51,9 @@
 #include <xmhfhw.h>
 #include <xmhf-debug.h>
 
+/*@
+	assigns \nothing;
+@*/
 uint64_t read_priv_config_reg(uint32_t reg)
 {
     return CASM_FUNCCALL(read_config_reg,TXT_PRIV_CONFIG_REGS_BASE, reg);
