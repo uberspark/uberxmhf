543ae6c3db821ade6578e2bf31ee5b59c0b0c44c
Amit Vasudevan <amitvasudevan@acm.org>
2015-09-23 19:04:35 -0500

    geec_prime: localize verified hypervisor slab (vhslab) memory page table setup and load; successful build and test.

--- xmhf-slabs/geec_prime/geec_prime_code.c
+++ xmhf-slabs/geec_prime/geec_prime_code.c
@@ -194,7 +194,8 @@ void _geec_prime_main(slab_params_t *sp){
 
     //switch to prime page tables
     _XDPRINTF_("Proceeding to switch to GEEC_PRIME pagetables...\n");
-    CASM_FUNCCALL(write_cr3,(u32)xmhfgeec_slab_info_table[XMHFGEEC_SLAB_GEEC_PRIME].mempgtbl_cr3);
+    //CASM_FUNCCALL(write_cr3,(u32)xmhfgeec_slab_info_table[XMHFGEEC_SLAB_GEEC_PRIME].mempgtbl_cr3);
+    CASM_FUNCCALL(write_cr3,(u32)&gp_vhslabmempgtbl_lvl4t);
     _XDPRINTF_("Switched to GEEC_PRIME pagetables...\n");
 
 /*    //transfer control to geec_primesmp
@@ -1984,8 +1985,6 @@ void xmhfhic_arch_setup_slab_mem_page_tables(void){
     //_geec_prime_populate_slab_pagetables_pae4k(XMHFGEEC_SLAB_GEEC_PRIME);
     gp_setup_vhslab_mempgtbl();
    	_XDPRINTF_("%s: populated verified slabs' memory page tables\n", __func__);
-	_XDPRINTF_("%s: halting\n", __func__);
-	HALT();
 
 
     //setup unverified slabs's page tables
