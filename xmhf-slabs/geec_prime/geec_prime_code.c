4cad4743cb05a328e304eaab2892b57a3538ca30
Amit Vasudevan <amitvasudevan@acm.org>
2015-09-24 10:28:25 -0500

    geec_prime: prepare to localize gp_setup_uhslab_mempgtbl

--- xmhf-slabs/geec_prime/geec_prime_code.c
+++ xmhf-slabs/geec_prime/geec_prime_code.c
@@ -1823,80 +1823,68 @@ static void gp_setup_ugslab_mempgtbl(u32 slabid){
 
 
 
-//pae4k
+//setup unverified hypervisor (uh) slab memory page tables
 static void gp_setup_uhslab_mempgtbl(u32 slabid){
 	u64 gpa;
 	u64 flags;
 	u32 spatype;
-    u32 spa_slabregion, spa_slabtype;
-    u32 slabtype = xmhfgeec_slab_info_table[slabid].slabtype;
+	u32 spa_slabregion, spa_slabtype;
+	u32 slabtype = xmhfgeec_slab_info_table[slabid].slabtype;
 
 
-    slab_params_t spl;
-    xmhfgeec_uapi_slabmempgtbl_setentryforpaddr_params_t *setentryforpaddrp =
-        (xmhfgeec_uapi_slabmempgtbl_setentryforpaddr_params_t *)spl.in_out_params;
+	slab_params_t spl;
+	xmhfgeec_uapi_slabmempgtbl_setentryforpaddr_params_t *setentryforpaddrp =
+	(xmhfgeec_uapi_slabmempgtbl_setentryforpaddr_params_t *)spl.in_out_params;
 
-    spl.src_slabid = XMHFGEEC_SLAB_GEEC_PRIME;
-    spl.dst_slabid = XMHFGEEC_SLAB_UAPI_SLABMEMPGTBL;
-    spl.cpuid = 0; //XXX: fixme, need to plug in BSP cpuid
+	spl.src_slabid = XMHFGEEC_SLAB_GEEC_PRIME;
+	spl.dst_slabid = XMHFGEEC_SLAB_UAPI_SLABMEMPGTBL;
+	spl.cpuid = 0; //XXX: fixme, need to plug in BSP cpuid
 
 	for(gpa=0; gpa < ADDR_4GB; gpa += PAGE_SIZE_4K){
-        spatype =  _geec_prime_slab_getspatype(slabid, (u32)gpa);
-        spa_slabregion = spatype & 0x0000000FUL;
-        spa_slabtype =spatype & 0x000000F0UL;
-        flags = _geec_prime_slab_getptflagsforspa_pae(slabid, (u32)gpa, spatype);
-        //_XDPRINTF_("gpa=%08x, flags=%016llx\n", (u32)gpa, flags);
+		spatype =  _geec_prime_slab_getspatype(slabid, (u32)gpa);
+		spa_slabregion = spatype & 0x0000000FUL;
+		spa_slabtype =spatype & 0x000000F0UL;
+		flags = _geec_prime_slab_getptflagsforspa_pae(slabid, (u32)gpa, spatype);
+		//_XDPRINTF_("gpa=%08x, flags=%016llx\n", (u32)gpa, flags);
 
-        if(spa_slabregion == _SLAB_SPATYPE_GEEC_PRIME_IOTBL &&
-           slabtype != XMHFGEEC_SLABTYPE_VfT_PROG && slabtype != XMHFGEEC_SLABTYPE_VfT_SENTINEL){
-            //map unverified slab iotbl instead (12K)
-            spl.dst_uapifn = XMHFGEEC_UAPI_SLABMEMPGTBL_SETENTRYFORPADDR;
-            setentryforpaddrp->dst_slabid = slabid;
-            setentryforpaddrp->gpa = gpa;
-            setentryforpaddrp->entry = pae_make_pte(xmhfgeec_slab_info_table[slabid].iotbl_base, flags);
-            XMHF_SLAB_CALLNEW(&spl);
-            //_XDPRINTF_("slab %u: iotbl mapping, orig gpa=%08x, revised entry=%016llx\n", slabid,
-            //           (u32)gpa, setentryforpaddrp->entry);
+		if(spa_slabregion == _SLAB_SPATYPE_GEEC_PRIME_IOTBL &&
+		   slabtype != XMHFGEEC_SLABTYPE_VfT_PROG && slabtype != XMHFGEEC_SLABTYPE_VfT_SENTINEL){
+			//map unverified slab iotbl instead (12K)
+			spl.dst_uapifn = XMHFGEEC_UAPI_SLABMEMPGTBL_SETENTRYFORPADDR;
+			setentryforpaddrp->dst_slabid = slabid;
+			setentryforpaddrp->gpa = gpa;
+			setentryforpaddrp->entry = pae_make_pte(xmhfgeec_slab_info_table[slabid].iotbl_base, flags);
+			XMHF_SLAB_CALLNEW(&spl);
+			//_XDPRINTF_("slab %u: iotbl mapping, orig gpa=%08x, revised entry=%016llx\n", slabid,
+			//           (u32)gpa, setentryforpaddrp->entry);
 
-            gpa += PAGE_SIZE_4K;
-            spl.dst_uapifn = XMHFGEEC_UAPI_SLABMEMPGTBL_SETENTRYFORPADDR;
-            setentryforpaddrp->dst_slabid = slabid;
-            setentryforpaddrp->gpa = gpa;
-            setentryforpaddrp->entry = pae_make_pte(xmhfgeec_slab_info_table[slabid].iotbl_base+PAGE_SIZE_4K, flags);
-            XMHF_SLAB_CALLNEW(&spl);
-            //_XDPRINTF_("slab %u: iotbl mapping, orig gpa=%08x, revised entry=%016llx\n", slabid,
-            //           (u32)gpa, setentryforpaddrp->entry);
+			gpa += PAGE_SIZE_4K;
+			spl.dst_uapifn = XMHFGEEC_UAPI_SLABMEMPGTBL_SETENTRYFORPADDR;
+			setentryforpaddrp->dst_slabid = slabid;
+			setentryforpaddrp->gpa = gpa;
+			setentryforpaddrp->entry = pae_make_pte(xmhfgeec_slab_info_table[slabid].iotbl_base+PAGE_SIZE_4K, flags);
+			XMHF_SLAB_CALLNEW(&spl);
+			//_XDPRINTF_("slab %u: iotbl mapping, orig gpa=%08x, revised entry=%016llx\n", slabid,
+			//           (u32)gpa, setentryforpaddrp->entry);
 
-            gpa += PAGE_SIZE_4K;
-            spl.dst_uapifn = XMHFGEEC_UAPI_SLABMEMPGTBL_SETENTRYFORPADDR;
-            setentryforpaddrp->dst_slabid = slabid;
-            setentryforpaddrp->gpa = gpa;
-            setentryforpaddrp->entry = pae_make_pte(xmhfgeec_slab_info_table[slabid].iotbl_base+(2*PAGE_SIZE_4K), flags);
-            XMHF_SLAB_CALLNEW(&spl);
-            //_XDPRINTF_("slab %u: iotbl mapping, orig gpa=%08x, revised entry=%016llx\n", slabid,
-            //           (u32)gpa, setentryforpaddrp->entry);
+			gpa += PAGE_SIZE_4K;
+			spl.dst_uapifn = XMHFGEEC_UAPI_SLABMEMPGTBL_SETENTRYFORPADDR;
+			setentryforpaddrp->dst_slabid = slabid;
+			setentryforpaddrp->gpa = gpa;
+			setentryforpaddrp->entry = pae_make_pte(xmhfgeec_slab_info_table[slabid].iotbl_base+(2*PAGE_SIZE_4K), flags);
+			XMHF_SLAB_CALLNEW(&spl);
+			//_XDPRINTF_("slab %u: iotbl mapping, orig gpa=%08x, revised entry=%016llx\n", slabid,
+			//           (u32)gpa, setentryforpaddrp->entry);
 
-            gpa += PAGE_SIZE_4K;
-        }else{
-            spl.dst_uapifn = XMHFGEEC_UAPI_SLABMEMPGTBL_SETENTRYFORPADDR;
-            setentryforpaddrp->dst_slabid = slabid;
-            setentryforpaddrp->gpa = gpa;
-            setentryforpaddrp->entry = pae_make_pte(gpa, flags);
-            XMHF_SLAB_CALLNEW(&spl);
-        }
-
+			gpa += PAGE_SIZE_4K;
+		}else{
+			spl.dst_uapifn = XMHFGEEC_UAPI_SLABMEMPGTBL_SETENTRYFORPADDR;
+			setentryforpaddrp->dst_slabid = slabid;
+			setentryforpaddrp->gpa = gpa;
+			setentryforpaddrp->entry = pae_make_pte(gpa, flags);
+			XMHF_SLAB_CALLNEW(&spl);
+		}
 	}
-
-/*#if defined (__DEBUG_SERIAL__)
-	for(gpa=ADDR_LIBXMHFDEBUGDATA_START; gpa < ADDR_LIBXMHFDEBUGDATA_END; gpa += PAGE_SIZE_4K){
-        spl.dst_uapifn = XMHFGEEC_UAPI_SLABMEMPGTBL_SETENTRYFORPADDR;
-        setentryforpaddrp->dst_slabid = slabid;
-        setentryforpaddrp->gpa = gpa;
-        setentryforpaddrp->entry = pae_make_pte(gpa, (_PAGE_PRESENT |  _PAGE_RW | _PAGE_USER));
-        XMHF_SLAB_CALLNEW(&spl);
-	}
-#endif
-*/
 
 }
 
