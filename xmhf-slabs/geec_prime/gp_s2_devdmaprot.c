b0ffeeb451cde055100e18d1f984981dc1b768a7
Amit Vasudevan <amitvasudevan@acm.org>
2015-10-02 18:46:58 -0500

    geec_prime: move in all logic from uapi_slabdevmempgtbl; successful build and test.

--- xmhf-slabs/geec_prime/gp_s2_devdmaprot.c
+++ xmhf-slabs/geec_prime/gp_s2_devdmaprot.c
@@ -167,12 +167,162 @@ static u32 _geec_prime_getslabfordevice(u32 bus, u32 dev, u32 func){
 
 
 
+__attribute__((section(".data"))) __attribute__((aligned(4096))) static vtd_ret_entry_t _slabdevpgtbl_vtd_ret[VTD_RET_MAXPTRS];
+__attribute__((section(".data"))) __attribute__((aligned(4096))) static vtd_cet_entry_t _slabdevpgtbl_vtd_cet[VTD_RET_MAXPTRS][VTD_CET_MAXPTRS];
+
+__attribute__((section(".data"))) static bool _slabdevpgtbl_init_done = false;
+__attribute__((section(".data"))) static bool _slabdevpgtbl_initretcet_done = false;
+//__attribute__((section(".data"))) static u32 _slabdevpgtbl_vtd_pagewalk_level = VTD_PAGEWALK_NONE;
+
+
+
+
+__attribute__((section(".data"))) __attribute__((aligned(4096))) vtd_pml4te_t _slabdevpgtbl_pml4t[XMHFGEEC_TOTAL_SLABS][PAE_MAXPTRS_PER_PML4T];
+__attribute__((section(".data"))) __attribute__((aligned(4096))) vtd_pdpte_t _slabdevpgtbl_pdpt[XMHFGEEC_TOTAL_SLABS][PAE_MAXPTRS_PER_PDPT];
+__attribute__((section(".data"))) __attribute__((aligned(4096))) vtd_pdte_t _slabdevpgtbl_pdt[XMHFGEEC_TOTAL_SLABS][PAE_PTRS_PER_PDPT][PAE_PTRS_PER_PDT];
+__attribute__((section(".data"))) __attribute__((aligned(4096))) vtd_pte_t _slabdevpgtbl_pt[XMHFGEEC_TOTAL_SLABS][MAX_SLAB_DMADATA_PDT_ENTRIES][PAE_PTRS_PER_PT];
+__attribute__((section(".data"))) _slabdevpgtbl_infotable_t _slabdevpgtbl_infotable[XMHFGEEC_TOTAL_SLABS];
+
+static void _slabdevpgtbl_init(void){
+    u32 i;
+
+    for(i=0; i < XMHFGEEC_TOTAL_SLABS; i++){
+        _slabdevpgtbl_infotable[i].devpgtbl_initialized=false;
+    }
+}
+
+
+static void _slabdevpgtbl_initretcet(void){
+    u32 i, j;
+
+    for(i=0; i< VTD_RET_MAXPTRS; i++){
+        _slabdevpgtbl_vtd_ret[i].qwords[0] =
+            vtd_make_rete((u64)&_slabdevpgtbl_vtd_cet[i], VTD_RET_PRESENT);
+        _slabdevpgtbl_vtd_ret[i].qwords[1] = 0ULL;
+
+        for(j=0; j < VTD_CET_MAXPTRS; j++){
+            _slabdevpgtbl_vtd_cet[i][j].qwords[0] =
+                _slabdevpgtbl_vtd_cet[i][j].qwords[1] = 0ULL;
+        }
+    }
+}
+
+
+static void _slabdevpgtbl_initdevpgtbl(u32 slabid){
+    u32 i;
+    u64 default_flags = (VTD_PAGE_READ | VTD_PAGE_WRITE);
+    u32 paddr=0, pt_paddr;
+    u32 pt_index=0;
+    u32 paddr_dmadata_start, paddr_dmadata_end;
+
+    paddr_dmadata_start =
+        xmhfgeec_slab_info_table[slabid].slab_physmem_extents[3].addr_start;
+    paddr_dmadata_end =
+        xmhfgeec_slab_info_table[slabid].slab_physmem_extents[3].addr_end;
+
+    //sanity checks
+    if(slabid > XMHFGEEC_TOTAL_SLABS){
+        _XDPRINTF_("%s: Error: slabid (%u) > XMHFGEEC_TOTAL_SLABS(%u). bailing out!\n", __func__, slabid, XMHFGEEC_TOTAL_SLABS);
+        return;
+    }
+    if( (paddr_dmadata_end - paddr_dmadata_start) >
+        MAX_SLAB_DMADATA_SIZE ){
+        _XDPRINTF_("%s: Error: slab %u dmadata section over limit. bailing out!\n",
+                   __func__, slabid);
+        return;
+    }
+
+
+    //initialize lvl1 page table (pml4t)
+    memset(&_slabdevpgtbl_pml4t[slabid], 0, sizeof(_slabdevpgtbl_pml4t[0]));
+    _slabdevpgtbl_pml4t[slabid][0] =
+        vtd_make_pml4te((u64)_slabdevpgtbl_pdpt[slabid], default_flags);
+
+    //initialize lvl2 page table (pdpt)
+    memset(&_slabdevpgtbl_pdpt[slabid], 0, sizeof(_slabdevpgtbl_pdpt[0]));
+    for(i=0; i < VTD_PTRS_PER_PDPT; i++){
+        _slabdevpgtbl_pdpt[slabid][i] =
+            vtd_make_pdpte((u64)_slabdevpgtbl_pdt[slabid][i], default_flags);
+    }
+
+
+    paddr = paddr_dmadata_start;
+
+    do {
+        //grab index of pdpt, pdt this paddr
+        u32 pdpt_index = pae_get_pdpt_index(paddr);
+        u32 pdt_index = pae_get_pdt_index(paddr);
+
+        //stick a pt for the pdt entry
+        _slabdevpgtbl_pdt[slabid][pdpt_index][pdt_index] =
+            vtd_make_pdte((u64)_slabdevpgtbl_pt[slabid][pt_index], default_flags);
+
+        //populate pt entries
+        pt_paddr = paddr;
+        for(i=0; i < VTD_PTRS_PER_PT; i++){
+            _slabdevpgtbl_pt[slabid][pt_index][i] =
+                vtd_make_pte(pt_paddr, default_flags);
+            pt_paddr += PAGE_SIZE_4K;
+        }
+
+        pt_index++;
+        paddr += PAGE_SIZE_2M;
+    } while (paddr < paddr_dmadata_end);
+
+
+    _slabdevpgtbl_infotable[slabid].devpgtbl_initialized = true;
+}
+
+
+
+static void _slabdevpgtbl_binddevice(u32 slabid, u32 pagewalk_lvl,  u32 bus, u32 dev, u32 func){
+    //sanity checks
+    if(slabid > XMHFGEEC_TOTAL_SLABS){
+        _XDPRINTF_("%s: Error: slabid (%u) > XMHFGEEC_TOTAL_SLABS(%u). bailing out!\n", __func__, slabid, XMHFGEEC_TOTAL_SLABS);
+        return;
+    }
+
+    if(!_slabdevpgtbl_infotable[slabid].devpgtbl_initialized){
+        _XDPRINTF_("%s: Error: slabid (%u) devpgtbl not initialized\n",
+                   __func__, slabid);
+        return;
+    }
+
+    if ( !(bus < PCI_BUS_MAX &&
+           dev < PCI_DEVICE_MAX &&
+           func < PCI_FUNCTION_MAX) ){
+        _XDPRINTF_("%s: Error: slabid (%u) b:d:f out of limits\n",
+                   __func__, slabid);
+        return;
+    }
+
+    //b is our index into ret
+    // (d* PCI_FUNCTION_MAX) + f = index into the cet
+    if(pagewalk_lvl == VTD_PAGEWALK_4LEVEL){
+        _slabdevpgtbl_vtd_cet[bus][((dev*PCI_FUNCTION_MAX) + func)].qwords[0] =
+            vtd_make_cete((u64)&_slabdevpgtbl_pml4t[slabid], VTD_CET_PRESENT);
+        _slabdevpgtbl_vtd_cet[bus][((dev*PCI_FUNCTION_MAX) + func)].qwords[1] =
+            vtd_make_cetehigh(2, (slabid+1));
+    }else if (pagewalk_lvl == VTD_PAGEWALK_3LEVEL){
+        _slabdevpgtbl_vtd_cet[bus][((dev*PCI_FUNCTION_MAX) + func)].qwords[0] =
+            vtd_make_cete((u64)&_slabdevpgtbl_pdpt[slabid], VTD_CET_PRESENT);
+        _slabdevpgtbl_vtd_cet[bus][((dev*PCI_FUNCTION_MAX) + func)].qwords[1] =
+            vtd_make_cetehigh(1, (slabid+1));
+    }else{ //unknown page walk length, fail
+        _XDPRINTF_("%s: Error: slabid (%u) unknown pagewalk\n",
+                   __func__, slabid);
+        return;
+    }
+
+
+}
 
 
 void xmhfhic_arch_setup_slab_device_allocation(void){
     u32 i, vtd_pagewalk_level;
+
     //slab_platformdevices_t ddescs;
-    slab_params_t spl;
+    /*slab_params_t spl;
     xmhfgeec_uapi_slabdevpgtbl_initretcet_params_t *initretcetp =
         (xmhfgeec_uapi_slabdevpgtbl_initretcet_params_t *)spl.in_out_params;
     xmhfgeec_uapi_slabdevpgtbl_initdevpgtbl_params_t *initdevpgtblp =
@@ -184,55 +334,84 @@ void xmhfhic_arch_setup_slab_device_allocation(void){
     spl.src_slabid = XMHFGEEC_SLAB_GEEC_PRIME;
     spl.dst_slabid = XMHFGEEC_SLAB_UAPI_SLABDEVPGTBL;
     spl.cpuid = 0; //XXX: fixme, needs to be BSP id
+*/
 
 
-
+	u32 retpaddr;
 
 
     //slabdevpgtbl:init
-    spl.dst_uapifn = XMHFGEEC_UAPI_SDEVPGTBL_INIT;
-    XMHF_SLAB_CALLNEW(&spl);
+    //spl.dst_uapifn = XMHFGEEC_UAPI_SDEVPGTBL_INIT;
+    //XMHF_SLAB_CALLNEW(&spl);
+	if(!_slabdevpgtbl_init_done){
+	_slabdevpgtbl_init();
+	_slabdevpgtbl_init_done=true;
+	}
+
+
 
     //slabdevpgtbl:initretcet
-    spl.dst_uapifn = XMHFGEEC_UAPI_SDEVPGTBL_INITRETCET;
-    XMHF_SLAB_CALLNEW(&spl);
+    //spl.dst_uapifn = XMHFGEEC_UAPI_SDEVPGTBL_INITRETCET;
+    //XMHF_SLAB_CALLNEW(&spl);
+	if(_slabdevpgtbl_init_done){
+	if(!_slabdevpgtbl_initretcet_done){
+	    _slabdevpgtbl_initretcet();
+	    _slabdevpgtbl_initretcet_done = true;
+	}
+
+	retpaddr = (u32)&_slabdevpgtbl_vtd_ret;
+	}else{
+	retpaddr = 0;
+	}
+
+
 
     //use slabdevpgtbl:initdevpgtbl to initialize all slab device page tables
     for(i=0; i < XMHFGEEC_TOTAL_SLABS; i++){
-        spl.dst_uapifn = XMHFGEEC_UAPI_SDEVPGTBL_INITDEVPGTBL;
-        initdevpgtblp->dst_slabid = i;
-        XMHF_SLAB_CALLNEW(&spl);
+        //spl.dst_uapifn = XMHFGEEC_UAPI_SDEVPGTBL_INITDEVPGTBL;
+        //initdevpgtblp->dst_slabid = i;
+        //XMHF_SLAB_CALLNEW(&spl);
+
+            if(_slabdevpgtbl_init_done)
+                _slabdevpgtbl_initdevpgtbl(i);
+
     }
     _XDPRINTF_("%s: initialized slab device page tables\n", __func__);
 
     //intialize VT-d subsystem and obtain
-    vtd_pagewalk_level = _geec_prime_vtd_initialize(initretcetp->result_retpaddr);
+    vtd_pagewalk_level = _geec_prime_vtd_initialize(retpaddr);
     _XDPRINTF_("%s: setup vt-d, page-walk level=%u\n", __func__, vtd_pagewalk_level);
 
 
     //allocate system devices to slabs for direct DMA
     {
         u32 i;
+        u32 dst_slabid;
         for(i=0; i <numentries_sysdev_memioregions; i++){
             if(sysdev_memioregions[i].dtype == SYSDEV_MEMIOREGIONS_DTYPE_GENERAL ||
                sysdev_memioregions[i].dtype == SYSDEV_MEMIOREGIONS_DTYPE_BRIDGE ||
                sysdev_memioregions[i].dtype == SYSDEV_MEMIOREGIONS_DTYPE_UNKNOWN){
 
-                spl.dst_uapifn = XMHFGEEC_UAPI_SDEVPGTBL_BINDDEVICE;
-                binddevicep->dst_slabid = _geec_prime_getslabfordevice(sysdev_memioregions[i].b, sysdev_memioregions[i].d, sysdev_memioregions[i].f);
-                if(binddevicep->dst_slabid == 0xFFFFFFFFUL){
+                //spl.dst_uapifn = XMHFGEEC_UAPI_SDEVPGTBL_BINDDEVICE;
+                //binddevicep->dst_slabid = _geec_prime_getslabfordevice(sysdev_memioregions[i].b, sysdev_memioregions[i].d, sysdev_memioregions[i].f);
+		dst_slabid = _geec_prime_getslabfordevice(sysdev_memioregions[i].b, sysdev_memioregions[i].d, sysdev_memioregions[i].f);
+                if(dst_slabid == 0xFFFFFFFFUL){
                     _XDPRINTF_("Could not find slab for device %x:%x:%x (vid:did=%x:%x, type=%x), skipping...\n", sysdev_memioregions[i].b,
                                sysdev_memioregions[i].d, sysdev_memioregions[i].f, sysdev_memioregions[i].vendor_id,
                                sysdev_memioregions[i].device_id, sysdev_memioregions[i].dtype);
                 }else{
-                    binddevicep->bus = sysdev_memioregions[i].b;
-                    binddevicep->dev = sysdev_memioregions[i].d;
-                    binddevicep->func = sysdev_memioregions[i].f;
-                    binddevicep->pagewalk_level = vtd_pagewalk_level;
-                    XMHF_SLAB_CALLNEW(&spl);
+                    //binddevicep->bus = sysdev_memioregions[i].b;
+                    //binddevicep->dev = sysdev_memioregions[i].d;
+                    //binddevicep->func = sysdev_memioregions[i].f;
+                    //binddevicep->pagewalk_level = vtd_pagewalk_level;
+                    //XMHF_SLAB_CALLNEW(&spl);
+		    if(_slabdevpgtbl_init_done)
+			_slabdevpgtbl_binddevice(dst_slabid, vtd_pagewalk_level,
+						sysdev_memioregions[i].b, sysdev_memioregions[i].d, sysdev_memioregions[i].f);
+
                     _XDPRINTF_("Allocated device %x:%x:%x (vid:did=%x:%x, type=%x) to slab %u...\n", sysdev_memioregions[i].b,
                                sysdev_memioregions[i].d, sysdev_memioregions[i].f, sysdev_memioregions[i].vendor_id,
-                               sysdev_memioregions[i].device_id, sysdev_memioregions[i].dtype, binddevicep->dst_slabid);
+                               sysdev_memioregions[i].device_id, sysdev_memioregions[i].dtype, dst_slabid);
                 }
             }
         }
