713aaa1bedc3746859ea56672d1679dc063ec3cd
Amit Vasudevan <amitvasudevan@acm.org>
2015-10-04 21:27:38 -0500

    xc_nwlog: move xcnwlog_ls into dma data region

--- xmhf-slabs/include/xc_nwlog.h
+++ xmhf-slabs/include/xc_nwlog.h
@@ -3044,7 +3044,7 @@ typedef struct {
     u32 logbuf[16];
 }__attribute__((packed)) xcnwlog_ls_element_t;
 
-extern __attribute__((section(".data"))) xcnwlog_ls_element_t xcnwlog_ls[64];
+extern __attribute__((section(".slab_dmadata"))) xcnwlog_ls_element_t xcnwlog_ls[64];
 extern __attribute__((section(".data"))) u32 xcnwlog_ls_index;
 
 
--- xmhf-slabs/xc_nwlog/xc_nwlog_data.c
+++ xmhf-slabs/xc_nwlog/xc_nwlog_data.c
@@ -51,5 +51,5 @@
 #include <xc.h>
 #include <xc_nwlog.h>
 
-__attribute__((section(".data"))) xcnwlog_ls_element_t xcnwlog_ls[64];
+__attribute__((section(".slab_dmadata"))) xcnwlog_ls_element_t xcnwlog_ls[64];
 __attribute__((section(".data"))) u32 xcnwlog_ls_index=0;
