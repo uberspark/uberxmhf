e44bd79b577cf58bf69d2f7a1f468222fb700874
Amit Vasudevan <amitvasudevan@acm.org>
2015-10-08 10:41:27 -0500

    geec_prime: split _geec_prime_slab_getptflagsforspa_pae into gp_vhslab_mempgtbl_getptflagsforspa_pae and gp_uhslab_mempgtbl_getptflagsforspa_par; successful test.

--- xmhf-slabs/geec_prime/gp_s2_setupmempgtbl.c
+++ xmhf-slabs/geec_prime/gp_s2_setupmempgtbl.c
@@ -73,13 +73,13 @@
 
 #define _SLAB_SPATYPE_OTHER	    				(0x6)
 
-#if 0
-/*@
-  //requires n >= 0;
-	assigns \nothing;
-	ensures 0 <= \result < PAGE_SIZE_4K;
-@*/
-static u64 _geec_prime_slab_getptflagsforspa_pae(u32 slabid, u32 spa, u32 spatype);
+#if 1
+// /*@
+//   //requires n >= 0;
+// 	assigns \nothing;
+// 	ensures 0 <= \result < PAGE_SIZE_4K;
+// @*/
+//static u64 _geec_prime_slab_getptflagsforspa_pae(u32 slabid, u32 spa, u32 spatype);
 #endif // 0
 
 //@ ghost u32 gretval;
@@ -116,7 +116,7 @@ static u32 _geec_prime_slab_getspatype(u32 slab_index, u32 spa);
 static bool _geec_prime_smt_slab_getspatype_isdevicemmio(u32 slabid, u32 spa);
 
 //done
-#if 0
+#if 1
 /*@
 	requires 0 <= slabid < XMHFGEEC_TOTAL_SLABS ;
 	requires \forall u32 x; 0 <= x < MAX_PLATFORM_CPUS ==> (_sda_slab_devicemap[slabid].sysdev_mmioregions_indices[x] < MAX_PLATFORM_DEVICES);
@@ -183,7 +183,7 @@ static bool _geec_prime_smt_slab_getspatype_isdevicemmio(u32 slabid, u32 spa){
 @*/
 static bool _geec_prime_smt_slab_getspatype_isiotbl(u32 slabid, u32 spa);
 
-#if 0
+#if 1
 /*@
 	requires 0 <= slabid < XMHFGEEC_TOTAL_SLABS ;
 	assigns \nothing;
@@ -238,7 +238,7 @@ static bool _geec_prime_smt_slab_getspatype_isiotbl(u32 slabid, u32 spa){
 //shared mappings
 
 
-#if 0
+#if 1
 /*@
 	requires 0 <= slab_index < XMHFGEEC_TOTAL_SLABS ;
 	assigns \nothing;
@@ -257,7 +257,7 @@ static u32 gp_slab_getspatype_for_slab(u32 slab_index, u32 spa);
 
 
 //[DONE]
-#if 0
+#if 1
 //@ghost bool gisiotbl, gisdevicemmio;
 /*@
 	requires 0 <= slab_index < XMHFGEEC_TOTAL_SLABS ;
@@ -325,7 +325,7 @@ static u32 gp_slab_getspatype_for_slab(u32 slab_index, u32 spa){
 
 
 
-#if 0
+#if 1
 //done
 
 //@ ghost u32 gretval;
@@ -375,21 +375,16 @@ static u32 _geec_prime_slab_getspatype(u32 slab_index, u32 spa){
 
 
 
-/*@
-	requires 0 <= slabid < XMHFGEEC_TOTAL_SLABS;
-	assigns \nothing;
-@*/
-static u64 _geec_prime_slab_getptflagsforspa_pae(u32 slabid, u32 spa, u32 spatype){
+static u64 gp_uhslab_mempgtbl_getptflagsforspa_pae(u32 slabid, u32 spa, u32 spatype){
 	u64 flags=0;
-    u8 spa_slabtype, spa_slabregion;
-    bool spa_sameslab=false;
-	//_XDPRINTF_("\n%s: slab_index=%u, spa=%08x, spatype = %x\n", __func__, slab_index, spa, spatype);
-    u32 slabtype = xmhfgeec_slab_info_table[slabid].slabtype;
+	u8 spa_slabtype, spa_slabregion;
+	bool spa_sameslab=false;
+	u32 slabtype = xmhfgeec_slab_info_table[slabid].slabtype;
 
-    spa_slabregion = spatype & 0x0000000FUL;
-    spa_slabtype =spatype & 0x000000F0UL;
-    if(spatype & _SLAB_SPATYPE_MASK_SAMESLAB)
-        spa_sameslab = true;
+	spa_slabregion = spatype & 0x0000000FUL;
+	spa_slabtype =spatype & 0x000000F0UL;
+	if(spatype & _SLAB_SPATYPE_MASK_SAMESLAB)
+	spa_sameslab = true;
 
 
     switch(slabtype){
@@ -433,8 +428,32 @@ static u64 _geec_prime_slab_getptflagsforspa_pae(u32 slabid, u32 spa, u32 spatyp
         }
         break;
 
+        default:
+            //_XDPRINTF_("%s: invalid slab type=%x. Halting!\n", __func__, slabtype);
+            //HALT();
+            flags = 0;
+            break;
+
+    }
+
+    return flags;
+}
 
 
+
+static u64 gp_vhslab_mempgtl_getptflagsforspa_pae(u32 slabid, u32 spa, u32 spatype){
+	u64 flags=0;
+	u8 spa_slabtype, spa_slabregion;
+	bool spa_sameslab=false;
+	u32 slabtype = xmhfgeec_slab_info_table[slabid].slabtype;
+
+	spa_slabregion = spatype & 0x0000000FUL;
+	spa_slabtype =spatype & 0x000000F0UL;
+	if(spatype & _SLAB_SPATYPE_MASK_SAMESLAB)
+	spa_sameslab = true;
+
+
+    switch(slabtype){
         case XMHFGEEC_SLABTYPE_VfT_PROG:{
             //self slab: code=rx, data,stack,dmadata,mmio=rw, perms=SUPER
             //other slab vft: code=rx, data,stack,dmadata,mmio=rw, perms=SUPER
@@ -473,9 +492,10 @@ static u64 _geec_prime_slab_getptflagsforspa_pae(u32 slabid, u32 spa, u32 spatyp
         break;
 
         default:
-            _XDPRINTF_("%s: invalid slab type=%x. Halting!\n", __func__,
-                       slabtype);
-            HALT();
+            //_XDPRINTF_("%s: invalid slab type=%x. Halting!\n", __func__, slabtype);
+            //HALT();
+            flags = 0;
+            break;
 
     }
 
@@ -484,8 +504,7 @@ static u64 _geec_prime_slab_getptflagsforspa_pae(u32 slabid, u32 spa, u32 spatyp
 
 
 
-
-#if 0
+#if 1
 
 
 // only for uVU_PROG_GUEST, uVU_PROG_RICHGUEST and uVT_PROG_GUEST
@@ -686,7 +705,7 @@ static void gp_setup_uhslab_mempgtbl(u32 slabid){
 		spatype =  _geec_prime_slab_getspatype(slabid, (u32)gpa);
 		spa_slabregion = spatype & 0x0000000FUL;
 		spa_slabtype =spatype & 0x000000F0UL;
-		flags = _geec_prime_slab_getptflagsforspa_pae(slabid, (u32)gpa, spatype);
+		flags = gp_uhslab_mempgtbl_getptflagsforspa_pae(slabid, (u32)gpa, spatype);
 
 		//_XDPRINTF_("gpa=%08x, flags=%016llx\n", (u32)gpa, flags);
 
@@ -724,7 +743,7 @@ static void gp_setup_uhslab_mempgtbl(u32 slabid){
 #endif // 0
 
 
-#if 0
+#if 1
 //@ghost u64 gflags[PAE_PTRS_PER_PDPT * PAE_PTRS_PER_PDT * PAE_PTRS_PER_PT];
 /*@
 	assigns gp_rwdatahdr.gp_vhslabmempgtbl_lvl4t[0..(PAGE_SIZE_4K-1)];
@@ -783,7 +802,7 @@ static void gp_setup_vhslab_mempgtbl(void){
 	for(i=0; i < (PAE_PTRS_PER_PDPT * PAE_PTRS_PER_PDT * PAE_PTRS_PER_PT); ++i){
 		spatype =  _geec_prime_slab_getspatype(slabid, (u32)(i * PAGE_SIZE_4K));
 
-		flags = _geec_prime_slab_getptflagsforspa_pae(slabid, (u32)(i * PAGE_SIZE_4K), spatype);
+		flags = gp_vhslab_mempgtl_getptflagsforspa_pae(slabid, (u32)(i * PAGE_SIZE_4K), spatype);
 		//@ghost gflags[i] = flags;
 
 		gp_vhslabmempgtbl_lvl1t[i] = pae_make_pte( (i*PAGE_SIZE_4K), flags);
@@ -794,7 +813,7 @@ static void gp_setup_vhslab_mempgtbl(void){
 #endif // 0
 
 
-#if 0
+#if 1
 
 void gp_s2_setupmempgtbl(void){
     slab_params_t spl;
