476ddd36a1dcfe5d050ea71a99fa2bd7faeff9f0
Amit Vasudevan <amitvasudevan@acm.org>
2015-11-18 23:29:12 -0600

    xmhf-slabs: xcnwlog: refine logdata method logic; successful build.

--- xmhf-slabs/xc_nwlog/xcnwlog_logdata.c
+++ xmhf-slabs/xc_nwlog/xcnwlog_logdata.c
@@ -58,11 +58,13 @@
  */
 
 void xcnwlog_logdata(xcnwlog_ls_element_t *elem){
-	if(!xcnwlog_ls_push(elem)){
+	if(xcnwlog_ls_index >= XC_NWLOG_BUF_MAXELEM){
+		memcpy(&xcnwlog_lsdma, &xcnwlog_ls, sizeof(xcnwlog_ls_element_t)*XC_NWLOG_BUF_MAXELEM);
 		e1000_xmit(0);
 		e1000_wait4xmit();
 		memset(&xcnwlog_ls, 0, sizeof(xcnwlog_ls));
 		xcnwlog_ls_index=0;
+	}else{
 		xcnwlog_ls_push(elem);
 	}
 }
