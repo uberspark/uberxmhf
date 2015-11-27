a4c84c911b2bec4f8af844bf8ff7c1ebaefa10f9
Amit Vasudevan <amitvasudevan@acm.org>
2015-11-27 00:19:31 -0600

    xmhf-slabs: prime: refactor s1_iommuinit in preparation for verification; successful runtime test.

--- xmhf-slabs/geec_prime/gp_s1_iommuinit.c
+++ xmhf-slabs/geec_prime/gp_s1_iommuinit.c
@@ -55,62 +55,61 @@
 #include <xc_init.h>
 
 
-//initialize vtd hardware and return vt-d pagewalk level
-static u32 _geec_prime_vtd_initialize(u32 vtd_ret_addr){
-    u32 vtd_pagewalk_level = VTD_PAGEWALK_NONE;
-    //vtd_drhd_handle_t vtd_drhd_maxhandle=0;
-	vtd_drhd_handle_t drhd_handle;
-	//u32 vtd_dmar_table_physical_address=0;
-    vtd_slpgtbl_handle_t vtd_slpgtbl_handle;
-    u32 i, b, d, f;
 
+
+void gp_s1_iommuinit(void){
+	vtd_drhd_handle_t drhd_handle;
+	vtd_slpgtbl_handle_t vtd_slpgtbl_handle;
+	//u32 i, b, d, f;
+
+	vtd_pagewalk_level = VTD_PAGEWALK_NONE;
 
 	//initialize all DRHD units
 	for(drhd_handle=0; drhd_handle < vtd_drhd_maxhandle; drhd_handle++){
-   		VTD_CAP_REG cap;
+		VTD_CAP_REG cap;
 
 		_XDPRINTF_("%s: Setting up DRHD unit %u...\n", __func__, drhd_handle);
 
 		if(!xmhfhw_platform_x86pc_vtd_drhd_initialize(&vtd_drhd[drhd_handle]) ){
-            _XDPRINTF_("%s: error setting up DRHD unit %u. halting!\n", __func__, drhd_handle);
-			HALT();
+			_XDPRINTF_("%s: error setting up DRHD unit %u. halting!\n", __func__, drhd_handle);
+			CASM_FUNCCALL(xmhfhw_cpu_hlt, CASM_NOPARAM);
 		}
 
-        //read and store DRHD supported page-walk length
-        unpack_VTD_CAP_REG(&cap, _vtd_reg_read(&vtd_drhd[drhd_handle], VTD_CAP_REG_OFF));
-        if(cap.sagaw & 0x2){
-            if(vtd_pagewalk_level == VTD_PAGEWALK_NONE || vtd_pagewalk_level == VTD_PAGEWALK_3LEVEL){
-                vtd_pagewalk_level = VTD_PAGEWALK_3LEVEL;
-                _XDPRINTF_("%s: DRHD unit %u - 3-level page-walk\n", __func__, drhd_handle);
-            }else{
-                _XDPRINTF_("%s: Halting: mixed hardware supported page-walk lengths\n",
-                            __func__);
-                HALT();
-            }
-        }
+		//read and store DRHD supported page-walk length
+		unpack_VTD_CAP_REG(&cap, _vtd_reg_read(&vtd_drhd[drhd_handle], VTD_CAP_REG_OFF));
+		if(cap.sagaw & 0x2){
+		    if(vtd_pagewalk_level == VTD_PAGEWALK_NONE || vtd_pagewalk_level == VTD_PAGEWALK_3LEVEL){
+			vtd_pagewalk_level = VTD_PAGEWALK_3LEVEL;
+			_XDPRINTF_("%s: DRHD unit %u - 3-level page-walk\n", __func__, drhd_handle);
+		    }else{
+			_XDPRINTF_("%s: Halting: mixed hardware supported page-walk lengths\n",
+				    __func__);
+			CASM_FUNCCALL(xmhfhw_cpu_hlt, CASM_NOPARAM);
+		    }
+		}
 
-        if(cap.sagaw & 0x4){
-            if(vtd_pagewalk_level == VTD_PAGEWALK_NONE || vtd_pagewalk_level == VTD_PAGEWALK_4LEVEL){
-                vtd_pagewalk_level = VTD_PAGEWALK_4LEVEL;
-                _XDPRINTF_("%s: DRHD unit %u - 4-level page-walk\n", __func__, drhd_handle);
-            }else{
-                _XDPRINTF_("%s: Halting: mixed hardware supported page-walk lengths\n",
-                            __func__);
-                HALT();
-            }
-        }
+		if(cap.sagaw & 0x4){
+		    if(vtd_pagewalk_level == VTD_PAGEWALK_NONE || vtd_pagewalk_level == VTD_PAGEWALK_4LEVEL){
+			vtd_pagewalk_level = VTD_PAGEWALK_4LEVEL;
+			_XDPRINTF_("%s: DRHD unit %u - 4-level page-walk\n", __func__, drhd_handle);
+		    }else{
+			_XDPRINTF_("%s: Halting: mixed hardware supported page-walk lengths\n",
+				    __func__);
+			CASM_FUNCCALL(xmhfhw_cpu_hlt, CASM_NOPARAM);
+		    }
+		}
 
 
 		//set DRHD root entry table
-		if(!xmhfhw_platform_x86pc_vtd_drhd_set_root_entry_table(&vtd_drhd[drhd_handle], vtd_ret_addr)){
-            _XDPRINTF_("%s: Halting: error in setting DRHD RET\n", __func__);
-            HALT();
+		if(!xmhfhw_platform_x86pc_vtd_drhd_set_root_entry_table(&vtd_drhd[drhd_handle], (u32)&_slabdevpgtbl_vtd_ret)){
+			_XDPRINTF_("%s: Halting: error in setting DRHD RET\n", __func__);
+			CASM_FUNCCALL(xmhfhw_cpu_hlt, CASM_NOPARAM);
 		}
 
 		//invalidate caches
 		if(!xmhfhw_platform_x86pc_vtd_drhd_invalidatecaches(&vtd_drhd[drhd_handle])){
-            _XDPRINTF_("%s: Halting: error in invalidating caches\n", __func__);
-            HALT();
+			_XDPRINTF_("%s: Halting: error in invalidating caches\n", __func__);
+			CASM_FUNCCALL(xmhfhw_cpu_hlt, CASM_NOPARAM);
 		}
 
 		//enable VT-d translation
@@ -125,21 +124,8 @@ static u32 _geec_prime_vtd_initialize(u32 vtd_ret_addr){
 	//zap VT-d presence in ACPI table...
 	//TODO: we need to be a little elegant here. eventually need to setup
 	//EPT/NPTs such that the DMAR pages are unmapped for the guest
-	xmhfhw_sysmemaccess_writeu32(vtd_dmar_table_physical_address, 0UL);
+	CASM_FUNCCALL(xmhfhw_sysmemaccess_writeu32, vtd_dmar_table_physical_address, 0UL);
 
 
-    _XDPRINTF_("%s: final page-walk level=%u\n", __func__, vtd_pagewalk_level);
-
-    return vtd_pagewalk_level;
-}
-
-
-
-void gp_s1_iommuinit(void){
-
-	//intialize VT-d subsystem and obtain page-walk level
-	vtd_pagewalk_level = _geec_prime_vtd_initialize((u32)&_slabdevpgtbl_vtd_ret);
-	_XDPRINTF_("%s: setup vt-d, page-walk level=%u\n", __func__, vtd_pagewalk_level);
-
-
+	_XDPRINTF_("%s: final page-walk level=%u\n", __func__, vtd_pagewalk_level);
 }
