077c4fc29746a8c7b1b033bc93840d9409167a8c
Amit Vasudevan <amitvasudevan@acm.org>
2015-12-09 21:54:24 -0600

    xmhf-slab: prime: pare-away uh uobj memory page table setup top-level function into its own module and continue refactoring...

--- xmhf-slabs/geec_prime/gp_s2_setupmempgtbl.c
+++ xmhf-slabs/geec_prime/gp_s2_setupmempgtbl.c
@@ -58,7 +58,7 @@
 
 
 #if 1
-static u64 gp_uhslab_mempgtbl_getptflagsforspa_pae(u32 slabid, u32 spa, u32 spatype){
+u64 gp_uhslab_mempgtbl_getptflagsforspa_pae(u32 slabid, u32 spa, u32 spatype){
 	u64 flags=0;
 	u8 spa_slabtype, spa_slabregion;
 	bool spa_sameslab=false;
@@ -129,94 +129,6 @@ static u64 gp_uhslab_mempgtbl_getptflagsforspa_pae(u32 slabid, u32 spa, u32 spat
 
 
 
-#if 1
-
-
-
-//setup unverified hypervisor (uh) slab memory page tables
-static void gp_setup_uhslab_mempgtbl(u32 slabid){
-	//u64 gpa;
-	u64 flags;
-	u32 spatype;
-	u32 spa_slabregion, spa_slabtype;
-	u32 slabtype = xmhfgeec_slab_info_table[slabid].slabtype;
-	u32 uhslabmempgtbl_idx;
-	u32 i, j;
-	u64 default_flags = (u64)(_PAGE_PRESENT);
-
-	if(!(slabid >= XMHFGEEC_UHSLAB_BASE_IDX && slabid <= XMHFGEEC_UHSLAB_MAX_IDX)){
-		_XDPRINTF_("%s: slab %u --> Fatal error uV{T,U} slab out of UH slab idx bound!\n", __func__, i);
-		HALT();
-	}
-
-	uhslabmempgtbl_idx = slabid - XMHFGEEC_UHSLAB_BASE_IDX;
-
-	//pdpt
-	memset(&gp_rwdatahdr.gp_uhslabmempgtbl_lvl4t[uhslabmempgtbl_idx], 0, PAGE_SIZE_4K);
-	for(i=0; i < PAE_PTRS_PER_PDPT; i++){
-		gp_rwdatahdr.gp_uhslabmempgtbl_lvl4t[uhslabmempgtbl_idx][i] =
-		    pae_make_pdpe(&gp_uhslabmempgtbl_lvl2t[uhslabmempgtbl_idx][i], default_flags);
-	}
-
-	//pdt
-	default_flags = (u64)(_PAGE_PRESENT | _PAGE_RW | _PAGE_USER);
-	for(i=0; i < PAE_PTRS_PER_PDPT; i++){
-		for(j=0; j < PAE_PTRS_PER_PDT; j++){
-			gp_uhslabmempgtbl_lvl2t[uhslabmempgtbl_idx][i][j] =
-				pae_make_pde(&gp_uhslabmempgtbl_lvl1t[uhslabmempgtbl_idx][(i*PAE_PTRS_PER_PDT*PAE_PTRS_PER_PT)+(j*PAE_PTRS_PER_PT)], default_flags);
-		}
-	}
-
-
-	//pts
-	for(i=0; i < (PAE_PTRS_PER_PDPT * PAE_PTRS_PER_PDT * PAE_PTRS_PER_PT); i++){
-		//u64 pdpt_index = pae_get_pdpt_index(gpa);
-		//u64 pdt_index = pae_get_pdt_index(gpa);
-		//u64 pt_index = pae_get_pt_index(gpa);
-		//u32 pt_index = (gpa/PAGE_SIZE_4K);
-
-		spatype =  gp_s2_setupmpgtbl_getspatype(slabid, (u32)(i*PAGE_SIZE_4K));
-		spa_slabregion = spatype & 0x0000000FUL;
-		spa_slabtype =spatype & 0x000000F0UL;
-		flags = gp_uhslab_mempgtbl_getptflagsforspa_pae(slabid, (u32)(i*PAGE_SIZE_4K), spatype);
-
-		//_XDPRINTF_("gpa=%08x, flags=%016llx\n", (u32)gpa, flags);
-
-		if(spa_slabregion == _SLAB_SPATYPE_GEEC_PRIME_IOTBL &&
-		   slabtype != XMHFGEEC_SLABTYPE_VfT_PROG && slabtype != XMHFGEEC_SLABTYPE_VfT_SENTINEL){
-			//map unverified slab iotbl instead (12K)
-			gp_uhslabmempgtbl_lvl1t[uhslabmempgtbl_idx][i] =
-				pae_make_pte(xmhfgeec_slab_info_table[slabid].iotbl_base, flags) & (~0x80);
-			//_XDPRINTF_("slab %u: iotbl mapping, orig gpa=%08x, revised entry=%016llx\n", slabid,
-			//           (u32)gpa, setentryforpaddrp->entry);
-
-			//gpa += PAGE_SIZE_4K;
-			i++;
-
-			gp_uhslabmempgtbl_lvl1t[uhslabmempgtbl_idx][i] =
-				pae_make_pte(xmhfgeec_slab_info_table[slabid].iotbl_base+PAGE_SIZE_4K, flags) & (~0x80);
-			//_XDPRINTF_("slab %u: iotbl mapping, orig gpa=%08x, revised entry=%016llx\n", slabid,
-			//           (u32)gpa, setentryforpaddrp->entry);
-
-			//gpa += PAGE_SIZE_4K;
-			i++;
-
-			gp_uhslabmempgtbl_lvl1t[uhslabmempgtbl_idx][i] =
-				pae_make_pte(xmhfgeec_slab_info_table[slabid].iotbl_base+(2*PAGE_SIZE_4K), flags) & (~0x80);
-			//_XDPRINTF_("slab %u: iotbl mapping, orig gpa=%08x, revised entry=%016llx\n", slabid,
-			//           (u32)gpa, setentryforpaddrp->entry);
-
-			//gpa += PAGE_SIZE_4K;
-			//i++;
-		}else{
-			gp_uhslabmempgtbl_lvl1t[uhslabmempgtbl_idx][i] =
-				pae_make_pte((i*PAGE_SIZE_4K), flags) & (~0x80);
-		}
-	}
-
-}
-
-#endif // 0
 
 
 
@@ -250,7 +162,7 @@ void gp_s2_setupmpgtblu(void){
             case XMHFGEEC_SLABTYPE_uVT_PROG:
             case XMHFGEEC_SLABTYPE_uVU_PROG:{
               	_XDPRINTF_("%s: slab %u --> ppopulating uV{T,U} page-tables...\n", __func__, i);
-                gp_setup_uhslab_mempgtbl(i);
+                gp_s2_setupmpgtbluh(i);
               	_XDPRINTF_("%s: slab %u --> uV{T,U}_prog page-tables populated\n", __func__, i);
             }
             break;
