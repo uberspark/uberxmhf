6d5859d0c1ca6abb481522a0e8eda1475eb0ade1
Amit Vasudevan <amitvasudevan@acm.org>
2015-12-11 22:36:40 -0600

    xmhf-slabs: prime: checkpoint as we verify s2_setupmpgtbluh_getflags...

--- xmhf-slabs/geec_prime/gp_s2_setupmpgtbluh_getflags.c
+++ xmhf-slabs/geec_prime/gp_s2_setupmpgtbluh_getflags.c
@@ -55,20 +55,97 @@
 	requires 0 <= slabid < XMHFGEEC_TOTAL_SLABS;
 
 	behavior othermem:
+		assumes (
+			 (xmhfgeec_slab_info_table[slabid].slabtype == XMHFGEEC_SLABTYPE_uVT_PROG ||
+			  xmhfgeec_slab_info_table[slabid].slabtype == XMHFGEEC_SLABTYPE_uVU_PROG
+			 ) &&
+			 ((spatype & 0x0000000FUL) == _SLAB_SPATYPE_OTHER)
+			);
+		ensures (\result == (u64)(_PAGE_PRESENT | _PAGE_RW));
 
 	behavior vobjcode:
+		assumes (
+			 (xmhfgeec_slab_info_table[slabid].slabtype == XMHFGEEC_SLABTYPE_uVT_PROG ||
+			  xmhfgeec_slab_info_table[slabid].slabtype == XMHFGEEC_SLABTYPE_uVU_PROG
+			 ) &&
+			 !((spatype & 0x0000000FUL) == _SLAB_SPATYPE_OTHER) &&
+			 ((spatype & 0x000000F0UL) == XMHFGEEC_SLABTYPE_VfT_PROG) &&
+			 ((spatype & 0x0000000FUL) == _SLAB_SPATYPE_SLAB_CODE)
+			);
+		ensures (\result == (_PAGE_PRESENT));
 
 	behavior vobjdata:
+		assumes (
+			 (xmhfgeec_slab_info_table[slabid].slabtype == XMHFGEEC_SLABTYPE_uVT_PROG ||
+			  xmhfgeec_slab_info_table[slabid].slabtype == XMHFGEEC_SLABTYPE_uVU_PROG
+			 ) &&
+			 !((spatype & 0x0000000FUL) == _SLAB_SPATYPE_OTHER) &&
+			 ((spatype & 0x000000F0UL) == XMHFGEEC_SLABTYPE_VfT_PROG) &&
+			!((spatype & 0x0000000FUL) == _SLAB_SPATYPE_SLAB_CODE) &&
+			 ((spatype & 0x0000000FUL) == _SLAB_SPATYPE_SLAB_DATA ||
+			  (spatype & 0x0000000FUL) == _SLAB_SPATYPE_SLAB_STACK ||
+			  (spatype & 0x0000000FUL) == _SLAB_SPATYPE_SLAB_DMADATA ||
+			  (spatype & 0x0000000FUL) == _SLAB_SPATYPE_GEEC_PRIME_IOTBL
+			 )
+			);
+		ensures (\result == (_PAGE_PRESENT | _PAGE_RW | _PAGE_NX));
 
 	behavior vobjmmio:
+		assumes (
+			 (xmhfgeec_slab_info_table[slabid].slabtype == XMHFGEEC_SLABTYPE_uVT_PROG ||
+			  xmhfgeec_slab_info_table[slabid].slabtype == XMHFGEEC_SLABTYPE_uVU_PROG
+			 ) &&
+			 !((spatype & 0x0000000FUL) == _SLAB_SPATYPE_OTHER) &&
+			 ((spatype & 0x000000F0UL) == XMHFGEEC_SLABTYPE_VfT_PROG) &&
+			!((spatype & 0x0000000FUL) == _SLAB_SPATYPE_SLAB_CODE) &&
+			!((spatype & 0x0000000FUL) == _SLAB_SPATYPE_SLAB_DATA ||
+			  (spatype & 0x0000000FUL) == _SLAB_SPATYPE_SLAB_STACK ||
+			  (spatype & 0x0000000FUL) == _SLAB_SPATYPE_SLAB_DMADATA ||
+			  (spatype & 0x0000000FUL) == _SLAB_SPATYPE_GEEC_PRIME_IOTBL
+			 ) &&
+			 ((spatype & 0x0000000FUL) == _SLAB_SPATYPE_SLAB_DEVICEMMIO)
+			);
+		ensures (\result == (_PAGE_PRESENT | _PAGE_RW | _PAGE_NX | _PAGE_PCD));
 
-	behavior objcode:
+	//behavior objcode:
 
-	behavior objdata:
+	//behavior objdata:
 
-	behavior objmmio:
+	//behavior objmmio:
+
+	behavior otheruhobj:
+		assumes (
+			 (xmhfgeec_slab_info_table[slabid].slabtype == XMHFGEEC_SLABTYPE_uVT_PROG ||
+			  xmhfgeec_slab_info_table[slabid].slabtype == XMHFGEEC_SLABTYPE_uVU_PROG
+			 ) &&
+			 !((spatype & 0x0000000FUL) == _SLAB_SPATYPE_OTHER) &&
+			 !((spatype & 0x000000F0UL) == XMHFGEEC_SLABTYPE_VfT_PROG)
+			);
+		ensures (\result == (_PAGE_PRESENT | _PAGE_RW | _PAGE_NX));
+
 
         behavior invalid:
+		assumes (
+			 !(xmhfgeec_slab_info_table[slabid].slabtype == XMHFGEEC_SLABTYPE_uVT_PROG ||
+			  xmhfgeec_slab_info_table[slabid].slabtype == XMHFGEEC_SLABTYPE_uVU_PROG
+			 ) ||
+
+			 (
+			 (xmhfgeec_slab_info_table[slabid].slabtype == XMHFGEEC_SLABTYPE_uVT_PROG ||
+			  xmhfgeec_slab_info_table[slabid].slabtype == XMHFGEEC_SLABTYPE_uVU_PROG
+			 ) &&
+			 !((spatype & 0x0000000FUL) == _SLAB_SPATYPE_OTHER) &&
+			 ((spatype & 0x000000F0UL) == XMHFGEEC_SLABTYPE_VfT_PROG) &&
+			!((spatype & 0x0000000FUL) == _SLAB_SPATYPE_SLAB_CODE) &&
+			!((spatype & 0x0000000FUL) == _SLAB_SPATYPE_SLAB_DATA ||
+			  (spatype & 0x0000000FUL) == _SLAB_SPATYPE_SLAB_STACK ||
+			  (spatype & 0x0000000FUL) == _SLAB_SPATYPE_SLAB_DMADATA ||
+			  (spatype & 0x0000000FUL) == _SLAB_SPATYPE_GEEC_PRIME_IOTBL
+			 ) &&
+			 !((spatype & 0x0000000FUL) == _SLAB_SPATYPE_SLAB_DEVICEMMIO)
+			 )
+			);
+		ensures (\result == 0);
 
         complete behaviors;
         disjoint behaviors;
@@ -100,7 +177,7 @@ u64 gp_s2_setupmpgtbluh_getflags(u32 slabid, u32 spa, u32 spatype){
 				    flags = 0;
 				}
 
-			}else if((spatype & _SLAB_SPATYPE_MASK_SAMESLAB) || (spatype & 0x000000F0UL) == XMHFGEEC_SLABTYPE_VfT_SENTINEL){
+			/*}else if((spatype & _SLAB_SPATYPE_MASK_SAMESLAB) || (spatype & 0x000000F0UL) == XMHFGEEC_SLABTYPE_VfT_SENTINEL){
                                 //same object or sentinel
 				if((spatype & 0x0000000FUL) == _SLAB_SPATYPE_SLAB_CODE){
 					flags = (_PAGE_PRESENT | _PAGE_USER);
@@ -114,7 +191,7 @@ u64 gp_s2_setupmpgtbluh_getflags(u32 slabid, u32 spa, u32 spatype){
 				}else{
 				    flags = 0;
 				}
-
+			*/
 			}else{
 			    flags = (_PAGE_PRESENT | _PAGE_RW | _PAGE_NX);
 			}
