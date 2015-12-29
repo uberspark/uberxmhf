d499cfae237a727f0023d396f69dfaf7a94ad4f1
Amit Vasudevan <amitvasudevan@acm.org>
2015-12-28 21:50:48 -0600

    xmhf-slabs: prime: successful verification of setptentries

--- xmhf-hwm/include/xmhfhwm_vtd.h
+++ xmhf-hwm/include/xmhfhwm_vtd.h
@@ -214,8 +214,11 @@ typedef u64 vtd_pte_t;
 
 
 /* make a page table entry from individual fields */
+//#define vtd_make_pte(paddr, flags) \
+//  ((u64)(paddr) & (~(((u64)PAGE_SIZE_4K - 1)))) | (u64)(flags)
+
 #define vtd_make_pte(paddr, flags) \
-  ((u64)(paddr) & (~(((u64)PAGE_SIZE_4K - 1)))) | (u64)(flags)
+  ((u64)(paddr) & (0x7FFFFFFFFFFFF000ULL)) | (u64)(flags)
 
 
 
