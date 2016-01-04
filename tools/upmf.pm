aad3a45452aa54a96f957ee55ee7005f486f5388
Amit Vasudevan <amitvasudevan@acm.org>
2016-01-04 13:19:22 -0600

    xmhf: tools: add base template for new object manifest parsing module

---
+++ tools/upmf.pm
@@ -0,0 +1,24 @@
+# module to parse object manifest
+# author: amit vasudevan (amitvasudevan@acm.org)
+
+package upmf;
+use strict;
+use warnings;
+use Exporter;
+
+our @ISA= qw( Exporter );
+
+# these are exported by default.
+our @EXPORT = qw( export_me export_me_too);
+
+sub export_me {
+    # stuff
+}
+
+sub export_me_too {
+    # stuff
+}
+
+1;
+
+
