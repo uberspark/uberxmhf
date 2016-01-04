8b4a6b0c0c884f71fff1a72a9abcf1457b14f798
Amit Vasudevan <amitvasudevan@acm.org>
2016-01-04 14:47:38 -0600

    xmhf: tools: move parse_mmap into upmf module; successful build.

--- tools/upmf.pm
+++ tools/upmf.pm
@@ -9,7 +9,11 @@ use Exporter;
 our @ISA= qw( Exporter );
 
 # these are exported by default.
-our @EXPORT = qw( export_me export_me_too);
+our @EXPORT = qw( export_me export_me_too parse_mmap %slab_idtomemoffsets);
+
+
+our %slab_idtomemoffsets;
+
 
 sub export_me {
     # stuff
@@ -18,6 +22,40 @@ sub export_me {
 sub export_me_too {
     # stuff
 }
+
+
+######
+# TODO: move into module
+# parses a mmap file and populates relevant global structures
+######
+sub parse_mmap {
+    my($filename, $slabid, $totalslabs) = @_;
+    my $i = 0;
+
+    chomp($filename);
+    #print "filename:$filename\n";
+    tie my @array, 'Tie::File', $filename or die $!;
+
+    #print "parse_mmap: $filename, $slabid...\n";
+
+    while( $i <= $#array) {
+        my $line = $array[$i];
+        chomp($line);
+        $line =~ s/^\s+|\s+$//g ;     # remove both leading and trailing whitespace
+
+        my @lineentry = split(/:/, $line);
+
+        $lineentry[0] =~ s/^\s+|\s+$//g ;     # remove both leading and trailing whitespace
+        $lineentry[1] =~ s/^\s+|\s+$//g ;     # remove both leading and trailing whitespace
+
+        $slab_idtomemoffsets{$slabid}{$lineentry[0]} = $lineentry[1];
+
+        $i = $i +1;
+    }
+
+    return;
+}
+
 
 1;
 
