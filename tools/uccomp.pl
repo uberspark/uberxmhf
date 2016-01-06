e6ee550ca181d9342e90c9a2ed7fff4b237b22f0
Amit Vasudevan <amitvasudevan@acm.org>
2016-01-05 18:21:49 -0600

    xmhf: tools: add new tool (uccomp) to generate composition check files for the slabs; plug in base command line params and invocation to parse slab manifests.

---
+++ tools/uccomp.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+# script to generate composition check files
+# author: amit vasudevan (amitvasudevan@acm.org)
+
+use Tie::File;
+use File::Basename;
+use Data::Dumper;
+
+use lib dirname (__FILE__);
+use upmf;	#load up the manifest parsing module
+
+
+# command line inputs
+my $g_slabsfile = $ARGV[0];
+my $g_memoffsets = $ARGV[1];
+my $g_ccompdriverfile = $ARGV[2];
+my $g_ccompcheckfile = $ARGV[3];
+
+$g_maxincldevlistentries = $ARGV[4];
+$g_maxexcldevlistentries = $ARGV[5];
+$g_maxmemoffsetentries = $ARGV[6];
+
+
+my $g_rootdir;
+
+
+
+
+
+$g_rootdir = dirname($g_slabsfile)."/";
+
+#print "slabsfile:", $g_slabsfile, "\n";
+#print "rootdir:", $g_rootdir, "\n";
+
+upmf_init($g_slabsfile, $g_memoffsets, $g_rootdir);
+
+
+
+
+
+
+
+
+
+
+
+
+exit 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
