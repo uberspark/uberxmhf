ccadb3d05a51b474c74573b546d2e52c4914cf86
Amit Vasudevan <amitvasudevan@acm.org>
2016-03-31 11:50:21 -0500

    uccf: dump out global variables (both declared and defined); successful test.

--- tools/uccf.ml
+++ tools/uccf.ml
@@ -2,10 +2,18 @@
 (* author: amit vasudevan (amitvasudevan@acm.org) *)
 (* load using frama-c -load-script uccf.ml *)
 
+(*
 let run () =
 	let chan = open_out "hello.out" in
 		Printf . fprintf chan "Hello, world!\n";
 		close_out chan
 
 let () = Db.Main.extend run
+*)
 
+
+let do_var v _init =
+      Format.printf "Global variable %a (%s)@." Cil_datatype.Varinfo.pretty v
+        (if v.Cil_types.vdefined then "defined" else "declared")
+
+let () = Db.Main.extend (fun () -> Globals.Vars.iter do_var)
