4f1529b0a424390ecba4640fd1748341d99fd008
Amit Vasudevan <amitvasudevan@acm.org>
2016-03-31 11:46:39 -0500

    uccf: hello world frama-c plugin harness; successful test

--- tools/uccf.ml
+++ tools/uccf.ml
@@ -1,5 +1,11 @@
 (* frama-c coding conformance plugin *)
 (* author: amit vasudevan (amitvasudevan@acm.org) *)
+(* load using frama-c -load-script uccf.ml *)
 
+let run () =
+	let chan = open_out "hello.out" in
+		Printf . fprintf chan "Hello, world!\n";
+		close_out chan
 
+let () = Db.Main.extend run
 
