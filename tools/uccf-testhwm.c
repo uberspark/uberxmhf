61ce3516b2bbaa834d7d396de5a841adde5122f0
Amit Vasudevan <amitvasudevan@acm.org>
2016-04-07 12:10:53 -0500

    uccf: add logic to process option -uccf-checkhwmnsadherence within process_varinfo; successful test.

--- tools/uccf-testhwm.c
+++ tools/uccf-testhwm.c
@@ -6,7 +6,7 @@
 int xmhfhwm_myfunc_ptrfunc(int a);
 
 unsigned char xmhfhwm_buf[512];
-unsigned char buf[512];
+//unsigned char buf[512];
 int xmhfhwm_glob_a=5, xmhfhwm_glob_b;
 int (*xmhfhwm_ptr_myfunc_ptrfunc)(int a) = xmhfhwm_myfunc_ptrfunc;
 
@@ -17,7 +17,7 @@ int xmhfhwm_myfunc_ptrfunc(int a){
 }
 
 static void xmhfhwm_myfunc_static(void){
-	puts("this is just a static function \n");
+	xmhfhwm_puts("this is just a static function \n");
 }
 
 void xmhfhwm_myfunc(int a, int b){
@@ -25,9 +25,9 @@ void xmhfhwm_myfunc(int a, int b){
 	a = a+b;
 	b = b-a;
 	if(a > b)
-		printf("a=%u, b=%u\n", a, b);
+		xmhfhwm_printf("a=%u, b=%u\n", a, b);
 	else
-		printf("b=%u, a=%u\n", b, a);
+		xmhfhwm_printf("b=%u, a=%u\n", b, a);
 
 	//a = myfunc_ptrfunc(b);
 	a = xmhfhwm_ptr_myfunc_ptrfunc(b);
