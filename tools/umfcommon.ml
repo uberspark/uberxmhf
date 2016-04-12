87aa6be612658afd462a7618fc29168b737a6b84
Amit Vasudevan <amitvasudevan@acm.org>
2016-04-11 23:23:53 -0500

    umf: checkpoint in fleshing out umfcommon_init...

--- tools/umfcommon.ml
+++ tools/umfcommon.ml
@@ -3,6 +3,15 @@
 	author: amit vasudevan (amitvasudevan@acm.org)
 *)
 
+
+(*
+	**************************************************************************
+	global variables
+	**************************************************************************
+*)
+let g_totalslabs = ref 0;;
+
+
 let m_hash = ((Hashtbl.create 32) : ((string,string)  Hashtbl.t));;
       
 let subrun () =
@@ -10,10 +19,75 @@ let subrun () =
 	Format.printf "returning back to main module\n";
 	()
 
+let left_pos s len =
+  let rec aux i =
+    if i >= len then None
+    else match s.[i] with
+    | ' ' | '\n' | '\t' | '\r' -> aux (succ i)
+    | _ -> Some i
+  in
+  aux 0
+ 
+let right_pos s len =
+  let rec aux i =
+    if i < 0 then None
+    else match s.[i] with
+    | ' ' | '\n' | '\t' | '\r' -> aux (pred i)
+    | _ -> Some i
+  in
+  aux (pred len)
+  
+let trim s =
+  let len = String.length s in
+  match left_pos s len, right_pos s len with
+  | Some i, Some j -> String.sub s i (j - i + 1)
+  | None, None -> ""
+  | _ -> assert false
+
+
 let umfcommon_init g_slabsfile g_memoffsets g_rootdir = 
-	Format.printf "g_slabsfile=%s\n" g_slabsfile;
-	Format.printf "g_memoffsets=%b\n" g_memoffsets;
-	Format.printf "g_rootdir=%s\n" g_rootdir;
+	let i = ref 0 in
+	let slabdir = ref "" in
+	let slabname = ref "" in  
+	let slabtype = ref "" in
+	let slabsubtype = ref "" in
+	let slabgsmfile = ref "" in
+	let slabmmapfile = ref "" in
+	let sfh = open_in g_slabsfile in
+	let trimline = ref "" in
+
+		Format.printf "g_slabsfile=%s\n" g_slabsfile;
+		Format.printf "g_memoffsets=%b\n" g_memoffsets;
+		Format.printf "g_rootdir=%s\n" g_rootdir;
+			
+		try
+    		while true do
+      			trimline := trim (input_line sfh);
+				let slabinfo = Str.split (Str.regexp ",") !trimline in
+					slabdir := trim (List.nth slabinfo 0);
+					slabname := Filename.basename !slabdir; 
+					slabtype := trim (List.nth slabinfo 1);
+					slabsubtype := trim (List.nth slabinfo 2);
+				    slabgsmfile := !slabdir ^ "/" ^ !slabname ^ ".gsm.pp";
+	    			slabmmapfile := g_rootdir ^ "_objects/_objs_slab_" ^ !slabname ^ "/" ^ !slabname ^ ".mmap";
+					
+					Format.printf "%s\n" !trimline;      			
+					Format.printf "  slabdir=%s\n" !slabdir;      			
+					Format.printf "  slabname=%s\n" !slabname;      			
+					Format.printf "  slabtype=%s\n" !slabtype;      			
+					Format.printf "  slabsubtype=%s\n" !slabsubtype;      			
+					Format.printf "  slabgsmfile=%s\n" !slabgsmfile;      			
+					Format.printf "  slabmmapfile=%s\n" !slabmmapfile;      			
+
+					i := !i + 1;
+		    done;
+		with End_of_file -> 
+    			close_in sfh;
+    	;		
+
+		g_totalslabs := !i;
+		Format.printf "total slabs=%d\n" !g_totalslabs;      			
+		
 	()
 
 (*
