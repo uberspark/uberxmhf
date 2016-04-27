894df8c06ac684ed3e7f7cbc4026843c5bd8acac
Amit Vasudevan <amitvasudevan@acm.org>
2016-04-27 11:28:33 -0500

    prime: add DMA page-tables data structure definition

--- xmhf-slabs/include/geec_prime.h
+++ xmhf-slabs/include/geec_prime.h
@@ -264,6 +264,9 @@ extern __attribute__((section(".data"))) __attribute__((aligned(4096))) vtd_pdpt
 extern __attribute__((section(".data"))) __attribute__((aligned(4096))) vtd_pdte_t _slabdevpgtbl_pdt[XMHFGEEC_TOTAL_SLABS][PAE_PTRS_PER_PDPT * PAE_PTRS_PER_PDT];
 extern __attribute__((section(".data"))) __attribute__((aligned(4096))) vtd_pte_t _slabdevpgtbl_pt[XMHFGEEC_TOTAL_SLABS][MAX_SLAB_DMADATA_PDT_ENTRIES][PAE_PTRS_PER_PT];
 
+//rich-guest DMA page-tables
+extern __attribute__((section(".data"))) __attribute__((aligned(4096))) vtd_pte_t _slabdevpgtbl_pt_rg[VTD_PTRS_PER_PDT][VTD_PTRS_PER_PT];
+
 
 extern __attribute__((section(".data"))) _slabdevpgtbl_infotable_t _slabdevpgtbl_infotable[XMHFGEEC_TOTAL_SLABS];
 extern __attribute__((section(".data"))) u32 vtd_pagewalk_level;
