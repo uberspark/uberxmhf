31324e10fd62fff6abe5c4a5da7b27e69bc36470
Amit Vasudevan <amitvasudevan@acm.org>
2016-04-28 16:04:14 -0500

    rgapp-hyperdep: flesh out test harness for hyperdep hypapp and plug in basic build targets. successful build and test.

--- xmhf-rgapps/linux/rgapp-hyperdep/rgapp-hyperdep.c
+++ xmhf-rgapps/linux/rgapp-hyperdep/rgapp-hyperdep.c
@@ -3,3 +3,53 @@
  * author: amit vasudevan (amitvasudevan@acm.org)
  */
 
+#include <stdio.h>
+
+typedef unsigned char u8;
+typedef unsigned int u32;
+
+//////
+// hyperdep test harness
+
+//////////////////////////////////////////////////////////////////////////////
+// xhhyperdep test
+
+__attribute__((aligned(4096))) static u8 testxhhyperdep_page[4096];
+
+#define HYPERDEP_ACTIVATEDEP			0xC0
+#define HYPERDEP_DEACTIVATEDEP			0xC1
+
+typedef void (*DEPFN)(void);
+
+void do_testxhhyperdep(void){
+    u32 gpa = &testxhhyperdep_page;
+    DEPFN fn = (DEPFN)gpa;
+
+    testxhhyperdep_page[0] = 0xC3; //ret instruction
+
+    printf("\n%s: Going to activate DEP on page %x", __FUNCTION__, gpa);
+
+
+    //vmcall(HYPERDEP_ACTIVATEDEP,  0, gpa);
+
+    printf("\n%s: Activated DEP", __FUNCTION__);
+
+    //fn();
+
+    printf("\n%s: Going to de-activate DEP on page %x", __FUNCTION__, gpa);
+
+    //vmcall(HYPERDEP_DEACTIVATEDEP,  0, gpa);
+
+    printf("\n%s: Deactivated DEP", __FUNCTION__);
+
+}
+
+
+
+void main(void){
+    printf("\n%s: DEP buffer at 0x%08x", __FUNCTION__, &testxhhyperdep_page);
+
+	do_testxhhyperdep();
+
+    printf("\n\n");
+}
