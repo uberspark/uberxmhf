caff15afe1a1e655ffcf9e64222ba8a51b70897b
Amit Vasudevan <amitvasudevan@acm.org>
2016-05-24 10:24:03 -0500

    prime: s2: split gp_s2_sdminitdevmap into individual modules and successful reverification of s2_sdminitdevmap_adddeventry

--- xmhf-slabs/geec_prime/gp_s2_sdminitdevmap_addalldevstouobj.c
+++ xmhf-slabs/geec_prime/gp_s2_sdminitdevmap_addalldevstouobj.c
@@ -50,63 +50,42 @@
 
 #include <geec_prime.h>
 
+//@ghost bool gp_s2_sdminitdevmap_addalldevstouobj_coradddeventry[MAX_PLATFORM_DEVICES];
+//@ghost bool gp_s2_sdminitdevmap_addalldevstouobj_isdevinexcl[MAX_PLATFORM_DEVICES];
 
+/*@
+	requires 0 <= slabid < XMHFGEEC_TOTAL_SLABS;
+	requires 0 <= numentries_sysdev_memioregions <= MAX_PLATFORM_DEVICES;
+	requires 0 <= xmhfgeec_slab_info_table[slabid].excl_devices_count <= XMHF_CONFIG_MAX_EXCLDEVLIST_ENTRIES;
 
-static void gp_s2_sdminitdevmap_adddevstouobj(u32 slabid, u32 vendor_id, u32 device_id){
+	assigns gp_s2_sdminitdevmap_addalldevstouobj_isdevinexcl[0..(numentries_sysdev_memioregions-1)];
+	assigns gp_s2_sdminitdevmap_addalldevstouobj_coradddeventry[0..(numentries_sysdev_memioregions-1)];
+
+@*/
+void gp_s2_sdminitdevmap_addalldevstouobj(u32 slabid){
 	u32 k;
+	bool isdevinexcl;
 
-	for(k=0; k < numentries_sysdev_memioregions; k++){
-        if( (sysdev_memioregions[k].vendor_id == vendor_id) &&
-            (sysdev_memioregions[k].device_id == device_id) &&
-            !gp_s2_sdminitdevmap_isdevinexcl(slabid, vendor_id, device_id)
-        ){
+	/*@
+		loop invariant k1: 0 <= k <= numentries_sysdev_memioregions;
+		loop assigns k;
+		loop assigns isdevinexcl;
+		loop assigns gp_s2_sdminitdevmap_addalldevstouobj_isdevinexcl[0..(numentries_sysdev_memioregions-1)];
+		loop assigns gp_s2_sdminitdevmap_addalldevstouobj_coradddeventry[0..(numentries_sysdev_memioregions-1)];
+		loop variant numentries_sysdev_memioregions - k;
+	@*/
+    for(k=0; k < numentries_sysdev_memioregions; k++){
+        isdevinexcl = gp_s2_sdminitdevmap_isdevinexcl(slabid, sysdev_memioregions[k].vendor_id, sysdev_memioregions[k].device_id);
+    	//@ghost gp_s2_sdminitdevmap_addalldevstouobj_isdevinexcl[k] = isdevinexcl;
+        //@assert (gp_s2_sdminitdevmap_addalldevstouobj_isdevinexcl[k] == true) || (gp_s2_sdminitdevmap_addalldevstouobj_isdevinexcl[k] == false);
+
+        if(!isdevinexcl){
         	gp_s2_sdminitdevmap_adddeventry(slabid, k);
+        	//@ghost gp_s2_sdminitdevmap_addalldevstouobj_coradddeventry[k] = true;
+        }else{
+        	//@ghost gp_s2_sdminitdevmap_addalldevstouobj_coradddeventry[k] = false;
         }
     }
-
-}
-
-
-
-void gp_s2_sdminitdevmap(void){
-    u32 i, j, k;
-
-    _XDPRINTF_("%s: numentries_sysdev_mmioregions=%u\n", __func__, numentries_sysdev_memioregions);
-
-    for(i=0; i < XMHFGEEC_TOTAL_SLABS; i++){
-        _sda_slab_devicemap[i].device_count = 0;
-
-        for(j=0; j < xmhfgeec_slab_info_table[i].incl_devices_count; j++){
-            if( xmhfgeec_slab_info_table[i].incl_devices[j].vendor_id == 0xFFFF &&
-               xmhfgeec_slab_info_table[i].incl_devices[j].device_id == 0xFFFF){
-            	gp_s2_sdminitdevmap_addalldevstouobj(i);
-            }else{
-            	gp_s2_sdminitdevmap_adddevstouobj(i, xmhfgeec_slab_info_table[i].incl_devices[j].vendor_id,
-            			xmhfgeec_slab_info_table[i].incl_devices[j].device_id);
-            }
-        }
-
-        #if defined (__DEBUG_SERIAL__)
-        //add device SERIAL0 to all the slabs if debugging is enabled
-    	gp_s2_sdminitdevmap_adddevstouobj(i, PCI_VENDOR_ID_XMHFGEEC, PCI_DEVICE_ID_XMHFGEEC_SERIAL0);
-        #endif // defined
-
-    }
-
-	#if defined (__DEBUG_SERIAL__)
-    //debug dump
-    {
-        u32 i, j;
-        for(i=0; i < XMHFGEEC_TOTAL_SLABS; i++){
-            _XDPRINTF_("%s: slab %u...\n", __func__, i);
-            for(j=0; j < _sda_slab_devicemap[i].device_count; j++){
-                _XDPRINTF_("     device idx=%u\n", _sda_slab_devicemap[i].sysdev_mmioregions_indices[j]);
-            }
-        }
-    }
-	#endif // defined
-
-
 }
 
 
--- xmhf-slabs/geec_prime/gp_s2_sdminitdevmap_isdevinexcl.c
+++ xmhf-slabs/geec_prime/gp_s2_sdminitdevmap_isdevinexcl.c
@@ -50,64 +50,46 @@
 
 #include <geec_prime.h>
 
+//returns true if a given device vendor_id:device_id is in the slab device exclusion
+//list
+/*@
+	requires 0 <= slabid < XMHFGEEC_TOTAL_SLABS;
+	requires 0 <= xmhfgeec_slab_info_table[slabid].excl_devices_count <= XMHF_CONFIG_MAX_EXCLDEVLIST_ENTRIES;
 
+	assigns \nothing;
 
-static void gp_s2_sdminitdevmap_adddevstouobj(u32 slabid, u32 vendor_id, u32 device_id){
-	u32 k;
+	ensures isinexclres: \exists integer x; 0 <= x < xmhfgeec_slab_info_table[slabid].excl_devices_count &&
+			(xmhfgeec_slab_info_table[slabid].excl_devices[x].vendor_id == vendor_id &&
+           xmhfgeec_slab_info_table[slabid].excl_devices[x].device_id == device_id) ==>
+			(\result == true);
 
-	for(k=0; k < numentries_sysdev_memioregions; k++){
-        if( (sysdev_memioregions[k].vendor_id == vendor_id) &&
-            (sysdev_memioregions[k].device_id == device_id) &&
-            !gp_s2_sdminitdevmap_isdevinexcl(slabid, vendor_id, device_id)
-        ){
-        	gp_s2_sdminitdevmap_adddeventry(slabid, k);
-        }
+	ensures isnotinexclres: !(\exists integer x; 0 <= x < xmhfgeec_slab_info_table[slabid].excl_devices_count &&
+			(xmhfgeec_slab_info_table[slabid].excl_devices[x].vendor_id == vendor_id &&
+           xmhfgeec_slab_info_table[slabid].excl_devices[x].device_id == device_id)) ==>
+			(\result == false);
+	ensures (\result == true) || (\result == false);
+@*/
+bool gp_s2_sdminitdevmap_isdevinexcl(u32 slabid, u32 vendor_id, u32 device_id){
+    u32 i;
+
+	/*@
+		loop invariant a1: 0 <= i <= xmhfgeec_slab_info_table[slabid].excl_devices_count;
+		loop invariant a2: \forall integer x; 0 <= x < i ==>
+			!(xmhfgeec_slab_info_table[slabid].excl_devices[x].vendor_id == vendor_id &&
+           xmhfgeec_slab_info_table[slabid].excl_devices[x].device_id == device_id);
+		loop assigns i;
+		loop variant xmhfgeec_slab_info_table[slabid].excl_devices_count - i;
+	@*/
+    for(i=0; i < xmhfgeec_slab_info_table[slabid].excl_devices_count; i++){
+        if(xmhfgeec_slab_info_table[slabid].excl_devices[i].vendor_id == vendor_id &&
+           xmhfgeec_slab_info_table[slabid].excl_devices[i].device_id == device_id)
+            return true;
     }
 
+    /*@assert a3: \forall integer x; 0 <= x < xmhfgeec_slab_info_table[slabid].excl_devices_count ==>
+		!(xmhfgeec_slab_info_table[slabid].excl_devices[x].vendor_id == vendor_id &&
+   	   xmhfgeec_slab_info_table[slabid].excl_devices[x].device_id == device_id);
+   	@*/
+    return false;
 }
-
-
-
-void gp_s2_sdminitdevmap(void){
-    u32 i, j, k;
-
-    _XDPRINTF_("%s: numentries_sysdev_mmioregions=%u\n", __func__, numentries_sysdev_memioregions);
-
-    for(i=0; i < XMHFGEEC_TOTAL_SLABS; i++){
-        _sda_slab_devicemap[i].device_count = 0;
-
-        for(j=0; j < xmhfgeec_slab_info_table[i].incl_devices_count; j++){
-            if( xmhfgeec_slab_info_table[i].incl_devices[j].vendor_id == 0xFFFF &&
-               xmhfgeec_slab_info_table[i].incl_devices[j].device_id == 0xFFFF){
-            	gp_s2_sdminitdevmap_addalldevstouobj(i);
-            }else{
-            	gp_s2_sdminitdevmap_adddevstouobj(i, xmhfgeec_slab_info_table[i].incl_devices[j].vendor_id,
-            			xmhfgeec_slab_info_table[i].incl_devices[j].device_id);
-            }
-        }
-
-        #if defined (__DEBUG_SERIAL__)
-        //add device SERIAL0 to all the slabs if debugging is enabled
-    	gp_s2_sdminitdevmap_adddevstouobj(i, PCI_VENDOR_ID_XMHFGEEC, PCI_DEVICE_ID_XMHFGEEC_SERIAL0);
-        #endif // defined
-
-    }
-
-	#if defined (__DEBUG_SERIAL__)
-    //debug dump
-    {
-        u32 i, j;
-        for(i=0; i < XMHFGEEC_TOTAL_SLABS; i++){
-            _XDPRINTF_("%s: slab %u...\n", __func__, i);
-            for(j=0; j < _sda_slab_devicemap[i].device_count; j++){
-                _XDPRINTF_("     device idx=%u\n", _sda_slab_devicemap[i].sysdev_mmioregions_indices[j]);
-            }
-        }
-    }
-	#endif // defined
-
-
-}
-
-
 
