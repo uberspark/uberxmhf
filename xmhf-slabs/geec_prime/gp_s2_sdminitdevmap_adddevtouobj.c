a88d7ccdf29e3d2dcd7f4f3ebf631ad788174feb
Amit Vasudevan <amitvasudevan@acm.org>
2016-05-24 11:02:29 -0500

    prime: s2: successful verification of s2_sdminitdevmap_adddevtouobj

--- xmhf-slabs/geec_prime/gp_s2_sdminitdevmap_adddevtouobj.c
+++ xmhf-slabs/geec_prime/gp_s2_sdminitdevmap_adddevtouobj.c
@@ -51,16 +51,64 @@
 #include <geec_prime.h>
 
 
+//@ghost bool gp_s2_sdminitdevmap_adddevtouobj_coradddeventry[MAX_PLATFORM_DEVICES];
+//@ghost bool gp_s2_sdminitdevmap_adddevtouobj_isdevinexcl[MAX_PLATFORM_DEVICES];
 
+/*@
+	requires 0 <= slabid < XMHFGEEC_TOTAL_SLABS;
+	requires 0 <= numentries_sysdev_memioregions <= MAX_PLATFORM_DEVICES;
+	requires 0 <= xmhfgeec_slab_info_table[slabid].excl_devices_count <= XMHF_CONFIG_MAX_EXCLDEVLIST_ENTRIES;
+
+	assigns gp_s2_sdminitdevmap_adddevtouobj_isdevinexcl[0..(numentries_sysdev_memioregions-1)];
+	assigns gp_s2_sdminitdevmap_adddevtouobj_coradddeventry[0..(numentries_sysdev_memioregions-1)];
+
+	ensures e2: \forall integer x; 0 <= x < numentries_sysdev_memioregions &&
+			((sysdev_memioregions[x].vendor_id == vendor_id) &&
+             (sysdev_memioregions[x].device_id == device_id) &&
+			 (gp_s2_sdminitdevmap_adddevtouobj_isdevinexcl[x] == false)) ==>
+			(gp_s2_sdminitdevmap_adddevtouobj_coradddeventry[x] == true);
+	ensures e3: \forall integer x; 0 <= x < numentries_sysdev_memioregions &&
+			!((sysdev_memioregions[x].vendor_id == vendor_id) &&
+             (sysdev_memioregions[x].device_id == device_id) &&
+			 (gp_s2_sdminitdevmap_adddevtouobj_isdevinexcl[x] == false)) ==>
+			(gp_s2_sdminitdevmap_adddevtouobj_coradddeventry[x] == false);
+@*/
 void gp_s2_sdminitdevmap_adddevtouobj(u32 slabid, u32 vendor_id, u32 device_id){
 	u32 k;
+	bool isdevinexcl;
+
+	/*@
+		loop invariant k1: 0 <= k <= numentries_sysdev_memioregions;
+		loop invariant k2: \forall integer x; 0 <= x < k &&
+			((sysdev_memioregions[x].vendor_id == vendor_id) &&
+             (sysdev_memioregions[x].device_id == device_id) &&
+			 (gp_s2_sdminitdevmap_adddevtouobj_isdevinexcl[x] == false)) ==>
+			(gp_s2_sdminitdevmap_adddevtouobj_coradddeventry[x] == true);
+		loop invariant k3: \forall integer x; 0 <= x < k &&
+			!((sysdev_memioregions[x].vendor_id == vendor_id) &&
+             (sysdev_memioregions[x].device_id == device_id) &&
+			 (gp_s2_sdminitdevmap_adddevtouobj_isdevinexcl[x] == false)) ==>
+			(gp_s2_sdminitdevmap_adddevtouobj_coradddeventry[x] == false);
+		loop assigns k;
+		loop assigns isdevinexcl;
+		loop assigns gp_s2_sdminitdevmap_adddevtouobj_isdevinexcl[0..(numentries_sysdev_memioregions-1)];
+		loop assigns gp_s2_sdminitdevmap_adddevtouobj_coradddeventry[0..(numentries_sysdev_memioregions-1)];
+		loop variant numentries_sysdev_memioregions - k;
+	@*/
 
 	for(k=0; k < numentries_sysdev_memioregions; k++){
-        if( (sysdev_memioregions[k].vendor_id == vendor_id) &&
+        isdevinexcl = gp_s2_sdminitdevmap_isdevinexcl(slabid, vendor_id, device_id);
+    	//@ghost gp_s2_sdminitdevmap_adddevtouobj_isdevinexcl[k] = isdevinexcl;
+        //@assert (gp_s2_sdminitdevmap_adddevtouobj_isdevinexcl[k] == true) || (gp_s2_sdminitdevmap_adddevtouobj_isdevinexcl[k] == false);
+
+		if( (sysdev_memioregions[k].vendor_id == vendor_id) &&
             (sysdev_memioregions[k].device_id == device_id) &&
-            !gp_s2_sdminitdevmap_isdevinexcl(slabid, vendor_id, device_id)
+            !isdevinexcl
         ){
         	gp_s2_sdminitdevmap_adddeventry(slabid, k);
+        	//@ghost gp_s2_sdminitdevmap_adddevtouobj_coradddeventry[k] = true;
+        }else{
+        	//@ghost gp_s2_sdminitdevmap_adddevtouobj_coradddeventry[k] = false;
         }
     }
 
