adc3d56b59ea9c808141ec2cf7433e376eac001c
Amit Vasudevan <amitvasudevan@acm.org>
2016-05-24 23:57:27 -0500

    prime: inline value-analysis harness within s2_gathersysmemtypes; successful reverification

--- xmhf-slabs/geec_prime/gp_s2_gathersysmemtypes.c
+++ xmhf-slabs/geec_prime/gp_s2_gathersysmemtypes.c
@@ -51,6 +51,27 @@
 
 #include <geec_prime.h>
 
+#if defined (__XMHF_VERIFICATION__) && defined (__USPARK_FRAMAC_VA__)
+u32 check_esp, check_eip = CASM_RET_EIP;
+
+void xmhfhwm_vdriver_writeesp(u32 oldval, u32 newval){
+	//@assert (newval >= ((u32)&_init_bsp_cpustack + 4)) && (newval <= ((u32)&_init_bsp_cpustack + MAX_PLATFORM_CPUSTACK_SIZE)) ;
+}
+
+void main(void){
+	//populate hardware model stack and program counter
+	xmhfhwm_cpu_gprs_esp = (u32)&_init_bsp_cpustack + MAX_PLATFORM_CPUSTACK_SIZE;
+	xmhfhwm_cpu_gprs_eip = check_eip;
+	check_esp = xmhfhwm_cpu_gprs_esp; // pointing to top-of-stack
+
+	//execute harness
+	gp_s2_gathersysmemtypes();
+
+	//@assert xmhfhwm_cpu_gprs_esp == check_esp;
+	//@assert xmhfhwm_cpu_gprs_eip == check_eip;
+}
+#endif
+
 //---gather memory types for system physical memory------------------------------
 void gp_s2_gathersysmemtypes(void){
  	u32 eax, ebx, ecx, edx;
