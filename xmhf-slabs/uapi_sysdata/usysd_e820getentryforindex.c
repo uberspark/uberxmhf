c81f145f7fa6d45267aa610976172a50336b8b68
Amit Vasudevan <amitvasudevan@acm.org>
2016-06-06 14:16:48 -0500

    usysd: successful reverificaiton of usysd_e820getentryforindex after minor refactoring; successful

--- xmhf-slabs/uapi_sysdata/usysd_e820getentryforindex.c
+++ xmhf-slabs/uapi_sysdata/usysd_e820getentryforindex.c
@@ -60,10 +60,9 @@
 
 /*@
 	requires \valid(gentryp);
-	requires (usysd_memmapinfo_maxindex <= MAX_E820_ENTRIES);
 
 	behavior returnentry:
-		assumes (gentryp->index < usysd_memmapinfo_maxindex);
+		assumes (usysd_memmapinfo_maxindex <= MAX_E820_ENTRIES && gentryp->index < usysd_memmapinfo_maxindex);
 		assigns	gentryp->baseaddr_high;
 		assigns	gentryp->baseaddr_low;
 		assigns gentryp->length_high;
@@ -71,13 +70,12 @@
 		assigns gentryp->type;
 
 	behavior invalid:
-		assumes (usysd_memmapinfo_maxindex > MAX_E820_ENTRIES);
-		assumes (gentryp->index >= usysd_memmapinfo_maxindex);
+		assumes !(usysd_memmapinfo_maxindex <= MAX_E820_ENTRIES && gentryp->index < usysd_memmapinfo_maxindex);
 		assigns \nothing;
 @*/
 void usysd_e820getentryforindex(uxmhf_uapi_sysdata_e820getentryforindex_t *gentryp){
 
-	if(gentryp->index < usysd_memmapinfo_maxindex){
+	if(usysd_memmapinfo_maxindex <= MAX_E820_ENTRIES && gentryp->index < usysd_memmapinfo_maxindex){
 		gentryp->baseaddr_high = usysd_memmapinfo[gentryp->index].baseaddr_high;
 		gentryp->baseaddr_low = usysd_memmapinfo[gentryp->index].baseaddr_low;
 		gentryp->length_high = usysd_memmapinfo[gentryp->index].length_high;
