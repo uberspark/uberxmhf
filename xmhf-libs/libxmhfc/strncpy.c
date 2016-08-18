36ff569e1aa5e87a65b60015cbb6a59b8c319476
Amit Vasudevan <amitvasudevan@acm.org>
2016-08-18 13:00:11 -0500

    libxmhfc: cleanup and successful runtime test.

--- xmhf-libs/libxmhfc/strncpy.c
+++ xmhf-libs/libxmhfc/strncpy.c
@@ -38,29 +38,6 @@
 
 #include <stdint.h>
 #include <string.h>
-/*
- * Copy src to dst, truncating or null-padding to always copy n bytes.
- * Return dst.
- */
-/*char *strncpy(char * dst, const char * src, size_t n)
-{
-	if (n != 0) {
-		register char *d = dst;
-		register const char *s = src;
-
-		do {
-			if ((*d++ = *s++) == 0) {
-				// NUL pad the remaining n-1 bytes
-				while (--n != 0)
-					*d++ = 0;
-				break;
-			}
-		} while (--n != 0);
-	}
-	return (dst);
-}
-*/
-
 
 /*@
 	requires n >= 0;
