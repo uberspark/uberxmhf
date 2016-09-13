879bfd9750e1562c3ae3112d8da37d65da17a5c1
Amit Vasudevan <amitvasudevan@acm.org>
2016-09-13 13:30:22 -0500

    uhwm: tweak embed_hwm_visitor to handle normal functions and replace within AST

--- tools/uhwm-test.c
+++ tools/uhwm-test.c
@@ -5,7 +5,9 @@ int main (p_func* func, int *arg1, int arg2)
   int result;
 s1:
   	//@assert 1;
-	result = (*func)(arg1, arg2);
+	//result = (*func)(arg1, arg2);
+	result = casm_func(arg1, arg2);
+
 s2:
   return result;
 }
