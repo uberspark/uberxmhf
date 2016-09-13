a1c2eae605f1978461fe6ebb6b82ae2418838a61
Amit Vasudevan <amitvasudevan@acm.org>
2016-09-13 14:16:03 -0500

    uhwm: refine embed_hwm_visitor to only act on CASM function invocations

--- tools/uhwm-test.c
+++ tools/uhwm-test.c
@@ -1,4 +1,4 @@
-typedef int p_func (int *, int);
+/*typedef int p_func (int *, int);
 
 int main (p_func* func, int *arg1, int arg2)
 {
@@ -10,4 +10,36 @@ s1:
 
 s2:
   return result;
+}*/
+#define RET_EIP 0xabcd0000
+
+extern unsigned int xmhfhwm_cpu_gprs_edx, xmhfhwm_cpu_gprs_eax;
+
+
+int main (void)
+{
+	int result;
+
+/*	result = casm_funccall(5, 6); */
+
+/*	result = (
+			ci_pushl_esp(0),
+			ci_pushl_esp(0),
+			ci_pushl_esp(RET_EIP),
+			casm_funccall(5, 6),
+			ci_popl_esp(0x4),
+		    ((unsigned long long int)(xmhfhwm_cpu_gprs_edx << 32) | xmhfhwm_cpu_gprs_eax)
+	);*/
+
+	(
+				ci_pushl_esp(0),
+				ci_pushl_esp(0),
+				ci_pushl_esp(RET_EIP),
+				casm_funccall(5, 6),
+				ci_popl_esp(0x4),
+			    ((unsigned long long int)(xmhfhwm_cpu_gprs_edx << 32) | xmhfhwm_cpu_gprs_eax)
+		);
+
+
 }
+
