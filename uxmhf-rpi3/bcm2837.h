a382c022afd29a8cde836a03129eee787071d0cd
Amit Vasudevan <amitvasudevan@acm.org>
2017-02-06 13:28:34 -0600

    uxmhf-rpi3: prepare to plug in broadcom 2837 definitions for pi3...

---
+++ uxmhf-rpi3/bcm2837.h
@@ -0,0 +1,19 @@
+/*
+	broadcom 2837 (raspberry pi 3) definitions
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#ifndef __BCM2837_H__
+#define __BCM2837_H__
+
+
+
+#ifndef __ASSEMBLY__
+
+
+#endif // __ASSEMBLY__
+
+
+
+#endif //__BCM2837_H__
