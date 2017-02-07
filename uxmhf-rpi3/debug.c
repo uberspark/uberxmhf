97688b029c3c36dea0fbcdd5d19539e426b33c5e
Amit Vasudevan <amitvasudevan@acm.org>
2017-02-07 11:03:59 -0600

    uxmhf-rpi3: add debug.{c,h} to house debugging functions

---
+++ uxmhf-rpi3/debug.c
@@ -0,0 +1,9 @@
+/*
+	debug module definitions
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#include <bcm2837.h>
+#include <miniuart.h>
+#include <debug.h>
---
+++ uxmhf-rpi3/debug.h
@@ -0,0 +1,17 @@
+/*
+	debug module definitions
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
+#ifndef __ASSEMBLY__
+
+
+#endif // __ASSEMBLY__
+
+
+
+#endif //__DEBUG_H__
