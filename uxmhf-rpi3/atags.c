f1c7e23cf784ae89192beea121152454cee7fc8d
Amit Vasudevan <amitvasudevan@acm.org>
2017-02-07 10:09:32 -0600

    uxmhf-rpi3: start fleshing out atag_dumptags to dump ATAG info; refine logic to invoke atag_dumptags before chainloading. successful runtime test.

--- uxmhf-rpi3/atags.c
+++ uxmhf-rpi3/atags.c
@@ -6,3 +6,9 @@
 
 #include <bcm2837.h>
 #include <atags.h>
+
+void atag_dumptags(struct atag *at){
+	bcm2837_miniuart_puts("uXMHF-rpi3: Dumping ATAG list...\n");
+	bcm2837_miniuart_flush();
+
+}
--- uxmhf-rpi3/main.c
+++ uxmhf-rpi3/main.c
@@ -7,9 +7,15 @@ extern void mmio_write32 (u32 address, u32 value);
 extern void chainload_os(u32 r0, u32 id, const u8 *atag);
 
 
-void main(u32 r0, u32 id, const u8 *atag){
+void main(u32 r0, u32 id, struct atag *at){
 	bcm2837_miniuart_init();
+
 	bcm2837_miniuart_puts("uXMHF-rpi3: Hello World!\n");
 	bcm2837_miniuart_flush();
-	chainload_os(r0, id, atag);
+
+	atag_dumptags(at);
+
+	bcm2837_miniuart_puts("uXMHF-rpi3: Chainloading OS kernel...\n");
+	bcm2837_miniuart_flush();
+	chainload_os(r0, id, at);
 }
