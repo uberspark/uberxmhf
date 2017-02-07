4c036f98367bb30d2afde158e23c2563c88d353a
Amit Vasudevan <amitvasudevan@acm.org>
2017-02-07 09:52:53 -0600

    uxmhf-rpi3: prepare to flesh out ATAGS implementation...

---
+++ uxmhf-rpi3/atags.c
@@ -0,0 +1,8 @@
+/*
+	ATAGS (ARM bootloader tags) implementation
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#include <bcm2837.h>
+#include <atags.h>
---
+++ uxmhf-rpi3/atags.h
@@ -0,0 +1,19 @@
+/*
+	ATAGS (ARM bootloader tags) definitions
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#ifndef __ATAGS_H__
+#define __ATAGS_H__
+
+
+#ifndef __ASSEMBLY__
+
+
+
+#endif // __ASSEMBLY__
+
+
+
+#endif //__ATAGS_H__
