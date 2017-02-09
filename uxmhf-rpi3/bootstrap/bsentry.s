f89e8cd4ff96f1aa25382aa1b816654a44637297
Amit Vasudevan <amitvasudevan@acm.org>
2017-02-08 23:44:27 -0600

    uxmhf-rpi3: sync up bootstrap to match core binary and link final image with bootstrap.bin at the beginning; successful runtime test.

--- uxmhf-rpi3/bootstrap/bsentry.s
+++ uxmhf-rpi3/bootstrap/bsentry.s
@@ -10,8 +10,26 @@
 
 .globl bootstrap_entry
 bootstrap_entry:
-	ldr r3, =0x30000000
-	blx r3
+	ldr sp, =0x00007f00
+	bl bsmain
 
 halt:
 	b halt
+
+
+
+.globl mmio_write32
+mmio_write32:
+    str r1,[r0]
+    bx lr
+
+.globl mmio_read32
+mmio_read32:
+    ldr r0,[r0]
+    bx lr
+
+
+.globl chainload_os
+chainload_os:
+	ldr r3, =0x00008000
+	blx r3
---
+++ uxmhf-rpi3/bootstrap/bsmain.c
@@ -0,0 +1,32 @@
+#include <bcm2837.h>
+#include <miniuart.h>
+#include <atags.h>
+#include <debug.h>
+
+extern u32 mmio_read32 (u32 address);
+extern void mmio_write32 (u32 address, u32 value);
+extern void chainload_os(u32 r0, u32 id, struct atag *at);
+
+
+void bsmain(u32 r0, u32 id, struct atag *at){
+	bcm2837_miniuart_init();
+
+	bcm2837_miniuart_puts("uXMHF-rpi3: Hello World!\n");
+	bcm2837_miniuart_puts("uXMHF-rpi3: r0= ");
+	debug_hexdumpu32(r0);
+	bcm2837_miniuart_puts("uXMHF-rpi3: id= ");
+	debug_hexdumpu32(id);
+	bcm2837_miniuart_puts("uXMHF-rpi3: ATAGS= ");
+	debug_hexdumpu32(at);
+
+	if(at->size == 0xedfe0dd0)
+		bcm2837_miniuart_puts("uXMHF-rpi3: ATAGS pointer is a FDT blob so no worries\n");
+	else{
+		bcm2837_miniuart_puts("uXMHF-rpi3: Error: require ATAGS to be FDT blob. Halting!\n");
+	}
+
+	bcm2837_miniuart_puts("uXMHF-rpi3: Chainloading OS kernel...\n");
+
+	bcm2837_miniuart_flush();
+	chainload_os(r0, id, at);
+}
