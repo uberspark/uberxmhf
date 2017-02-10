d02fa3aa4d3f93220fcc76a1add3de1023d30b4d
Amit Vasudevan <amitvasudevan@acm.org>
2017-02-09 23:07:30 -0600

    uxmhf-rpi3: bootstap: hard-code os kernel and core binary start/size; dump them. successful runtime test.

--- uxmhf-rpi3/bootstrap/bsentry.s
+++ uxmhf-rpi3/bootstrap/bsentry.s
@@ -33,3 +33,33 @@ mmio_read32:
 chainload_os:
 	ldr r3, =0x00008000
 	blx r3
+
+
+.section ".data"
+
+	.align 2
+	.global g_oskrnl_startaddr
+	.type g_oskrnl_startaddr, %object
+	.size g_oskrnl_startaddr, 4
+	g_oskrnl_startaddr:	.word	0x8000
+
+	.align 2
+	.global g_oskrnl_size
+	.type g_oskrnl_size, %object
+	.size g_oskrnl_size, 4
+	g_oskrnl_size:	.word	0x420000
+
+	.align 2
+	.global g_core_startaddr
+	.type g_core_startaddr, %object
+	.size g_core_startaddr, 4
+	g_core_startaddr:	.word	0x428000
+
+
+	.align 2
+	.global g_core_size
+	.type g_core_size, %object
+	.size g_core_size, 4
+	g_core_size:	.word	0x1000
+
+
--- uxmhf-rpi3/bootstrap/bsmain.c
+++ uxmhf-rpi3/bootstrap/bsmain.c
@@ -6,12 +6,25 @@
 extern u32 mmio_read32 (u32 address);
 extern void mmio_write32 (u32 address, u32 value);
 extern void chainload_os(u32 r0, u32 id, struct atag *at);
-
+extern u32 g_oskrnl_startaddr;
+extern u32 g_oskrnl_size;
+extern u32 g_core_startaddr;
+extern u32 g_core_size;
 
 void bsmain(u32 r0, u32 id, struct atag *at){
 	bcm2837_miniuart_init();
 
 	bcm2837_miniuart_puts("uXMHF-rpi3: Hello World!\n");
+
+	bcm2837_miniuart_puts(" g_oskrnl_startaddr= ");
+	debug_hexdumpu32(g_oskrnl_startaddr);
+	bcm2837_miniuart_puts(" g_oskrnl_size= ");
+	debug_hexdumpu32(g_oskrnl_size);
+	bcm2837_miniuart_puts(" g_core_startaddr= ");
+	debug_hexdumpu32(g_core_startaddr);
+	bcm2837_miniuart_puts(" g_core_size= ");
+	debug_hexdumpu32(g_core_size);
+
 	bcm2837_miniuart_puts("uXMHF-rpi3: r0= ");
 	debug_hexdumpu32(r0);
 	bcm2837_miniuart_puts("uXMHF-rpi3: id= ");
