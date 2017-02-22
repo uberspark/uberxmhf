2d325edbfd283eb47c7772c926f07e5d6a07eaa3
Amit Vasudevan <amitvasudevan@acm.org>
2017-02-22 17:46:49 -0600

    uxmhf-rpi3: guestos: add module svcvtable.s by copying over hypvtable.s in preparation to add SVC mode exception vector support...

---
+++ uxmhf-rpi3/guestos/svcvtable.s
@@ -0,0 +1,158 @@
+/*
+	HYP mode verctor table and stubs
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+
+.section ".text"
+/*
+	G1.12.1 (Table G1-6 ARMv8)
+	HYP vector table format:
+	offsets:
+		0x0: 	not used
+		0x4: 	undefined instruction form HYP mode
+		0x8: 	HVC in HYP mode
+		0x0C:	prefetch abort from HYP mode
+		0x10: 	data abort from HYP mode
+		0x14:	HVC in non-HYP mode
+		0x18:	IRQ
+		0x1C:	FIQ
+*/
+
+/* need lower 5 bits (0-4) of the table address as 0, so use balign 32 */
+	.balign	32
+	.global g_hypvtable
+g_hypvtable:
+	b hypvtable_reserved_handler
+	b hypvtable_reserved_handler
+	b hypvtable_hyphvc_handler
+	b hypvtable_reserved_handler
+	b hypvtable_reserved_handler
+	b hypvtable_hypsvc_handler
+	b hypvtable_reserved_handler
+	b hypvtable_reserved_handler
+
+	.balign 32
+	.global	hypvtable_reserved_handler
+hypvtable_reserved_handler:
+	hrh_halt:
+	b hrh_halt
+
+
+/*
+	G1.12.3 ARMv8
+	exception return address is stored in ELR_hyp register and
+	points to the instruction *after* the HVC instruction (Table G1-9)
+*/
+	.global	hypvtable_hyphvc_handler
+hypvtable_hyphvc_handler:
+	ldr sp, =hypvtable_stack_top
+
+	/* G1.9.2 (Figure G1-3)
+	   HYP mode uses LR_usr, i.e, does not have LR banking, so save
+	   since we are going to be using LR for C calling
+	*/
+	push {lr}
+
+
+	/* 5.1.1 AAPCS
+	   callee preserves r4-r8, r10, r11, r13 (SP)
+	   save the rest
+	*/
+	push {r0}
+	push {r1}
+	push {r2}
+	push {r3}
+	push {r9}
+	push {r12}
+
+	/* invoke C handler */
+	bl hyphvc_handler
+
+
+	/* restore all saved registers */
+	pop	{r12}
+	pop	{r9}
+	pop	{r3}
+	pop	{r2}
+	pop	{r1}
+	pop	{r0}
+
+	pop	{lr}
+
+	/*
+		G1.13.1 ARMv8
+		exception returns from HYP mode is made via ERET instruction
+		which basically returns to ELR_hyp and restores appropriate
+		PE (processor execution) state
+	*/
+	eret
+
+
+
+/*
+	G1.12.3 ARMv8
+	exception return address is stored in ELR_hyp register and
+	points to the instruction *after* the HVC instruction (Table G1-9)
+*/
+	.global	hypvtable_hypsvc_handler
+hypvtable_hypsvc_handler:
+	ldr sp, =hypvtable_hypsvc_stack_top
+
+	/* G1.9.2 (Figure G1-3)
+	   HYP mode uses LR_usr, i.e, does not have LR banking, so save
+	   since we are going to be using LR for C calling
+	*/
+	push {lr}
+
+
+	/* 5.1.1 AAPCS
+	   callee preserves r4-r8, r10, r11, r13 (SP)
+	   save the rest
+	*/
+	push {r0}
+	push {r1}
+	push {r2}
+	push {r3}
+	push {r9}
+	push {r12}
+
+	/* invoke C handler */
+	bl hypsvc_handler
+
+
+	/* restore all saved registers */
+	pop	{r12}
+	pop	{r9}
+	pop	{r3}
+	pop	{r2}
+	pop	{r1}
+	pop	{r0}
+
+	pop	{lr}
+
+	/*
+		G1.13.1 ARMv8
+		exception returns from HYP mode is made via ERET instruction
+		which basically returns to ELR_hyp and restores appropriate
+		PE (processor execution) state
+	*/
+	eret
+
+
+
+.section ".stack"
+	.balign 8
+	.global hypvtable_stack
+	stack:	.space	256
+	.global hypvtable_stack_top
+	hypvtable_stack_top:
+
+	.balign 8
+	.global hypvtable_hypsvc_stack
+	hypvtable_hypsvc_stack:	.space	256
+	.global hypvtable_hypsvc_stack_top
+	hypvtable_hypsvc_stack_top:
+
+
