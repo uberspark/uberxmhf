0476cb8944011fbc2dc07359a2ea93f41ca6ddba
Amit Vasudevan <amitvasudevan@acm.org>
2017-02-22 18:01:34 -0600

    uxmhf-rpi3: guestos: load VBAR with g_svcvtable

--- uxmhf-rpi3/guestos/main.c
+++ uxmhf-rpi3/guestos/main.c
@@ -14,6 +14,9 @@ extern void sysreg_write_vbar(u32 value);
 extern void hypcall(void);
 extern void svccall(void);
 
+extern u32 g_svcvtable[];
+
+
 void svc_handler(void){
 	bcm2837_miniuart_puts("uXMHF-rpi3: guestos: SVC_handler [IN]\n");
 	bcm2837_miniuart_puts("uXMHF-rpi3: guestos: SVC_handler [OUT]\n");
@@ -40,6 +43,14 @@ void main(u32 r0, u32 id, struct atag *at){
 
 	vbar = sysreg_read_vbar();
 	bcm2837_miniuart_puts(" VBAR before= ");
+	debug_hexdumpu32(vbar);
+
+	bcm2837_miniuart_puts(" g_svcvtable at= ");
+	debug_hexdumpu32((u32)&g_svcvtable);
+	sysreg_write_vbar((u32)&g_svcvtable);
+
+	vbar = sysreg_read_vbar();
+	bcm2837_miniuart_puts(" loaded VBAR with g_svcvtable; VBAR after= ");
 	debug_hexdumpu32(vbar);
 
 
