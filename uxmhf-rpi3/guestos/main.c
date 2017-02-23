416cd495434be4e32b9b9de0924730013cc4ec47
Amit Vasudevan <amitvasudevan@acm.org>
2017-02-22 23:22:52 -0600

    uxmhf-rpi3: guest: enable USR mode access to performance counters; we can now successfull read cycle count within USR mode

--- uxmhf-rpi3/guestos/main.c
+++ uxmhf-rpi3/guestos/main.c
@@ -48,6 +48,10 @@ void pmu_initperfcounters(u32 reset, u32 enable_divider){
   //enable all counters
   asm volatile ("mcr p15, 0, %0, c9, c12, 1\t\n" :: "r"(0x8000000f));
 
+  //enable USR mode access to performance counters
+  asm volatile ("mcr p15, 0, %0, c9, c14, 0\t\n" :: "r"(0x0000000f));
+
+
   // clear overflows:
   asm volatile ("MCR p15, 0, %0, c9, c12, 3\t\n" :: "r"(0x8000000f));
 }
