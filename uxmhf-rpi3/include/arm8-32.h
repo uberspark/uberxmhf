c166952183d614de90c695ce9337d416ae4d7451
Amit Vasudevan <amitvasudevan@acm.org>
2017-02-25 14:03:17 -0600

    uxmhf-rpi3: add arm8-32.h header file template

---
+++ uxmhf-rpi3/include/arm8-32.h
@@ -0,0 +1,17 @@
+/*
+	ARM 8 32-bit (aarch32) architecture definitions
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#ifndef __ARM8_32_H__
+#define __ARM8_32_H__
+
+
+
+#ifndef __ASSEMBLY__
+
+
+#endif // __ASSEMBLY__
+
+#endif //__ARM8_32_H__
