f4552ae6904c553e8ff985dcabaccdb09df2fd57
Amit Vasudevan <amitvasudevan@acm.org>
2017-02-25 14:20:15 -0600

    uxmhf-rpi3: core: move low-level cpu functions from entry.s into arm8-32.s

--- uxmhf-rpi3/common/arm8-32.s
+++ uxmhf-rpi3/common/arm8-32.s
@@ -7,4 +7,66 @@
 #include <types.h>
 #include <arm8-32.h>
 
+.globl mmio_write32
+mmio_write32:
+    str r1,[r0]
+    bx lr
+
+.globl mmio_read32
+mmio_read32:
+    ldr r0,[r0]
+    bx lr
+
+.globl sysreg_read_spsr_hyp
+sysreg_read_spsr_hyp:
+	mrs r0, SPSR_hyp
+	bx lr
+
+
+.globl sysreg_read_scr
+sysreg_read_scr:
+	mrc p15, 0, r0, c1, c1, 0
+	bx lr
+
+.globl sysreg_read_cpsr
+sysreg_read_cpsr:
+	mrs r0, cpsr
+	bx lr
+
+.global sysreg_read_hvbar
+sysreg_read_hvbar:
+	mrc p15, 4, r0, c12, c0, 0
+	bx lr
+
+
+.global sysreg_write_hvbar
+sysreg_write_hvbar:
+	mcr p15, 4, r0, c12, c0, 0
+	bx lr
+
+
+.global sysreg_read_hcr
+sysreg_read_hcr:
+	mrc p15, 4, r0, c1, c1, 0
+	bx lr
+
+.global sysreg_write_hcr
+sysreg_write_hcr:
+	mcr p15, 4, r0, c1, c1, 0
+	bx lr
+
+.global sysreg_read_hsctlr
+sysreg_read_hsctlr:
+	mrc p15, 4, r0, c1, c0, 0
+	bx lr
+
+.global sysreg_write_hsctlr
+sysreg_write_hsctlr:
+	mcr p15, 4, r0, c1, c0, 0
+	bx lr
+
+.global hypcall
+hypcall:
+	hvc #0
+	bx lr
 
