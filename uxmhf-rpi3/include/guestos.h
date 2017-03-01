556178dc23641c28a0dd8bffe043cb6df1520c2c
Amit Vasudevan <amitvasudevan@acm.org>
2017-03-01 15:47:47 -0600

    uxmhf-rpi3: include: add guestos.h to house dummy guest os definitions; add read/write memory extents and testpage address (harcoded for now)

---
+++ uxmhf-rpi3/include/guestos.h
@@ -0,0 +1,21 @@
+/*
+	dummy guest OS definitions
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#ifndef __GUESTOS_H__
+#define __GUESTOS_H__
+
+#define GUESTOS_RWMEM_ADDR_START	0x31000000
+#define GUESTOS_RWMEM_ADDR_END		0x31001000
+#define GUESTOS_TESTPAGE_ADDR		0x31002000
+
+#ifndef __ASSEMBLY__
+
+
+#endif // __ASSEMBLY__
+
+
+
+#endif //__GUESTOS_H__
