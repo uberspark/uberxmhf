749fdf634b830cd0a3b488c1f6221af40b6fa1bf
Amit Vasudevan <amitvasudevan@acm.org>
2017-03-01 22:43:17 -0600

    uxmhf-rpi3: include: fdt.h: define top-level FDT header structure and magic header start value

--- uxmhf-rpi3/include/fdt.h
+++ uxmhf-rpi3/include/fdt.h
@@ -7,9 +7,24 @@
 #ifndef __FDT_H__
 #define __FDT_H__
 
+#define FDT_MAGIC	0xedfe0dd0
 
 #ifndef __ASSEMBLY__
 
+// DT spec. v0.1
+
+struct fdt_header {
+	u32 magic;
+	u32 totalsize;
+	u32 off_dt_struct;
+	u32 off_dt_strings;
+	u32 off_mem_rsvmap;
+	u32 version;
+	u32 last_comp_version;
+	u32 boot_cpuid_phys;
+	u32 size_dt_strings;
+	u32 size_dt_struct;
+};
 
 #endif // __ASSEMBLY__
 
