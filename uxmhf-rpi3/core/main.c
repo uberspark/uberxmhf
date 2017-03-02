e6fd154796787ba7bc514bc1c7ec41b77a5de7cc
Amit Vasudevan <amitvasudevan@acm.org>
2017-03-01 22:47:40 -0600

    uxmhf-rpi3: core: refine main to use FDT_MAGIC and halt if passed blob is not a FDT; successful runtime test.

--- uxmhf-rpi3/core/main.c
+++ uxmhf-rpi3/core/main.c
@@ -3,6 +3,7 @@
 #include <bcm2837.h>
 #include <miniuart.h>
 #include <atags.h>
+#include <fdt.h>
 #include <debug.h>
 
 
@@ -108,10 +109,11 @@ void main(u32 r0, u32 id, struct atag *at){
 	bcm2837_miniuart_puts(" ATAGS= ");
 	debug_hexdumpu32(at);
 
-	if(at->size == 0xedfe0dd0)
+	if(at->size == FDT_MAGIC)
 		bcm2837_miniuart_puts("uXMHF-rpi3: core: ATAGS pointer is a FDT blob so no worries\n");
 	else{
 		bcm2837_miniuart_puts("uXMHF-rpi3: core: Error: require ATAGS to be FDT blob. Halting!\n");
+		HALT();
 	}
 
 	//store guest OS boot register values
