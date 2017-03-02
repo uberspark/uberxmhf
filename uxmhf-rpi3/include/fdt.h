b17613411ed1adcfa7b112d7a24db25acbde0a29
Amit Vasudevan <amitvasudevan@acm.org>
2017-03-01 22:40:35 -0600

    uxmhf-rpi3: include: add fdt.h header template to house flattened device tree blob definitions

---
+++ uxmhf-rpi3/include/fdt.h
@@ -0,0 +1,17 @@
+/*
+	flattened device tree blob definitions
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#ifndef __FDT_H__
+#define __FDT_H__
+
+
+#ifndef __ASSEMBLY__
+
+
+#endif // __ASSEMBLY__
+
+
+#endif //__FDT_H__
