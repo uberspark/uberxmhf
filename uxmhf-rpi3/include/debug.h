8f2bd9a70c092c90587fdec69831e6bf50c63b9e
Amit Vasudevan <amitvasudevan@acm.org>
2017-03-30 10:36:44 -0500

    uxmhf-rpi3: include: add _XDPRINTF_ debug printf definition within debug.h

--- uxmhf-rpi3/include/debug.h
+++ uxmhf-rpi3/include/debug.h
@@ -10,11 +10,40 @@
 #ifndef __ASSEMBLY__
 
 
+#if defined (__DEBUG_SERIAL__)
+
+
+//extern __attribute__(( section(".data") )) u32 libxmhfdebug_lock;
+
+static inline void _XDPRINTF_(const char *fmt, ...){
+    va_list       ap;
+	int retval;
+	char buffer[1024];
+
+	va_start(ap, fmt);
+	retval = vsnprintf(&buffer, 1024, fmt, ap);
+	//spin_lock(&libxmhfdebug_lock);
+	bcm2837_miniuart_puts(&buffer);
+	//spin_unlock(&libxmhfdebug_lock);
+    va_end(ap);
+}
+
+#else
+
+#define _XDPRINTF_(format, args...)
+
+#endif // defined
+
+
+
+
 void debug_hexdumpu32(u32 value);
 
 #define HALT() while(1);
 
 #endif // __ASSEMBLY__
+
+
 
 
 
