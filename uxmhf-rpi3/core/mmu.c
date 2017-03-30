f1ca5e0bbba4e4ffeb1b8adeaae99a53f3bc7c06
Amit Vasudevan <amitvasudevan@acm.org>
2017-03-30 16:51:21 -0500

    uxmhf-rpi3: flesh out mmu_disableallcaches

--- uxmhf-rpi3/core/mmu.c
+++ uxmhf-rpi3/core/mmu.c
@@ -11,6 +11,18 @@
 #include <debug.h>
 
 void mmu_disableallcaches(void){
+	u32 hsctlr;
 
+	//invalidate all TLB
+	sysreg_tlbiallh();
+
+	//invalidate instruction caches
+	sysreg_iciallu();
+
+	//disable instruction and data caching
+	hsctlr = sysreg_read_hsctlr();
+	hsctlr &= ~(1 << 12);	//disable instruction caching
+	hsctlr &= ~(1 << 2);	//disable data caching
+	sysreg_write_hsctlr(hsctlr);
 
 }
