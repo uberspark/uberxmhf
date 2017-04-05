b6a9bd5fc8f9c81e5d7884e0c2e1f046310b593b
Amit Vasudevan <amitvasudevan@acm.org>
2017-04-05 10:51:46 -0500

    uxmhf-rpi3: prepare to plug in BCM2837 package specific functionality...

---
+++ uxmhf-rpi3/core/bcm2837.c
@@ -0,0 +1,12 @@
+/*
+	BCM2837 (pi3) specific functions
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#include <types.h>
+#include <arm8-32.h>
+#include <bcm2837.h>
+#include <miniuart.h>
+#include <debug.h>
+
