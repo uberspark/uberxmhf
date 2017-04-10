edfa095de0c0c3579f130630160c734aa34a0b10
Amit Vasudevan <amitvasudevan@acm.org>
2017-04-10 12:18:35 -0500

    uxmhf-rpi3: core: secondary_main_svc: spit out sp and alive message; cpu-1 is successfully entering SVC mode...

--- uxmhf-rpi3/core/main.c
+++ uxmhf-rpi3/core/main.c
@@ -12,6 +12,8 @@ extern void chainload_os(u32 r0, u32 id, struct atag *at);
 extern void cpumodeswitch_hyp2svc(u32 r0, u32 id, struct atag *at, u32 address, u32 cpuid);
 
 extern void entry_svc(void);
+extern void secondary_cpu_entry_svc(void);
+
 
 extern u32 g_hypvtable[];
 
@@ -216,6 +218,7 @@ void core_fixresmemmap(u32 fdt_address){
 volatile __attribute__((aligned(32))) u32 my_lock=1;
 extern u32 cpu_smpready[];
 extern u8 cpu_stacks[];
+extern u8 cpu_stacks_svc[];
 
 
 void main(u32 r0, u32 id, struct atag *at){
@@ -386,30 +389,10 @@ void secondary_main(u32 cpuid){
 	_XDPRINTF_("%s[%u]: HDCR=0x%08x\n", __func__, cpuid, sysreg_read_hdcr());
 
 
-	_XDPRINTF_("%s[%u]: Signalling SMP readiness...\n", __func__, cpuid);
-	cpu_smpready[cpuid]=1;
-
-	//use XDPRINTFSMP from hereon
-	start_address=bcm2837_platform_waitforstartup(cpuid);
-
-	//if(cpuid == 1){
-	//	_XDPRINTFSMP_("%s[%u]: Boooting CPU within guest at 0x%08x...\n", __func__, cpuid, start_address);
-	//	cpumodeswitch_hyp2svc(0, 0, 0, start_address);
-	//}
-
-
-
-	_XDPRINTF_("%s[%u]: We should never be here. Halting!\n", __func__, cpuid);
-	HALT();
-}
-
-
-//all secondary CPUs get here in SVC mode and enter the wait-for-startup loop
-void secondary_main_svc(u32 cpuid){
-	_XDPRINTF_("%s[%u]: ENTER...\n", __func__, cpuid);
-
-	_XDPRINTF_("%s[%u]: Signalling SMP readiness...\n", __func__, cpuid);
-	cpu_smpready[cpuid]=1;
+	_XDPRINTF_("%s[%u]: proceeding to switch to SVC mode...\n", __func__, cpuid);
+	//
+	//_XDPRINTF_("%s[%u]: Signalling SMP readiness...\n", __func__, cpuid);
+	//cpu_smpready[cpuid]=1;
 
 	//use XDPRINTFSMP from hereon
 	//start_address=bcm2837_platform_waitforstartup(cpuid);
@@ -419,6 +402,30 @@ void secondary_main_svc(u32 cpuid){
 	//	cpumodeswitch_hyp2svc(0, 0, 0, start_address);
 	//}
 
-	_XDPRINTFSMP_("%s[%u]: We should never be here. Halting!\n", __func__, cpuid);
+	cpumodeswitch_hyp2svc(0, 0, 0, &secondary_cpu_entry_svc, cpuid);
+
+
+	_XDPRINTF_("%s[%u]: We should never be here. Halting!\n", __func__, cpuid);
+	HALT();
+}
+
+
+//all secondary CPUs get here in SVC mode and enter the wait-for-startup loop
+void secondary_main_svc(u32 cpuid){
+	_XDPRINTF_("%s[%u]: ENTER: sp=0x%08x (cpu_stacks_svc=0x%08x)\n", __func__, cpuid,
+			cpu_read_sp(), &cpu_stacks_svc);
+
+	//_XDPRINTF_("%s[%u]: Signalling SMP readiness...\n", __func__, cpuid);
+	//cpu_smpready[cpuid]=1;
+
+	//use XDPRINTFSMP from hereon
+	//start_address=bcm2837_platform_waitforstartup(cpuid);
+
+	//if(cpuid == 1){
+	//	_XDPRINTFSMP_("%s[%u]: Boooting CPU within guest at 0x%08x...\n", __func__, cpuid, start_address);
+	//	cpumodeswitch_hyp2svc(0, 0, 0, start_address);
+	//}
+
+	_XDPRINTF_("%s[%u]: We should never be here. Halting!\n", __func__, cpuid);
 	HALT();
 }
