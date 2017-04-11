69d103a6ee69bb09b2885f0580928a547fff9806
Amit Vasudevan <amitvasudevan@acm.org>
2017-04-11 17:03:05 -0500

    uxmhf-rpi3: core: refine chainload_os to switch to SVC mode with appropriate PE state; linux kernel boots with secondary processors reported as failed to start as expected

--- uxmhf-rpi3/core/entry.s
+++ uxmhf-rpi3/core/entry.s
@@ -37,12 +37,53 @@ hlt_entry_svc:
 	b hlt_entry_svc
 
 
+//r0,r1,r2 go through to the guest os
+//r3 contains the starting address to execute the guestos
+//clobbers r7,r9
 .globl chainload_os
 chainload_os:
-//	ldr r3, =0x30800000
-//	ldr r3, =0x00008000
-//	blx r3
-	blx r3
+
+	mov	r7, #0
+	mcr	p15, 4, r7, c1, c1, 0	// HCR=0
+	mcr	p15, 4, r7, c1, c1, 2	// HCPTR=0
+	mcr	p15, 4, r7, c1, c1, 3	// HSTR=0
+
+	//mcr	p15, 4, r7, c1, c0, 0	// HSCTLR=0
+
+	mrc	p15, 4, r7, c1, c1, 1	// HDCR
+	and	r7, #0x1f				// Preserve HPMN
+	mcr	p15, 4, r7, c1, c1, 1	// HDCR
+
+
+	// make CNTP_* and CNTPCT accessible from guest
+	mrc	p15, 0, r7, c0, c1, 1	// ID_PFR1
+	lsr	r7, #16
+	and	r7, #0xf
+	cmp	r7, #1
+	bne	1f
+	mrc	p15, 4, r7, c14, c1, 0	// CNTHCTL
+	orr	r7, r7, #3				// PL1PCEN | PL1PCTEN
+	mcr	p15, 4, r7, c14, c1, 0	// CNTHCTL
+	mov	r7, #0
+	mcrr	p15, 4, r7, r7, c14	// CNTVOFF
+
+	//disable virtual timer
+	mrc	p15, 0, r7, c14, c3, 1	// CNTV_CTL
+	bic	r7, #1					// Clear ENABLE
+	mcr	p15, 0, r7, c14, c3, 1	// CNTV_CTL
+
+1:
+
+	mrs	r9 , cpsr
+	eor	r9, r9, #0x0000001a		//HYP_MODE
+	tst	r9, #0x0000001f			//MODE_MASK
+	bic	r9 , r9 , #0x0000001f	//MODE_MASK
+	orr	r9 , r9 , #(0x00000080 | 0x00000040 | 0x00000013) //PSR_I_BIT | PSR_F_BIT | SVC_MODE
+	orr	r9, r9, #0x00000100		//PSR_A_BIT
+	msr	spsr_cxsf, r9
+	msr ELR_hyp, r3			//store address to begin execution in SVC mode in ELR_hyp
+	eret
+
 
 .globl chainload_os_svc
 chainload_os_svc:
