a06169ea6c1a7fa7b183d22b9efe4a201ee61d31
Amit Vasudevan <amitvasudevan@acm.org>
2017-04-20 10:43:41 -0500

    guestos: rjl: add hypcall kernel module template

---
+++ uxmhf-rpi3/guestos/raspbian-jessie-lite/hypcall/hypcall.c
@@ -0,0 +1,35 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+
+
+void hypcall_hvc(void){
+
+	asm volatile
+		("hvc #0"
+	           : /* output */
+	           : /* inputs */
+	           : /* clobber */
+	    );
+
+	return result;
+}
+
+
+
+//module initialization function
+int hypcall_init(void)
+{
+	printk(KERN_INFO "hypcall_init: LOAD\n");
+	printk(KERN_INFO "author: amit vasudevan (amitvasudevan@acm.org)\n");
+	return 0;
+}
+
+//module unload function
+void hypcall_exit(void)
+{
+	printk(KERN_INFO "hypcall_init: UNLOAD\n");
+}
+
+module_init(hypcall_init);
+module_exit(hypcall_exit);
