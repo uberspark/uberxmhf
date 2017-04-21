0e5adcbe66f66514647080d56555e16377e9ab01
Amit Vasudevan <amitvasudevan@acm.org>
2017-04-21 09:58:48 -0500

    uxmhf-rpi3: core: add uapi_s2pgtbl_setprot to set s2pgtbl memory protections for given physical address

--- uxmhf-rpi3/core/uapi_s2pgtbl.c
+++ uxmhf-rpi3/core/uapi_s2pgtbl.c
@@ -13,5 +13,14 @@
 
 extern u64 l3_ldesc_table[L1_LDESC_TABLE_ENTRIES * L2_LDESC_TABLE_MAXENTRIES * L3_LDESC_TABLE_MAXENTRIES];
 
+void uapi_s2pgtbl_setprot(u32 address, u64 protection){
+	u32 index;
 
+	if ( !((address >= UXMHF_CORE_START_ADDR) &&
+			  (address < UXMHF_CORE_END_ADDR)) ){
+		index = address/PAGE_SIZE_4K;
+		l3_ldesc_table[index] = ldesc_make_s2_l3e_page(address, protection);
+	}
+
+}
 
