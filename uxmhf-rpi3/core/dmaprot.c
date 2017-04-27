7eff3165f2e7b4a33fdddfcfa0769ad155d35e92
Amit Vasudevan <amitvasudevan@acm.org>
2017-04-26 22:46:12 -0500

    uxmhf-rpi3: core: add dmaprot module in preparation to flesh out dma protection logic

---
+++ uxmhf-rpi3/core/dmaprot.c
@@ -0,0 +1,12 @@
+/*
+	Pi3 DMA protection implementation
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#include <types.h>
+#include <arm8-32.h>
+#include <bcm2837.h>
+#include <miniuart.h>
+#include <debug.h>
+
