f951bb8a2154c5da7e9860cfc0e38eab8d1a4669
Amit Vasudevan <amitvasudevan@acm.org>
2017-05-03 23:27:23 -0500

    uxmhf-rpi3: core: dmaprot_activate: temporarily comment out base DMA controller read/write interception

--- uxmhf-rpi3/core/dmaprot.c
+++ uxmhf-rpi3/core/dmaprot.c
@@ -15,10 +15,10 @@ __attribute__((section(".palign2mdata"))) __attribute__((align(PAGE_SIZE_2M))) d
 
 //activate DMA protection mechanism
 void dmaprot_activate(void){
-	u64 attrs_dev = (LDESC_S2_MC_DEVnGnRnE << LDESC_S2_MEMATTR_MC_SHIFT) |
-			(LDESC_S2_S2AP_NO_ACCESS << LDESC_S2_MEMATTR_S2AP_SHIFT) |
-			(MEM_INNER_SHAREABLE << LDESC_S2_MEMATTR_SH_SHIFT) |
-			LDESC_S2_MEMATTR_AF_MASK;
+	//u64 attrs_dev = (LDESC_S2_MC_DEVnGnRnE << LDESC_S2_MEMATTR_MC_SHIFT) |
+	//		(LDESC_S2_S2AP_NO_ACCESS << LDESC_S2_MEMATTR_S2AP_SHIFT) |
+	//		(MEM_INNER_SHAREABLE << LDESC_S2_MEMATTR_SH_SHIFT) |
+	//		LDESC_S2_MEMATTR_AF_MASK;
 
 	u64 attrs_dev_dwc = (LDESC_S2_MC_DEVnGnRnE << LDESC_S2_MEMATTR_MC_SHIFT) |
 			(LDESC_S2_S2AP_READ_ONLY << LDESC_S2_MEMATTR_S2AP_SHIFT) |
@@ -26,10 +26,10 @@ void dmaprot_activate(void){
 			LDESC_S2_MEMATTR_AF_MASK;
 
 
-	uapi_s2pgtbl_setprot(BCM2837_DMA0_REGS_BASE, attrs_dev);
-	sysreg_tlbiallis();
-	uapi_s2pgtbl_setprot(BCM2837_DMA15_REGS_BASE, attrs_dev);
-	sysreg_tlbiallis();
+	//uapi_s2pgtbl_setprot(BCM2837_DMA0_REGS_BASE, attrs_dev);
+	//sysreg_tlbiallis();
+	//uapi_s2pgtbl_setprot(BCM2837_DMA15_REGS_BASE, attrs_dev);
+	//sysreg_tlbiallis();
 
 	//USB DMA controller
 	uapi_s2pgtbl_setprot(DWC_REGS_BASE, attrs_dev_dwc);
