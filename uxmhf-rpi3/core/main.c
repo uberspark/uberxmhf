53fe0094dc431781d0454d031b702494ac002eab
Amit Vasudevan <amitvasudevan@acm.org>
2017-05-10 00:07:49 -0500

    uxmhf-rpi3: core: guest_data_abort_handler: plug in a note for hooking up base dmac controller accesses later on...

--- uxmhf-rpi3/core/main.c
+++ uxmhf-rpi3/core/main.c
@@ -226,6 +226,11 @@ void guest_data_abort_handler(arm8_32_regs_t *r, u32 hsr){
 		HALT();
 	}
 
+	/*if( (da_pa_page == BCM2837_DMA0_REGS_BASE) ||
+		(da_pa_page == BCM2837_DMA15_REGS_BASE) ){
+		dmaprot_handle_dmacontroller_access(&ida);
+	*/
+
 }
 
 
