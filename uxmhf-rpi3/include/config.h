0bbc7d9a52d688b3c7d7babc7d13c6e62dd71c4d
Amit Vasudevan <amitvasudevan@acm.org>
2017-05-15 10:01:54 -0500

    uxmhf-rpi3: add top-level config.h placeholder

---
+++ uxmhf-rpi3/include/config.h
@@ -0,0 +1,18 @@
+/*
+	configuration
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#define CONFIG_PLACEHOLDER 	0x0
+
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#ifndef __ASSEMBLY__
+
+
+#endif // __ASSEMBLY__
+
+
+#endif //__TYPES_H__
