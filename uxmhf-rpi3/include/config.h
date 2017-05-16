f7cd2ecd00295ef993fc97605c8853a5ad9a9b4c
Amit Vasudevan <amitvasudevan@acm.org>
2017-05-15 22:33:50 -0500

    uxmhf-rpi3: include/config.h: add UXMHF_CORE_SIZE configuration definition

--- uxmhf-rpi3/include/config.h
+++ uxmhf-rpi3/include/config.h
@@ -4,8 +4,10 @@
 	author: amit vasudevan (amitvasudevan@acm.org)
 */
 
-#define UXMHF_CORE_START_ADDR (0x28000000)
-#define UXMHF_CORE_END_ADDR (0x28000000+0xC00000)
+#define UXMHF_CORE_START_ADDR 	0x28000000
+#define UXMHF_CORE_SIZE			0xC00000
+
+#define UXMHF_CORE_END_ADDR 	(0x28000000 + 0xC00000)
 
 
 #ifndef __CONFIG_H__
