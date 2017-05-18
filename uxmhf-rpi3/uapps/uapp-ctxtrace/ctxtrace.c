ad65b29fb2301351561cbc67ec4db35cfe6c887c
Amit Vasudevan <amitvasudevan@acm.org>
2017-05-18 12:27:43 -0500

    uxmhf-rpi3: ctxtrace: add ctxtrace_init to initialize guest context tracing by setting CP15 c2 trap in HSTR

--- uxmhf-rpi3/uapps/uapp-ctxtrace/ctxtrace.c
+++ uxmhf-rpi3/uapps/uapp-ctxtrace/ctxtrace.c
@@ -10,3 +10,18 @@
 #include <miniuart.h>
 #include <debug.h>
 #include <ctxtrace.h>
+
+//initialize guest context tracing
+void ctxtrace_init(void){
+	u32 hstr;
+
+	hstr = sysreg_read_hstr();
+	_XDPRINTFSMP_("%s: HSTR before=0x%08x\n", __func__, hstr);
+
+	hstr = hstr | (1UL << 2);	//activate trap on CP15, c2
+
+	_XDPRINTFSMP_("%s: HSTR after=0x%08x\n", __func__, hstr);
+	sysreg_write_hstr(hstr);
+
+	_XDPRINTFSMP_("%s: initialized guest context tracing\n", __func__);
+}
