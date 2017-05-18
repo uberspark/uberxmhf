708c49a22daa5995fd1926cc7aa715e99ea3aac4
Amit Vasudevan <amitvasudevan@acm.org>
2017-05-18 11:54:56 -0500

    uxmhf-rpi3: ctxtrace: add hypapp main module and header

---
+++ uxmhf-rpi3/uapps/uapp-ctxtrace/ctxtrace.c
@@ -0,0 +1,12 @@
+/*
+	guest context switch tracer
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#include <types.h>
+#include <arm8-32.h>
+#include <bcm2837.h>
+#include <miniuart.h>
+#include <debug.h>
+#include <ctxtrace.h>
