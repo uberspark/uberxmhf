45ddf501f8f879fd1cb7ebb2bc8ffa3d1ddf2679
Amit Vasudevan <amitvasudevan@acm.org>
2017-05-28 12:39:43 -0500

    uxmhf-rpi3: core: hypirq_timer: set IRQ generation for cpu0 hyp timer

--- uxmhf-rpi3/core/hyptimer.c
+++ uxmhf-rpi3/core/hyptimer.c
@@ -34,21 +34,29 @@ void hyptimer_test(u32 cpuid){
 
 	_XDPRINTFSMP_("%s[%u]: ENTER\n", __func__, cpuid);
 
-	cpsr = sysreg_read_cpsr();
-	_XDPRINTFSMP_("%s[%u]: CPSR[before]=0x%08x; CPSR.A=%u, CPSR.I=%u, CPSR.F=%u\n",
-			__func__, cpuid, cpsr, ((cpsr & (1UL << 8)) >> 8),
-			((cpsr & (1UL << 7)) >> 7),
-			((cpsr & (1UL << 6)) >> 6) );
+	//enable cpu0 timer interrupt control to generate IRQs
+	cpu0_tintctl_value = mmio_read32(LOCAL_TIMER_INT_CONTROL0);
+	_XDPRINTFSMP_("%s[%u]: cpu0_tintctl_value[before]=0x%08x, CNTHPFIQ=%u, CNTHPIRQ=%u\n",
+			__func__, cpuid,
+			cpu0_tintctl_value,
+			((cpu0_tintctl_value & (1UL << 6)) >> 6),
+			((cpu0_tintctl_value & (1UL << 2)) >> 2)
+			);
 
-	cpsr &= ~(1UL << 7);	//clear CPSR.I to allow IRQss
-	sysreg_write_cpsr(cpsr);
+	cpu0_tintctl_value |= (1UL << 2); //enable IRQs
+	cpu0_tintctl_value &= ~(1UL << 6); //disable FIQs
+	mmio_write32(LOCAL_TIMER_INT_CONTROL0, cpu0_tintctl_value);
 
 
-	cpsr = sysreg_read_cpsr();
-	_XDPRINTFSMP_("%s[%u]: CPSR[after]=0x%08x; CPSR.A=%u, CPSR.I=%u, CPSR.F=%u\n",
-			__func__, cpuid, cpsr, ((cpsr & (1UL << 8)) >> 8),
-			((cpsr & (1UL << 7)) >> 7),
-			((cpsr & (1UL << 6)) >> 6) );
+	cpu0_tintctl_value = mmio_read32(LOCAL_TIMER_INT_CONTROL0);
+	_XDPRINTFSMP_("%s[%u]: cpu0_tintctl_value[after]=0x%08x, CNTHPFIQ=%u, CNTHPIRQ=%u\n",
+			__func__, cpuid,
+			cpu0_tintctl_value,
+			((cpu0_tintctl_value & (1UL << 6)) >> 6),
+			((cpu0_tintctl_value & (1UL << 2)) >> 2)
+			);
+
+
 
 
 	cntpct_val = sysreg_read_cntpct();
@@ -71,13 +79,25 @@ void hyptimer_test(u32 cpuid){
 	_XDPRINTFSMP_("%s[%u]: CNTHP_TVAL[current]=%d\n", __func__, cpuid, sysreg_read_cnthp_tval());
 	_XDPRINTFSMP_("%s[%u]: CNTHP_CTL[current]=%d\n", __func__, cpuid, sysreg_read_cnthp_ctl());
 
-	cpu0_tintctl_value = mmio_read32(LOCAL_TIMER_INT_CONTROL0);
-	_XDPRINTFSMP_("%s[%u]: cpu0_tintctl_value=0x%08x, CNTHPFIQ=%u, CNTHPIRQ=%u\n",
-			__func__, cpuid,
-			cpu0_tintctl_value,
-			((cpu0_tintctl_value & (1UL << 6)) >> 6),
-			((cpu0_tintctl_value & (1UL << 2)) >> 2)
-			);
+
+
+	cpsr = sysreg_read_cpsr();
+	_XDPRINTFSMP_("%s[%u]: CPSR[before]=0x%08x; CPSR.A=%u, CPSR.I=%u, CPSR.F=%u\n",
+			__func__, cpuid, cpsr, ((cpsr & (1UL << 8)) >> 8),
+			((cpsr & (1UL << 7)) >> 7),
+			((cpsr & (1UL << 6)) >> 6) );
+
+/*	cpsr &= ~(1UL << 7);	//clear CPSR.I to allow IRQss
+	sysreg_write_cpsr(cpsr);
+
+
+	cpsr = sysreg_read_cpsr();
+	_XDPRINTFSMP_("%s[%u]: CPSR[after]=0x%08x; CPSR.A=%u, CPSR.I=%u, CPSR.F=%u\n",
+			__func__, cpuid, cpsr, ((cpsr & (1UL << 8)) >> 8),
+			((cpsr & (1UL << 7)) >> 7),
+			((cpsr & (1UL << 6)) >> 6) );
+*/
+
 
 	_XDPRINTFSMP_("%s[%u]: now moving into endless loop...\n", __func__, cpuid);
 	HALT();
