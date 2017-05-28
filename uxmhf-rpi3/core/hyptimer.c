05abcf4bc7b81d12806ca4c4ffc36c6bbda2d0ab
Amit Vasudevan <amitvasudevan@acm.org>
2017-05-28 00:27:55 -0500

    uxmhf-rpi3: core: hyptimer_test: dump CPSR.A,I,F bits to sanity check masking of IRQ/FIQs

--- uxmhf-rpi3/core/hyptimer.c
+++ uxmhf-rpi3/core/hyptimer.c
@@ -17,7 +17,16 @@
 // test function for hypervisor timers
 //////
 void hyptimer_test(u32 cpuid){
+	u32 cpsr;
+
 	_XDPRINTFSMP_("%s[%u]: ENTER\n", __func__, cpuid);
+
+	cpsr = sysreg_read_cpsr();
+	_XDPRINTFSMP_("%s[%u]: CPSR=0x%08x; CPSR.A=%u, CPSR.I=%u, CPSR.F=%u\n",
+			__func__, cpuid, cpsr, ((cpsr & (1UL << 8)) >> 8),
+			((cpsr & (1UL << 7)) >> 7),
+			((cpsr & (1UL << 6)) >> 6) );
+
 
 	_XDPRINTFSMP_("%s[%u]: EXIT\n", __func__, cpuid);
 }
