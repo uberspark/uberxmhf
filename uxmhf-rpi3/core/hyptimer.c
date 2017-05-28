fd4b7bb85db45216d4ff50f2addd5354fe610031
Amit Vasudevan <amitvasudevan@acm.org>
2017-05-27 23:44:09 -0500

    uxmhf-rpi3: core: add hyptimer.c to house hypervisor timer module

---
+++ uxmhf-rpi3/core/hyptimer.c
@@ -0,0 +1,12 @@
+/*
+	ARM 8 hypervisor timer module
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#include <types.h>
+#include <arm8-32.h>
+#include <bcm2837.h>
+#include <miniuart.h>
+#include <debug.h>
+
