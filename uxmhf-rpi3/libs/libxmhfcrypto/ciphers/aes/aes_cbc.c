08f5c3c0380ab2d64ee897cc80a5e824e86605d5
Amit Vasudevan <amitvasudevan@acm.org>
2017-06-05 23:22:18 -0500

    uxmhf-rpi3: libxmhfcrypto: add aes_cbc module for aes cbc functions

---
+++ uxmhf-rpi3/libs/libxmhfcrypto/ciphers/aes/aes_cbc.c
@@ -0,0 +1,21 @@
+/* LibTomCrypt, modular cryptographic library -- Tom St Denis
+ *
+ * LibTomCrypt is a library that provides various cryptographic
+ * algorithms in a highly modular and flexible manner.
+ *
+ * The library is free for all purposes without any express
+ * guarantee it works.
+ *
+ * Tom St Denis, tomstdenis@gmail.com, http://libtom.org
+ */
+
+/*
+	aes_cbc.c
+	aes cbc mode functions
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#include <string.h>
+#include <xmhfcrypto.h>
+#include <aes.h>
+
