9ff499a874138fbf32e7bc628cf0ab7c2fd26d5c
Amit Vasudevan <amitvasudevan@acm.org>
2017-06-15 18:59:12 -0500

    uxmhf-rpi3: utpm: add new module utpm.c to house utpm interface functions and tie it to the build process

---
+++ uxmhf-rpi3/uapps/uapp-utpm/utpm.c
@@ -0,0 +1,12 @@
+/*
+ * utpm interface
+ * adapted from trustvisor utpm by amit vasudevan (amitvasudevan@acm.org
+ */
+
+#include <types.h>
+#include <arm8-32.h>
+#include <bcm2837.h>
+#include <miniuart.h>
+#include <debug.h>
+#include <utpm.h>
+
