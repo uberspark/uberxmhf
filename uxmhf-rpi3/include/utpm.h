62b470236c6f67429f9b28cd1ad98b4f2885468c
Amit Vasudevan <amitvasudevan@acm.org>
2017-06-15 11:29:47 -0500

    uxmhf-rpi3: utpm: carve out utpm common functions from header into utpm-common.c

--- uxmhf-rpi3/include/utpm.h
+++ uxmhf-rpi3/include/utpm.h
@@ -127,27 +127,6 @@ typedef struct tdTPM_PCR_SELECTION TPM_PCR_SELECTION;
     + TPM_RSA_KEY_LEN)          /* sig */
 
 
-static inline void utpm_pcr_select_i(TPM_PCR_SELECTION *tpmsel, uint32_t i) {
-    /* TODO: fail loudly if any of these conditions do not hold */
-    if(NULL == tpmsel) return;
-    if(i >= TPM_PCR_NUM) return;    
-    /*if(i/8 >= tpmsel->sizeOfSelect) return; */ /* deprecated in favor of
-                                                  * auto-growing, as in the
-                                                  * next line. */
-
-    if(tpmsel->sizeOfSelect < i/8+1) { tpmsel->sizeOfSelect = i/8+1; } /* XXX not future-proof */
-    /* Set the bit corresponding to PCR i */
-    tpmsel->pcrSelect[i/8] |= (1 << (i%8));
-}
-
-static inline bool utpm_pcr_is_selected(TPM_PCR_SELECTION *tpmsel, uint32_t i) {
-    /* TODO: fail loudly if any of these conditions do not hold */
-    if(NULL == tpmsel) return false;
-    if(i >= TPM_PCR_NUM) return false;
-    if(i/8 >= tpmsel->sizeOfSelect) return false;
-
-    return (tpmsel->pcrSelect[i/8] & (1 << (i%8)));
-}
 
 typedef struct tdTPM_DIGEST{
   uint8_t value[TPM_HASH_SIZE];
@@ -170,50 +149,7 @@ typedef struct tdTPM_PCR_INFO {
                                             not used for authorization */
 } TPM_PCR_INFO; 
 
-/**
- * Return the amount of space overhead (in bytes) expected when
- * 'inlen' bytes of plaintext are sealed to the PCRs specified in
- * 'tpmsel'.
- *
- * NOTE: This _must_ remain consistent with the logic in utpm_seal().
- * 
- * TODO: Refactor utpm_seal() to have a size-only operating mode
- * (i.e., when certain input parameters are NULL).  It will be
- * reasonable to change this function into a wrapper to call
- * utpm_seal() with NULL buffers once that refactoring is complete.
- */
 #define ALIGN_UP(n, boundary)	(((n)+((boundary)-1))&(~((boundary)-1)))
-static inline unsigned int utpm_seal_output_size(unsigned int inlen,
-                                                 bool usingPcrs)
-{
-    unsigned int pad, size = 0;
-
-    /**
-     * 6 Contributors:
-     * 0. IV for symmetric encryption
-     * 1. TPM_PCR_SELECTION (two cases: 0 PCRs, 1+ PCRs)
-     * 2. input_len
-     * 3. input itself
-     * 4. pad out to symmetric encryption block size
-     * 5. SHA1-HMAC
-     */
-
-    /* 0 */
-    size += TPM_AES_KEY_LEN_BYTES;
-    /* 1 */
-    size += usingPcrs ? sizeof(TPM_PCR_INFO) : sizeof(uint16_t);
-    /* 2 */
-    size += sizeof(uint32_t);
-    /* 3 */
-    size += inlen;
-    /* 4 */
-    pad = ALIGN_UP(size, TPM_AES_KEY_LEN_BYTES) - size;
-    size += pad;
-    /* 5 */    
-    size += TPM_HASH_SIZE;
-
-    return size;
-}
 
 #define TPM_NONCE_SIZE 20
 typedef struct tdTPM_NONCE{
@@ -259,6 +195,16 @@ typedef struct tdTPM_STORED_DATA{ /* XXX inconsistent with hardware TPM */
 typedef struct utpm_master_state {
 	TPM_DIGEST pcr_bank[TPM_PCR_NUM];
 } __attribute__ ((packed)) utpm_master_state_t;
+
+
+// utpm common functions
+unsigned int utpm_seal_output_size(unsigned int inlen, bool usingPcrs);
+bool utpm_pcr_is_selected(TPM_PCR_SELECTION *tpmsel, uint32_t i);
+void utpm_pcr_select_i(TPM_PCR_SELECTION *tpmsel, uint32_t i);
+
+
+
+
 
 /* TPM functions  */
 TPM_RESULT utpm_pcrread(TPM_DIGEST* pcr_value,
--- uxmhf-rpi3/uapps/uapp-utpm/utpm-common.c
+++ uxmhf-rpi3/uapps/uapp-utpm/utpm-common.c
@@ -9,3 +9,71 @@
 #include <miniuart.h>
 #include <debug.h>
 #include <utpm.h>
+
+void utpm_pcr_select_i(TPM_PCR_SELECTION *tpmsel, uint32_t i) {
+    /* TODO: fail loudly if any of these conditions do not hold */
+    if(NULL == tpmsel) return;
+    if(i >= TPM_PCR_NUM) return;
+    /*if(i/8 >= tpmsel->sizeOfSelect) return; */ /* deprecated in favor of
+                                                  * auto-growing, as in the
+                                                  * next line. */
+
+    if(tpmsel->sizeOfSelect < i/8+1) { tpmsel->sizeOfSelect = i/8+1; } /* XXX not future-proof */
+    /* Set the bit corresponding to PCR i */
+    tpmsel->pcrSelect[i/8] |= (1 << (i%8));
+}
+
+bool utpm_pcr_is_selected(TPM_PCR_SELECTION *tpmsel, uint32_t i) {
+    /* TODO: fail loudly if any of these conditions do not hold */
+    if(NULL == tpmsel) return false;
+    if(i >= TPM_PCR_NUM) return false;
+    if(i/8 >= tpmsel->sizeOfSelect) return false;
+
+    return (tpmsel->pcrSelect[i/8] & (1 << (i%8)));
+}
+
+
+/**
+ * Return the amount of space overhead (in bytes) expected when
+ * 'inlen' bytes of plaintext are sealed to the PCRs specified in
+ * 'tpmsel'.
+ *
+ * NOTE: This _must_ remain consistent with the logic in utpm_seal().
+ *
+ * TODO: Refactor utpm_seal() to have a size-only operating mode
+ * (i.e., when certain input parameters are NULL).  It will be
+ * reasonable to change this function into a wrapper to call
+ * utpm_seal() with NULL buffers once that refactoring is complete.
+ */
+
+unsigned int utpm_seal_output_size(unsigned int inlen,
+                                                 bool usingPcrs)
+{
+    unsigned int pad, size = 0;
+
+    /**
+     * 6 Contributors:
+     * 0. IV for symmetric encryption
+     * 1. TPM_PCR_SELECTION (two cases: 0 PCRs, 1+ PCRs)
+     * 2. input_len
+     * 3. input itself
+     * 4. pad out to symmetric encryption block size
+     * 5. SHA1-HMAC
+     */
+
+    /* 0 */
+    size += TPM_AES_KEY_LEN_BYTES;
+    /* 1 */
+    size += usingPcrs ? sizeof(TPM_PCR_INFO) : sizeof(uint16_t);
+    /* 2 */
+    size += sizeof(uint32_t);
+    /* 3 */
+    size += inlen;
+    /* 4 */
+    pad = ALIGN_UP(size, TPM_AES_KEY_LEN_BYTES) - size;
+    size += pad;
+    /* 5 */
+    size += TPM_HASH_SIZE;
+
+    return size;
+}
