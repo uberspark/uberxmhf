eabcf6bd3c894c3c209cbb2977df97a050558b1a
Amit Vasudevan <amitvasudevan@acm.org>
2017-06-14 23:30:36 -0500

    uxmhf-rpi3: rgapp-pa5encfs: revise do_crypt with libxmhfcrypto aes cbc functions

--- uxmhf-rpi3/rgapps/linux/rgapp-pa5encfs/aes-crypt.c
+++ uxmhf-rpi3/rgapps/linux/rgapp-pa5encfs/aes-crypt.c
@@ -44,25 +44,28 @@ extern int do_crypt(FILE* in, FILE* out, int action, char* key_str){
     fprintf(stderr, "aes-crypt debug: at location 1\n");
     /* Setup Encryption Key and Cipher Engine if in cipher mode */
     if(action >= 0){
-      fprintf(stderr, "aes-crypt debug: at location: 2\n");
-	if(!key_str){
-	  fprintf(stderr, "aes-crypt debug: at location: 3\n");
-	    /* Error */
-	    fprintf(stderr, "Key_str must not be NULL\n");
-	    return 0;
-	}
-	/* Build Key from String */
-	i = EVP_BytesToKey(EVP_aes_256_cbc(), EVP_sha1(), NULL,
-			   (unsigned char*)key_str, strlen(key_str), nrounds, key, iv);
-	if (i != 32) {
-	  fprintf(stderr, "aes-crypt debug: at location: 4\n");
-	    /* Error */
-	    fprintf(stderr, "Key size is %d bits - should be 256 bits\n", i*8);
-	    return 0;
-	}
-	/* Init Engine */
-	EVP_CIPHER_CTX_init(&ctx);
-	EVP_CipherInit_ex(&ctx, EVP_aes_256_cbc(), NULL, key, iv, action);
+		fprintf(stderr, "aes-crypt debug: at location: 2\n");
+		if(!key_str){
+			fprintf(stderr, "aes-crypt debug: at location: 3\n");
+			/* Error */
+			fprintf(stderr, "Key_str must not be NULL\n");
+			return 0;
+		}
+
+		/* Build Key from String */
+		i = EVP_BytesToKey(EVP_aes_256_cbc(), EVP_sha1(), NULL,
+				   (unsigned char*)key_str, strlen(key_str), nrounds, key, iv);
+
+		if (i != 32) {
+		  fprintf(stderr, "aes-crypt debug: at location: 4\n");
+			/* Error */
+			fprintf(stderr, "Key size is %d bits - should be 256 bits\n", i*8);
+			return 0;
+		}
+
+		/* Init Engine */
+		EVP_CIPHER_CTX_init(&ctx);
+		EVP_CipherInit_ex(&ctx, EVP_aes_256_cbc(), NULL, key, iv, action);
     }    
 
     /* Loop through Input File*/
@@ -144,6 +147,8 @@ uint8_t aes_key[AES_KEY_LEN_BYTES] =
 			0xfb, 0xeb, 0xdb, 0xcb, 0xbb, 0xab, 0x9b, 0x8b
 	};
 
+
+#if 0
 extern int do_crypt(FILE* in, FILE* out, int action, char* key_str){
     symmetric_CBC cbc_ctx;
     int status;
@@ -169,6 +174,108 @@ extern int do_crypt(FILE* in, FILE* out, int action, char* key_str){
     if(status != CRYPT_OK){
     	return 0;
     }
+
+    return 1;
+}
+#endif //0
+
+#define ENCRYPT 1
+#define DECRYPT 0
+#define PASS_THROUGH (-1)
+
+
+extern int do_crypt(FILE* in, FILE* out, int action, char* key_str){
+    /* Local Vars */
+
+    /* Buffers */
+    unsigned char inbuf[BLOCKSIZE];
+    int inlen;
+    /* Allow enough space in output buffer for additional cipher block */
+    unsigned char outbuf[BLOCKSIZE];
+    int outlen;
+    int writelen;
+
+	symmetric_CBC cbc_ctx;
+
+    //action can be ENCRYPT (1), DECRYPT (0) or PASS_THROUGH (-1)
+
+    fprintf(stderr, "aes-crypt debug: at location 1\n");
+
+    /* Setup Encryption Key and Cipher Engine if in cipher mode */
+    if(action == ENCRYPT || action == DECRYPT){
+		fprintf(stderr, "aes-crypt debug: at location: 2\n");
+
+		if(!key_str){
+			fprintf(stderr, "aes-crypt debug: at location: 3\n");
+			/* Error */
+			fprintf(stderr, "Key_str must not be NULL\n");
+			return 0;
+		}
+
+		/* Build Key from String */
+		//TBD
+
+		/* Init Engine */
+		if( rijndael_cbc_start(aes_iv, aes_key, AES_KEY_LEN_BYTES, 0, &cbc_ctx) != CRYPT_OK )
+			return 0;
+    }
+
+    /* Loop through Input File*/
+    for(;;){
+    	/* Read Block */
+    	inlen = fread(inbuf, sizeof(*inbuf), BLOCKSIZE, in);
+    	if(inlen <= 0){
+    		/* EOF -> Break Loop */
+    		break;
+    	}
+
+    	/* If in cipher mode, perform cipher transform on block */
+    	if(action == ENCRYPT){
+    		fprintf(stderr, "aes-crypt debug: at location: 4\n");
+
+    	    if( rijndael_cbc_encrypt(inbuf, outbuf, inlen, &cbc_ctx) != CRYPT_OK)
+    	    	return 0;
+
+    	    outlen = inlen;
+    		fprintf(stderr, "aes-crypt debug: at location: 5\n");
+
+    	    if( rijndael_cbc_done( &cbc_ctx) != CRYPT_OK)
+    	    	return 0;
+
+    		fprintf(stderr, "aes-crypt debug: at location: 6\n");
+
+    	}else if (action == DECRYPT){
+    		fprintf(stderr, "aes-crypt debug: at location: 7\n");
+
+    	    if( rijndael_cbc_decrypt(inbuf, outbuf, inlen, &cbc_ctx) != CRYPT_OK)
+    	    	return 0;
+
+    	    outlen = inlen;
+    		fprintf(stderr, "aes-crypt debug: at location: 8\n");
+
+    	    if( rijndael_cbc_done( &cbc_ctx) != CRYPT_OK)
+    	    	return 0;
+
+    		fprintf(stderr, "aes-crypt debug: at location: 9\n");
+
+    	}else{
+    		/* If in pass-through mode. copy block as is */
+    		fprintf(stderr, "aes-crypt debug: at location: 10\n");
+    		memcpy(outbuf, inbuf, inlen);
+    		outlen = inlen;
+    	}
+
+    	/* Write Block */
+    	writelen = fwrite(outbuf, sizeof(*outbuf), outlen, out);
+    	if(writelen != outlen){
+    		fprintf(stderr, "aes-crypt debug: at location: 11\n");
+    		/* Error */
+    		perror("fwrite error");
+    		return 0;
+    	}
+    }	//end-for
+
+	fprintf(stderr, "aes-crypt debug: at location: 12\n");
 
     return 1;
 }
