dde4154cd31a3783e02a57a6f681231fbd359fd0
Amit Vasudevan <amitvasudevan@acm.org>
2017-06-15 22:47:30 -0500

    uxmhf-rpi3: libxmhfcrypto: add hmac-sha1 module in preparation to plug in hmac-sha1 implementation

---
+++ uxmhf-rpi3/libs/libxmhfcrypto/mac/hmac-sha1/hmac-sha1.c
@@ -0,0 +1,9 @@
+/*
+ * hmac-sha1 implementation
+ * author: amit vasudevan (amitvasudevan@acm.org)
+ * adapted from libtomcrypto
+ */
+
+#include <string.h>
+#include <xmhfcrypto.h>
+#include <sha1.h>
