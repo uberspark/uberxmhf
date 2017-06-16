8af036672782bd09a0433b4307b14f54962a8e13
Amit Vasudevan <amitvasudevan@acm.org>
2017-06-15 19:50:25 -0500

    uxmhf-rpi3: utpm: add utpm_init_master_entropy function

--- uxmhf-rpi3/uapps/uapp-utpm/utpm.c
+++ uxmhf-rpi3/uapps/uapp-utpm/utpm.c
@@ -18,3 +18,39 @@ __attribute__((section(".data"))) uint8_t g_aeskey[TPM_AES_KEY_LEN_BYTES];
 __attribute__((section(".data"))) uint8_t g_hmackey[TPM_HMAC_KEY_LEN];
 //rsa_key g_rsa_key;
 
+
+
+//////
+// interface functions
+//////
+
+
+////// initialize master entropy
+/**
+ * This function is expected to only be called once during the life of
+ * anything that uses this uTPM implementation.  This function will
+ * need significant modification if we ever decide to support
+ * arbitrarily many signing keys for uTPM instances.
+ */
+TPM_RESULT utpm_init_master_entropy(uint8_t *aeskey,
+                                    uint8_t *hmackey,
+                                    void /*rsa_context*/ *rsa) {
+    if(!aeskey || !hmackey || !rsa) {
+        return UTPM_ERR_INSUFFICIENT_ENTROPY;
+
+    }
+
+    memcpy(g_aeskey, aeskey, TPM_AES_KEY_LEN_BYTES);
+    memcpy(g_hmackey, hmackey, TPM_HMAC_KEY_LEN);
+    //memcpy(&g_rsa_key, rsa, sizeof(g_rsa_key));
+
+    /* ensure libtomcrypto's math descriptor is initialized */
+    //if (!ltc_mp.name) {
+    //  ltc_mp = ltm_desc;
+    //}
+
+    return UTPM_SUCCESS;
+}
+
+
+
