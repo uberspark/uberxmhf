3ebea40d77046846ad2030faf8eb75b53648f845
Amit Vasudevan <amitvasudevan@acm.org>
2017-06-15 23:02:14 -0500

    uxmhf-rpi3: libxmhfcrypto: flesh out hmac_sha1_memory; successful build.

--- uxmhf-rpi3/libs/libxmhfcrypto/mac/hmac-sha1/hmac-sha1.c
+++ uxmhf-rpi3/libs/libxmhfcrypto/mac/hmac-sha1/hmac-sha1.c
@@ -6,7 +6,7 @@
 
 #include <string.h>
 #include <xmhfcrypto.h>
-#include <sha1.h>
+#include <hmac-sha1.h>
 
 
 /**
@@ -49,51 +49,46 @@ int hmac_sha1_memory(const unsigned char *key,  unsigned long keylen,
                 const unsigned char *in,   unsigned long inlen,
                       unsigned char *out,  unsigned long *outlen)
 {
-    hmac_state *hmac;
+    //hmac_state *hmac;
+	hmac_state hmac;
     int         err;
 
-#if 0
     LTC_ARGCHK(key    != NULL);
     LTC_ARGCHK(in     != NULL);
     LTC_ARGCHK(out    != NULL);
     LTC_ARGCHK(outlen != NULL);
 
     /* make sure hash descriptor is valid */
-    if ((err = hash_is_valid(hash)) != CRYPT_OK) {
-       return err;
-    }
+    //if ((err = hash_is_valid(hash)) != CRYPT_OK) {
+    //   return err;
+    //}
 
     /* is there a descriptor? */
-    if (hash_descriptor[hash].hmac_block != NULL) {
-        return hash_descriptor[hash].hmac_block(key, keylen, in, inlen, out, outlen);
-    }
+    //if (hash_descriptor[hash].hmac_block != NULL) {
+    //    return hash_descriptor[hash].hmac_block(key, keylen, in, inlen, out, outlen);
+    //}
 
     /* nope, so call the hmac functions */
     /* allocate ram for hmac state */
-    hmac = XMALLOC(sizeof(hmac_state));
-    if (hmac == NULL) {
-       return CRYPT_MEM;
-    }
+    //hmac = XMALLOC(sizeof(hmac_state));
+    //if (hmac == NULL) {
+    //   return CRYPT_MEM;
+    //}
 
-    if ((err = hmac_init(hmac, hash, key, keylen)) != CRYPT_OK) {
+    if ((err = hmac_sha1_init(&hmac, key, keylen)) != CRYPT_OK) {
        goto LBL_ERR;
     }
 
-    if ((err = hmac_process(hmac, in, inlen)) != CRYPT_OK) {
+    if ((err = hmac_sha1_process(&hmac, in, inlen)) != CRYPT_OK) {
        goto LBL_ERR;
     }
 
-    if ((err = hmac_done(hmac, out, outlen)) != CRYPT_OK) {
+    if ((err = hmac_sha1_done(&hmac, out, outlen)) != CRYPT_OK) {
        goto LBL_ERR;
     }
 
    err = CRYPT_OK;
 LBL_ERR:
-#ifdef LTC_CLEAN_STACK
-   zeromem(hmac, sizeof(hmac_state));
-#endif
 
-   XFREE(hmac);
-#endif
    return err;
 }
