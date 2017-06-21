aaafe16e5803f7ef22dc11188978e50fc217478c
Amit Vasudevan <amitvasudevan@acm.org>
2017-06-21 00:13:07 -0500

    uxmhf-rpi3: libuhcall: add top-level header

---
+++ uxmhf-rpi3/libs/libuhcall/include/uhcall.h
@@ -0,0 +1,18 @@
+/*
+	libuhcall header
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#ifndef __UHCALL_H__
+#define __UHCALL_H__
+
+
+#ifndef __ASSEMBLY__
+
+
+#endif // __ASSEMBLY__
+
+
+
+#endif //__UHCALL_H__
