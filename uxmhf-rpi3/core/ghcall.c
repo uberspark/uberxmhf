08899979253be4416a2cd45d9f88eb003b8e81f2
Amit Vasudevan <amitvasudevan@acm.org>
2017-06-20 23:34:30 -0500

    uxmhf-rpi3: core: revise guest_hypercall_handler to invoke uapp hcall handlers; currently thread through uapp_uhcalltest_handlehcall. successful runtime test.

--- uxmhf-rpi3/core/ghcall.c
+++ uxmhf-rpi3/core/ghcall.c
@@ -42,9 +42,11 @@ void guest_hypercall_handler(arm8_32_regs_t *r, u32 hsr){
 		 * r2 = size of the guest buffer
 		 * note: r1+r2 cannot cross page-boundary
 		 */
-		_XDPRINTFSMP_("%s: hcall: r0=0x%08x, r1=0x%08x, r2=0x%08x\n", __func__,
-				r->r0, r->r1, r->r2);
+		if( uapp_uhcalltest_handlehcall(r->r0, r->r1, r->r2) )
+			return;
 
+		_XDPRINTFSMP_("%s: hcall unhandled. Halting!\n", __func__);
+		HALT();
 
 	}else{
 		_XDPRINTFSMP_("%s: unknown HVC instruction imm16=0x%08x. Halting!\n", __func__,
--- uxmhf-rpi3/uapps/uapp-uhcalltest/uapp-uhcalltest.c
+++ uxmhf-rpi3/uapps/uapp-uhcalltest/uapp-uhcalltest.c
@@ -11,4 +11,15 @@
 #include <miniuart.h>
 #include <debug.h>
 
+#define UAPP_UHCALLTEST_FUNCTION_TEST	10
 
+//return true if handled the hypercall, false if not
+bool uapp_uhcalltest_handlehcall(u32 uhcall_function, void *uhcall_buffer, u32 uhcall_buffer_len){
+	if(uhcall_function != UAPP_UHCALLTEST_FUNCTION_TEST)
+		return false;
+
+	_XDPRINTFSMP_("%s: hcall: uhcall_function=0x%08x, uhcall_buffer=0x%08x, uhcall_buffer_len=0x%08x\n", __func__,
+			uhcall_function, uhcall_buffer, uhcall_buffer_len);
+
+	return true;
+}
