ff7ad3933d68bf4efaaef171fe61d03f25d3aa16
Amit Vasudevan <amitvasudevan@acm.org>
2017-06-20 23:02:26 -0500

    uxmhf-rpi3: create rgapp-uhcalltest; move and adapt apphcall

---
+++ uxmhf-rpi3/rgapps/linux/rgapp-uhcalltest/uhcalltest.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<string.h>
+#include<unistd.h>
+
+
+int main(){
+   int ret, fd;
+
+   printf("Starting usr mode hypercall test...\n");
+
+   fd = open("/dev/hypcallchar", O_RDWR);             // Open the device with read/write access
+   if (fd < 0){
+      perror("Failed to open the device...");
+      return errno;
+   }
+
+   ret = write(fd, NULL, 1);
+   if (ret < 0){
+      perror("Failed to write the message to the device.");
+      return errno;
+   }
+
+   printf("End of test\n");
+   return 0;
+}
