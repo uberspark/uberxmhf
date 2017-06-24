129f5b24db096a3db5f85f24ea7913917715b32d
Amit Vasudevan <amitvasudevan@acm.org>
2017-06-23 23:33:11 -0500

    uxmhf-rpi3: rgapp-uhcalltest: refine main to use uhcalltest_param_t variable and pass the same into uhcall

--- uxmhf-rpi3/rgapps/linux/rgapp-uhcalltest/uhcalltest.c
+++ uxmhf-rpi3/rgapps/linux/rgapp-uhcalltest/uhcalltest.c
@@ -17,15 +17,45 @@
 #include <uhcall.h>
 #include <uhcalltest.h>
 
+__attribute__((aligned(4096))) static uhcalltest_param_t uhctp;
 
 int main(){
+	uint8_t ch='a';
+	uint32_t i;
 
-   printf("Starting usr mode hypercall test: Proceeding to issue hypercall...\n");
+   printf("Starting usr mode hypercall test\n");
 
-   if(!uhcall(UAPP_UHCALLTEST_FUNCTION_TEST, NULL, 0))
+
+   printf("populating uhctp.in[] and uhctp.out[]...\n");
+   for(i=0; i < 16; i++)
+	   uhctp.in[i] = ch + i;
+   memset(&uhctp.out, 0, 16);
+
+   printf("dumping uhctp.in[]...\n");
+   for(i=0; i < 16; i++)
+	   printf("%c", uhctp.in[i]);
+   printf("\ndumped uhctp.in[]\n");
+   printf("dumping uhctp.out[]...\n");
+   for(i=0; i < 16; i++)
+	   printf("%c", uhctp.out[i]);
+   printf("\ndumped uhctp.out[]\n");
+
+
+   printf("Proceeding to issue hypercall...\n");
+
+   if(!uhcall(UAPP_UHCALLTEST_FUNCTION_TEST, &uhctp, sizeof(uhcalltest_param_t)))
 	   printf("hypercall FAILED\n");
    else
 	   printf("hypercall SUCCESS\n");
+
+   printf("dumping uhctp.in[]...\n");
+   for(i=0; i < 16; i++)
+	   printf("%c", uhctp.in[i]);
+   printf("\ndumped uhctp.in[]\n");
+   printf("dumping uhctp.out[]...\n");
+   for(i=0; i < 16; i++)
+	   printf("%c", uhctp.out[i]);
+   printf("\ndumped uhctp.out[]\n");
 
    printf("End of test\n");
    return 0;
