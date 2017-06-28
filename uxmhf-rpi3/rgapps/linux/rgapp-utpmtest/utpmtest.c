c7b14ffb87e5c3f5413efdf74ac7e4899467a675
Amit Vasudevan <amitvasudevan@acm.org>
2017-06-28 18:27:25 -0500

    uxmhf-rpi3: rgapp-utpmtest: successful build with empty template

--- uxmhf-rpi3/rgapps/linux/rgapp-utpmtest/utpmtest.c
+++ uxmhf-rpi3/rgapps/linux/rgapp-utpmtest/utpmtest.c
@@ -1,5 +1,5 @@
 /*
- * hypercall test program (uhcalltest)
+ * micro-tpm test program (utpmtest)
  * author: amit vasudevan (amitvasudevan@acm.org)
  *
  */
@@ -15,48 +15,14 @@
 #include <unistd.h>
 
 #include <uhcall.h>
-#include <uhcalltest.h>
 
-__attribute__((aligned(4096))) static uhcalltest_param_t uhctp;
+//__attribute__((aligned(4096))) static uhcalltest_param_t uhctp;
 
 int main(){
-	uint8_t ch='a';
-	uint32_t i;
 
-   printf("Starting usr mode hypercall test\n");
+   printf("Starting usr mode utpm test...\n");
 
 
-   printf("populating uhctp.in[] and uhctp.out[]...\n");
-   for(i=0; i < 16; i++)
-	   uhctp.in[i] = ch + i;
-   memset(&uhctp.out, 0, 16);
-
-   printf("dumping uhctp.in[]...\n");
-   for(i=0; i < 16; i++)
-	   printf("%c", uhctp.in[i]);
-   printf("\ndumped uhctp.in[]\n");
-   printf("dumping uhctp.out[]...\n");
-   for(i=0; i < 16; i++)
-	   printf("%c", uhctp.out[i]);
-   printf("\ndumped uhctp.out[]\n");
-
-
-   printf("Proceeding to issue hypercall...\n");
-
-   if(!uhcall(UAPP_UHCALLTEST_FUNCTION_TEST, &uhctp, sizeof(uhcalltest_param_t)))
-	   printf("hypercall FAILED\n");
-   else
-	   printf("hypercall SUCCESS\n");
-
-   printf("dumping uhctp.in[]...\n");
-   for(i=0; i < 16; i++)
-	   printf("%c", uhctp.in[i]);
-   printf("\ndumped uhctp.in[]\n");
-   printf("dumping uhctp.out[]...\n");
-   for(i=0; i < 16; i++)
-	   printf("%c", uhctp.out[i]);
-   printf("\ndumped uhctp.out[]\n");
-
-   printf("End of test\n");
+   printf("End of utpm test\n");
    return 0;
 }
