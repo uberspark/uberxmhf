1a2540f3d79bf1765871110f8c0a7422e852f784
Amit Vasudevan <amitvasudevan@acm.org>
2017-06-28 22:09:04 -0500

    uxmhf-rpi3: include: add utpmtest.h header file

---
+++ uxmhf-rpi3/include/utpmtest.h
@@ -0,0 +1,18 @@
+/*
+	micro-tpm test application
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#ifndef __UTPMTEST_H__
+#define __UTPMTEST_H__
+
+
+#ifndef __ASSEMBLY__
+
+
+#endif // __ASSEMBLY__
+
+
+
+#endif //__UTPMTEST_H__
