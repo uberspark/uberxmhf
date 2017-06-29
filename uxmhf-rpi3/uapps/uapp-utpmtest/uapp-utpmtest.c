f6b6fe63d6d5ea7e3c30534d5dc9b93fc0f7ad5b
Amit Vasudevan <amitvasudevan@acm.org>
2017-06-28 22:41:11 -0500

    uxmhf-rpi3: uapp-utpmtest: refine uapp_utpmtest_handlehcall to invoke utpm_init_master_entropy; successful runtime test.

--- uxmhf-rpi3/uapps/uapp-utpmtest/uapp-utpmtest.c
+++ uxmhf-rpi3/uapps/uapp-utpmtest/uapp-utpmtest.c
@@ -19,7 +19,16 @@
 //return true if handled the hypercall, false if not
 bool uapp_utpmtest_handlehcall(u32 uhcall_function, void *uhcall_buffer, u32 uhcall_buffer_len){
 	if(uhcall_function == UAPP_UTPM_FUNCTION_INIT_MASTER_ENTROPY){
+		utpm_init_master_entropy_param_t *utpm_init_master_entropy_param =
+				(utpm_init_master_entropy_param_t *)uhcall_buffer;
+
+		utpm_init_master_entropy_param->result =
+				utpm_init_master_entropy(&utpm_init_master_entropy_param->g_aeskey,
+						&utpm_init_master_entropy_param->g_hmackey,
+						&utpm_init_master_entropy_param->g_rsakey);
+
 		return true;
+
 	}else{
 		return false;
 	}
