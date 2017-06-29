093d8e71c01bcb6558c62b065553cf870382684c
Amit Vasudevan <amitvasudevan@acm.org>
2017-06-28 22:26:59 -0500

    uxmhf-rpi3: utpmtest.h: define utpm_init_master_entropy_t typedef

--- uxmhf-rpi3/include/utpmtest.h
+++ uxmhf-rpi3/include/utpmtest.h
@@ -11,6 +11,11 @@
 
 #ifndef __ASSEMBLY__
 
+typedef struct {
+	uint8_t g_aeskey[TPM_AES_KEY_LEN_BYTES];
+	uint8_t g_hmackey[TPM_HMAC_KEY_LEN];
+	uint8_t g_rsakey[4]; //TODO: fix this to RSA key len when implemented
+}utpm_init_master_entropy_t;
 
 #endif // __ASSEMBLY__
 
