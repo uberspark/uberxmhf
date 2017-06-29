c7c2b0c877840847bb42c5e397c3812c7cb28bb1
Amit Vasudevan <amitvasudevan@acm.org>
2017-06-28 19:15:00 -0500

    uxmhf-rpi3: libutpm: successful build; successful runtime test with utpm harness

--- uxmhf-rpi3/libs/libutpm/utpm-common.c
+++ uxmhf-rpi3/libs/libutpm/utpm-common.c
@@ -3,11 +3,13 @@
  * adapted from trustvisor utpm by amit vasudevan (amitvasudevan@acm.org
  */
 
-#include <types.h>
-#include <arm8-32.h>
-#include <bcm2837.h>
-#include <miniuart.h>
-#include <debug.h>
+//#include <types.h>
+//#include <arm8-32.h>
+//#include <bcm2837.h>
+//#include <miniuart.h>
+//#include <debug.h>
+
+#include <string.h>
 #include <utpm.h>
 
 void utpm_pcr_select_i(TPM_PCR_SELECTION *tpmsel, uint32_t i) {
