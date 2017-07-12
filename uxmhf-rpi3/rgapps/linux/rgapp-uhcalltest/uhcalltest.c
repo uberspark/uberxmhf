de26dd22ebd8f582137bf8657f9e06cba28466a9
Amit Vasudevan <amitvasudevan@acm.org>
2017-07-11 22:47:07 -0500

    rgapp-uhcalltest: verify mlock and physical memory addressing with a simple harness; works!

--- uxmhf-rpi3/rgapps/linux/rgapp-uhcalltest/uhcalltest.c
+++ uxmhf-rpi3/rgapps/linux/rgapp-uhcalltest/uhcalltest.c
@@ -13,11 +13,14 @@
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/mman.h>
 
 #include <uhcall.h>
 #include <uhcalltest.h>
 
 __attribute__((aligned(4096))) __attribute__((section(".data"))) uhcalltest_param_t uhctp;
+
+#if 0
 
 int main(){
 	uint8_t ch='a';
@@ -57,4 +60,41 @@ int main(){
 
    printf("End of test\n");
    return 0;
+}
+#endif
+
+int main(){
+	uint8_t ch='a';
+	uint64_t paddr;
+	uhcalltest_param_t *myptr;
+    int fd = 0;
+
+	uhctp.in[0] = ch;
+
+    if(mlock(&uhctp, sizeof(&uhctp)) == -1){
+	    printf("%s: error: line %u\n", __FUNCTION__);
+    	exit(1);
+    }
+
+    //get buffer physical address
+    if(!uhcall_va2pa(&uhctp, &paddr) ){
+	    printf("%s: error: line %u\n", __FUNCTION__);
+    	exit(1);
+    }
+
+    printf("%s: paddr=0x%016llx\n", __FUNCTION__);
+
+    fd = open("/dev/mem", O_RDWR|O_SYNC);
+    myptr = (uhcalltest_param_t *)mmap(0, getpagesize(),
+    		PROT_READ|PROT_WRITE,
+			MAP_SHARED, fd, (uint32_t)paddr);
+
+
+    printf("%s: char=%c\n", __FUNCTION__, myptr->in[0]);
+
+
+    if(munlock(&uhctp, sizeof(&uhctp)) == -1){
+	    printf("%s: error: line %u\n", __FUNCTION__);
+    	exit(1);
+    }
 }
