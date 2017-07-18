3e68c17e6b448e84e7642c25907ea8d70fada6a5
Amit Vasudevan <amitvasudevan@acm.org>
2017-07-17 19:35:07 -0500

    pa5-encfs: add common header file

---
+++ uxmhf-rpi3/include/pa5encfs.h
@@ -0,0 +1,22 @@
+/*
+	pa5-encfs encrypted filesystem test application
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#ifndef __PA5ENCFS_H__
+#define __PA5ENCFS_H__
+
+//#define UAPP_PA5ENCFS_FUNCTION_xxx	0x30
+
+
+#ifndef __ASSEMBLY__
+
+
+
+
+#endif // __ASSEMBLY__
+
+
+
+#endif //__PA5ENCFS_H__
