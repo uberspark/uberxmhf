a5fa8712864bec388d8bac87fa029afdbf857e1d
Amit Vasudevan <amitvasudevan@acm.org>
2017-07-18 10:09:49 -0500

    uapp-pa5encfs: add base implementation file

---
+++ uxmhf-rpi3/uapps/uapp-pa5encfs/uapp-pa5encfs.c
@@ -0,0 +1,22 @@
+/*
+	pa5encfs hypapp
+	FUSE encrypted filesystem hypapp
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#include <types.h>
+#include <arm8-32.h>
+#include <bcm2837.h>
+#include <miniuart.h>
+#include <debug.h>
+
+#include <pa5encfs.h>
+
+
+//return true if handled the hypercall, false if not
+bool uapp_pa5encfs_handlehcall(u32 uhcall_function, void *uhcall_buffer, u32 uhcall_buffer_len){
+	pa5encfs_param_t *ep;
+
+	return false;
+}
