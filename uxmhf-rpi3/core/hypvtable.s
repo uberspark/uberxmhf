47d269ec12dd92fef4b4e2ca971cc33611b13696
Amit Vasudevan <amitvasudevan@acm.org>
2017-07-20 22:18:03 -0500

    core: cleanup now redundant IRQ stubs, stack and C handler

--- uxmhf-rpi3/core/hypvtable.s
+++ uxmhf-rpi3/core/hypvtable.s
@@ -113,47 +113,6 @@ hypvtable_hypsvc_handler3:
 	b hypvtable_hypsvc_handler_common
 
 
-	.balign 32
-	.global	hypvtable_irq_handler0
-hypvtable_irq_handler0:
-	ldr sp, =hypvtable_irqhandler_stack_top0
-
-	push {lr}
-
-	push {r12}
-	push {r11}
-	push {r10}
-	push {r9}
-	push {r8}
-	push {r7}
-	push {r6}
-	push {r5}
-	push {r4}
-	push {r3}
-	push {r2}
-	push {r1}
-	push {r0}
-
-	bl hyp_irqhandler
-
-	// restore all saved registers
-	pop {r0}
-	pop {r1}
-	pop {r2}
-	pop {r3}
-	pop {r4}
-	pop {r5}
-	pop {r6}
-	pop {r7}
-	pop {r8}
-	pop {r9}
-	pop {r10}
-	pop {r11}
-	pop {r12}
-
-	pop	{lr}
-
-	eret
 
 	.balign 32
 	.global	hypvtable_fiq_handler0
@@ -317,11 +276,6 @@ hypvtable_hypsvc_handler_common:
 	.global hypvtable_rsvhandler_stack_top3
 	hypvtable_rsvhandler_stack_top3:
 
-	.balign 8
-	.global hypvtable_irqhandler_stack0
-	hypvtable_irqhandler_stack0:	.space	8192
-	.global hypvtable_irqhandler_stack_top0
-	hypvtable_irqhandler_stack_top0:
 
 	.balign 8
 	.global hypvtable_fiqhandler_stack0
--- uxmhf-rpi3/core/main.c
+++ uxmhf-rpi3/core/main.c
@@ -39,10 +39,6 @@ void hyp_rsvhandler(void){
 }
 
 
-void hyp_irqhandler(void){
-	bcm2837_miniuart_puts("IRQ EXCEPTION-- Halting!\n");
-	HALT();sysreg_write_cnthp_tval(10*1024*1024);
-}
 
 void hyp_fiqhandler(void){
 	bcm2837_miniuart_puts("FIQ EXCEPTION-- Resuming\n");
