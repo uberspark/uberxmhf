4cc4788fb7fc2d6c7b3f76ad16da0053ada0c082
Amit Vasudevan <amitvasudevan@acm.org>
2017-07-20 22:34:59 -0500

    uapp-watchdog: add main module template

---
+++ uxmhf-rpi3/uapps/uapp-watchdog/uapp-watchdog.c
@@ -0,0 +1,12 @@
+/*
+	watchdog hypapp
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#include <types.h>
+#include <arm8-32.h>
+#include <bcm2837.h>
+#include <miniuart.h>
+#include <debug.h>
+
