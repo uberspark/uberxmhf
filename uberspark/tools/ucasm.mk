0858fcd8df334231a2f0d3d7c823445eddb7b041
Amit Vasudevan <amitvasudevan@acm.org>
2017-09-06 22:19:26 -0500

    uberspark: tools: cleanup defunct files

--- uberspark/tools/ucasm.mk
+++
@@ -1,5 +0,0 @@
-FRAMAC_SHARE :=$(shell frama-c-config -print-share-path)
-FRAMAC_LIBDIR :=$(shell frama-c-config -print-libpath)
-PLUGIN_NAME = Ucasm
-PLUGIN_CMO = umfcommon ucasm
-include $(FRAMAC_SHARE)/Makefile.dynamic
