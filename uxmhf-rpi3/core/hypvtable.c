f8cf625bccabbfdeef2fd2eb53c7a49f80dc8f44
Amit Vasudevan <amitvasudevan@acm.org>
2018-01-10 12:04:03 -0600

    uxmhf-rpi3: core: test hypvtable_setentry; success!

--- uxmhf-rpi3/core/hypvtable.c
+++ uxmhf-rpi3/core/hypvtable.c
@@ -25,15 +25,16 @@ extern void hypvtable_reserved_handler0(void);
 // return 0 on success; non-zero on fail
 //////
 u32 hypvtable_setentry(u32 cpuid, u32 entry_idx, u32 entry_addr){
-	u32 vector_table_base;
+	u32 vector_table_entry_base;
 	u32 entry_value;
 
 	if(entry_idx > 7)
 		return 1;
 
-	vector_table_base =(u32)&g_hypvtable[cpuid];
+	vector_table_entry_base =(u32)&g_hypvtable[cpuid];
+	vector_table_entry_base += sizeof(u32) * entry_idx;
 
-	if(entry_addr < vector_table_base)
+	if(entry_addr < vector_table_entry_base)
 		return 2;
 
 	//formula to setup vector table value based on given 32-bit address
@@ -41,7 +42,7 @@ u32 hypvtable_setentry(u32 cpuid, u32 entry_idx, u32 entry_addr){
 	//2. divide 1 by 4
 	//3. subtract 1
 	//4. add to unsigned 0xEA000000
-	entry_value = 0xEA000000 + (u32)(((entry_addr - vector_table_base + 0x4) / 4) - 1);
+	entry_value = 0xEA000000 + (u32)(((entry_addr - vector_table_entry_base + 0x4) / 4) - 1);
 
 	g_hypvtable[cpuid][entry_idx]=entry_value;
 }
@@ -57,6 +58,14 @@ void hypvtable_initialize(u32 cpuid){
 	//debug
 	_XDPRINTFSMP_("%s[%u]: hypvtable_fiq_handler0 at 0x%08x\n", __func__, cpuid, (u32)&hypvtable_fiq_handler0);
 	_XDPRINTFSMP_("%s[%u]: hypvtable_reserved_handler0 at 0x%08x\n", __func__, cpuid, (u32)&hypvtable_reserved_handler0);
+	_XDPRINTFSMP_("%s[%u]: dumping (ghypvtable at 0x%08x) contents...\n", __func__, cpuid, (u32)&g_hypvtable[cpuid]);
+	for(i=0; i < 8; i++){
+		_XDPRINTFSMP_("%s[%u]:   0x%08x\n", __func__, cpuid, g_hypvtable[cpuid][i]);
+	}
+	_XDPRINTFSMP_("%s[%u]: dumped ghypvtable\n", __func__, cpuid);
+
+	//test
+	hypvtable_setentry(cpuid, 7, (u32)&hypvtable_fiq_handler0);
 	_XDPRINTFSMP_("%s[%u]: dumping (ghypvtable at 0x%08x) contents...\n", __func__, cpuid, (u32)&g_hypvtable[cpuid]);
 	for(i=0; i < 8; i++){
 		_XDPRINTFSMP_("%s[%u]:   0x%08x\n", __func__, cpuid, g_hypvtable[cpuid][i]);
