a9eb08404fbf156592a50b397ec2dcef99c8cc78
Amit Vasudevan <amitvasudevan@acm.org>
2018-01-10 09:31:52 -0600

    uxmhf-rpi3: core: move vector table initialization into hypvtable_initialize for both primary and secondary CPUs; successful runtime test.

--- uxmhf-rpi3/core/hypvtable.c
+++ uxmhf-rpi3/core/hypvtable.c
@@ -11,4 +11,23 @@
 #include <debug.h>
 
 
+//////
+// externs
+//////
+extern u32 g_hypvtable[BCM2837_MAXCPUS][8];
 
+
+//////
+// hypvtable_initialize
+// initialize vector table for given CPU
+//////
+void hypvtable_initialize(u32 cpuid){
+
+	//setup HVBAR for vectors
+	_XDPRINTFSMP_("%s[%u]: HVBAR[before]=0x%08x\n", __func__, cpuid, sysreg_read_hvbar());
+
+	_XDPRINTFSMP_("%s[%u]: ghypvtable at 0x%08x\n", __func__, cpuid, (u32)&g_hypvtable[cpuid]);
+	sysreg_write_hvbar((u32)&g_hypvtable[cpuid]);
+
+	_XDPRINTFSMP_("%s[%u]: HVBAR[after]=0x%08x\n", __func__, cpuid, sysreg_read_hvbar());
+}
--- uxmhf-rpi3/core/main.c
+++ uxmhf-rpi3/core/main.c
@@ -19,7 +19,6 @@
 extern u8 cpu_stacks[];
 extern void chainload_os(u32 r0, u32 id, struct atag *at, u32 address);
 extern void cpumodeswitch_hyp2svc(u32 r0, u32 id, struct atag *at, u32 address, u32 cpuid);
-extern u32 g_hypvtable[BCM2837_MAXCPUS][8];
 
 
 
@@ -523,12 +522,9 @@ void main(u32 r0, u32 id, struct atag *at, u32 cpuid){
 	hyppgtbl_activate();
 	_XDPRINTF_("%s[%u]: hyp page-tables activated\n", __func__, cpuid);
 
-	//setup HVBAR for vectors
-	_XDPRINTFSMP_("%s[%u]: HVBAR[before]=0x%08x\n", __func__, cpuid, sysreg_read_hvbar());
 
-	_XDPRINTFSMP_("%s[%u]: ghypvtable at 0x%08x\n", __func__, cpuid, (u32)&g_hypvtable[cpuid]);
-	sysreg_write_hvbar((u32)&g_hypvtable[cpuid]);
-	_XDPRINTFSMP_("%s[%u]: HVBAR[after]=0x%08x\n", __func__, cpuid, sysreg_read_hvbar());
+	//initialize CPU vector table
+	hypvtable_initialize(cpuid);
 
 
 #if 0
@@ -742,10 +738,9 @@ void secondary_main(u32 cpuid){
 	_XDPRINTF_("%s[%u]: HSTR=0x%08x\n", __func__, cpuid, sysreg_read_hstr());
 	_XDPRINTF_("%s[%u]: HCPTR=0x%08x\n", __func__, cpuid, sysreg_read_hcptr());
 	_XDPRINTF_("%s[%u]: HDCR=0x%08x\n", __func__, cpuid, sysreg_read_hdcr());
-	_XDPRINTF_("%s[%u]: HVBAR[before]=0x%08x\n", __func__, cpuid, sysreg_read_hvbar());
-	_XDPRINTF_("%s[%u]: ghypvtable at 0x%08x\n", __func__, cpuid, (u32)&g_hypvtable[cpuid]);
-	sysreg_write_hvbar((u32)&g_hypvtable[cpuid]);
-	_XDPRINTF_("%s[%u]: HVBAR[after]=0x%08x\n", __func__, cpuid, sysreg_read_hvbar());
+
+	//setup vector table for CPU
+	hypvtable_initialize(cpuid);
 
 	// initialize cpu support for second stage page table translations
 	s2pgtbl_initialize();
