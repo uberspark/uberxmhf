aa773fcf02049c7e113b1460e08305ef204a88d0
Amit Vasudevan <amitvasudevan@acm.org>
2018-03-23 11:37:54 -0500

    uapp-hypmtscheduler: add top-level header

---
+++ uxmhf-rpi3/include/hypmtscheduler.h
@@ -0,0 +1,18 @@
+/*
+	hypervisor mixed-trust scheduler
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#ifndef __HYPMTSCHEDULER_H__
+#define __HYPMTSCHEDULER_H__
+
+
+#ifndef __ASSEMBLY__
+
+
+#endif // __ASSEMBLY__
+
+
+
+#endif //__HYPMTSCHEDULER_H__
