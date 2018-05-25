1855f28b0a5632c0e94a3d60e4e301479b9be57f
Amit Vasudevan <amitvasudevan@acm.org>
2018-05-25 00:43:05 -0500

    hypmtscheduler: zsrmv: flesh out hypmtscheduler_createhyptask

--- uxmhf-rpi3/rgapps/linux/ugapp-hypmtscheduler-zsrmv/hypmtscheduler_kmodlib.c
+++ uxmhf-rpi3/rgapps/linux/ugapp-hypmtscheduler-zsrmv/hypmtscheduler_kmodlib.c
@@ -37,7 +37,10 @@
 #include <linux/device.h>         // header to support the kernel Driver Model
 #include <linux/kernel.h>         // contains types, macros, functions for the kernel
 #include <linux/fs.h>             // header for the Linux file system support
+#include <linux/mm.h>             // header for the Linux memory management support
+#include <linux/highmem.h>             // header for the Linux memory management support
 #include <asm/uaccess.h>          // required for the copy to user function
+#include <asm/io.h>          // required for the copy to user function
 
 #include <hypmtscheduler.h>
 
@@ -56,7 +59,37 @@ void __hvc(u32 uhcall_function, void *uhcall_buffer,
 	    );
 }
 
-void hypmtscheduler_createhyptask(u32 first_period, u32 regular_period,
-			u32 priority, u32 hyptask_id){
+bool hypmtscheduler_createhyptask(u32 first_period, u32 regular_period,
+			u32 priority, u32 hyptask_id, u32 *hyptask_handle){
 
+	ugapp_hypmtscheduler_param_t *hmtsp;
+	struct page *hmtsp_page;
+	u32 hmtsp_paddr;
+
+	hmtsp_page = alloc_page(GFP_KERNEL | __GFP_ZERO);
+
+	if(!hmtsp_page){
+		__free_page(hmtsp_page);
+		return false;
+	}
+
+	hmtsp = (ugapp_hypmtscheduler_param_t *)page_address(hmtsp_page);
+
+    hmtsp->iparam_1 = first_period;	//first period
+    hmtsp->iparam_2 = regular_period;	//regular period thereafter
+    hmtsp->iparam_3 = priority;						//priority
+    hmtsp->iparam_4 = hyptask_id;						//hyptask id
+
+	hmtsp_paddr = page_to_phys(hmtsp_page);
+	__hvc(UAPP_HYPMTSCHEDULER_UHCALL, hmtsp_paddr, sizeof(ugapp_hypmtscheduler_param_t));
+
+	if(!hmtsp->status){
+		__free_page(hmtsp_page);
+		return false;
+	}
+
+	*hyptask_handle = hmtsp->oparam_1;
+
+	__free_page(hmtsp_page);
+	return true;
 }
