a82fb548bd5e0ef25172823de9dbcc438d722aed
Amit Vasudevan <amitvasudevan@acm.org>
2018-08-27 11:37:53 -0500

    uxmhf-rpi3: hypmtscheduler: add debug_log_tsc logging interface to log time-stamps to memory buffer

--- uxmhf-rpi3/uapps/uapp-hypmtscheduler/logging.c
+++ uxmhf-rpi3/uapps/uapp-hypmtscheduler/logging.c
@@ -6,7 +6,6 @@
 
 #include <types.h>
 
-#define PRIORITY_QUEUE_SIZE 5
 
 typedef struct {
 	u32 hyptask_id;
@@ -14,3 +13,20 @@ typedef struct {
 	u32 event_type;
 } hypmtscheduler_logentry_t;
 
+#define DEBUG_LOG_SIZE (4096/sizeof(hypmtscheduler_logentry_t))
+
+
+__attribute__((section(".data"))) hypmtscheduler_logentry_t debug_log_buffer[DEBUG_LOG_SIZE];
+__attribute__((section(".data"))) u32 debug_log_buffer_index = 0;
+
+void debug_log_tsc(u32 hyptask_id, u64 timestamp, u32 event_type){
+
+	if(debug_log_buffer_index >= DEBUG_LOG_SIZE){
+		bcm2837_miniuart_puts("\n[debug_log_tsc]: WARNING: buffer full, not logging anymore!");
+	}else{
+		debug_log_buffer[debug_log_buffer_index].hyptask_id = hyptask_id;
+		debug_log_buffer[debug_log_buffer_index].timestamp = timestamp;
+		debug_log_buffer[debug_log_buffer_index].event_type = event_type;
+		debug_log_buffer_index++;
+	}
+}
