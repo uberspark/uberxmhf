4f20ed903c66073688a9107e54a5214248449ee8
Amit Vasudevan <amitvasudevan@acm.org>
2018-08-27 11:44:41 -0500

    uxmhf-rpi3: hypmtscheduler: introduce DEBUG_LOGGING build time configuration to dump serial output within debug_log_tsc

--- uxmhf-rpi3/uapps/uapp-hypmtscheduler/logging.c
+++ uxmhf-rpi3/uapps/uapp-hypmtscheduler/logging.c
@@ -6,6 +6,8 @@
 
 #include <types.h>
 
+#define DEBUG_LOGGING_SERIAL	1
+
 
 typedef struct {
 	u32 hyptask_id;
@@ -27,6 +29,17 @@ void debug_log_tsc(u32 hyptask_id, u64 timestamp, u32 event_type){
 		debug_log_buffer[debug_log_buffer_index].hyptask_id = hyptask_id;
 		debug_log_buffer[debug_log_buffer_index].timestamp = timestamp;
 		debug_log_buffer[debug_log_buffer_index].event_type = event_type;
+
+#ifdef DEBUG_LOGGING_SERIAL
+		bcm2837_miniuart_puts("\n[debug_log_tsc]: hid=0x");
+		debug_hexdumpu32(hyptask_id);
+		bcm2837_miniuart_puts(", timestamp=0x");
+		debug_hexdumpu32(timestamp >> 32);
+		debug_hexdumpu32((u32)timestamp);
+		bcm2837_miniuart_puts(", event=0x");
+		debug_hexdumpu32(event_type);
+#endif
+
 		debug_log_buffer_index++;
 	}
 }
