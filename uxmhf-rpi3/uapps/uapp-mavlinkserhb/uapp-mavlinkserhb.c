26351383ded64147433913812625ed100813b679
Amit Vasudevan <amitvasudevan@acm.org>
2018-09-10 14:33:23 -0500

    uxmhf-rpi3: mavlinkserhb: add  uapp_mavlinkserhb_uart_init to initialize serial UART hw

--- uxmhf-rpi3/uapps/uapp-mavlinkserhb/uapp-mavlinkserhb.c
+++ uxmhf-rpi3/uapps/uapp-mavlinkserhb/uapp-mavlinkserhb.c
@@ -19,6 +19,45 @@
 
 
 //////////////////////////////////////////////////////////////////////////////
+// mavlinkserhb serial port hw interface
+//////////////////////////////////////////////////////////////////////////////
+void uapp_mavlinkserhb_uart_init(void){
+    unsigned int gpio_fnsel, i;
+
+    mmio_write32(AUX_ENABLES,1);
+    mmio_write32(AUX_MU_IER_REG,0);		//clear interrupt enable
+    mmio_write32(AUX_MU_CNTL_REG,0);
+    mmio_write32(AUX_MU_LCR_REG,3);
+    mmio_write32(AUX_MU_MCR_REG,0);
+    mmio_write32(AUX_MU_IER_REG,0);
+    mmio_write32(AUX_MU_IIR_REG,0xC6);
+    mmio_write32(AUX_MU_BAUD_REG,270); //((250,000,000/115200)/8)-1 = 270
+
+    gpio_fnsel=mmio_read32(GPFSEL1);
+    gpio_fnsel &= ~(7<<12); 			//GPIO 14 (TX)
+    gpio_fnsel |= 2<<12;    			//GPIO 14 -- Alternate Function 5
+    gpio_fnsel &= ~(7<<15); 			//GPIO 15 (RX)
+    gpio_fnsel |= 2<<15;    			//GPIO 15 -- Alternate Function 5
+    mmio_write32(GPFSEL1,gpio_fnsel);
+
+    mmio_write32(GPPUD,0);
+    for(i=0; i<150; i++);				//delay
+    mmio_write32(GPPUDCLK0,(1<<14)|(1<<15));
+    for(i=0; i<150; i++);				//delay
+    mmio_write32(GPPUDCLK0,0);
+    mmio_write32(AUX_MU_CNTL_REG,3);
+}
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////
 // mavlinkserhb async timer functions
 //////////////////////////////////////////////////////////////////////////////
 
