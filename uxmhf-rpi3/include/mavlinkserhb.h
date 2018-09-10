791999a6404d275742893892bf219971044e62ab
Amit Vasudevan <amitvasudevan@acm.org>
2018-09-10 11:44:35 -0500

    uxmhf-rpi3: mavlinkserhb: add base header file template

---
+++ uxmhf-rpi3/include/mavlinkserhb.h
@@ -0,0 +1,19 @@
+/*
+	MAVLINK serial heart-beat uberapp
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#ifndef __MAVLINKSERHB_H__
+#define __MAVLINKSERHB_H__
+
+
+#ifndef __ASSEMBLY__
+
+
+
+#endif // __ASSEMBLY__
+
+
+
+#endif //__MAVLINKSERHB_H__
