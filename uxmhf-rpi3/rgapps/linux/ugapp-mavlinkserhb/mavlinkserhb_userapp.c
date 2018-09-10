102004c9eb423fe3ffa421385edb9516cd56c630
Amit Vasudevan <amitvasudevan@acm.org>
2018-09-10 13:25:58 -0500

    uxmhf-rpi3: mavlinkserhb: refine user-space applications to interface with initialize hypercall

--- uxmhf-rpi3/rgapps/linux/ugapp-mavlinkserhb/mavlinkserhb_userapp.c
+++ uxmhf-rpi3/rgapps/linux/ugapp-mavlinkserhb/mavlinkserhb_userapp.c
@@ -1,5 +1,5 @@
 /*
- * hypmtscheduler test program
+ * mavlinkserhb user-mode application
  * author: amit vasudevan (amitvasudevan@acm.org)
  *
  */
@@ -15,18 +15,19 @@
 #include <unistd.h>
 #include <sys/mman.h>
 
-//#include <hypmtscheduler.h>
-extern unsigned int sysreg_read_cntfrq();
-extern unsigned long long int sysreg_read_cntvct();
+#include "../../../include/mavlinkserhb.h"
 
+//////
+// function to communicate with kernel-module
+//////
 void kmod_comms(unsigned int function){
 	int ret, fd;
 
-	//open uhcallkmod device
-	fd = open("/dev/hypmtschedulerkmod", O_RDWR);
+	//open device
+	fd = open("/dev/mavlinkserhbkmod", O_RDWR);
 	if (fd < 0){
 	    printf("%s: error: line %u\n", __FUNCTION__, __LINE__);
-		exit(1); //Failed to open /dev/uhcallkmod
+		exit(1); //Failed to open
 	}
 
 	ret = write(fd, NULL, function);
@@ -42,73 +43,16 @@ void kmod_comms(unsigned int function){
 }
 
 
-void test_createhyptask(void){
-	kmod_comms(1);
-}
-
-void test_disablehyptask(void){
-	kmod_comms(2);
-}
-
-void test_deletehyptask(void){
-	kmod_comms(3);
-}
-
-void test_dumpdebuglog(void){
-	kmod_comms(4);
-}
-
-void test_cyclecounter(void){
-	unsigned long long te, ts;
-	unsigned long freq;
-
-	//asm volatile ("isb; mrrc %0, cntvct_el0" : "=r" (ts));
-	ts = sysreg_read_cntvct();
-	sleep (2);
-	te = sysreg_read_cntvct();
-	printf("Cycles Elapsed: %llu\n", (te-ts));
-	printf("Frequency = %u\n",sysreg_read_cntfrq());
-}
-
+//////
+// main function
+//////
 int main(int argc, char *argv[]){
     unsigned int testcase_num;
 	printf("%s: start\n", __FUNCTION__);
 
-    if(argc != 2){
-  	   printf("usage: ugapp-hypmtschedulertest <testcasenum>\n");
-  	   exit(1);
-    }
+	kmod_comms(UAPP_MAVLINKSERHB_UHCALL_INITIALIZE);
 
-    testcase_num = atoi(argv[1]);
-
-	printf("%s: testcase_num=%u\n", __FUNCTION__, testcase_num);
-
-	switch(testcase_num){
-	case 1:
-		test_createhyptask();
-		break;
-
-	case 2:
-		test_disablehyptask();
-		break;
-
-	case 3:
-		test_deletehyptask();
-		break;
-
-	case 4:
-		//test_cyclecounter();
-		test_dumpdebuglog();
-		break;
-
-
-	default:
-		printf("%s: unknown testcase_num=%u, exiting!\n", __FUNCTION__, testcase_num);
-		exit(1);
-	}
-
-
-    printf("%s: end-of-test\n", __FUNCTION__);
+    printf("%s: end\n", __FUNCTION__);
     return 0;
 }
 
