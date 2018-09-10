ecb4c6b8ff24e8446f5a83275a781e150b021e9d
Amit Vasudevan <amitvasudevan@acm.org>
2018-09-10 11:42:56 -0500

    uxmhf-rpi3: mavlinkserhb: add main module base template

---
+++ uxmhf-rpi3/uapps/uapp-mavlinkserhb/uapp-mavlinkserhb.c
@@ -0,0 +1,18 @@
+/*
+	MAVLINK serial heart-beat uberapp
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+//////////////////////////////////////////////////////////////////////////////
+// headers
+//////////////////////////////////////////////////////////////////////////////
+
+#include <types.h>
+#include <arm8-32.h>
+#include <bcm2837.h>
+#include <miniuart.h>
+#include <debug.h>
+
+#include <mavlinkserhb.h>
+
