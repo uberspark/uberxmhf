4de3b9ea8b60a70540e233104fbdae13f0be8698
Amit Vasudevan <amitvasudevan@acm.org>
2018-09-10 12:14:14 -0500

    uxmhf-rpi3: mavlinkserhb: add initialization function

--- uxmhf-rpi3/uapps/uapp-mavlinkserhb/uapp-mavlinkserhb.c
+++ uxmhf-rpi3/uapps/uapp-mavlinkserhb/uapp-mavlinkserhb.c
@@ -15,3 +15,20 @@
 #include <debug.h>
 
 #include <mavlinkserhb.h>
+
+//////
+// the main initialization function
+//////
+void uapp_mavlinkserhb_initialize(u32 cpuid){
+
+	if(cpuid == 0){
+		_XDPRINTFSMP_("%s[%u]: Initializing mavlinkserhb...\n", __func__, cpuid);
+
+	}else{
+		_XDPRINTFSMP_("%s[%u]: AP CPU: nothing to do, moving on...\n", __func__, cpuid);
+	}
+
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
