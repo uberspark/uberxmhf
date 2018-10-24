72bdf251132e1f191d17e021764c38a6b8243115
Amit Vasudevan <amitvasudevan@acm.org>
2018-10-24 14:11:46 -0500

    uxmhf: exhub: refine main function to handle just synchronous api calls

--- uxmhf/xmhf-uobjs/xc_exhub/xcexhub_main.c
+++ uxmhf/xmhf-uobjs/xc_exhub/xcexhub_main.c
@@ -60,39 +60,6 @@
 bool ihub_exit_status = false;
 u32 ihub_exit_info = 0;
 
-static void _xcexhub_unhandled(x86vmx_exception_frame_t *exframe){
-    //dump relevant info
-    _XDPRINTF_("exception %x\n", exframe->vector);
-    _XDPRINTF_("state dump:\n\n");
-    _XDPRINTF_("errorcode=0x%08x\n", exframe->error_code);
-    _XDPRINTF_("CS:EIP:EFLAGS = 0x%08x:0x%08x:0x%08x\n", exframe->orig_cs, exframe->orig_rip, exframe->orig_rflags);
-    _XDPRINTF_("SS:ESP = 0x%08x:0x%08x\n", exframe->orig_ss, exframe->orig_rsp);
-    _XDPRINTF_("CR0=0x%08x, CR2=0x%08x\n", CASM_FUNCCALL(read_cr0,CASM_NOPARAM), CASM_FUNCCALL(read_cr2,CASM_NOPARAM));
-    _XDPRINTF_("CR3=0x%08x, CR4=0x%08x\n", CASM_FUNCCALL(read_cr3,CASM_NOPARAM), CASM_FUNCCALL(read_cr4,CASM_NOPARAM));
-    _XDPRINTF_("CS=0x%04x, DS=0x%04x, ES=0x%04x, SS=0x%04x\n",
-               (u16)read_segreg_cs(CASM_NOPARAM), (u16)read_segreg_ds(CASM_NOPARAM),
-               (u16)read_segreg_es(CASM_NOPARAM), (u16)read_segreg_ss(CASM_NOPARAM));
-    _XDPRINTF_("FS=0x%04x, GS=0x%04x\n", (u16)read_segreg_fs(CASM_NOPARAM), (u16)read_segreg_gs(CASM_NOPARAM));
-    _XDPRINTF_("TR=0x%04x\n", (u16)read_tr_sel(CASM_NOPARAM));
-    _XDPRINTF_("EAX=0x%08x, EBX=0x%08x\n", exframe->eax, exframe->ebx);
-    _XDPRINTF_("ECX=0x%08x, EDX=0x%08x\n", exframe->ecx, exframe->edx);
-    _XDPRINTF_("ESI=0x%08x, EDI=0x%08x\n", exframe->esi, exframe->edi);
-    _XDPRINTF_("EBP=0x%08x, ESP=0x%08x\n", exframe->ebp, exframe->esp);
-
-    ////do a stack dump in the hopes of getting more info.
-    //{
-    //    u64 i;
-    //    u64 stack_start = exframe->orig_rsp;
-    //    _XDPRINTF_("\n-----stack dump (8 entries)-----\n");
-    //    for(i=stack_start; i < stack_start+(8*sizeof(u64)); i+=sizeof(u64)){
-    //        _XDPRINTF_("Stack(0x%016llx) -> 0x%016llx\n", i, *(u64 *)i);
-    //    }
-    //    _XDPRINTF_("\n-----stack dump end-------------\n");
-    //}
-
-}
-
-
 
 
 #if defined (__XMHF_VERIFICATION__) && defined (__USPARK_FRAMAC_VA__)
@@ -135,25 +102,9 @@ void slab_main(slab_params_t *sp){
 		ihub_exit_status = sp->in_out_params[0];
 		ihub_exit_info = sp->in_out_params[1];
 
+
 	}else{
-
-		x86vmx_exception_frame_t *exframe = (x86vmx_exception_frame_t *)&sp->in_out_params[0];
-
-		_XDPRINTF_("XC_EXHUB[%u]: Got control: ESP=%08x, src_slabid=%u, dst_slabid=%u\n",
-	                (u16)sp->cpuid, CASM_FUNCCALL(read_esp,CASM_NOPARAM), sp->src_slabid, sp->dst_slabid);
-		_XDPRINTF_("XC_EXHUB[%u]: ihub_exit_status=%u, ihub_exit_info=%u\n",
-					          (u16)sp->cpuid, ihub_exit_status, ihub_exit_info);
-
-
-		if(exframe->vector == 0x3){
-			_xcexhub_unhandled(exframe);
-			_XDPRINTF_("%s: exception 3, returning\n", __func__);
-		}else{
-			_xcexhub_unhandled(exframe);
-			_XDPRINTF_("\nHalting System!\n");
-			CASM_FUNCCALL(xmhfhw_cpu_hlt, CASM_NOPARAM);
-		}
-
+		//unknown api ignore and return
 	    return;
 	}
 
