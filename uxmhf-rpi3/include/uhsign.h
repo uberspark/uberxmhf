f7729db2b74750e521611332bdb8e6a8ac404427
slab14 <30778419+slab14@users.noreply.github.com>
2019-10-09 16:16:19 -0400

    correcting struct definition

--- uxmhf-rpi3/include/uhsign.h
+++ uxmhf-rpi3/include/uhsign.h
@@ -2,17 +2,27 @@
 #define __UHSIGN_H__
 
 #define UAPP_UHSIGN_FUNCTION_SIGN    0x69
+#define DIGEST_SIZE 20
 
 #ifndef __Assembly__
 
 typedef struct {
+  uint8_t *key;
+  uint32_t key_size;
+  uint32_t digest_size;
+} uhsign_const_params;
+
+struct uhsign_const_params uhsign_consts = { "super_secret_key_for_hmac", 25, 20 };
+  
+typedef struct {
   uint8_t *pkt;
   uint32_t pkt_size;
-  uint8_t *key="super_secret_key_for_hmac";
-  uint32_t key_size=25;
+  uint8_t *key;
+  uint32_t key_size;
   uint8_t *digest;
-  uint32_t digest_size=20;
+  uint32_t digest_size;  
 }uhsign_param_t;
+
 
 #endif // __ASSEMBLY__
 
--- uxmhf-rpi3/rgapps/linux/rgapp-uhsign/uhsign.c
+++ uxmhf-rpi3/rgapps/linux/rgapp-uhsign/uhsign.c
@@ -17,9 +17,15 @@ __attribute__((aligned(4096))) __attribute__((section(".data"))) uhcalltest_para
 
 void do_uhsign(uint8_t *pkt, uint32_t pkt_size) {
   uint32_t i;
+  uint8_t key="super_secret_hmac_key";
+  uint32_t key_size = 21;
+  uint32_t digest_size=20;
   uhsign_param_t *ptr_uhcp;
   memcpy(ptr_uhcp->pkt, pkt, pkt_size);
+  memcpy(ptr_uhcp->key, key, key_size);  
   ptr_uhcp->pkt_size=pkt_size;
+  ptr_uhcp->key_size=key_size;
+  ptr_uhcp->digest_size=digest_size;
 
   if(!uhcall(UAPP_UHSIGN_FUNCTION_SIGN, ptr_uhcp, sizeof(uhsign_param_t)))
     printf("hypercall FAILED\n");
