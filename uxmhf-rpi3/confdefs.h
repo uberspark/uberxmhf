47f2082f0802130619b9d9780bf34e39a754a55a
matty <you@example.com>
2019-12-13 15:53:36 -0500

    more cleaning of unnecessary files

--- uxmhf-rpi3/confdefs.h
+++
@@ -1,7 +0,0 @@
-/* confdefs.h */
-#define PACKAGE_NAME "uxmhf-rpi3"
-#define PACKAGE_TARNAME "uxmhf-rpi3"
-#define PACKAGE_VERSION "5.0"
-#define PACKAGE_STRING "uxmhf-rpi3 5.0"
-#define PACKAGE_BUGREPORT ""
-#define PACKAGE_URL ""
