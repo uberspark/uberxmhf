bee29763daa8d6e3d7207cf99d39b2dedcbb34bc
matty <you@example.com>
2019-12-13 15:29:57 -0500

    cleaning up PR, adding hooks to enable selectively enabling HMAC signing feature in hypervisor

--- uxmhf-rpi3/core/ghcall.c
+++ uxmhf-rpi3/core/ghcall.c
@@ -83,10 +83,10 @@ void guest_hypercall_handler(arm8_32_regs_t *r, u32 hsr){
 
 		if( uapp_pa5encfs_handlehcall(r->r0, r->r1, r->r2) )
 			return;
-
+#if defined (__ENABLE_UAPP_UHSIGN__)
 		if( uapp_uhsign_handlehcall(r->r0, r->r1, r->r2) )
-			return;		
-
+			return;
+#endif
 		_XDPRINTFSMP_("%s: hcall unhandled. Halting!\n", __func__);
 		HALT();
 
