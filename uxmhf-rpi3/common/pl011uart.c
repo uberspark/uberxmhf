a4c71a1c37fa03c883c50dd132cc047f1cb06af4
Amit Vasudevan <amitvasudevan@acm.org>
2020-02-21 15:50:03 -0600

    uxmhf-rpi3: add pl011 uart header and implementation module with high-level interface declarations

---
+++ uxmhf-rpi3/common/pl011uart.c
@@ -0,0 +1,73 @@
+/*
+ * @UBERXMHF_LICENSE_HEADER_START@
+ *
+ * uber eXtensible Micro-Hypervisor Framework (Raspberry Pi)
+ *
+ * Copyright 2018 Carnegie Mellon University. All Rights Reserved.
+ *
+ * NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING
+ * INSTITUTE MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON
+ * UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
+ * AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR
+ * PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF
+ * THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF
+ * ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT
+ * INFRINGEMENT.
+ *
+ * Released under a BSD (SEI)-style license, please see LICENSE or
+ * contact permission@sei.cmu.edu for full terms.
+ *
+ * [DISTRIBUTION STATEMENT A] This material has been approved for public
+ * release and unlimited distribution.  Please see Copyright notice for
+ * non-US Government use and distribution.
+ *
+ * Carnegie Mellon is registered in the U.S. Patent and Trademark Office by
+ * Carnegie Mellon University.
+ *
+ * @UBERXMHF_LICENSE_HEADER_END@
+ */
+
+/*
+ * Author: Amit Vasudevan (amitvasudevan@acm.org)
+ *
+ */
+
+/*
+	broadcom 2837 mini uart support
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#include <types.h>
+#include <bcm2837.h>
+#include <pl011uart.h>
+
+
+extern u32 mmio_read32 (u32 address);
+extern void mmio_write32 (u32 address, u32 value);
+
+
+/* UART initialization function */
+void bcm2837_pl011uart_init(void){
+
+}
+
+
+/* UART character output function */
+void bcm2837_pl011uart_putc(u8 ch){
+
+}
+
+/* UART string output function */
+void bcm2837_pl011uart_puts(char *buffer){
+	while (*buffer)
+		bcm2837_pl011uart_putc(*buffer++);
+}
+
+
+/* UART flush */
+void bcm2837_pl011uart_flush(void){
+
+}
+
+
