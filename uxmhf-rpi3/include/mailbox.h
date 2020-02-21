0a80dd1742772eead0e8d0238d461f84cbc2a4e1
Amit Vasudevan <amitvasudevan@acm.org>
2020-02-21 13:16:49 -0600

    uxmhf-rpi3: add mailbox implementation header file template

---
+++ uxmhf-rpi3/include/mailbox.h
@@ -0,0 +1,55 @@
+/*
+ * @UBERXMHF_LICENSE_HEADER_START@
+ *
+ * uber eXtensible Micro-Hypervisor Framework (Raspberry Pi)
+ *
+ * Copyright 2018 Carnegie Mellon University. All Rights Reserved.
+ *
+ * NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING
+ * INSTITUTE MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON
+ * UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
+ * AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR
+ * PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF
+ * THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF
+ * ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT
+ * INFRINGEMENT.
+ *
+ * Released under a BSD (SEI)-style license, please see LICENSE or
+ * contact permission@sei.cmu.edu for full terms.
+ *
+ * [DISTRIBUTION STATEMENT A] This material has been approved for public
+ * release and unlimited distribution.  Please see Copyright notice for
+ * non-US Government use and distribution.
+ *
+ * Carnegie Mellon is registered in the U.S. Patent and Trademark Office by
+ * Carnegie Mellon University.
+ *
+ * @UBERXMHF_LICENSE_HEADER_END@
+ */
+
+/*
+ * Author: Amit Vasudevan (amitvasudevan@acm.org)
+ *
+ */
+
+/*
+	broadcom 2837 mailbox support
+
+	author: amit vasudevan (amitvasudevan@acm.org)
+*/
+
+#ifndef __MAILBOX_H__
+#define __MAILBOX_H__
+
+
+
+#ifndef __ASSEMBLY__
+
+//void bcm2837_xxx(void);
+
+
+#endif // __ASSEMBLY__
+
+
+
+#endif //__MAILBOX_H__
