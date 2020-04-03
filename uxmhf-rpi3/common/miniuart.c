b897f9da34a5fdc37b00cd2a19ec97b94566a1f6
Amit Vasudevan <amitvasudevan@acm.org>
2020-04-03 11:51:34 -0500

    uxmhf-rpi3: mini UART: add bcm2837_miniuart_getc function to receive a character via mini UART

--- uxmhf-rpi3/common/miniuart.c
+++ uxmhf-rpi3/common/miniuart.c
@@ -86,6 +86,18 @@ void bcm2837_miniuart_puts(char *buffer){
 		bcm2837_miniuart_putc(*buffer++);
 }
 
+
+// UART character read function (non-blocking)
+u8 bcm2837_miniuart_getc(void) {
+    u8 recv_ch=0;
+
+    // logic TBD
+
+    return recv_ch;
+    //return recv_ch=='\r'?'\n':r;
+}
+
+
 void bcm2837_miniuart_flush(void){
     while( (mmio_read32(AUX_MU_LSR_REG) & 0x100) );
 }
