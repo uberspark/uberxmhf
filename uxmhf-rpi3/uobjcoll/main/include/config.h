f808e2fc21d7096456a275557458e615c8497b45
Amit Vasudevan <amitvasudevan@acm.org>
2020-11-11 12:06:51 -0600

    uxmhf-rpi3: uobjcoll: add installation script to embed kernel image within uobjcoll init sentinel; revise config.h to adjust memory map; successful bootup test!

--- uxmhf-rpi3/uobjcoll/main/include/config.h
+++ uxmhf-rpi3/uobjcoll/main/include/config.h
@@ -4,11 +4,18 @@
 	author: amit vasudevan (amitvasudevan@acm.org)
 */
 
+// TBD: auto-generate these compile time definitions
+#define __DEBUG_UART__ 1
+#define __ENABLE_UART_MINI__ 1
+
+
 
 // configure script parameters
+//TBD: the start add is start of first public method sentinel section in uobjcoll.lscript
+// end is the end of the last section in uobjcoll.lscript
 
 #define UXMHF_CORE_START_ADDR 	0x01000000
-#define UXMHF_CORE_SIZE			0x02e00000
+#define UXMHF_CORE_SIZE			0x03000000
 
 #define UXMHF_BOOT_PARTITION_START	8192
 #define UXMHF_BOOT_PARTITION_END	137215
