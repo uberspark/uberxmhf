75ec55b0b988ca6eee6fc919b84def43383f78ac
Amit Vasudevan <amitvasudevan@acm.org>
2020-12-21 10:30:31 -0600

    uxmhf-rpi3: uobjcoll: sync uapps/ folder

--- uxmhf-rpi3/uobjcoll/main/uapps/uapp-uhsign.c
+++ uxmhf-rpi3/uobjcoll/main/uapps/uapp-uhsign.c
@@ -47,106 +47,13 @@
 #include <uberspark/uobjcoll/platform/rpi3/uxmhf/main/include/debug.h>
 
 #include <uberspark/uobjcoll/platform/rpi3/uxmhf/main/include/uhsign.h>
-#include <uberspark/uobjrtl/crypto/include/mac/hmacsha1/hmacsha1.h>
+#include <uberspark/uobjrtl/crypto/include/mac/hmacsha256/hmacsha256.h>
 #include <uberspark/uobjrtl/crypto/include/basedefs.h>
 //#include <uberspark/include/uberspark.h>
 
 
-//////
-// access control via code white-listing
-//////
-
-
-// golden white-listing hash for libuhcall code
-unsigned char whitelist_hash[] = {
-        0x8f, 0xd1, 0x2d, 0x2a, 0xb8, 
-        0x68, 0x0f, 0x67, 0x1f, 0x81,
-        0xc8, 0xe3, 0x47, 0x69, 0x7d, 
-        0x86, 0x05, 0x4d, 0xd9, 0xfd
-};
-
-#define HASH_SIZE (sizeof(whitelist_hash)/sizeof(unsigned char))
-#define WHITELIST_COMPARE_BYTES 32
-
-
-//check white-listing hash with a memory regions specified by
-//physical address and size
-//return: true if ok, false if not
-bool uapp_uhsign_check_whitelist(uint32_t paddr, uint32_t size){
-  hash_state md;
-  int i;
-  unsigned char computed_hash[HASH_SIZE];
-
-  if ( sha1_memory((const unsigned char *)paddr, size, &computed_hash, HASH_SIZE) == CRYPT_OK ){
-    #if 0
-     _XDPRINTFSMP_("Hash follows:\n\n");
-    for (i=0; i < HASH_SIZE; i++)
-      _XDPRINTFSMP_("0x%02x ", computed_hash[i]);
-     _XDPRINTFSMP_("\n\n");
-    #endif
-    if (memcmp (computed_hash, whitelist_hash, HASH_SIZE) != 0) {
-      //hash did not match
-      return false;
-    }else{
-      //hash matched
-      return true;
-    }
-
-  }else{
-    //sha1_memory barfed, so just return false
-    return false;
-  }
-}
-
-// translate virtual address to physical address
-bool uapp_uhsign_va2pa(uint32_t va, u32 *pa){
-	u32 par;
-
-	sysreg_ats1cpr(va);
-	par = sysreg_read_par();
-
-	if(par & 0x1)
-		return false; 	
-
-	par &= 0xFFFFF000UL;
-
-	*pa = par;
-	return true;
-}
-
-// main function to perform access control of signing facility
-// acl is based on code white-list hashing
-void uapp_uhsign_checkacl(uint32_t va){
-    u32 paddr;
-    
-    #if 0
-    _XDPRINTFSMP_("%s: enter\n", __func__);
-    #endif
-
-    if(!va2pa((uint32_t)va, &paddr)){
-      #if 0
-      _XDPRINTFSMP_("%s: no va to pa mapping for 0\n", __func__);
-      #endif
-      //__SECURITY ACTION__: no va to pa mapping in guest; fail silently for now
-    }else{
-      #if 0
-      _XDPRINTFSMP_("va to pa mapping=0x%08x\n", __func__, paddr);
-      #endif
-      if(uapp_uhsign_check_whitelist(paddr, WHITELIST_COMPARE_BYTES)){
-          //_XDPRINTFSMP_("ACL passed\n");
-          //acl passed
-      }else{
-         //__SECURITY ACTION__: acl check error; fail silently for now
-      }
-    }
-    #if 0
-    _XDPRINTFSMP_("%s: exit\n", __func__);
-    #endif
-}
-
-
-__attribute__((section(".data"))) unsigned char key[]="super_secret_key_for_hmac";
-#define KEY_SIZE (sizeof(key))
+__attribute__((section(".data"))) unsigned char uhsign_key[]="super_secret_key_for_hmac";
+#define UHSIGN_KEY_SIZE (sizeof(uhsign_key))
 
 bool uapp_uhsign_handlehcall(u32  uhcall_function, void *uhcall_buffer, u32 uhcall_buffer_len)
 {
@@ -163,17 +70,15 @@ bool uapp_uhsign_handlehcall(u32  uhcall_function, void *uhcall_buffer, u32 uhca
   #endif
 
   //call acl function
-  uapp_uhsign_checkacl(sysreg_read_elrhyp());
+  uapp_checkacl(sysreg_read_elrhyp());
 
   //Call HMAC function
   unsigned long digest_size = HMAC_DIGEST_SIZE;
-  unsigned char *digest_result;
-  int i;
+  unsigned char digest_result[HMAC_DIGEST_SIZE];
 
-  if(hmac_sha1_memory(key, (unsigned long) KEY_SIZE, (unsigned char *) uhcp->pkt, (unsigned long) uhcp->pkt_size, digest_result, &digest_size)==CRYPT_OK) {
-    for(i=0;i<digest_size;i++) {
-      uhcp->digest[i]=(uint8_t)*(digest_result+i);
-    }
+  if(hmac_sha256_memory(uhsign_key, (unsigned long) UHSIGN_KEY_SIZE, (unsigned char *) uhcp->pkt, (unsigned long) uhcp->pkt_size, &digest_result, &digest_size)==CRYPT_OK) {
+    memcpy(uhcp->digest, digest_result, HMAC_DIGEST_SIZE);
+
     return true;
   } else {
     return false;
