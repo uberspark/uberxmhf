404840c6870641d11afbe376bdf92b97c39358b8
Amit Vasudevan <amitvasudevan@acm.org>
2020-12-21 13:57:33 -0600

    uxmhf-rpi3: uobjcoll: bring in all build configuration definitions into config.h for the time-being

--- uxmhf-rpi3/uobjcoll/main/include/config.h
+++ uxmhf-rpi3/uobjcoll/main/include/config.h
@@ -4,29 +4,35 @@
 	author: amit vasudevan (amitvasudevan@acm.org)
 */
 
-// TBD: auto-generate these compile time definitions
-#define __DEBUG_UART__ 1
-#define __ENABLE_UART_MINI__ 1
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
 
 
-
-// configure script parameters
-//TBD: the start add is start of first public method sentinel section in uobjcoll.lscript
-// end is the end of the last section in uobjcoll.lscript
-
+// TBD: auto-generate these compile time definitions via uobjcoll manifest
 #define UXMHF_CORE_START_ADDR 	0x01000000
 #define UXMHF_CORE_SIZE			0x03000000
-
+#define UXMHF_CORE_END_ADDR 	( 0x01000000 + 0x02e00000 )
 #define UXMHF_BOOT_PARTITION_START	8192
 #define UXMHF_BOOT_PARTITION_END	137215
 
 
-// computed parameters from above
-#define UXMHF_CORE_END_ADDR 	( 0x01000000 + 0x02e00000 )
+//#define __SECBOOT__
+//#define __DMAPROT__
+//#define __INTPROT__
+//#define __FIQREFLECTION__
+#define __DEBUG_UART__
+#define __ENABLE_UART_MINI__
+//#define __ENABLE_UART_PL011__
+//#define __ENABLE_UART_PL011_CTSRTS__
+
+//#define __ENABLE_UAPP_PVDRIVER_UART__
+//#define __ENABLE_UAPP_UAGENT__
+//#define __ENABLE_UAPP_UHSIGN__
+//#define __ENABLE_UAPP_STATEDB__
 
 
-#ifndef __CONFIG_H__
-#define __CONFIG_H__
+
+
 
 #ifndef __ASSEMBLY__
 
