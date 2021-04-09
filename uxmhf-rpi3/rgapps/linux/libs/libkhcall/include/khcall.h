73d81c1aa6da75f99529180c6b78b052e466ae5c
Amit Vasudevan <amitvasudevan@acm.org>
2021-04-09 13:54:16 -0500

    uxmhf-rpi3: rgapps/linux/lib: libkhcall: add khcall_fast_retu32 implementation

--- uxmhf-rpi3/rgapps/linux/libs/libkhcall/include/khcall.h
+++ uxmhf-rpi3/rgapps/linux/libs/libkhcall/include/khcall.h
@@ -48,6 +48,7 @@
 
 bool khcall(uint32_t khcall_function, void *khcall_buffer, uint32_t khcall_buffer_len);
 void khcall_fast(uint32_t khcall_function, uint32_t param1, uint32_t param2);
+uint32_t khcall_fast_retu32(uint32_t khcall_function, uint32_t param1, uint32_t param2);
 
 
 #endif // __ASSEMBLY__
