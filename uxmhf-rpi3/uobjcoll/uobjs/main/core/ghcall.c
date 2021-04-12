73214ccd9d8b38acb0623ed1403a5f75bc8c211f
Amit Vasudevan <amitvasudevan@acm.org>
2021-04-12 16:31:30 -0500

    uxmhf-rpi3: uobjs/main/core: temporarily comment out uapp_i2c_ioaccess handling within guest_hypercall_handler

--- uxmhf-rpi3/uobjcoll/uobjs/main/core/ghcall.c
+++ uxmhf-rpi3/uobjcoll/uobjs/main/core/ghcall.c
@@ -79,10 +79,10 @@ void guest_hypercall_handler(arm8_32_regs_t *r, u32 hsr){
 		// this is fast hypercall path where all parameters
 		// are passed via registers
 
-		#if defined (__UBERSPARK_UOBJCOLL_CONFIGDEF_ENABLE_UAPP_I2C_IOACCESS__)
-			if( uapp_i2c_ioaccess_handle_fast_hcall(r) )
-				return;
-		#endif
+		//#if defined (__UBERSPARK_UOBJCOLL_CONFIGDEF_ENABLE_UAPP_I2C_IOACCESS__)
+		//	if( uapp_i2c_ioaccess_handle_fast_hcall(r) )
+		//		return;
+		//#endif
 
 		//do nothing; null hypercall
 
