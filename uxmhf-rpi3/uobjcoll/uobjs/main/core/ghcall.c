cd10358fe4bb22f3d4591558a7a7918964d8bd65
Amit Vasudevan <amitvasudevan@acm.org>
2021-04-13 10:36:51 -0500

    uxmhf-rpi3: add another class of hypercall for null invocation testing...

--- uxmhf-rpi3/uobjcoll/uobjs/main/core/ghcall.c
+++ uxmhf-rpi3/uobjcoll/uobjs/main/core/ghcall.c
@@ -80,11 +80,11 @@ void guest_hypercall_handler(arm8_32_regs_t *r, u32 hsr){
 		// are passed via registers
 
 		#if defined (__UBERSPARK_UOBJCOLL_CONFIGDEF_ENABLE_UAPP_I2C_IOACCESS__)
-			if (r->r0 == UAPP_I2C_IOACCESS_WRITEL){
-				if( uapp_i2c_ioaccess_handle_fast_hcall(r) )
-					return;
-			}
+			if( uapp_i2c_ioaccess_handle_fast_hcall(r) )
+				return;
 		#endif
+
+	}else if (hvc_imm16 == 2){
 
 		//do nothing; null hypercall
 
