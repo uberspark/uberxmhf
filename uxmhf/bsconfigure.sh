669ffe4253f6f6389c8217f64e8bdddde36ee394
Eric Li <xiaoyili@andrew.cmu.edu>
2021-11-01 12:36:21 -0400

    remove uberXMHF from repo

--- uxmhf/bsconfigure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# top-level configure boot-strap script
-# author: amit vasudevan (amitvasudevan@acm.org)
-
-autoconf --output=configure configure.ac
-autoconf --output=configure-uxmhfuobj configure-uxmhfuobj.ac
-exit 0
