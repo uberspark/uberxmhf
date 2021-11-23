7bf294603b035b6686b0af19917803aae1f0f8a5
Eric Li <xiaoyili@andrew.cmu.edu>
2021-11-22 23:43:37 -0500

    Fix printf because stdarg was only implemented for x86

--- xmhf/src/libbaremetal/libxmhfc/include/stdarg.h
+++ xmhf/src/libbaremetal/libxmhfc/include/stdarg.h
@@ -54,6 +54,12 @@
 
 #ifndef __ASSEMBLY__
 
+#ifdef __X86_64__
+
+#include <stdarg64.h>
+
+#else /* !__X86_64__ */
+
 typedef void *va_list;
 
 #define	va_start(list, name) (void) (list = (void *)((char *)&name + \
@@ -62,6 +68,7 @@ typedef void *va_list;
 
 #define	va_end(list) (void)0
 
+#endif /* __X86_64__ */
 
 #endif //#ifndef __ASSEMBLY__
 
