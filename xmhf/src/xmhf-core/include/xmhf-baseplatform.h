b1aef79605ec139c4465a05c87370a2d5ab5ffc6
Eric Li <xiaoyili@andrew.cmu.edu>
2021-11-29 17:41:06 -0500

    Revert to a more stable state

--- xmhf/src/xmhf-core/include/xmhf-baseplatform.h
+++ xmhf/src/xmhf-core/include/xmhf-baseplatform.h
@@ -63,11 +63,9 @@
 //NOTE: The size of this structure _MUST_ be _EXACTLY_EQUAL_ to 8 bytes
 //as it is made use of in low-level assembly language stubs
 typedef struct _midtab {
-    union {
-        u32 cpu_lapic_id;       // CPU LAPIC id (unique)
-        hva_t _cpu_lapic_id_64; // Keep struct aligned in x86_64; don't use this
-    };
-    hva_t vcpu_vaddr_ptr;   // virt. addr. pointer to vcpu struct for this CPU
+  u32 cpu_lapic_id;         //CPU LAPIC id (unique)
+  hva_t vcpu_vaddr_ptr; //virt. addr. pointer to vcpu struct for this CPU
+  // TODO: table no longer aligned in 64-bit
 } __attribute__((packed)) MIDTAB;
 
 #define SIZE_STRUCT_MIDTAB  (sizeof(struct _midtab))
