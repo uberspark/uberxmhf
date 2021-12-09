0495917bae8d114f2150f982acefda9283fad6db
Eric Li <xiaoyili@andrew.cmu.edu>
2021-12-09 15:38:43 -0500

    Update GitHub Actions to compile QEMU workaround

---
+++ .github/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -xe
+
+CONFIGURE_ARGS="--with-approot=hypapps/trustvisor"
+CONFIGURE_ARGS="${CONFIGURE_ARGS} --disable-drt"
+CONFIGURE_ARGS="${CONFIGURE_ARGS} --enable-debug-symbols"
+
+if [ "$1" == "x86" ]; then
+	CONFIGURE_ARGS="${CONFIGURE_ARGS} CC=i686-linux-gnu-gcc"
+	CONFIGURE_ARGS="${CONFIGURE_ARGS} LD=i686-linux-gnu-ld"
+else if [ "$1" == "x86_64" ]; then
+	CONFIGURE_ARGS="${CONFIGURE_ARGS} --with-target-hwplatform=x86_64"
+else
+	echo '$1 incorrect'; exit 1
+fi; fi
+
+if [ "$2" == "release" ]; then
+	CONFIGURE_ARGS="${CONFIGURE_ARGS}"
+else if [ "$2" == "debug" ]; then
+	CONFIGURE_ARGS="${CONFIGURE_ARGS} --enable-qemu-workaround"
+else
+	echo '$2 incorrect'; exit 1
+fi; fi
+
+./autogen.sh
+./configure ${CONFIGURE_ARGS}
+make -j "$(nproc)"
+
