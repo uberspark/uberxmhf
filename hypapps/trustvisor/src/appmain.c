1cb915f36b09b3593cb40dc327d2c3587e6e08b9
Eric Li <xiaoyili@andrew.cmu.edu>
2022-01-07 13:25:23 -0500

    Fix bug in 3b6f66898cd1ac651f6cf139121c4ee6f7994ba0

--- hypapps/trustvisor/src/appmain.c
+++ hypapps/trustvisor/src/appmain.c
@@ -204,9 +204,9 @@ static u64 do_TV_HC_TEST(VCPU *vcpu, struct regs *r)
 {
   (void)r;
 #ifdef __XMHF_X86_64__
-  eu_trace("CPU(0x%02x): test hypercall, ecx=0x%08x", vcpu->id, r->ecx);
-#else /* !__XMHF_X86_64__ */
   eu_trace("CPU(0x%02x): test hypercall, rcx=0x%016x", vcpu->id, r->rcx);
+#else /* !__XMHF_X86_64__ */
+  eu_trace("CPU(0x%02x): test hypercall, ecx=0x%08x", vcpu->id, r->ecx);
 #endif /* __XMHF_X86_64__ */
   return 0;
 }
