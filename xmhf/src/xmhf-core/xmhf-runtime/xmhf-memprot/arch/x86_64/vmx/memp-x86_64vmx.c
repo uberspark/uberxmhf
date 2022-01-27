uberspark/uberxmhf

commit 44eb4d9c6d00c4a606da2fc9a267b173603525d8

Remove hardcoding of memorytype=WB in _vmx_setupEPT()

xmhf/src/xmhf-core/xmhf-runtime/xmhf-memprot/arch/x86_64/vmx/memp-x86_64vmx.c
@@ -302,14 +302,19 @@ static void _vmx_setupEPT(VCPU *vcpu){
 			u64 i = paddr / PAGE_SIZE_4K;
 			u64 memorytype = _vmx_getmemorytypeforphysicalpage(vcpu, paddr);
 			u64 lower;
+			/*
+			 * For memorytype equal to 0 (UC), 1 (WC), 4 (WT), 5 (WP), 6 (WB),
+			 * MTRR memory type and EPT memory type are the same encoding.
+			 * Currently other encodings are reserved.
+			 */
+			HALT_ON_ERRORCOND(memorytype == 0 || memorytype == 1 ||
+								memorytype == 4 || memorytype == 5 ||
+								memorytype == 6);
 			if ((paddr >= (rpb->XtVmmRuntimePhysBase - PAGE_SIZE_2M)) &&
 				(paddr < (rpb->XtVmmRuntimePhysBase + rpb->XtVmmRuntimeSize))) {
 				lower = 0x0;	/* not present */
 			} else if (memorytype == 0) {
-				lower = 0x7;	/* present, UC */
-			} else {
-				memorytype = 6;
-				lower = 0x7;	/* present, WB, track host MTRR */
+				lower = 0x7;	/* present */
 			}
 			p_entry[i] = paddr | (memorytype << 3) | lower;
 		}

xmhf/src/xmhf-core/xmhf-runtime/xmhf-memprot/arch/x86/vmx/memp-x86vmx.c
@@ -288,6 +288,14 @@ static void _vmx_setupEPT(VCPU *vcpu){
 			
 			for(k=0; k < PAE_PTRS_PER_PT; k++){
 				u32 memorytype = _vmx_getmemorytypeforphysicalpage(vcpu, (u64)paddr);
+				/*
+				 * For memorytype equal to 0 (UC), 1 (WC), 4 (WT), 5 (WP), 6 (WB),
+				 * MTRR memory type and EPT memory type are the same encoding.
+				 * Currently other encodings are reserved.
+				 */
+				HALT_ON_ERRORCOND(memorytype == 0 || memorytype == 1 ||
+									memorytype == 4 || memorytype == 5 ||
+									memorytype == 6);
 				//the XMHF memory region includes the secure loader +
 				//the runtime (core + app). this runs from 
 				//(rpb->XtVmmRuntimePhysBase - PAGE_SIZE_2M) with a size
@@ -295,12 +303,9 @@ static void _vmx_setupEPT(VCPU *vcpu){
 				//make XMHF physical pages inaccessible
 				if( (paddr >= (rpb->XtVmmRuntimePhysBase - PAGE_SIZE_2M)) &&
 					(paddr < (rpb->XtVmmRuntimePhysBase + rpb->XtVmmRuntimeSize)) ){
-					p_table[k] = (u64) (paddr)  | ((u64)memorytype << 3) | (u64)0x0 ;	//not-present
+					p_table[k] = (u64) (paddr) | ((u64)memorytype << 3) | (u64)0x0 ;	//not-present
 				}else{
-					if(memorytype == 0)
-						p_table[k] = (u64) (paddr)  | ((u64)memorytype << 3) |  (u64)0x7 ;	//present, UC
-					else
-						p_table[k] = (u64) (paddr)  | ((u64)6 << 3) | (u64)0x7 ;	//present, WB, track host MTRR
+					p_table[k] = (u64) (paddr) | ((u64)memorytype << 3) | (u64)0x7 ;	//present
 				}
 				
 				paddr += PAGE_SIZE_4K;

commit 25c2f085beceaa22ce8420a38c5f1a2de750f62d

Fix bug introduced in 44eb4d9c6d00c4a606da2fc9a267b173603525d8

xmhf/src/xmhf-core/xmhf-runtime/xmhf-memprot/arch/x86_64/vmx/memp-x86_64vmx.c
@@ -313,7 +313,7 @@ static void _vmx_setupEPT(VCPU *vcpu){
 			if ((paddr >= (rpb->XtVmmRuntimePhysBase - PAGE_SIZE_2M)) &&
 				(paddr < (rpb->XtVmmRuntimePhysBase + rpb->XtVmmRuntimeSize))) {
 				lower = 0x0;	/* not present */
-			} else if (memorytype == 0) {
+			} else {
 				lower = 0x7;	/* present */
 			}
 			p_entry[i] = paddr | (memorytype << 3) | lower;
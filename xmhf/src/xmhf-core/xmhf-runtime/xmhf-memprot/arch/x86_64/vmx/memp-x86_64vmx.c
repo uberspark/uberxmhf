25c2f085beceaa22ce8420a38c5f1a2de750f62d
Eric Li <xiaoyili@andrew.cmu.edu>
2022-01-26 21:14:51 -0500

    Fix bug introduced in 44eb4d9c6d00c4a606da2fc9a267b173603525d8

--- xmhf/src/xmhf-core/xmhf-runtime/xmhf-memprot/arch/x86_64/vmx/memp-x86_64vmx.c
+++ xmhf/src/xmhf-core/xmhf-runtime/xmhf-memprot/arch/x86_64/vmx/memp-x86_64vmx.c
@@ -313,7 +313,7 @@ static void _vmx_setupEPT(VCPU *vcpu){
 			if ((paddr >= (rpb->XtVmmRuntimePhysBase - PAGE_SIZE_2M)) &&
 				(paddr < (rpb->XtVmmRuntimePhysBase + rpb->XtVmmRuntimeSize))) {
 				lower = 0x0;	/* not present */
-			} else if (memorytype == 0) {
+			} else {
 				lower = 0x7;	/* present */
 			}
 			p_entry[i] = paddr | (memorytype << 3) | lower;
