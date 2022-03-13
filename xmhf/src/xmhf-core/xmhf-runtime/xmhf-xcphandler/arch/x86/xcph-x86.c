63e942cc1a4fc2f4f7957aa87504618635d26605
Eric Li <xiaoyili@andrew.cmu.edu>
2022-03-13 17:03:14 -0400

    expand -t 4 xcph-x86.c

--- xmhf/src/xmhf-core/xmhf-runtime/xmhf-xcphandler/arch/x86/xcph-x86.c
+++ xmhf/src/xmhf-core/xmhf-runtime/xmhf-xcphandler/arch/x86/xcph-x86.c
@@ -81,31 +81,31 @@ static VCPU *_svm_and_vmx_getvcpu(void){
 
 //initialize EMHF core exception handlers
 void xmhf_xcphandler_arch_initialize(void){
-	u32 *pexceptionstubs;
-	u32 i;
+    u32 *pexceptionstubs;
+    u32 i;
 
-	printf("\n%s: setting up runtime IDT...", __FUNCTION__);
-	
-	//pexceptionstubs=(u32 *)&xmhf_xcphandler_exceptionstubs;
-	pexceptionstubs=(u32 *)&xmhf_xcphandler_exceptionstubs;
-	
-	for(i=0; i < EMHF_XCPHANDLER_MAXEXCEPTIONS; i++){
-		idtentry_t *idtentry=(idtentry_t *)((hva_t)xmhf_xcphandler_arch_get_idt_start()+ (i*8));
-		idtentry->isrLow= (u16)pexceptionstubs[i];
-		idtentry->isrHigh= (u16) ( (u32)pexceptionstubs[i] >> 16 );
-		idtentry->isrSelector = __CS;
-		idtentry->count=0x0;
-		idtentry->type=0x8E;	//32-bit interrupt gate
-								//present=1, DPL=00b, system=0, type=1110b
-	}
-	
-	printf("\n%s: IDT setup done.", __FUNCTION__);
+    printf("\n%s: setting up runtime IDT...", __FUNCTION__);
+    
+    //pexceptionstubs=(u32 *)&xmhf_xcphandler_exceptionstubs;
+    pexceptionstubs=(u32 *)&xmhf_xcphandler_exceptionstubs;
+    
+    for(i=0; i < EMHF_XCPHANDLER_MAXEXCEPTIONS; i++){
+        idtentry_t *idtentry=(idtentry_t *)((hva_t)xmhf_xcphandler_arch_get_idt_start()+ (i*8));
+        idtentry->isrLow= (u16)pexceptionstubs[i];
+        idtentry->isrHigh= (u16) ( (u32)pexceptionstubs[i] >> 16 );
+        idtentry->isrSelector = __CS;
+        idtentry->count=0x0;
+        idtentry->type=0x8E;    //32-bit interrupt gate
+                                //present=1, DPL=00b, system=0, type=1110b
+    }
+    
+    printf("\n%s: IDT setup done.", __FUNCTION__);
 }
 
 
 //get IDT start address
 u8 * xmhf_xcphandler_arch_get_idt_start(void){
-	return (u8 *)&xmhf_xcphandler_idt_start;
+    return (u8 *)&xmhf_xcphandler_idt_start;
 }
 
 extern uint8_t _begin_xcph_table[];
@@ -113,98 +113,98 @@ extern uint8_t _end_xcph_table[];
 
 //EMHF exception handler hub
 void xmhf_xcphandler_arch_hub(uintptr_t vector, struct regs *r){
-	u32 cpu_vendor = get_cpu_vendor_or_die();	//determine CPU vendor
-	VCPU *vcpu;
-	
-	vcpu=_svm_and_vmx_getvcpu();
+    u32 cpu_vendor = get_cpu_vendor_or_die();   //determine CPU vendor
+    VCPU *vcpu;
+    
+    vcpu=_svm_and_vmx_getvcpu();
 
-	/*
-	 * Cannot print anything before event handler returns if this exception
-	 * is for quiescing (vector == CPU_EXCEPTION_NMI), otherwise will deadlock.
-	 * See xmhf_smpguest_arch_x86vmx_quiesce().
-	 */
+    /*
+     * Cannot print anything before event handler returns if this exception
+     * is for quiescing (vector == CPU_EXCEPTION_NMI), otherwise will deadlock.
+     * See xmhf_smpguest_arch_x86vmx_quiesce().
+     */
 
-	switch(vector){
-	case CPU_EXCEPTION_NMI:
-		xmhf_smpguest_arch_x86_eventhandler_nmiexception(vcpu, r, 0);
-		break;
+    switch(vector){
+    case CPU_EXCEPTION_NMI:
+        xmhf_smpguest_arch_x86_eventhandler_nmiexception(vcpu, r, 0);
+        break;
 
-	default:
-		{
-			/*
-			 * Search for matching exception in .xcph_table section.
-			 * Each entry in .xcph_table has 3 long values. If the first value
-			 * matches the exception vector and the second value matches the
-			 * current PC, then jump to the third value.
-			 */
-			u32 exception_cs, exception_eip, exception_eflags;
+    default:
+        {
+            /*
+             * Search for matching exception in .xcph_table section.
+             * Each entry in .xcph_table has 3 long values. If the first value
+             * matches the exception vector and the second value matches the
+             * current PC, then jump to the third value.
+             */
+            u32 exception_cs, exception_eip, exception_eflags;
             u32 error_code_available = 0;
-			hva_t *found = NULL;
+            hva_t *found = NULL;
 
-			if(vector == CPU_EXCEPTION_DF ||
-				vector == CPU_EXCEPTION_TS ||
-				vector == CPU_EXCEPTION_NP ||
-				vector == CPU_EXCEPTION_SS ||
-				vector == CPU_EXCEPTION_GP ||
-				vector == CPU_EXCEPTION_PF ||
-				vector == CPU_EXCEPTION_AC){
-				error_code_available = 1;
-				r->esp += sizeof(uint32_t);	//skip error code on stack if applicable
-			}
+            if(vector == CPU_EXCEPTION_DF ||
+                vector == CPU_EXCEPTION_TS ||
+                vector == CPU_EXCEPTION_NP ||
+                vector == CPU_EXCEPTION_SS ||
+                vector == CPU_EXCEPTION_GP ||
+                vector == CPU_EXCEPTION_PF ||
+                vector == CPU_EXCEPTION_AC){
+                error_code_available = 1;
+                r->esp += sizeof(uint32_t); //skip error code on stack if applicable
+            }
 
-			exception_eip = *(uint32_t *)(r->esp+0);
-			exception_cs = *(uint32_t *)(r->esp+sizeof(uint32_t));
-			exception_eflags = *(uint32_t *)(r->esp+(2*sizeof(uint32_t)));
+            exception_eip = *(uint32_t *)(r->esp+0);
+            exception_cs = *(uint32_t *)(r->esp+sizeof(uint32_t));
+            exception_eflags = *(uint32_t *)(r->esp+(2*sizeof(uint32_t)));
 
-			for (hva_t *i = (hva_t *)_begin_xcph_table; i < (hva_t *)_end_xcph_table; i += 3) {
-				if (i[0] == vector && i[1] == exception_eip) {
-					found = i;
-					break;
-				}
-			}
+            for (hva_t *i = (hva_t *)_begin_xcph_table; i < (hva_t *)_end_xcph_table; i += 3) {
+                if (i[0] == vector && i[1] == exception_eip) {
+                    found = i;
+                    break;
+                }
+            }
 
-			if (found) {
-				/* Found in xcph table; Modify EIP on stack and iret */
-				printf("\nFound in xcph table");
-				*(uint32_t *)(r->esp+0) = found[2];
-				break;
-			}
+            if (found) {
+                /* Found in xcph table; Modify EIP on stack and iret */
+                printf("\nFound in xcph table");
+                *(uint32_t *)(r->esp+0) = found[2];
+                break;
+            }
 
-			/* Print exception and halt */
-			printf("\n[%02x]: unhandled exception %d (0x%x), halting!",
-					vcpu->id, vector, vector);
-			if (error_code_available) {
-				printf("\n[%02x]: error code: 0x%08lx", vcpu->id, ((uint32_t *)(r->esp))[-1]);
-			}
-			printf("\n[%02x]: state dump follows...", vcpu->id);
-			//things to dump
-			printf("\n[%02x] CS:EIP 0x%04x:0x%08x with EFLAGS=0x%08x", vcpu->id,
-				(u16)exception_cs, exception_eip, exception_eflags);
-			printf("\n[%02x]: VCPU at 0x%08x", vcpu->id, (u32)vcpu, vcpu->id);
-			printf("\n[%02x] EAX=0x%08x EBX=0x%08x ECX=0x%08x EDX=0x%08x", vcpu->id,
-					r->eax, r->ebx, r->ecx, r->edx);
-			printf("\n[%02x] ESI=0x%08x EDI=0x%08x EBP=0x%08x ESP=0x%08x", vcpu->id,
-					r->esi, r->edi, r->ebp, r->esp);
-			printf("\n[%02x] CS=0x%04x, DS=0x%04x, ES=0x%04x, SS=0x%04x", vcpu->id,
-				(u16)read_segreg_cs(), (u16)read_segreg_ds(),
-				(u16)read_segreg_es(), (u16)read_segreg_ss());
-			printf("\n[%02x] FS=0x%04x, GS=0x%04x", vcpu->id,
-				(u16)read_segreg_fs(), (u16)read_segreg_gs());
-			printf("\n[%02x] TR=0x%04x", vcpu->id, (u16)read_tr_sel());
+            /* Print exception and halt */
+            printf("\n[%02x]: unhandled exception %d (0x%x), halting!",
+                    vcpu->id, vector, vector);
+            if (error_code_available) {
+                printf("\n[%02x]: error code: 0x%08lx", vcpu->id, ((uint32_t *)(r->esp))[-1]);
+            }
+            printf("\n[%02x]: state dump follows...", vcpu->id);
+            //things to dump
+            printf("\n[%02x] CS:EIP 0x%04x:0x%08x with EFLAGS=0x%08x", vcpu->id,
+                (u16)exception_cs, exception_eip, exception_eflags);
+            printf("\n[%02x]: VCPU at 0x%08x", vcpu->id, (u32)vcpu, vcpu->id);
+            printf("\n[%02x] EAX=0x%08x EBX=0x%08x ECX=0x%08x EDX=0x%08x", vcpu->id,
+                    r->eax, r->ebx, r->ecx, r->edx);
+            printf("\n[%02x] ESI=0x%08x EDI=0x%08x EBP=0x%08x ESP=0x%08x", vcpu->id,
+                    r->esi, r->edi, r->ebp, r->esp);
+            printf("\n[%02x] CS=0x%04x, DS=0x%04x, ES=0x%04x, SS=0x%04x", vcpu->id,
+                (u16)read_segreg_cs(), (u16)read_segreg_ds(),
+                (u16)read_segreg_es(), (u16)read_segreg_ss());
+            printf("\n[%02x] FS=0x%04x, GS=0x%04x", vcpu->id,
+                (u16)read_segreg_fs(), (u16)read_segreg_gs());
+            printf("\n[%02x] TR=0x%04x", vcpu->id, (u16)read_tr_sel());
 
-			//do a stack dump in the hopes of getting more info.
-			{
-				//vcpu->esp is the TOS
-				uint32_t i;
-				//uint32_t stack_start = (r->esp+(3*sizeof(uint32_t)));
-				uint32_t stack_start = r->esp;
-				printf("\n[%02x]-----stack dump-----", vcpu->id);
-				for(i=stack_start; i < vcpu->esp; i+=sizeof(uint32_t)){
-					printf("\n[%02x]  Stack(0x%08x) -> 0x%08x", vcpu->id, i, *(uint32_t *)i);
-				}
-				printf("\n[%02x]-----end------------", vcpu->id);
-			}
-			HALT();
-		}
-	}
+            //do a stack dump in the hopes of getting more info.
+            {
+                //vcpu->esp is the TOS
+                uint32_t i;
+                //uint32_t stack_start = (r->esp+(3*sizeof(uint32_t)));
+                uint32_t stack_start = r->esp;
+                printf("\n[%02x]-----stack dump-----", vcpu->id);
+                for(i=stack_start; i < vcpu->esp; i+=sizeof(uint32_t)){
+                    printf("\n[%02x]  Stack(0x%08x) -> 0x%08x", vcpu->id, i, *(uint32_t *)i);
+                }
+                printf("\n[%02x]-----end------------", vcpu->id);
+            }
+            HALT();
+        }
+    }
 }
