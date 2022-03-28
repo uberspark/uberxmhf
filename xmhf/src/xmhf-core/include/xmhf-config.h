uberspark/uberxmhf

commit fc54fa5d2db3f0616ddc98caaa628c964d2e76e3

In all occurrences of __AMD64__, require __I386__ || __AMD64__ to be true

xmhf/src/xmhf-core/include/xmhf-config.h
@@ -59,7 +59,6 @@
 #ifdef __I386__
     // Note: 32-bit XMHF (non-PAE) always assumes the maximum physical memory size == 4GB.
 	#define MAX_PHYS_ADDR					ADDR_4GB
-#elif defined(__AMD64__)
 	#define MAX_PHYS_ADDR                   (AMD64_MAX_PHYS_ADDR)
 #else
     #error "Unsupported Arch"

xmhf/src/xmhf-core/include/xmhf-types.h
@@ -56,7 +56,6 @@
 #define KB(x)           (((size_t)(x)) << 10)
 
 // TODO: bootloader for 64 bit XMHF will have incorrect size
-// Ideally, should change __AMD64__ to __XMHF_AMD64__
 #ifdef __I386__
     typedef u32 hva_t;  // hypervisor virtual address
     typedef u64 spa_t;  // system physical address. [NOTE] spa_t could have a value larger than 4G; e.g., PC installs
@@ -65,7 +64,6 @@
     typedef u32 gva_t;  // guest virtual address
     typedef u64 gpa_t;  // guest physical address. can be 64-bit with PAE
     typedef u32 sla_t;  // secure loader address
-#elif defined(__AMD64__)
     typedef u64 hva_t;  // hypervisor virtual address
     typedef u64 spa_t;  // system physical address
     typedef u64	spfn_t; // pfn of system physical address
@@ -87,7 +85,6 @@
 #ifdef __I386__
     #define UINT32sToSPADDR(high, low) (spa_t)(UINT32sToUINT64(high, low))
     #define UINT32sToSIZE(high, low) (size_t)(low)
-#elif defined(__AMD64__)
     #define UINT32sToSPADDR(high, low) (spa_t)(UINT32sToUINT64(high, low))
     #define UINT32sToSIZE(high, low) (size_t)(UINT32sToUINT64(high, low))
 #else

hypapps/trustvisor/tee-sdk/tz/dev/null/src/svcapi.c
@@ -58,12 +58,14 @@ static inline uint64_t rdtsc64(void)
 
   __asm__ __volatile__ ("rdtsc" : "=a" (eax), "=d" (edx));
   return ((uint64_t)edx << 32) | eax;
-#else /* !__AMD64__ */
+#elif defined(__I386__)
   uint64_t rv;
 
   __asm__ __volatile__ ("rdtsc" : "=A" (rv));
   return (rv);
-#endif /* __AMD64__ */
+#else /* !defined(__I386__) && !defined(__AMD64__) */
+    #error "Unsupported Arch"
+#endif /* !defined(__I386__) && !defined(__AMD64__) */
 }
 
 int svc_time_elapsed_us(uint64_t *epoch_nonce, /* out */

hypapps/trustvisor/tee-sdk/tz/dev/tv/src/svc.c
@@ -56,12 +56,14 @@ static inline uint64_t rdtsc64(void)
 
   __asm__ __volatile__ ("rdtsc" : "=a" (eax), "=d" (edx));
   return ((uint64_t)edx << 32) | eax;
-#else /* !__AMD64__ */
+#elif defined(__I386__)
   uint64_t rv;
 
   __asm__ __volatile__ ("rdtsc" : "=A" (rv));
   return (rv);
-#endif /* __AMD64__ */
+#else /* !defined(__I386__) && !defined(__AMD64__) */
+    #error "Unsupported Arch"
+#endif /* !defined(__I386__) && !defined(__AMD64__) */
 }
 
 int svc_time_elapsed_us(uint64_t *epoch_nonce, /* out */

xmhf/src/libbaremetal/libtpm/tpm.c
@@ -188,12 +188,14 @@ static inline uint64_t rdtsc64(void)
 
   __asm__ __volatile__ ("rdtsc" : "=a" (eax), "=d" (edx));
   return ((uint64_t)edx << 32) | eax;
-#else /* !__AMD64__ */
+#elif defined(__I386__)
   uint64_t rv;
 
   __asm__ __volatile__ ("rdtsc" : "=A" (rv));
   return (rv);
-#endif /* __AMD64__ */
+#else /* !defined(__I386__) && !defined(__AMD64__) */
+    #error "Unsupported Arch"
+#endif /* !defined(__I386__) && !defined(__AMD64__) */
 }
 
 /*static inline*/

xmhf/src/libbaremetal/libxmhfc/include/endian.h
@@ -35,9 +35,11 @@
 
 #ifdef __AMD64__
 #include <sys/amd64_types.h>
-#else /* !__AMD64__ */
+#elif defined(__I386__)
 #include <sys/i386_types.h>
-#endif /* __AMD64__ */
+#else /* !defined(__I386__) && !defined(__AMD64__) */
+    #error "Unsupported Arch"
+#endif /* !defined(__I386__) && !defined(__AMD64__) */
 #include <sys/endian.h>
 
 #ifndef _UINT16_T_DECLARED

xmhf/src/libbaremetal/libxmhfc/include/limits.h
@@ -38,9 +38,11 @@
 
 #ifdef __AMD64__
 #include <sys/amd64_limits.h>
-#else /* !__AMD64__ */
+#elif defined(__I386__)
 #include <sys/i386_limits.h>
-#endif /* __AMD64__ */
+#else /* !defined(__I386__) && !defined(__AMD64__) */
+    #error "Unsupported Arch"
+#endif /* !defined(__I386__) && !defined(__AMD64__) */
 
 #define CHAR_BIT        __CHAR_BIT      /* number of bits in a char */
 

xmhf/src/libbaremetal/libxmhfc/include/stdarg.h
@@ -58,7 +58,7 @@
 
 #include <stdarg64.h>
 
-#else /* !__AMD64__ */
+#elif defined(__I386__)
 
 typedef void *va_list;
 
@@ -68,7 +68,9 @@ typedef void *va_list;
 
 #define	va_end(list) (void)0
 
-#endif /* __AMD64__ */
+#else /* !defined(__I386__) && !defined(__AMD64__) */
+    #error "Unsupported Arch"
+#endif /* !defined(__I386__) && !defined(__AMD64__) */
 
 #endif //#ifndef __ASSEMBLY__
 

commit 0a7127ebc084d5e241221f47617c517ee8a5577f

Fix bug caused by fc54fa5d2db3f0616ddc98caaa628c964d2e76e3

xmhf/src/xmhf-core/include/xmhf-config.h
@@ -59,6 +59,7 @@
 #ifdef __I386__
     // Note: 32-bit XMHF (non-PAE) always assumes the maximum physical memory size == 4GB.
 	#define MAX_PHYS_ADDR					ADDR_4GB
+#elif defined(__AMD64__)
 	#define MAX_PHYS_ADDR                   (AMD64_MAX_PHYS_ADDR)
 #else
     #error "Unsupported Arch"

xmhf/src/xmhf-core/include/xmhf-types.h
@@ -56,6 +56,7 @@
 #define KB(x)           (((size_t)(x)) << 10)
 
 // TODO: bootloader for 64 bit XMHF will have incorrect size
+// Ideally, should change __AMD64__ to __XMHF_AMD64__
 #ifdef __I386__
     typedef u32 hva_t;  // hypervisor virtual address
     typedef u64 spa_t;  // system physical address. [NOTE] spa_t could have a value larger than 4G; e.g., PC installs
@@ -64,6 +65,7 @@
     typedef u32 gva_t;  // guest virtual address
     typedef u64 gpa_t;  // guest physical address. can be 64-bit with PAE
     typedef u32 sla_t;  // secure loader address
+#elif defined(__AMD64__)
     typedef u64 hva_t;  // hypervisor virtual address
     typedef u64 spa_t;  // system physical address
     typedef u64	spfn_t; // pfn of system physical address
@@ -85,6 +87,7 @@
 #ifdef __I386__
     #define UINT32sToSPADDR(high, low) (spa_t)(UINT32sToUINT64(high, low))
     #define UINT32sToSIZE(high, low) (size_t)(low)
+#elif defined(__AMD64__)
     #define UINT32sToSPADDR(high, low) (spa_t)(UINT32sToUINT64(high, low))
     #define UINT32sToSIZE(high, low) (size_t)(UINT32sToUINT64(high, low))
 #else