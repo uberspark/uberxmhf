c4afaf4d87bb226b5175e3acea1d4912c866b1c2
Eric Li <xiaoyili@andrew.cmu.edu>
2022-06-05 23:27:54 -0700

    Be able to enter nested guest, but disabled EPT

--- xmhf/src/xmhf-core/xmhf-runtime/xmhf-nested/arch/x86/vmx/nested-x86vmx-handler.c
+++ xmhf/src/xmhf-core/xmhf-runtime/xmhf-nested/arch/x86/vmx/nested-x86vmx-handler.c
@@ -709,8 +709,8 @@ static u32 _vmx_vmentry(VCPU *vcpu, vmcs12_info_t *vmcs12_info, struct regs *r)
 		u32 fixed1 = vcpu->vmx_msrs[INDEX_IA32_VMX_PROCBASED_CTLS2_MSR] >> 32;
 		HALT_ON_ERRORCOND((~val & fixed0) == 0 && (val & ~fixed1) == 0);
 		/* XMHF needs the guest to run unrestricted and in EPT */
-		val |= VMX_SECPROCBASED_ENABLE_EPT;
-		val |= VMX_SECPROCBASED_UNRESTRICTED_GUEST;
+//		val |= VMX_SECPROCBASED_ENABLE_EPT;
+//		val |= VMX_SECPROCBASED_UNRESTRICTED_GUEST;
 		__vmx_vmwrite32(0x401E, val);
 	}
 	if (_vmx_has_pause_loop_exiting(vcpu)) {
