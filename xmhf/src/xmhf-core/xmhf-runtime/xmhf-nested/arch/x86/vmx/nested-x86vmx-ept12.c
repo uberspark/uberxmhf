uberspark/uberxmhf

commit c914beaf79370c8970051554d7b3d79f96f0bb8f

Extract LRU cache algorithm to nested-x86vmx-lru.h, using macros

xmhf/src/xmhf-core/xmhf-runtime/xmhf-nested/arch/x86/vmx/nested-x86vmx-ept12.c
@@ -50,30 +50,10 @@
 #include <xmhf.h>
 #include "nested-x86vmx-ept12.h"
 
-/* Maximum number of active EPTs per CPU */
-#define VMX_NESTED_MAX_ACTIVE_EPT 4
-
 /* Number of pages in page_pool in ept02_ctx_t */
 #define EPT02_PAGE_POOL_SIZE 128
 
-/* Format of EPT12 context information */
-typedef struct {
-	/* Context of EPT12 */
-	hptw_ctx_t ctx;
-	/* Context of EPT01 */
-	guestmem_hptw_ctx_pair_t ctx01;
-} ept12_ctx_t;
-
-/* Format of EPT02 context information */
-typedef struct {
-	/* Context */
-	hptw_ctx_t ctx;
-	/* List of pages to be allocated by ctx, limit = EPT02_PAGE_POOL_SIZE */
-	 u8(*page_pool)[PAGE_SIZE_4K];
-	/* Whether the corresponding page in page_pool is allocated */
-	u8 *page_alloc;
-} ept02_ctx_t;
-
+#if 0	// TODO
 /* Track list of EPT12's */
 typedef struct {
 	/*
@@ -88,9 +68,10 @@ typedef struct {
 	/* EPT12 context (for accessing guest EPT) */
 	ept12_ctx_t ept12_ctx;
 } ept02_cache_t;
+#endif
 
 /* For each CPU, information about all EPT12 -> EPT02 it caches */
-static ept02_cache_t ept02_cache[MAX_VCPU_ENTRIES][VMX_NESTED_MAX_ACTIVE_EPT];
+static ept02_cache_set_t ept02_cache[MAX_VCPU_ENTRIES];
 
 /* Page pool for ept02_cache */
 static u8 ept02_page_pool[MAX_VCPU_ENTRIES][VMX_NESTED_MAX_ACTIVE_EPT]
@@ -192,6 +173,7 @@ static void ept12_ctx_init(VCPU * vcpu, ept12_ctx_t * ept12_ctx, gpa_t ept12)
 	guestmem_init(vcpu, &ept12_ctx->ctx01);
 }
 
+#if 0	// TODO
 /*
  * Find ept12 in ept02_cache, return the index.
  * When ro = false, updates LRU order and evicts when cache is full.
@@ -259,6 +241,7 @@ static u32 ept02_cache_get(VCPU * vcpu, gpa_t ept12, bool ro, bool *cache_hit)
 
 	return ans;
 }
+#endif
 
 /*
  * Return whether bits 0 - 11 of eptp12 are legal for VMENTRY
@@ -304,12 +287,8 @@ bool xmhf_nested_arch_x86vmx_check_ept_lower_bits(u64 eptp12, gpa_t * ept_pml4t)
 /* Invalidate ept12 */
 void xmhf_nested_arch_x86vmx_invept_single_context(VCPU * vcpu, gpa_t ept12)
 {
-	bool cache_hit;
-	u32 index = ept02_cache_get(vcpu, ept12, true, &cache_hit);
-	if (cache_hit) {
-		HALT_ON_ERRORCOND(ept02_cache[vcpu->id][index].ept12 == ept12);
-		HALT_ON_ERRORCOND(ept02_cache[vcpu->id][index].valid);
-		ept02_cache[vcpu->id][index].valid = 0;
+	ept02_cache_line_t *line;
+	if (LRU_SET_INVALIDATE(&ept02_cache[vcpu->id], ept12, line)) {
 		/*
 		 * INVEPT will be executed in ept02_ctx_init() when this EPT02 is used
 		 * the next time.
@@ -320,16 +299,11 @@ void xmhf_nested_arch_x86vmx_invept_single_context(VCPU * vcpu, gpa_t ept12)
 /* Invalidate all EPTs */
 void xmhf_nested_arch_x86vmx_invept_global(VCPU * vcpu)
 {
-	u32 index;
-	for (index = 0; index < VMX_NESTED_MAX_ACTIVE_EPT; index++) {
-		if (ept02_cache[vcpu->id][index].valid) {
-			ept02_cache[vcpu->id][index].valid = 0;
-			/*
-			 * INVEPT will be executed in ept02_ctx_init() when this EPT02 is
-			 * used the next time.
-			 */
-		}
-	}
+	LRU_SET_INVALIDATE_ALL(&ept02_cache[vcpu->id]);
+	/*
+	 * INVEPT will be executed in ept02_ctx_init() when the EPT02 is used the
+	 * next time.
+	 */
 }
 
 /*
@@ -337,11 +311,21 @@ void xmhf_nested_arch_x86vmx_invept_global(VCPU * vcpu)
  * happens.
  */
 spa_t xmhf_nested_arch_x86vmx_get_ept02(VCPU * vcpu, gpa_t ept12,
-										u32 * cache_index, bool *cache_hit)
+										bool *cache_hit,
+										ept02_cache_line_t **cache_line)
 {
-	u32 index = ept02_cache_get(vcpu, ept12, false, cache_hit);
-	spa_t addr = (uintptr_t) ept02_cache[vcpu->id][index].ept02_ctx.ctx.root_pa;
-	*cache_index = index;
+	bool hit;
+	spa_t addr;
+	ept02_cache_index_t index;
+	ept02_cache_line_t *line = LRU_SET_FIND_EVICT(&ept02_cache[vcpu->id],
+												  ept12, index, hit);
+	if (!hit) {
+		ept02_ctx_init(vcpu, index, &line->value.ept02_ctx);
+		ept12_ctx_init(vcpu, &line->value.ept12_ctx, ept12);
+	}
+	*cache_hit = hit;
+	*cache_line = line;
+	addr = line->value.ept02_ctx.ctx.root_pa;
 	return addr | 0x1e;			// TODO: remove magic number
 }
 
@@ -358,9 +342,8 @@ spa_t xmhf_nested_arch_x86vmx_get_ept02(VCPU * vcpu, gpa_t ept12,
  */
 int xmhf_nested_arch_x86vmx_handle_ept02_exit(VCPU * vcpu,
 											  vmcs12_info_t * vmcs12_info,
-											  u32 cache_index)
+											  ept02_cache_line_t *cache_line)
 {
-	ept02_cache_t *ept02_cache_obj;
 	ept12_ctx_t *ept12_ctx;
 	u64 guest2_paddr = __vmx_vmread64(VMCSENC_guest_paddr);
 	gpa_t guest1_paddr;
@@ -371,10 +354,9 @@ int xmhf_nested_arch_x86vmx_handle_ept02_exit(VCPU * vcpu,
 	hpt_prot_t access_type;
 	ulong_t qualification = __vmx_vmreadNW(VMCSENC_info_exit_qualification);
 
-	ept02_cache_obj = &ept02_cache[vcpu->id][cache_index];
-	HALT_ON_ERRORCOND(ept02_cache_obj->valid);
-	HALT_ON_ERRORCOND(ept02_cache_obj->ept12 == vmcs12_info->guest_ept_root);
-	ept12_ctx = &ept02_cache_obj->ept12_ctx;
+	HALT_ON_ERRORCOND(cache_line->valid);
+	HALT_ON_ERRORCOND(cache_line->key == vmcs12_info->guest_ept_root);
+	ept12_ctx = &cache_line->value.ept12_ctx;
 	access_type = 0;
 	if (qualification & (1UL << 0)) {
 		access_type |= HPT_PROT_READ_MASK;
@@ -437,7 +419,7 @@ int xmhf_nested_arch_x86vmx_handle_ept02_exit(VCPU * vcpu,
 	}
 
 	/* Put page map entry into EPT02 */
-	HALT_ON_ERRORCOND(hptw_insert_pmeo_alloc(&ept02_cache_obj->ept02_ctx.ctx,
+	HALT_ON_ERRORCOND(hptw_insert_pmeo_alloc(&cache_line->value.ept02_ctx.ctx,
 											 &pmeo02, guest2_paddr) == 0);
 	printf("CPU(0x%02x): EPT: 0x%08llx 0x%08llx 0x%08llx\n", vcpu->id,
 		   guest2_paddr, guest1_paddr, xmhf_paddr);

xmhf/src/xmhf-core/xmhf-runtime/xmhf-nested/arch/x86/vmx/nested-x86vmx-lru.h
@@ -1,0 +1,199 @@
+/*
+ * @XMHF_LICENSE_HEADER_START@
+ *
+ * eXtensible, Modular Hypervisor Framework (XMHF)
+ * Copyright (c) 2009-2012 Carnegie Mellon University
+ * Copyright (c) 2010-2012 VDG Inc.
+ * All Rights Reserved.
+ *
+ * Developed by: XMHF Team
+ *               Carnegie Mellon University / CyLab
+ *               VDG Inc.
+ *               http://xmhf.org
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * Neither the names of Carnegie Mellon or VDG Inc, nor the names of
+ * its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * @XMHF_LICENSE_HEADER_END@
+ */
+
+// nested-x86vmx-lru.u
+// Provide a template for implementing fully associative software LRU cache
+// author: Eric Li (xiaoyili@andrew.cmu.edu)
+
+/*
+ * TODO: move this file out. Ideally components other than nested
+ * virtualization can also use LRU caches.
+ */
+
+/*
+ * Create a new type (LRU_LINE_TYPE) for storing a cache line and a new type
+ * (LRU_SET_TYPE) for storing a cache set. LRU_INDEX_TYPE needs to be able to
+ * hold integers in [0, LRU_SIZE].
+ */
+#define LRU_NEW_SET(LRU_SET_TYPE, LRU_LINE_TYPE, LRU_SIZE, LRU_INDEX_TYPE, \
+					LRU_KEY_TYPE, LRU_VALUE_TYPE) \
+	typedef struct LRU_LINE_TYPE { \
+		LRU_INDEX_TYPE valid; \
+		LRU_KEY_TYPE key; \
+		LRU_VALUE_TYPE value; \
+	} LRU_LINE_TYPE; \
+	\
+	typedef struct LRU_SET_TYPE { \
+		LRU_LINE_TYPE elems[LRU_SIZE]; \
+	} LRU_SET_TYPE;
+
+/*
+ * Iterate through all lines in a set using a forloop.
+ * CUR_INDEX is a variable of type LRU_INDEX_TYPE to hold the index.
+ * CUR_LINE is a variable of type LRU_LINE_TYPE * to hold the object.
+ * LRU_SET is the cache set to be iterated through of type LRU_SET_TYPE *.
+ */
+#define LRU_FOREACH(CUR_INDEX, CUR_LINE, LRU_SET) \
+	for (CUR_INDEX = 0, CUR_LINE = &((LRU_SET)->elems[0]); \
+		 CUR_INDEX < (sizeof((LRU_SET)->elems) / sizeof((LRU_SET)->elems[0])); \
+		 CUR_INDEX++, CUR_LINE++)
+
+/*
+ * Initialize all lines in a cache set.
+ * The type of LRU_SET is LRU_SET_TYPE *. It is the cache set to be initialized.
+ */
+#define LRU_SET_INIT(LRU_SET) \
+	do { \
+		typeof((LRU_SET)->elems[0].valid) i; \
+		typeof(&((LRU_SET)->elems[0])) line; \
+		LRU_FOREACH(i, line, LRU_SET) { \
+			line->valid = 0; \
+		} \
+	} while (0)
+
+/*
+ * Find a key KEY in cache set LRU_SET, without changing the cache state.
+ * If a line is found, the pointer to the line is put in FOUND_LINE
+ * (type LRU_LINE_TYPE *) and true is returned. Otherwise, false is returned.
+ */
+#define LRU_SET_FIND_IMMUTABLE(LRU_SET, KEY, FOUND_LINE) \
+	({ \
+		bool _ans = false; \
+		typeof((LRU_SET)->elems[0].valid) _i; \
+		LRU_FOREACH(_i, FOUND_LINE, LRU_SET) { \
+			if ((FOUND_LINE)->valid && (FOUND_LINE)->key == (KEY)) { \
+				_ans = true; \
+				break; \
+			} \
+		} \
+		_ans; \
+	})
+
+/*
+ * Find a key KEY in cache set LRU_SET, updating the LRU order, evicting when
+ * necessary. The pointer to the line found is returned (type LRU_LINE_TYPE *).
+ * The index of the line is set in INDEX (type LRU_INDEX_TYPE).
+ * When CACHE_HIT is false, the caller needs to initialize / re-initialize the
+ * value of the line.
+ */
+#define LRU_SET_FIND_EVICT(LRU_SET, KEY, INDEX, CACHE_HIT) \
+	({ \
+		const typeof((LRU_SET)->elems[0].valid) _nlines = \
+			(sizeof((LRU_SET)->elems) / sizeof((LRU_SET)->elems[0])); \
+		typeof((LRU_SET)->elems[0].valid) _ans_index = _nlines; \
+		typeof((LRU_SET)->elems[0].valid) _available_index = _nlines; \
+		typeof((LRU_SET)->elems[0].valid) _evict_index = _nlines; \
+		typeof((LRU_SET)->elems[0].valid) _max_valid = _nlines; \
+		typeof((LRU_SET)->elems[0].valid) _index; \
+		typeof(&((LRU_SET)->elems[0])) _line; \
+		typeof(&((LRU_SET)->elems[0])) _ans_line = NULL; \
+		bool _cache_hit = false; \
+		LRU_FOREACH(_index, _line, LRU_SET) { \
+			if (_line->valid == 0) { \
+				_available_index = _index; \
+			} else if (_line->valid == _nlines) { \
+				_evict_index = _index; \
+			} else if (_line->key == (KEY)) { \
+				_cache_hit = true; \
+				_ans_index = _index; \
+				_ans_line = _line; \
+				_max_valid = _line->valid; \
+				break; \
+			} \
+		} \
+		if (!_cache_hit) { \
+			if (_available_index < _nlines) { \
+				_ans_index = _available_index; \
+			} else { \
+				_ans_index = _evict_index; \
+			} \
+			_ans_line = &((LRU_SET)->elems[_ans_index]); \
+		} \
+		LRU_FOREACH(_index, _line, LRU_SET) { \
+			if (_line->valid && _line->valid < _max_valid) { \
+				_line->valid++; \
+			} \
+		} \
+		(INDEX) = _ans_index; \
+		(CACHE_HIT) = _cache_hit; \
+		_ans_line->valid = 1; \
+		_ans_line->key = (KEY); \
+		_ans_line; \
+	})
+
+/*
+ * Find a key KEY in cache set LRU_SET and invalidate it.
+ * If a line is found, the pointer to the line is put in FOUND_LINE
+ * (type LRU_LINE_TYPE *) and true is returned. Otherwise, false is returned.
+ *
+ * Note that when this function returns true, the returned line is already
+ * invalidated (valid = 0). However, when race condition is not considered,
+ * the caller can still safely inspect the value in FOUND_LINE.
+ */
+#define LRU_SET_INVALIDATE(LRU_SET, KEY, FOUND_LINE) \
+	({ \
+		bool _ans = false; \
+		typeof((LRU_SET)->elems[0].valid) _i; \
+		LRU_FOREACH(_i, FOUND_LINE, LRU_SET) { \
+			if ((FOUND_LINE)->valid && (FOUND_LINE)->key == (KEY)) { \
+				_ans = true; \
+				(FOUND_LINE)->valid = 0; \
+				break; \
+			} \
+		} \
+		_ans; \
+	})
+
+/* Invalidate all cache lines in cache set LRU_SET */
+#define LRU_SET_INVALIDATE_ALL(LRU_SET) \
+	do { \
+		typeof((LRU_SET)->elems[0].valid) _i; \
+		typeof(&((LRU_SET)->elems[0])) _line; \
+		LRU_FOREACH(_i, _line, LRU_SET) { \
+			_line->valid = 0; \
+		} \
+	} while (0)
+

xmhf/src/xmhf-core/xmhf-runtime/xmhf-nested/arch/x86/vmx/nested-x86vmx-vmcs12.h
@@ -125,7 +125,7 @@ typedef struct vmcs12_info {
 	/* Whether using EPT12 */
 	int guest_ept_enable;
 	/* When guest_ept_enable, index to nested-x86vmx-ept12.c cache */
-	u32 guest_ept_cache_index;
+	void *guest_ept_cache_line;	// TODO: use real type
 	/* When guest_ept_enable, pointer to EPT12 root */
 	gpa_t guest_ept_root;
 } vmcs12_info_t;

xmhf/src/xmhf-core/xmhf-runtime/xmhf-nested/arch/x86/vmx/nested-x86vmx-ept12.h
@@ -52,15 +52,53 @@
 #define _NESTED_X86VMX_EPT12_H_
 
 #include "nested-x86vmx-vmcs12.h"
+#include "nested-x86vmx-lru.h"
+
+/* Maximum number of active EPTs per CPU */
+#define VMX_NESTED_MAX_ACTIVE_EPT 4
+
+/* Format of EPT12 context information */
+typedef struct {
+	/* Context of EPT12 */
+	hptw_ctx_t ctx;
+	/* Context of EPT01 */
+	guestmem_hptw_ctx_pair_t ctx01;
+} ept12_ctx_t;
+
+/* Format of EPT02 context information */
+typedef struct {
+	/* Context */
+	hptw_ctx_t ctx;
+	/* List of pages to be allocated by ctx, limit = EPT02_PAGE_POOL_SIZE */
+	 u8(*page_pool)[PAGE_SIZE_4K];
+	/* Whether the corresponding page in page_pool is allocated */
+	u8 *page_alloc;
+} ept02_ctx_t;
+
+typedef u32 ept02_cache_index_t;
+
+/* Guest physical address of EPT12 (4K aligned) */
+typedef gpa_t ept02_cache_key_t;
+
+typedef struct {
+	/* EPT02 context (for allocating pages) */
+	ept02_ctx_t ept02_ctx;
+	/* EPT12 context (for accessing guest EPT) */
+	ept12_ctx_t ept12_ctx;
+} ept02_cache_value_t;
+
+LRU_NEW_SET(ept02_cache_set_t, ept02_cache_line_t, VMX_NESTED_MAX_ACTIVE_EPT,
+			ept02_cache_index_t, ept02_cache_key_t, ept02_cache_value_t);
 
 bool xmhf_nested_arch_x86vmx_check_ept_lower_bits(u64 eptp12,
 												  gpa_t * ept_pml4t);
 void xmhf_nested_arch_x86vmx_invept_single_context(VCPU * vcpu, gpa_t ept12);
 void xmhf_nested_arch_x86vmx_invept_global(VCPU * vcpu);
 spa_t xmhf_nested_arch_x86vmx_get_ept02(VCPU * vcpu, gpa_t ept12,
-										u32 * cache_index, bool *cache_hit);
+										bool *cache_hit,
+										ept02_cache_line_t **cache_line);
 int xmhf_nested_arch_x86vmx_handle_ept02_exit(VCPU * vcpu,
 											  vmcs12_info_t * vmcs12_info,
-											  u32 cache_index);
+											  ept02_cache_line_t *cache_line);
 
 #endif							/* _NESTED_X86VMX_EPT12_H_ */

xmhf/src/xmhf-core/xmhf-runtime/xmhf-nested/arch/x86/vmx/nested-x86vmx-handler.c
@@ -633,10 +633,10 @@ void xmhf_nested_arch_x86vmx_handle_vmexit(VCPU * vcpu, struct regs *r)
 	if (vmexit_reason == VMX_VMEXIT_EPT_VIOLATION) {
 		int status = 3;
 		if (vmcs12_info->guest_ept_enable) {
-			u32 cache_index = vmcs12_info->guest_ept_cache_index;
+			ept02_cache_line_t *cache_line = vmcs12_info->guest_ept_cache_line;
 			status =
 				xmhf_nested_arch_x86vmx_handle_ept02_exit(vcpu, vmcs12_info,
-														  cache_index);
+														  cache_line);
 		}
 		switch (status) {
 		case 1:

xmhf/src/xmhf-core/xmhf-runtime/xmhf-nested/arch/x86/vmx/nested-x86vmx-vmcs12.c
@@ -470,15 +470,15 @@ u32 xmhf_nested_arch_x86vmx_vmcs12_to_vmcs02(VCPU * vcpu,
 			/* Construct shadow EPT */
 			u64 eptp12 = vmcs12->control_EPT_pointer;
 			gpa_t ept12;
-			u32 cache_index;
+			ept02_cache_line_t *cache_line;
 			bool cache_hit;
 			vmcs12_info->guest_ept_enable = 1;
 			if (!xmhf_nested_arch_x86vmx_check_ept_lower_bits(eptp12, &ept12)) {
 				return VM_INST_ERRNO_VMENTRY_INVALID_CTRL;
 			}
-			ept02 = xmhf_nested_arch_x86vmx_get_ept02(vcpu, ept12, &cache_index,
-													  &cache_hit);
-			vmcs12_info->guest_ept_cache_index = cache_index;
+			ept02 = xmhf_nested_arch_x86vmx_get_ept02(vcpu, ept12, &cache_hit,
+													  &cache_line);
+			vmcs12_info->guest_ept_cache_line = cache_line;
 			vmcs12_info->guest_ept_root = ept12;
 		} else {
 			/* Guest does not use EPT, just use XMHF's EPT */
@@ -856,10 +856,10 @@ void xmhf_nested_arch_x86vmx_vmcs02_to_vmcs12(VCPU * vcpu,
 		HALT_ON_ERRORCOND(_vmx_hasctl_enable_ept(&vcpu->vmx_caps));
 		if (_vmx_hasctl_enable_ept(&ctls)) {
 			gpa_t ept12 = vmcs12_info->guest_ept_root;
-			u32 cache_index;
+			ept02_cache_line_t *cache_line;
 			bool cache_hit;
-			ept02 = xmhf_nested_arch_x86vmx_get_ept02(vcpu, ept12, &cache_index,
-													  &cache_hit);
+			ept02 = xmhf_nested_arch_x86vmx_get_ept02(vcpu, ept12, &cache_hit,
+													  &cache_line);
 			HALT_ON_ERRORCOND(cache_hit);
 		} else {
 			ept02 = vcpu->vmcs.control_EPT_pointer;

commit 4d54df6b95309e5ff7e978bd9f9b5f60d9221286

Remove TODOs caused by c914beaf79370c8970051554d7b3d79f96f0bb8f

xmhf/src/xmhf-core/xmhf-runtime/xmhf-nested/arch/x86/vmx/nested-x86vmx-ept12.c
@@ -53,23 +53,6 @@
 /* Number of pages in page_pool in ept02_ctx_t */
 #define EPT02_PAGE_POOL_SIZE 128
 
-#if 0	// TODO
-/* Track list of EPT12's */
-typedef struct {
-	/*
-	 * If 0, invalid (need to initialize). Otherwise, track LRU (1 is most
-	 * recently used; when all valid, VMX_NESTED_MAX_ACTIVE_EPT is LRU).
-	 */
-	u32 valid;
-	/* Guest physical address of EPT12 (4K aligned) */
-	gpa_t ept12;
-	/* EPT02 context (for allocating pages) */
-	ept02_ctx_t ept02_ctx;
-	/* EPT12 context (for accessing guest EPT) */
-	ept12_ctx_t ept12_ctx;
-} ept02_cache_t;
-#endif
-
 /* For each CPU, information about all EPT12 -> EPT02 it caches */
 static ept02_cache_set_t ept02_cache[MAX_VCPU_ENTRIES];
 
@@ -172,76 +155,6 @@ static void ept12_ctx_init(VCPU * vcpu, ept12_ctx_t * ept12_ctx, gpa_t ept12)
 	ept12_ctx->ctx.t = HPT_TYPE_EPT;
 	guestmem_init(vcpu, &ept12_ctx->ctx01);
 }
-
-#if 0	// TODO
-/*
- * Find ept12 in ept02_cache, return the index.
- * When ro = false, updates LRU order and evicts when cache is full.
- * When ro = true, do not update LRU order. When cache is full return undefined.
- * When cache hit, cache_hit = true.
- * When cache miss, evict a cache entry and cache_hit = false.
- */
-static u32 ept02_cache_get(VCPU * vcpu, gpa_t ept12, bool ro, bool *cache_hit)
-{
-	u32 ans = VMX_NESTED_MAX_ACTIVE_EPT;
-	u32 index;
-	u32 available_index = VMX_NESTED_MAX_ACTIVE_EPT;
-	u32 evict_index = VMX_NESTED_MAX_ACTIVE_EPT;
-	/* When updating LRU, do not exceed this amount */
-	u32 max_valid = VMX_NESTED_MAX_ACTIVE_EPT;
-	bool hit = false;
-	for (index = 0; index < VMX_NESTED_MAX_ACTIVE_EPT; index++) {
-		/* Prepare for cold miss */
-		if (ept02_cache[vcpu->id][index].valid == 0) {
-			available_index = index;
-			continue;
-		}
-		/* Prepare for capacity miss */
-		if (ept02_cache[vcpu->id][index].valid == VMX_NESTED_MAX_ACTIVE_EPT) {
-			evict_index = index;
-			continue;
-		}
-		/* Cache hit */
-		if (ept02_cache[vcpu->id][index].ept12 == ept12) {
-			hit = true;
-			ans = index;
-			max_valid = ept02_cache[vcpu->id][index].valid;
-			break;
-		}
-	}
-
-	*cache_hit = hit;
-
-	if (ro) {
-		return ans;
-	}
-
-	if (!hit) {
-		printf("CPU(0x%02x): EPT cache miss for EP4TA 0x%016llx\n", vcpu->id,
-			   ept12);
-		if (available_index < VMX_NESTED_MAX_ACTIVE_EPT) {
-			ans = available_index;
-		} else {
-			HALT_ON_ERRORCOND(evict_index < VMX_NESTED_MAX_ACTIVE_EPT);
-			ans = evict_index;
-		}
-		ept02_ctx_init(vcpu, ans, &ept02_cache[vcpu->id][ans].ept02_ctx);
-		ept12_ctx_init(vcpu, &ept02_cache[vcpu->id][ans].ept12_ctx, ept12);
-		ept02_cache[vcpu->id][ans].ept12 = ept12;
-	}
-
-	/* Update LRU */
-	for (index = 0; index < VMX_NESTED_MAX_ACTIVE_EPT; index++) {
-		if (ept02_cache[vcpu->id][index].valid &&
-			ept02_cache[vcpu->id][index].valid < max_valid) {
-			ept02_cache[vcpu->id][index].valid++;
-		}
-	}
-	ept02_cache[vcpu->id][ans].valid = 1;
-
-	return ans;
-}
-#endif
 
 /*
  * Return whether bits 0 - 11 of eptp12 are legal for VMENTRY

xmhf/src/xmhf-core/xmhf-runtime/xmhf-nested/arch/x86/vmx/nested-x86vmx-lru.h
@@ -61,8 +61,18 @@
 #define LRU_NEW_SET(LRU_SET_TYPE, LRU_LINE_TYPE, LRU_SIZE, LRU_INDEX_TYPE, \
 					LRU_KEY_TYPE, LRU_VALUE_TYPE) \
 	typedef struct LRU_LINE_TYPE { \
+		/*
+		 * If 0, cache line is invalid. Otherwise, track LRU order (1 is most
+		 * recently used; when all valid, LRU_SIZE is LRU).
+		 *
+		 * When considering the valid fields of all lines in a set, the
+		 * non-zero values never repeat and are within 1 and LRU_SIZE
+		 * (inclusive).
+		 */ \
 		LRU_INDEX_TYPE valid; \
+		/* The key to determine whether cache matches, using operator "==" */ \
 		LRU_KEY_TYPE key; \
+		/* The value of the cache */ \
 		LRU_VALUE_TYPE value; \
 	} LRU_LINE_TYPE; \
 	\
@@ -121,11 +131,16 @@
  */
 #define LRU_SET_FIND_EVICT(LRU_SET, KEY, INDEX, CACHE_HIT) \
 	({ \
+		/* _nlines = LRU_SIZE */ \
 		const typeof((LRU_SET)->elems[0].valid) _nlines = \
 			(sizeof((LRU_SET)->elems) / sizeof((LRU_SET)->elems[0])); \
+		/* Index to be returned */ \
 		typeof((LRU_SET)->elems[0].valid) _ans_index = _nlines; \
+		/* Index of invalid entry during search (more preferred than LRU) */ \
 		typeof((LRU_SET)->elems[0].valid) _available_index = _nlines; \
+		/* Index of LRU entry during search (less preferred than invalid) */ \
 		typeof((LRU_SET)->elems[0].valid) _evict_index = _nlines; \
+		/* When updating LRU order, do not exceed this amount */ \
 		typeof((LRU_SET)->elems[0].valid) _max_valid = _nlines; \
 		typeof((LRU_SET)->elems[0].valid) _index; \
 		typeof(&((LRU_SET)->elems[0])) _line; \
@@ -146,21 +161,24 @@
 		} \
 		if (!_cache_hit) { \
 			if (_available_index < _nlines) { \
+				/* Cold miss */ \
 				_ans_index = _available_index; \
 			} else { \
+				/* Capacity miss */ \
 				_ans_index = _evict_index; \
 			} \
 			_ans_line = &((LRU_SET)->elems[_ans_index]); \
 		} \
+		/* Update LRU order */ \
 		LRU_FOREACH(_index, _line, LRU_SET) { \
 			if (_line->valid && _line->valid < _max_valid) { \
 				_line->valid++; \
 			} \
 		} \
-		(INDEX) = _ans_index; \
-		(CACHE_HIT) = _cache_hit; \
 		_ans_line->valid = 1; \
 		_ans_line->key = (KEY); \
+		(INDEX) = _ans_index; \
+		(CACHE_HIT) = _cache_hit; \
 		_ans_line; \
 	})
 

xmhf/src/xmhf-core/xmhf-runtime/xmhf-nested/arch/x86/vmx/nested-x86vmx-vmcs12.h
@@ -124,8 +124,12 @@ typedef struct vmcs12_info {
 		__attribute__((aligned(16)));
 	/* Whether using EPT12 */
 	int guest_ept_enable;
-	/* When guest_ept_enable, index to nested-x86vmx-ept12.c cache */
-	void *guest_ept_cache_line;	// TODO: use real type
+	/*
+	 * When guest_ept_enable, EPT02 cache line.
+	 * The type of this member should be ept02_cache_line_t *. However,
+	 * currently casting to void * to avoid circular includes in header files.
+	 */
+	void *guest_ept_cache_line;
 	/* When guest_ept_enable, pointer to EPT12 root */
 	gpa_t guest_ept_root;
 } vmcs12_info_t;