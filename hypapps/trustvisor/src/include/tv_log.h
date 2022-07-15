uberspark/uberxmhf

commit a17801cc63ac99c981b5f6df880b5fc5e1ac47d5

Reduce TrustVisor and hpt output

hypapps/trustvisor/src/include/tv_log.h
@@ -48,7 +48,7 @@
 #define TV_LOG_H
 
 #ifndef EU_LOG_LVL
-#define EU_LOG_LVL EU_TRACE
+#define EU_LOG_LVL EU_WARN
 #endif
 
 #ifndef EU_LOG_PREFIX

xmhf/src/libbaremetal/libxmhfutil/hpt_log.h
@@ -47,7 +47,7 @@
 #ifndef HPT_LOG_H
 #define HPT_LOG_H
 
-#define EU_LOG_LVL EU_WARN
+#define EU_LOG_LVL EU_ERR
 #define EU_LOG_PREFIX "HPT"
 #include <eulog.h>
 #include <euchk.h>

xmhf/src/libbaremetal/libxmhfutil/hptw.c
@@ -388,9 +388,9 @@ void* hptw_checked_access_va(hptw_ctx_t *ctx,
     eu_trace("pmo t:%d pm:%p lvl:%d",
              pmo.t, pmo.pm, pmo.lvl);
     hpt_pm_get_pmeo_by_va( &pmeo, &pmo, va);
-    EU_CHK(((access_type & hpt_pmeo_getprot(&pmeo)) == access_type)
+    EU_CHK_W(((access_type & hpt_pmeo_getprot(&pmeo)) == access_type)
            && (cpl == HPTW_CPL0 || hpt_pmeo_getuser(&pmeo)),
-           eu_err_e("req-priv:%lld req-cpl:%d priv:%lld user-accessible:%d",
+           eu_warn_e("req-priv:%lld req-cpl:%d priv:%lld user-accessible:%d",
                     access_type, cpl, hpt_pmeo_getprot(&pmeo), hpt_pmeo_getuser(&pmeo)));
   } while (hptw_next_lvl(ctx, &pmo, va));
 

xmhf/src/libbaremetal/libxmhfutil/include/eulog.h
@@ -57,7 +57,7 @@
 #endif
 
 #ifndef EU_LOG_LVL
-#define EU_LOG_LVL EU_TRACE
+#define EU_LOG_LVL EU_WARN
 #endif
 
 #ifndef EU_LOG_PRINTLN

commit f41b83a6b62e315ff3c76855ab3160b1740fed08

Fix regression caused by a17801cc63ac99c981b5f6df880b5fc5e1ac47d5

hypapps/trustvisor/src/include/tv_log.h
@@ -48,7 +48,7 @@
 #define TV_LOG_H
 
 #ifndef EU_LOG_LVL
-#define EU_LOG_LVL EU_WARN
+#define EU_LOG_LVL EU_TRACE
 #endif
 
 #ifndef EU_LOG_PREFIX