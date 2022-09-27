uberspark/uberxmhf

commit ed84991cbab5b0d2b9fd8f28bc03de467d501e20

Remove redundant memcpy's

xmhf/src/xmhf-core/xmhf-runtime/xmhf-nested/arch/x86/vmx/nested-x86vmx-vmcs12.c
@@ -839,21 +839,7 @@ u32 xmhf_nested_arch_x86vmx_vmcs12_to_vmcs02(VCPU * vcpu,
 		u32 i;
 		gva_t guest_addr = vmcs12->control_VM_entry_MSR_load_address;
 
-		/*
-		 * By default, most MSRs in L1 are not changed after VMENTRY to L2.
-		 * This memcpy makes sure that XMHF managed MSRs follow this behavior.
-		 */
-		// TODO: this memcpy is redundant now
-		memcpy(vmcs12_info->vmcs02_vmentry_msr_load_area,
-			   (void *)vcpu->vmx_vaddr_msr_area_guest,
-			   vcpu->vmcs.control_VM_entry_MSR_load_count *
-			   sizeof(msr_entry_t));
-		__vmx_vmwrite32(VMCSENC_control_VM_entry_MSR_load_count,
-						vcpu->vmcs.control_VM_entry_MSR_load_count);
-		__vmx_vmwrite64(VMCSENC_control_VM_entry_MSR_load_address,
-						hva2spa(vmcs12_info->vmcs02_vmentry_msr_load_area));
-
-		/* Update IA32_PAT and IA32_EFER MSRs */
+		/* Set IA32_PAT and IA32_EFER in VMCS02 guest */
 		{
 			msr_entry_t *msr02 = vmcs12_info->vmcs02_vmentry_msr_load_area;
 			msr02[ia32_pat_index].data = guest_ia32_pat;
@@ -1304,17 +1290,7 @@ void xmhf_nested_arch_x86vmx_vmcs02_to_vmcs12(VCPU * vcpu,
 		HALT_ON_ERRORCOND(hva2spa(vmcs12_info->vmcs02_vmexit_msr_load_area) ==
 						  __vmx_vmread64(encoding));
 
-		/*
-		 * By default, most MSRs in L2 are not changed after VMEXIT to L1.
-		 * This memcpy makes sure that XMHF managed MSRs follow this behavior.
-		 */
-		// TODO: this memcpy is redundant now
-		memcpy((void *)vcpu->vmx_vaddr_msr_area_guest,
-			   vmcs12_info->vmcs02_vmentry_msr_load_area,
-			   vcpu->vmcs.control_VM_entry_MSR_load_count *
-			   sizeof(msr_entry_t));
-
-		/* Update IA32_PAT and IA32_EFER MSRs */
+		/* Set IA32_PAT and IA32_EFER in VMCS01 guest */
 		{
 			msr_entry_t *msr01 =
 				((msr_entry_t *) vcpu->vmx_vaddr_msr_area_guest);

xmhf/src/xmhf-core/xmhf-runtime/xmhf-nested/arch/x86/vmx/nested-x86vmx-handler1.c
@@ -335,13 +335,12 @@ static vmcs12_info_t *new_active_vmcs12(VCPU * vcpu, gpa_t vmcs_ptr, u32 rev)
 	memcpy(vmcs12_info->vmcs02_vmexit_msr_load_area,
 		   (void *)vcpu->vmx_vaddr_msr_area_host,
 		   vcpu->vmcs.control_VM_exit_MSR_load_count * sizeof(msr_entry_t));
-	/*
-	 * vmcs02_vmentry_msr_load_area need to process the same MSRs as VMCS01,
-	 * but the memcpy will happen in xmhf_nested_arch_x86vmx_vmcs12_to_vmcs02().
-	 */
-	// TODO: also memcpy here
+	/* vmcs02_vmentry_msr_load_area need to process the same MSRs as VMCS01 */
 	memset(&vmcs12_info->vmcs02_vmentry_msr_load_area, 0,
 		   sizeof(vmcs12_info->vmcs02_vmentry_msr_load_area));
+	memcpy(vmcs12_info->vmcs02_vmentry_msr_load_area,
+		   (void *)vcpu->vmx_vaddr_msr_area_guest,
+		   vcpu->vmcs.control_VM_entry_MSR_load_count * sizeof(msr_entry_t));
 	vmcs12_info->guest_ept_enable = 0;
 	vmcs12_info->guest_ept_root = 0;
 	vmcs12_info->guest_nmi_exiting = false;

commit 0b560ab7da6ec42a75cc96658650b79b2400cf36

Fix bug introduced in ed84991cbab5b0d2b9fd8f28bc03de467d501e20

xmhf/src/xmhf-core/xmhf-runtime/xmhf-nested/arch/x86/vmx/nested-x86vmx-vmcs12.c
@@ -839,6 +839,11 @@ u32 xmhf_nested_arch_x86vmx_vmcs12_to_vmcs02(VCPU * vcpu,
 		u32 i;
 		gva_t guest_addr = vmcs12->control_VM_entry_MSR_load_address;
 
+		__vmx_vmwrite32(VMCSENC_control_VM_entry_MSR_load_count,
+						vcpu->vmcs.control_VM_entry_MSR_load_count);
+		__vmx_vmwrite64(VMCSENC_control_VM_entry_MSR_load_address,
+						hva2spa(vmcs12_info->vmcs02_vmentry_msr_load_area));
+
 		/* Set IA32_PAT and IA32_EFER in VMCS02 guest */
 		msr02[ia32_pat_index].data = guest_ia32_pat;
 		msr02[ia32_efer_index].data = guest_ia32_efer;