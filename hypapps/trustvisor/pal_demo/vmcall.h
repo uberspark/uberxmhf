9713855ae54360397d058f2cfa98f06679160d47
Eric Li <xiaoyili@andrew.cmu.edu>
2022-11-02 21:01:49 -0400

    Able to set VMCALL_OFFSET in pal_demo

--- hypapps/trustvisor/pal_demo/vmcall.h
+++ hypapps/trustvisor/pal_demo/vmcall.h
@@ -2,6 +2,9 @@
 
 static inline uintptr_t vmcall(uintptr_t eax, uintptr_t ecx, uintptr_t edx,
 								uintptr_t esi, uintptr_t edi) {
+#ifdef VMCALL_OFFSET
+	eax += VMCALL_OFFSET;
+#endif
 	asm volatile ("vmcall\n\t" : "+a"(eax) : "c"(ecx), "d"(edx), "S"(esi),
 					"D"(edi));
 	return eax;
