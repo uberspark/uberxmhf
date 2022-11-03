2e520c3eb4d3e1ba8f02f977077fedad353c974e
Eric Li <xiaoyili@andrew.cmu.edu>
2022-11-02 20:44:26 -0400

    Fix bug in pal_demo about inline assembly

--- hypapps/trustvisor/pal_demo/vmcall.h
+++ hypapps/trustvisor/pal_demo/vmcall.h
@@ -2,7 +2,7 @@
 
 static inline uintptr_t vmcall(uintptr_t eax, uintptr_t ecx, uintptr_t edx,
 								uintptr_t esi, uintptr_t edi) {
-	asm volatile ("vmcall\n\t" : "=a"(eax) : "a"(eax), "c"(ecx), "d"(edx),
-					"S"(esi), "D"(edi));
+	asm volatile ("vmcall\n\t" : "+a"(eax) : "c"(ecx), "d"(edx), "S"(esi),
+					"D"(edi));
 	return eax;
 }
